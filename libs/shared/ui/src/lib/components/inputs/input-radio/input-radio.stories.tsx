c482af67ab35861f3a9e05ed67993056790500d1
Wiliam Traoré <hi@wil-liam.com>
2022-02-23 15:33:27 +0100

    feat(onboarding-storybook): adding input radio

--- libs/shared/ui/src/lib/components/inputs/input-radio/input-radio.stories.tsx
+++ libs/shared/ui/src/lib/components/inputs/input-radio/input-radio.stories.tsx
@@ -3,15 +3,18 @@ import { InputRadio, InputRadioProps } from './input-radio'
 
 export default {
   component: InputRadio,
-  title: 'Inputs/InputRadio',
+  title: 'Inputs/InputRadio'
 } as Meta
 
-const Template: Story<InputRadioProps> = (args) => (
-  <InputRadio {...args} />
-)
+const Template: Story<InputRadioProps> = (args) => <InputRadio {...args} />
+
+
 
 export const Primary = Template.bind({})
 Primary.args = {
   name: 'test',
   isChecked: false,
+  label: 'Choose Qovery',
+  labelVisible: true,
+  value: 'qovery'
 }
--- libs/shared/ui/src/lib/components/inputs/input-radio/input-radio.tsx
+++ libs/shared/ui/src/lib/components/inputs/input-radio/input-radio.tsx
@@ -1,28 +1,52 @@
-import { useRef } from 'react'
+import { useEffect, useState } from 'react'
 import './input-radio.module.scss'
 
 /* eslint-disable-next-line */
 export interface InputRadioProps {
   name: string,
+  label: string,
+  value: string,
   isChecked?: boolean,
   className?: string
+  labelVisible?: boolean,
+  getValue?: (checked: boolean, value: string) => void
 }
 
 export function InputRadio(props: InputRadioProps) {
 
   const {
     name,
+    value,
+    getValue,
     isChecked = false,
-    className = ''
+    className = '',
+    label = '',
+    labelVisible = true
   } = props
 
+  const [check, setCheck] = useState(isChecked);
+
+  useEffect(() => {
+    setCheck(isChecked)
+  }, [])
+
+  const inputChange = (check: boolean, value: string) => {
+    console.log(check, value)
+    setCheck(check)
+    if (getValue) getValue(check, value)
+  }
+
   return (
-    <div className={className}>
-      <input 
+    <div className={`input__radio ${className}`}>
+      <input
+        id={label}
         type="radio"
         name={name}
-        checked={isChecked}
+        value={value}
+        checked={check}
+        onChange={(e) => inputChange(e.currentTarget.checked, e.currentTarget.value)}
       />
+      <label htmlFor={label} className={!labelVisible ? 'invisible' : 'visible'}>{label}</label>
     </div>
   )
 }
