3fda42f310b85700fba0bca952c963a22acb0617
Rémi Bonnet <rbonnet@qovery.com>
2022-03-08 15:11:56 +0100

    feat(input-toggle): adding toggle small and normal size

---
+++ libs/shared/ui/src/lib/components/inputs/input-toggle/input-toggle.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, Story } from '@storybook/react'
+import { InputToggle, InputToggleProps } from './input-toggle'
+
+export default {
+  component: InputToggle,
+  title: 'Inputs/InputToggle',
+} as Meta
+
+const Template: Story<InputToggleProps> = (args) => <InputToggle {...args} />
+
+export const Primary = Template.bind({})
