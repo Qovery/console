3d808b5b49fc55cd8f69ec97e28748684b6eef20
Rémi Bonnet <rbonnet@qovery.com>
2022-03-10 11:30:56 +0100

    fix(environment): define process env

--- apps/console/src/environments/environment.ts
+++ apps/console/src/environments/environment.ts
@@ -1,9 +1,6 @@
-const { NX_QOVERY_API = '', NX_OAUTH_DOMAIN = '', NX_OAUTH_KEY = '', NX_OAUTH_AUDIENCE = '' } = process.env
-
 export const environment = {
-  production: true,
-  api: NX_QOVERY_API,
-  oauth_domain: NX_OAUTH_DOMAIN,
-  oauth_key: NX_OAUTH_KEY,
-  oauth_audience: NX_OAUTH_AUDIENCE,
+  api: process.env?.['NX_QOVERY_API'] || '',
+  oauth_domain: process.env?.['NX_OAUTH_DOMAIN'] || '',
+  oauth_key: process.env?.['NX_OAUTH_KEY'] || '',
+  oauth_audience: process.env?.['NX_OAUTH_AUDIENCE'] || '',
 }
