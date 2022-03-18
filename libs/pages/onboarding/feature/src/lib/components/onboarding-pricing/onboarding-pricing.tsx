438d53684f381dd645208847ec285023e5b2fa6b
Wiliam Traoré <hi@wil-liam.com>
2022-03-18 15:20:14 +0100

    feat(onboarding-api): add price on step pricing

--- libs/pages/onboarding/feature/src/lib/components/onboarding-pricing/onboarding-pricing.tsx
+++ libs/pages/onboarding/feature/src/lib/components/onboarding-pricing/onboarding-pricing.tsx
@@ -11,7 +11,6 @@ export interface Plan {
   text: string
   price?: number
   listPrice: Price[]
-  listDeploy: Value[]
 }
 
 const DEPLOYS: Value[] = [
@@ -45,7 +44,6 @@ const PLANS: Plan[] = [
     title: 'Free',
     text: 'Adapted for personnal project',
     price: 0,
-    listDeploy: [],
     listPrice: [],
   },
   {
@@ -53,19 +51,9 @@ const PLANS: Plan[] = [
     title: 'Professional',
     text: 'For 5-20 members',
     price: 49,
-    listDeploy: [
-      { label: '300/month', value: '300' },
-      { label: '400/month', value: '400' },
-      { label: '500/month', value: '500' },
-      { label: '600/month', value: '600' },
-      { label: '700/month', value: '700' },
-      { label: '800/month', value: '800' },
-      { label: '900/month', value: '900' },
-      { label: '1000/month', value: '1000' },
-      { label: '2000/month', value: '2000' },
-      { label: '3000/month', value: '3000' },
-    ],
     listPrice: [
+      { number: '100', price: '49' },
+      { number: '200', price: '49' },
       { number: '300', price: '49' },
       { number: '400', price: '99' },
       { number: '500', price: '149' },
@@ -74,8 +62,18 @@ const PLANS: Plan[] = [
       { number: '800', price: '299' },
       { number: '900', price: '349' },
       { number: '1000', price: '399' },
+      { number: '1100', price: '449' },
+      { number: '1200', price: '499' },
+      { number: '1300', price: '549' },
+      { number: '1400', price: '599' },
+      { number: '1500', price: '649' },
+      { number: '1600', price: '699' },
+      { number: '1700', price: '749' },
+      { number: '1800', price: '799' },
+      { number: '1900', price: '849' },
       { number: '2000', price: '899' },
       { number: '3000', price: '1399' },
+      { number: '4000', price: '1899' },
     ],
   },
   {
@@ -83,22 +81,16 @@ const PLANS: Plan[] = [
     title: 'Business',
     text: 'For medium compagny',
     price: 399,
-    listDeploy: [
-      { label: '1000/month', value: '1000' },
-      { label: '1100/month', value: '1100' },
-      { label: '1200/month', value: '1200' },
-      { label: '1300/month', value: '1300' },
-      { label: '1400/month', value: '1400' },
-      { label: '1500/month', value: '1500' },
-      { label: '1600/month', value: '1600' },
-      { label: '1700/month', value: '1700' },
-      { label: '1800/month', value: '1800' },
-      { label: '1900/month', value: '1900' },
-      { label: '2000/month', value: '2000' },
-      { label: '3000/month', value: '3000' },
-      { label: '4000/month', value: '4000' },
-    ],
     listPrice: [
+      { number: '100', price: '599' },
+      { number: '200', price: '599' },
+      { number: '300', price: '599' },
+      { number: '400', price: '599' },
+      { number: '500', price: '599' },
+      { number: '600', price: '599' },
+      { number: '700', price: '599' },
+      { number: '800', price: '599' },
+      { number: '900', price: '599' },
       { number: '1000', price: '599' },
       { number: '1100', price: '649' },
       { number: '1200', price: '699' },
@@ -119,17 +111,12 @@ const PLANS: Plan[] = [
     title: 'Enterprise',
     text: 'For large compagny',
     price: 0,
-    listDeploy: [],
     listPrice: [],
   },
 ]
 
 const PLAN_DEFAULT: string = PlanEnum.FREE
 const DEPLOY_DEFAULT: Value = DEPLOYS[0]
-const DEFAULT_VALUE = {
-  [PlanEnum.PRO]: PLANS.find((p) => p.name === PlanEnum.PRO)?.listDeploy[0],
-  [PlanEnum.BUSINESS]: PLANS.find((p) => p.name === PlanEnum.BUSINESS)?.listDeploy[0],
-}
 
 const DEFAULT_PRICE = {
   [PlanEnum.FREE]: { disable: false },
@@ -146,8 +133,6 @@ export function OnboardingPricing() {
   const [currentValue, setCurrentValue] = useState(DEFAULT_PRICE)
   const [currentDeploy, setCurrentDeploy] = useState(DEPLOY_DEFAULT)
 
-  const displayDeploy = select === PlanEnum.PRO || select === PlanEnum.BUSINESS
-
   useEffect(() => {
     navigate(`${ONBOARDING_URL}${ONBOARDING_PRICING_URL}/${select}`)
   }, [select, navigate])
@@ -156,7 +141,7 @@ export function OnboardingPricing() {
     if (value) {
       setCurrentDeploy(value)
 
-      if(parseFloat(value.value) > 300) {
+      if(parseFloat(value.value) > 100) {
         setSelect(PlanEnum.PRO)
         setCurrentValue({
           [PlanEnum.FREE]: { disable: true },
@@ -179,10 +164,8 @@ export function OnboardingPricing() {
     <StepPricing
       select={select}
       setSelect={setSelect}
-      defaultValue={DEFAULT_VALUE}
       currentValue={currentValue}
       plans={PLANS}
-      displayDeploy={displayDeploy}
       chooseDeploy={chooseDeploy}
       deploys={DEPLOYS}
       currentDeploy={currentDeploy}
