e588e881e33716faa452365af57e7d3de99facfa
Rémi Bonnet <rbonnet@qovery.com>
2022-04-25 16:35:13 +0200

    feat(tooltip): adding component

--- libs/shared/ui/src/lib/components/icon/icons-status/success.tsx
+++ libs/shared/ui/src/lib/components/icon/icons-status/success.tsx
@@ -2,13 +2,7 @@ import { IconProps } from '../icon'
 
 export function SuccessIcon(props: IconProps) {
   return (
-    // <svg
-    //   {...props}
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   className={`svg-icon ${props.className || ''}`}
-    //   xmlnsXlink="http://www.w3.org/1999/xlink"
-    // >
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 14 14">
+    <svg {...props} xmlns="http://www.w3.org/2000/svg">
       <path
         fill="#44C979"
         d="M5.908 2.16a4.958 4.958 0 013.11.308.875.875 0 10.713-1.599 6.708 6.708 0 103.978 6.132v-.538a.875.875 0 00-1.75 0V7a4.958 4.958 0 11-6.051-4.84z"
@@ -21,4 +15,4 @@ export function SuccessIcon(props: IconProps) {
   )
 }
 
-export default BitbucketIcon
+export default SuccessIcon
--- libs/shared/ui/src/lib/components/table/table.tsx
+++ libs/shared/ui/src/lib/components/table/table.tsx
@@ -1,12 +1,17 @@
+import { IconEnum } from '@console/shared/enums'
+import { Icon, IconFa, Tooltip } from '@console/shared/ui'
+
 export interface TableProps {
   data: Array<any>
   dataHead?: Array<any>
   className?: string
+  columnsWidth?: string
 }
 
 const fakeTableHead = [
   {
     title: 'Services',
+    className: 'px-4 py-2',
   },
   {
     title: 'Update',
@@ -24,7 +29,12 @@ const fakeTableHead = [
 ]
 
 export function Table(props: TableProps) {
-  const { data, className = 'bg-white rounded-sm', dataHead = fakeTableHead } = props
+  const {
+    data,
+    className = 'bg-white rounded-sm',
+    dataHead = fakeTableHead,
+    columnsWidth = `repeat(${dataHead.length},minmax(0,1fr))`,
+  } = props
 
   console.log(data)
 
@@ -35,7 +45,7 @@ export function Table(props: TableProps) {
     <div className={className}>
       <div
         className="grid items-center border-b-element-light-lighter-400 border-b"
-        style={{ gridTemplateColumns: `repeat(${dataHead.length},minmax(0,1fr))` }}
+        style={{ gridTemplateColumns: columnsWidth }}
       >
         {dataHead.map(({ title, className = 'px-4 py-2' }, index) => (
           <div key={index} className={className}>
@@ -47,11 +57,30 @@ export function Table(props: TableProps) {
         {data.map((currentData, index) => (
           <div
             key={index}
-            className="grid items-center h-14"
-            style={{ gridTemplateColumns: `repeat(${data.length},minmax(0,1fr))` }}
+            className="grid items-center h-14 border-b-element-light-lighter-400 border-b last:border-0 hover:bg-element-light-lighter-300"
+            style={{ gridTemplateColumns: columnsWidth }}
           >
-            <div>
-              <span className="text-sm text-text-500 font-medium">{currentData.name}</span>
+            <div className="flex items-center px-4">
+              <Icon className="mr-3 min-w-[14px]" width="0.875rem" viewBox="0 0 14 14" name={IconEnum.SUCCESS} />
+              <Tooltip
+                content={
+                  <p>
+                    {currentData.cloud_provider.provider} ({currentData.cloud_provider.cluster})
+                  </p>
+                }
+              >
+                <div className="w-4 h-4.5 min-w-[16px] mr-3 flex items-center justify-center text-xs text-text-400 text-center bg-element-light-lighter-400 rounded-sm font-bold">
+                  {currentData.cloud_provider.provider.charAt(0)}
+                </div>
+              </Tooltip>
+              <span className="text-sm text-text-500 font-medium truncate">{currentData.name}</span>
+            </div>
+            <div className="text-right px-3">
+              <span className="text-text-400 text-sm">1h ago</span>
+            </div>
+            <div className="flex items-center px-4 border-b-element-light-lighter-400 border-l h-full">
+              <IconFa name="icon-solid-infinity" className="text-success-500 mr-1 text-xs" />
+              <span className="f text-text-500 text-xs font-medium">Continuous running</span>
             </div>
           </div>
         ))}
