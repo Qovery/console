bc1a28dd20206c2d9172707f5e4003d0cd51224a
bdebon <benjamin.debon@gmail.com>
2022-05-09 11:25:23 +0200

    chore(redux): better way to call selector by entity id

--- libs/domains/application/src/lib/provider/application-provider.tsx
+++ libs/domains/application/src/lib/provider/application-provider.tsx
@@ -1,15 +1,14 @@
 import { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchApplication, selectApplicationById } from '../slices/applications.slice'
+import { useDispatch } from 'react-redux'
+import { fetchApplication } from '../slices/applications.slice'
 
 export function useApplication() {
   const dispatch = useDispatch<any>()
-  const application = (applicationId: string) => useSelector(selectApplicationById(applicationId))
 
   const getApplication = useCallback(
     async (applicationId: string) => dispatch(fetchApplication({ applicationId })),
     [dispatch]
   )
 
-  return { application, getApplication }
+  return { getApplication }
 }
--- libs/shared/layout/src/lib/layout.tsx
+++ libs/shared/layout/src/lib/layout.tsx
@@ -3,9 +3,10 @@ import { useParams } from 'react-router'
 import { useOrganization } from '@console/domains/organization'
 import { useEnviroments, useProjects } from '@console/domains/projects'
 import { useUser } from '@console/domains/user'
-import { useApplication, useApplications } from '@console/domains/application'
+import { selectApplicationById, useApplication, useApplications } from '@console/domains/application'
 import { LayoutPage } from '@console/shared/ui'
 import { useAuth } from '@console/shared/utils'
+import { useSelector } from 'react-redux'
 
 export interface LayoutProps {
   children: React.ReactElement
@@ -19,8 +20,10 @@ export function Layout(props: LayoutProps) {
   const { userSignUp, getUserSignUp } = useUser()
   const { environments, getEnvironments } = useEnviroments()
   const { applications, getApplications } = useApplications()
-  const { application, getApplication } = useApplication()
-  const { organizationId, projectId, environmentId, applicationId } = useParams()
+  const { getApplication } = useApplication()
+  const { organizationId, projectId, environmentId, applicationId = '' } = useParams()
+
+  const application = useSelector((state) => selectApplicationById(state, applicationId))
 
   useEffect(() => {
     getUserSignUp()
@@ -50,7 +53,7 @@ export function Layout(props: LayoutProps) {
       projects={projects}
       environments={environments}
       applications={applications}
-      application={application(applicationId || '')}
+      application={application}
     >
       {children}
     </LayoutPage>
