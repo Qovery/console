0b041b21d217d65f84aec8a41445808237d5b9b4
Wiliam Traoré <hi@wil-liam.com>
2022-05-11 14:28:08 +0200

    fix test + add status menu action on environment table

--- libs/shared/ui/src/lib/components/buttons/button-icon-action/button-icon-action-element/button-icon-action-element.tsx
+++ libs/shared/ui/src/lib/components/buttons/button-icon-action/button-icon-action-element/button-icon-action-element.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Menu } from '@console/shared/ui'
 import { MenuItemProps } from '../../../menu/menu-item/menu-item'
+import { GlobalDeploymentStatus } from 'qovery-typescript-axios'
+import StatusMenuAction from '../../../status-menu-action/status-menu-action'
 
 export interface ButtonIconActionElementProps {
   iconLeft: React.ReactNode
@@ -10,12 +12,15 @@ export interface ButtonIconActionElementProps {
     items: MenuItemProps[]
   }[]
   menusClassName?: string
+  status?: GlobalDeploymentStatus
 }
 
 export function ButtonIconActionElement(props: ButtonIconActionElementProps) {
-  const { iconLeft, iconRight, onClick, menus, menusClassName = '' } = props
+  const { iconLeft, iconRight, onClick, menus, menusClassName = '', status } = props
 
   const [open, setOpen] = useState(false)
+
+  console.log(status)
 
   if (menus) {
     return (
@@ -24,6 +29,23 @@ export function ButtonIconActionElement(props: ButtonIconActionElementProps) {
         menus={menus}
         width={248}
         onOpen={(e) => setOpen(e)}
+        trigger={
+          <div data-testid="element" className={`btn-icon-action__element ${open ? 'is-active' : ''}`}>
+            {iconLeft}
+            {iconRight}
+          </div>
+        }
+      />
+    )
+  } else if (status) {
+    return (
+      <StatusMenuAction
+        className={menusClassName}
+        width={248}
+        status={status}
+        onOpen={(e) => setOpen(e)}
+        paddingMenuX={8}
+        paddingMenuY={8}
         trigger={
           <div data-testid="element" className={`btn-icon-action__element ${open ? 'is-active' : ''}`}>
             {iconLeft}
--- libs/shared/ui/src/lib/components/buttons/button-icon-action/button-icon-action.tsx
+++ libs/shared/ui/src/lib/components/buttons/button-icon-action/button-icon-action.tsx
@@ -1,18 +1,20 @@
+import { GlobalDeploymentStatus } from 'qovery-typescript-axios'
 import ButtonIconActionElement, {
   ButtonIconActionElementProps,
 } from './button-icon-action-element/button-icon-action-element'
 
 export interface ButtonIconActionProps {
   actions: ButtonIconActionElementProps[]
+  status?: GlobalDeploymentStatus
 }
 
 export function ButtonIconAction(props: ButtonIconActionProps) {
-  const { actions } = props
+  const { actions, status } = props
 
   return (
     <div className="btn-icon-action" onClick={(e) => e.preventDefault()}>
       {actions.map((action, index) => (
-        <ButtonIconActionElement key={index} {...action} />
+        <ButtonIconActionElement key={index} {...action} status={status} />
       ))}
     </div>
   )
