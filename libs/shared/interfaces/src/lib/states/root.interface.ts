18c7d02f9f8119f0e9cda65d0a3c307db22b64b5
bdebon <benjamin.debon@gmail.com>
2022-05-11 16:38:33 +0200

    chore(typing): typing the whole store without circular dependencies

--- libs/shared/interfaces/src/lib/states/root.interface.ts
+++ libs/shared/interfaces/src/lib/states/root.interface.ts
@@ -1,11 +1,9 @@
-import {
-  ApplicationsState,
-  EnvironmentsState,
-  OrganizationState,
-  ProjectsState,
-  UserInterface,
-  UserSignUpState,
-} from '@console/shared/interfaces'
+import { UserInterface } from './user.interface'
+import { UserSignUpState } from './user-sign-up.interface'
+import { OrganizationState } from './organizations.interface'
+import { ProjectsState } from './projects.interface'
+import { EnvironmentsState } from './environments.interface'
+import { ApplicationsState } from './applications.interface'
 
 export type RootState = {
   user: UserInterface
