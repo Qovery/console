8c499f0b0945c10c16bfaaee9bed81556eafda07
bdebon <benjamin.debon@gmail.com>
2022-05-12 11:59:25 +0200

    chore(store): factorising redondant state interface

--- libs/shared/interfaces/src/lib/states/organizations.interface.ts
+++ libs/shared/interfaces/src/lib/states/organizations.interface.ts
@@ -1,7 +1,4 @@
 import { Organization } from 'qovery-typescript-axios'
 import { DefaultEntityState } from './default-entity-state.interface'
 
-export interface OrganizationState extends DefaultEntityState<Organization> {
-  loadingStatus: 'not loaded' | 'loading' | 'loaded' | 'error' | undefined
-  error: string | null | undefined
-}
+export interface OrganizationState extends DefaultEntityState<Organization> {}
--- libs/shared/interfaces/src/lib/states/projects.interface.ts
+++ libs/shared/interfaces/src/lib/states/projects.interface.ts
@@ -2,7 +2,5 @@ import { Project } from 'qovery-typescript-axios'
 import { DefaultEntityState } from './default-entity-state.interface'
 
 export interface ProjectsState extends DefaultEntityState<Project> {
-  loadingStatus: 'not loaded' | 'loading' | 'loaded' | 'error' | undefined
-  error: string | null | undefined
   joinOrganizationProject: Record<string, string[]>
 }
