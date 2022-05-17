d92afad04a864040ae53650a3c3c8ba329229ef8
bdebon <benjamin.debon@gmail.com>
2022-05-17 12:37:43 +0200

    feat(ui-kit): link

---
+++ libs/shared/ui/src/lib/components/link/link.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable-next-line */
+import Icon from '../icon/icon'
+import { IconEnum } from '@console/shared/enums'
+
+export interface BaseLink {
+  link: string
+  linkLabel?: string
+  external?: boolean
+}
+
+export interface LinkProps extends BaseLink {
+  className?: string
+  iconRight?: IconEnum | string
+  iconLeft?: IconEnum | string
+}
+
+export function Link(props: LinkProps) {
+  const { link, linkLabel, external = false, className = '', iconLeft, iconRight } = props
+  return (
+    <a
+      className={`${className} text-accent2-500 text-sm flex flex-center gap-1 hover:underline`}
+      href={link}
+      target={external ? '_blank' : '_self'}
+    >
+      {iconLeft && <Icon name={iconLeft} className="text-xs leading-5" />}
+      {linkLabel}
+      {iconRight && <Icon name={iconRight} className="text-xs leading-5" />}
+    </a>
+  )
+}
+
+export default Link
