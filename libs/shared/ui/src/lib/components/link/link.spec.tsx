b4f724fb0ba3998b5c5f65e1a6eba09ed0e4639c
bdebon <benjamin.debon@gmail.com>
2022-05-17 12:37:43 +0200

    feat(ui-kit): link

---
+++ libs/shared/ui/src/lib/components/link/link.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+
+import Link from './link'
+
+describe('Link', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Link />)
+    expect(baseElement).toBeTruthy()
+  })
+})
