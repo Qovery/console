960fe9f42c99a4004e88f57b0f9cce09fd22a91e
bdebon <benjamin.debon@gmail.com>
2022-05-17 14:54:51 +0200

    feat(application): About section

--- libs/pages/application/ui/src/index.ts
+++ libs/pages/application/ui/src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/components/about/about'
 export * from './lib/components/general/general'
 export * from './lib/components/container/container'
--- libs/pages/application/ui/src/lib/components/general/general.tsx
+++ libs/pages/application/ui/src/lib/components/general/general.tsx
@@ -2,6 +2,7 @@
 import { ApplicationEntity } from '@console/shared/interfaces'
 import HelpSection from '../../../../../../../shared/ui/src/lib/components/help-section/help-section'
 import { BaseLink } from '@console/shared/ui'
+import About from '../about/about'
 
 export interface GeneralProps {
   application?: ApplicationEntity
@@ -14,8 +15,22 @@ export function GeneralPage(props: GeneralProps) {
   return (
     <div className="mt-2 bg-white rounded flex flex-grow">
       <div className="flex h-full flex-col flex-grow">
-        <h1 className="border-gray-50 flex-grow">Welcome to {application?.name} overview page</h1>
+        <div className="py-7 px-10 flex-grow">
+          <h1>Hey {application?.name}</h1>
+        </div>
         <HelpSection description="Need help? You may find these links useful" links={listHelpfulLinks}></HelpSection>
+      </div>
+      <div className="w-left-help-sidebar py-10 border-l border-element-light-lighter-400">
+        <About
+          description={application?.description || ''}
+          link={{
+            link: application?.git_repository?.url || '',
+            linkLabel: application?.git_repository?.provider,
+            external: true,
+          }}
+          buildMode={application?.build_mode}
+          gitProvider={application?.git_repository?.provider}
+        />
       </div>
     </div>
   )
