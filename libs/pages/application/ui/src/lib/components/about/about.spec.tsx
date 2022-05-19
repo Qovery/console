e247f5b2d0f2d84a9723b02d9fd0260d7174b386
bdebon <benjamin.debon@gmail.com>
2022-05-19 12:07:38 +0200

    fix(test): fix a broken test

--- libs/pages/application/ui/src/lib/components/about/about.spec.tsx
+++ libs/pages/application/ui/src/lib/components/about/about.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react'
-
 import About from './about'
 
 describe('About', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<About />)
+    const { baseElement } = render(
+      <About link={{ link: '#', linkLabel: 'label', external: false }} description="A description" />
+    )
     expect(baseElement).toBeTruthy()
   })
 })
