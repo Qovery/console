98b26168f981eaff6b68a7fcd3d88f9a3dd8fad5
Wiliam Traoré <hi@wil-liam.com>
2022-06-13 15:36:12 +0200

    feat(application-deployments): fix tests

--- libs/pages/application/ui/src/lib/components/deployments-page/deployments-page.spec.tsx
+++ libs/pages/application/ui/src/lib/components/deployments-page/deployments-page.spec.tsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react'
+import { render } from '__tests__/utils/setup-jest'
+import { DeploymentsPage, DeploymentsProps } from './deployments-page'
 
-import DeploymentsPage from './deployments-page'
+let props: DeploymentsProps
+
+beforeEach(() => {
+  props = {
+    listHelpfulLinks: [
+      {
+        link: 'https://hub.qovery.com/docs/using-qovery/configuration/application',
+        linkLabel: 'How to configure my application',
+        external: true,
+      },
+    ],
+  }
+})
 
 describe('DeploymentsPage', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<DeploymentsPage />)
+    const { baseElement } = render(<DeploymentsPage {...props} />)
     expect(baseElement).toBeTruthy()
   })
 })
