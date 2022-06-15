673d462b259a5895df0861a63c53c3785abb60c1
Rémi Bonnet <rbonnet@qovery.com>
2022-06-15 18:44:31 +0200

    refactor(login): remove feature and ui libraries

--- libs/pages/login/src/lib/page-login.spec.tsx
+++ libs/pages/login/src/lib/page-login.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '__tests__/utils/setup-jest'
 
-import Login from './login'
+import PageLogin from './page-login'
 
-describe('Login', () => {
+describe('PageLogin', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Login />)
+    const { baseElement } = render(<PageLogin />)
     expect(baseElement).toBeTruthy()
   })
 })
