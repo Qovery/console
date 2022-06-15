f783fdb4a6f096521257661b7e4ea718c0e3f933
bdebon <benjamin.debon@gmail.com>
2022-06-15 11:38:12 +0200

    refactor(memo): better writing for memo

--- libs/pages/services/ui/src/lib/components/general/general.tsx
+++ libs/pages/services/ui/src/lib/components/general/general.tsx
@@ -96,10 +96,18 @@ function General(props: GeneralProps) {
 export const GeneralPage = React.memo(General, (prevProps, nextProps) => {
   // Stringify is necessary to avoid Redux selector behavior
   const isEqual =
-    JSON.stringify(prevProps.services.map((service) => service.status?.state)) ===
-      JSON.stringify(nextProps.services.map((service) => service.status?.state)) &&
-    JSON.stringify(prevProps.services.map((service) => service.running_status?.state)) ===
-      JSON.stringify(nextProps.services.map((service) => service.running_status?.state))
+    JSON.stringify(
+      prevProps.services.map((service) => ({
+        status: service.status?.state,
+        running_status: service.running_status?.state,
+      }))
+    ) ===
+    JSON.stringify(
+      nextProps.services.map((service) => ({
+        status: service.status?.state,
+        running_status: service.running_status?.state,
+      }))
+    )
 
   return isEqual
 })
