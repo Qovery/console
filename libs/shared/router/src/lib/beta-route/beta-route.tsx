ebe4c971d8cf054e54efeb1e5f8a44b6720ec16a
Rémi Bonnet <rbonnet@qovery.com>
2022-06-28 09:27:16 +0200

    test(beta-route): remove feature flag

--- libs/shared/router/src/lib/beta-route/beta-route.tsx
+++ libs/shared/router/src/lib/beta-route/beta-route.tsx
@@ -1,4 +1,4 @@
-import posthog from 'posthog-js'
+// import posthog from 'posthog-js'
 import { matchPath, useLocation, useParams } from 'react-router'
 import { INFRA_LOGS_URL } from '../router'
 import { NoBetaAccess } from '../pages/no-beta-access'
@@ -13,9 +13,9 @@ export const BetaRoute = ({ children }: IBetaRoute) => {
 
   const matchLogInfraRoute = matchPath(location.pathname || '', INFRA_LOGS_URL(organizationId))
 
-  if (posthog.isFeatureEnabled('v3-beta')) {
-    return children
-  }
+  // if (posthog.isFeatureEnabled('v3-beta')) {
+  return children
+  // }
 
   if (matchLogInfraRoute) {
     return children
