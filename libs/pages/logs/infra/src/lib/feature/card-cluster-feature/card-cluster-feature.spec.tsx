76cdc059e7909b86e5f1fcdda00d04a674f4f4e1
Rémi Bonnet <rbonnet@qovery.com>
2022-06-29 18:06:34 +0200

    test(card-cluster-feature): render

--- libs/pages/logs/infra/src/lib/feature/card-cluster-feature/card-cluster-feature.spec.tsx
+++ libs/pages/logs/infra/src/lib/feature/card-cluster-feature/card-cluster-feature.spec.tsx
@@ -1,5 +1,4 @@
-import { render } from '@testing-library/react'
-
+import { render } from '__tests__/utils/setup-jest'
 import CardClusterFeature from './card-cluster-feature'
 
 describe('CardClusterFeature', () => {
