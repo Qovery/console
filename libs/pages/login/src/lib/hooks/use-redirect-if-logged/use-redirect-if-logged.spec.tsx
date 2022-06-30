ce313e53a48a1737c0c5abd93206e008a8ade47f
bdebon <benjamin.debon@gmail.com>
2022-06-30 11:57:24 +0100

    test(redirect): implementing tests of redirects

--- libs/pages/login/src/lib/hooks/use-redirect-if-logged/use-redirect-if-logged.spec.tsx
+++ libs/pages/login/src/lib/hooks/use-redirect-if-logged/use-redirect-if-logged.spec.tsx
@@ -1,9 +1,25 @@
 import { useRedirectIfLogged } from './use-redirect-if-logged'
 import { renderHook } from '@testing-library/react-hooks'
+import { Wrapper } from '__tests__/utils/providers'
+import { getRedirectLoginUri } from './utils/utils'
+
+jest.mock('./utils/utils')
+
+const mockedUsedNavigate = jest.fn()
+jest.mock('react-router', () => ({
+  ...(jest.requireActual('react-router') as any),
+  useNavigate: () => mockedUsedNavigate,
+}))
 
 describe('UseRedirectIfLogged', () => {
   it('should render successfully', () => {
     const { result } = renderHook(useRedirectIfLogged)
     expect(result).toBeTruthy()
+  })
+
+  it('should call navigate', () => {
+    getRedirectLoginUri.mockImplementation(() => '/login')
+    renderHook(useRedirectIfLogged, { wrapper: Wrapper })
+    expect(mockedUsedNavigate).toHaveBeenCalledWith('/login')
   })
 })
