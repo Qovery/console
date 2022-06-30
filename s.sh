7172a8310f936d1de5055e6e0a2b787d6fa79d44
Rémi Bonnet <rbonnet@qovery.com>
2022-06-30 11:10:14 +0200

    feat(contributing): indications

---
+++ s.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+file=".env"
+nx="NX_"
+e="8CqHsc1kw41fG09u4R7A0"
+echo $nx'URL=https://console.qovery.com' >> $file
+echo $nx'QOVERY_API=https://api.qovery.com' >> $file
+echo $nx'OAUTH_DOMAIN=auth.qovery.com' >> $file
+echo $nx'OAUTH_KEY=S4fQF5rkTng'$e >> $file
+echo $nx'OAUTH_AUDIENCE=https://core.qovery.com' >> $file
+echo $nx'POSTHOG=__test__posthog__token' >> $file
+echo $nx'POSTHOG_APIHOST=__test__environment__posthog__apihost' >> $file
+cat $file
