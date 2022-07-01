0eb59b104b50ccc457011a33216a1d539455f486
Rémi Bonnet <rbonnet@qovery.com>
2022-07-01 08:54:14 +0200

    fix(beta-route): remove posthog

--- libs/shared/router/src/lib/beta-route/beta-route.tsx
+++ libs/shared/router/src/lib/beta-route/beta-route.tsx
@@ -2,21 +2,21 @@
 import { matchPath, useLocation, useParams } from 'react-router'
 import { INFRA_LOGS_URL } from '../router'
 import { NoBetaAccess } from '../pages/no-beta-access'
-import posthog from 'posthog-js'
+// import posthog from 'posthog-js'
 
 export interface IBetaRoute {
   children: React.ReactElement
 }
 
 export const BetaRoute = ({ children }: IBetaRoute) => {
-  const { organizationId = '' } = useParams()
+  const { organizationId = '', clusterId = '' } = useParams()
   const location = useLocation()
 
-  const matchLogInfraRoute = matchPath(location.pathname || '', INFRA_LOGS_URL(organizationId))
+  const matchLogInfraRoute = matchPath(location.pathname || '', INFRA_LOGS_URL(organizationId, clusterId))
 
-  if (posthog.isFeatureEnabled('v3-beta')) {
-    return children
-  }
+  // if (posthog.isFeatureEnabled('v3-beta')) {
+  return children
+  // }
 
   if (matchLogInfraRoute) {
     return children
