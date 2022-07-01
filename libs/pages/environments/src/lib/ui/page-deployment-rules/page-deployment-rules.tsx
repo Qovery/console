3684de79e9e8b2184ad98d784205eb14ea2939e8
Wiliam Traoré <hi@wil-liam.com>
2022-07-01 13:41:15 +0200

    feat(dep-rules): change the UI of the list

--- libs/pages/environments/src/lib/ui/page-deployment-rules/page-deployment-rules.tsx
+++ libs/pages/environments/src/lib/ui/page-deployment-rules/page-deployment-rules.tsx
@@ -1,4 +1,4 @@
-import { BaseLink, /*Button, ButtonSize,*/ HelpSection /*Icon*/ } from '@console/shared/ui'
+import { BaseLink, Button, ButtonSize, HelpSection, Icon } from '@console/shared/ui'
 import DeploymentRuleItem from '../deployment-rule-item/deployment-rule-item'
 import {
   Draggable,
@@ -27,7 +27,7 @@ export function PageDeploymentRules(props: PageDeploymentRulesProps) {
     updateDeploymentRulesOrder,
     isLoading = false,
     deleteDeploymentRule,
-    //linkNewRule = '',
+    linkNewRule = '',
   } = props
   const [listRules, setListRules] = useState<ProjectDeploymentRule[]>(deploymentRules)
 
@@ -54,21 +54,22 @@ export function PageDeploymentRules(props: PageDeploymentRulesProps) {
   return (
     <div className="mt-2 bg-white rounded flex flex-col flex-grow">
       <div className="py-7 px-10 flex-grow overflow-y-auto min-h-0">
-        <div className="flex">
-          <p className="mb-5 text-text-400 text-xs">
+        <div className="flex justify-between items-center mb-8 w-[640px]">
+          <p className="text-text-500 text-xs">
             Configure your default deployment rules. Drag & drop rules to prioritize them.
           </p>
-          {/*
-            <Button size={ButtonSize.SMALL} className="leading-none ml-5" link={linkNewRule}>
-              New rule
-              <Icon name="icon-solid-plus" className="ml-2 !text-base inline-block -mt-1" />
-            </Button>
-            */}
+
+          <Button size={ButtonSize.SMALL} className="leading-none" link={linkNewRule}>
+            Add rule
+            <Icon name="icon-solid-plus" className="ml-2 !text-base inline-block -mt-1" />
+          </Button>
         </div>
 
-        <div className={`max-w-2xl ${listRules?.length === 0 ? 'hidden' : ''}`}>
-          <div className="border-t border-l border-r rounded-t border-element-light-lighter-400">
-            <h2 className="text-sm text-text-500 font-medium py-2 px-4">Deployment Rules</h2>
+        <div className={`w-[640px] ${listRules?.length === 0 ? 'hidden' : ''}`}>
+          <div className="border-t border-l border-r rounded-t border-element-light-lighter-500">
+            <h2 className="text-sm text-text-500 font-medium py-2 px-4 bg-element-light-lighter-200">
+              Deployment Rules
+            </h2>
           </div>
           <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="rules-list">
