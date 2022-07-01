3c8651b25ff0235ba7d4374676df29ee653a371f
Rémi Bonnet <bonnet.rem@gmail.com>
2022-07-01 16:26:47 +0200

    fix(breadcrumb): remove useless render (#207)

--- libs/shared/ui/src/lib/components/layouts/breadcrumb/breadcrumb.spec.tsx
+++ libs/shared/ui/src/lib/components/layouts/breadcrumb/breadcrumb.spec.tsx
@@ -4,16 +4,18 @@ import { applicationFactoryMock } from '@console/domains/application'
 import { environmentFactoryMock } from '@console/domains/environment'
 import { render } from '__tests__/utils/setup-jest'
 
-import Breadcrumb from './breadcrumb'
+import { BreadcrumbMemo } from './breadcrumb'
+import { organizationFactoryMock } from '@console/domains/organization'
 
 describe('Breadcrumb', () => {
   const props: BreadcrumbProps = {
     projects: projectsFactoryMock(3),
+    organizations: organizationFactoryMock(3),
     environments: environmentFactoryMock(3),
     applications: applicationFactoryMock(3),
   }
   it('should render successfully', () => {
-    const { baseElement } = render(<Breadcrumb {...props} />)
+    const { baseElement } = render(<BreadcrumbMemo {...props} />)
     expect(baseElement).toBeTruthy()
   })
 })
