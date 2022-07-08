8789e9136b294b0a5bf28a9624d127a43fd055cb
Rémi Bonnet <rbonnet@qovery.com>
2022-07-08 13:19:38 +0200

    fix(tabs-logs): style

--- libs/shared/ui/src/lib/components/layouts/layout-logs/tabs-logs/tabs-logs.tsx
+++ libs/shared/ui/src/lib/components/layouts/layout-logs/tabs-logs/tabs-logs.tsx
@@ -86,7 +86,7 @@ export function TabsLogs(props: TabsLogsProps) {
                           onClick={() => scrollToError()}
                           className="transition-colors cursor-pointer text-error-500 hover:text-error-600 font-bold mr-2.5"
                         >
-                          <span className="text-xxs relative -top-[1px] mr-1">{errors[errors.length - 1].index}</span>
+                          <span className="text-xxs relative -top-px mr-1">{errors[errors.length - 1].index}</span>
                           <Icon name="icon-solid-arrow-circle-right" className=" cursor-pointer" />
                         </p>
                         <CopyToClipboard
