1acb797a04ff567283fef612363876cab2a7a5cb
Romaric Philogène <rphilogene@qovery.com>
2022-07-13 15:30:26 -0400

    chore: change onboarding page title and links

--- libs/pages/login/src/lib/ui/login/login.tsx
+++ libs/pages/login/src/lib/ui/login/login.tsx
@@ -19,7 +19,7 @@ export function Login(props: ILoginProps) {
           <p className="text-sm mb-10 text-text-500">
             By registering and using Qovery, you agree to the processing of your personal data by Qovery as described in
             the
-            <a href="/" className="link text-accent2-500 ml-1">
+            <a href="https://www.qovery.com/terms" className="link text-accent2-500 ml-1">
               Privacy Policy
             </a>
             .
@@ -41,36 +41,45 @@ export function Login(props: ILoginProps) {
       <div className="hidden xl:block flex-[1_1_0%] bg-element-light-lighter-300 px-20 before:absolute before:top-0 before:w-full before:h-full before:bg-element-light-lighter-300">
         <div className="max-w-md relative z-10">
           <h2 className="h1 text-text-700 mt-44 mb-6">
-            The Simplest Way To Deploy Your Apps on AWS and Kubernetes
+            The Next-Gen DevOps Platform to Build and Scale Your Apps on AWS
             <span className="ml-2" role="img" aria-label="rocket">
               🚀
             </span>
           </h2>
-          <p className="text-text-500 mb-6 text-sm">Push your code, Qovery handles the rest</p>
+          <p className="text-text-500 mb-6 text-sm">Built for DevOps, Loved by developers</p>
           <ul className="list-disc pl-4 text-sm">
             <li className="font-bold text-text-500 mb-2">
-              <a href="/" className="link text-brand-500 mr-1">
+              <a
+                href="https://hub.qovery.com/guides/getting-started/deploy-your-first-application/"
+                className="link text-brand-500 mr-1"
+              >
                 Deploy your apps
               </a>
               on AWS in seconds
             </li>
             <li className="font-bold text-text-500 mb-2">
-              <a href="/" className="link text-brand-500 mr-1">
+              <a
+                href="https://hub.qovery.com/docs/using-qovery/configuration/environment/#preview-environment"
+                className="link text-brand-500 mr-1"
+              >
                 Get instant preview
               </a>
               environments
             </li>
-            <li className="font-bold text-text-500 mb-2">
-              <a href="/" className="link text-brand-500 mr-1">
-                Reduce your Cloud
-              </a>
-              cost by 60%
-            </li>
             <li className="font-bold text-text-500">
-              <a href="/" className="link text-brand-500 mr-1">
-                Boost your team
+              <a
+                href="https://hub.qovery.com/docs/using-qovery/integration/terraform/"
+                className="link text-brand-500 mr-1"
+              >
+                Use Infrastructure as Code
               </a>
-              experience
+              with Terraform
+            </li>
+            <li className="font-bold text-text-500 mb-2">
+              <a href="https://hub.qovery.com/docs/using-qovery/integration" className="link text-brand-500 mr-1">
+                Integrate Qovery
+              </a>
+              with your tools
             </li>
           </ul>
         </div>
