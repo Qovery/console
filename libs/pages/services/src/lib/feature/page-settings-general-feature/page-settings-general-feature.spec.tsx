b697e072851ec51e33f393a54ddaa6bb45738ee6
Rémi Bonnet <rbonnet@qovery.com>
2022-07-18 09:39:14 +0200

    fix(page-settings): test

--- libs/pages/services/src/lib/feature/page-settings-general-feature/page-settings-general-feature.spec.tsx
+++ libs/pages/services/src/lib/feature/page-settings-general-feature/page-settings-general-feature.spec.tsx
@@ -1,5 +1,4 @@
-import { render } from '@testing-library/react'
-
+import { render } from '__tests__/utils/setup-jest'
 import PageSettingsGeneralFeature from './page-settings-general-feature'
 
 describe('PageSettingsGeneralFeature', () => {
