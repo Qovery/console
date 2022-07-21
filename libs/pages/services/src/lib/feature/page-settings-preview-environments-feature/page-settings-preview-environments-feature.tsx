5c7d87ea07cda3cca684e5648c66e0e5cc73989d
Rémi Bonnet <rbonnet@qovery.com>
2022-07-21 18:19:15 +0200

    wip

--- libs/pages/services/src/lib/feature/page-settings-preview-environments-feature/page-settings-preview-environments-feature.tsx
+++ libs/pages/services/src/lib/feature/page-settings-preview-environments-feature/page-settings-preview-environments-feature.tsx
@@ -1,18 +1,51 @@
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { EnvironmentDeploymentRule } from 'qovery-typescript-axios'
+import { useDispatch, useSelector } from 'react-redux'
 import { EnvironmentEntity } from '@console/shared/interfaces'
-import { RootState } from '@console/store/data'
-import { selectEnvironmentById } from '@console/domains/environment'
+import { AppDispatch, RootState } from '@console/store/data'
+import {
+  fetchEnvironmentDeploymentRules,
+  selectEnvironmentById,
+  selectEnvironmentDeploymentRulesByEnvId,
+} from '@console/domains/environment'
 import PageSettingsPreviewEnvironments from '../../ui/page-settings-preview-environments/page-settings-preview-environments'
+import { FormProvider, useForm } from 'react-hook-form'
 
 export function PageSettingsPreviewEnvironmentsFeature() {
   const { environmentId = '' } = useParams()
-  // const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>()
   const environment = useSelector<RootState, EnvironmentEntity | undefined>((state) =>
     selectEnvironmentById(state, environmentId)
   )
 
-  return <PageSettingsPreviewEnvironments environment={environment} />
+  const environmentDeploymentRules = useSelector<RootState, EnvironmentDeploymentRule | undefined>((state) =>
+    selectEnvironmentDeploymentRulesByEnvId(state, environmentId)
+  )
+
+  const methods = useForm({
+    mode: 'onChange',
+  })
+
+  const onSubmit = methods.handleSubmit(async (data) => {
+    console.log(data)
+    // if (data) {
+
+    // }
+  })
+
+  useEffect(() => {
+    async function fetchDeploymentRules() {
+      await dispatch(fetchEnvironmentDeploymentRules(environmentId))
+    }
+    fetchDeploymentRules()
+  }, [dispatch, environmentId])
+
+  return (
+    <FormProvider {...methods}>
+      <PageSettingsPreviewEnvironments onSubmit={onSubmit} environment={environment} />
+    </FormProvider>
+  )
 }
 
 export default PageSettingsPreviewEnvironmentsFeature
--- libs/pages/services/src/lib/ui/page-settings-preview-environments/page-settings-preview-environments.tsx
+++ libs/pages/services/src/lib/ui/page-settings-preview-environments/page-settings-preview-environments.tsx
@@ -1,21 +1,47 @@
+import { Controller, useFormContext } from 'react-hook-form'
 import { EnvironmentEntity } from '@console/shared/interfaces'
-import { BlockContent, HelpSection } from '@console/shared/ui'
+import { BlockContent, Button, ButtonSize, ButtonStyle, HelpSection, InputToggle } from '@console/shared/ui'
 
 export interface PageSettingsPreviewEnvironmentsProps {
+  onSubmit: () => void
   environment?: EnvironmentEntity
 }
 
 export function PageSettingsPreviewEnvironments(props: PageSettingsPreviewEnvironmentsProps) {
-  const { environment } = props
+  const { onSubmit, environment } = props
 
+  const { control, formState } = useFormContext()
   console.log(environment)
 
   return (
     <div className="flex flex-col justify-between w-full">
       <div className="p-8">
-        <BlockContent title="General">
-          <p>Test block</p>
-        </BlockContent>
+        <form onSubmit={onSubmit}>
+          <BlockContent title="General">
+            <Controller
+              name="auto_delete"
+              control={control}
+              render={({ field }) => (
+                <InputToggle
+                  value={field.value}
+                  onChange={field.onChange}
+                  title="Activate preview environment for all applications"
+                  description="Automatically create a preview environment when a merge request is submitted on one of your applications. Your environment will be cloned with the application synchronised on the branch waiting to be merged."
+                  small
+                />
+              )}
+            />
+          </BlockContent>
+          <Button
+            className="mb-6"
+            disabled={!formState.isValid}
+            size={ButtonSize.REGULAR}
+            style={ButtonStyle.BASIC}
+            type="submit"
+          >
+            Save
+          </Button>
+        </form>
       </div>
       <HelpSection
         description="Need help? You may find these links useful"
