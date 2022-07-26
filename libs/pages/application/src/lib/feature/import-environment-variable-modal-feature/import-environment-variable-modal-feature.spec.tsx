aba96d1df2c69e7ba7473064f39427079a7714f5
bdebon <benjamin.debon@gmail.com>
2022-07-26 16:39:50 +0200

    adding lib dropzone and testing dropzone

--- libs/pages/application/src/lib/feature/import-environment-variable-modal-feature/import-environment-variable-modal-feature.spec.tsx
+++ libs/pages/application/src/lib/feature/import-environment-variable-modal-feature/import-environment-variable-modal-feature.spec.tsx
@@ -1,8 +1,14 @@
 import ImportEnvironmentVariableModalFeature, {
   ImportEnvironmentVariableModalFeatureProps,
 } from './import-environment-variable-modal-feature'
-import { render, screen, waitFor } from '@testing-library/react'
+import { findAllByTestId, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+
+const envVariableFile = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+}
 
 describe('ImportEnvironmentVariableModalFeature', () => {
   const props: ImportEnvironmentVariableModalFeatureProps = {
@@ -11,11 +17,32 @@ describe('ImportEnvironmentVariableModalFeature', () => {
   }
 
   it('should render successfully', async () => {
-    const { baseElement } = render(<ImportEnvironmentVariableModalFeature {...props} />)
+    const { baseElement, debug } = render(<ImportEnvironmentVariableModalFeature {...props} />)
 
-    const submitButton = screen.getByRole('button', { name: 'Confirm' })
     await waitFor(() => {
       expect(baseElement).toBeTruthy()
+      debug()
+    })
+  })
+
+  it('should read file dropped and show table of inputs', async () => {
+    const { baseElement } = render(<ImportEnvironmentVariableModalFeature {...props} />)
+    const inputEl = screen.getByTestId('drop-input')
+    const str = JSON.stringify(envVariableFile)
+    const blob = new Blob([str])
+    const file = new File([blob], 'values.json', {
+      type: 'application/JSON',
+    })
+
+    fireEvent.change(inputEl, {
+      target: {
+        files: [file],
+      },
+    })
+
+    await waitFor(async () => {
+      const formRows = await findAllByTestId(baseElement, 'form-row')
+      expect(formRows).toHaveLength(Object.keys(envVariableFile).length)
     })
   })
 })
--- libs/pages/application/src/lib/feature/import-environment-variable-modal-feature/import-environment-variable-modal-feature.tsx
+++ libs/pages/application/src/lib/feature/import-environment-variable-modal-feature/import-environment-variable-modal-feature.tsx
@@ -1,12 +1,13 @@
 import { FormProvider, useForm } from 'react-hook-form'
 import ImportEnvironmentVariableModal from '../../ui/import-environment-variable-modal/import-environment-variable-modal'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { EnvironmentVariableScopeEnum } from 'qovery-typescript-axios'
 import { jsonToForm } from './utils/file-to-form'
 import { triggerToggleAll } from './utils/trigger-toggle-all'
 import { changeScopeForAll } from './utils/change-scope-all'
 import { handleSubmit } from './utils/handle-submit'
 import { computeAvailableScope } from '../../utils/compute-available-environment-variable-scope'
+import { useDropzone } from 'react-dropzone'
 
 export interface ImportEnvironmentVariableModalFeatureProps {
   applicationId: string
@@ -22,22 +23,55 @@ export function ImportEnvironmentVariableModalFeature(props: ImportEnvironmentVa
   const defaultValues = jsonToForm(json)
   const methods = useForm({ defaultValues, mode: 'onChange' })
 
-  // const [fileParsed, setFileParsed] = useState<boolean>(false)
+  const [fileParsed, setFileParsed] = useState<{ [key: string]: string }>(null)
   const [keys] = useState<string[]>(Object.keys(JSON.parse(json)))
 
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result
+        setFileParsed(jsonToForm(binaryStr as string))
+      }
+
+      reader.readAsText(file)
+    })
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   return (
-    <FormProvider {...methods}>
-      <ImportEnvironmentVariableModal
-        toggleAll={false}
-        triggerToggleAll={(b) => triggerToggleAll(b, methods.setValue, keys)}
-        changeScopeForAll={(scope) => changeScopeForAll(scope as EnvironmentVariableScopeEnum, methods.setValue, keys)}
-        keys={keys}
-        setOpen={props.setOpen}
-        loading={false}
-        availableScopes={computeAvailableScope(undefined, true)}
-        onSubmit={methods.handleSubmit(() => handleSubmit(methods.getValues()))}
-      />
-    </FormProvider>
+    <>
+      {!fileParsed ? (
+        <div {...getRootProps}>
+          <input data-testid="drop-input" {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
+        </div>
+      ) : (
+        <FormProvider {...methods}>
+          <ImportEnvironmentVariableModal
+            toggleAll={false}
+            triggerToggleAll={(b) => triggerToggleAll(b, methods.setValue, keys)}
+            changeScopeForAll={(scope) =>
+              changeScopeForAll(scope as EnvironmentVariableScopeEnum, methods.setValue, keys)
+            }
+            keys={keys}
+            setOpen={props.setOpen}
+            loading={false}
+            availableScopes={computeAvailableScope(undefined, true)}
+            onSubmit={methods.handleSubmit(() => handleSubmit(methods.getValues()))}
+          />
+        </FormProvider>
+      )}
+    </>
   )
 }
 
