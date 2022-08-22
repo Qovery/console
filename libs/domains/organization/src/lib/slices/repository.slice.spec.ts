632077a45eac2f2ce2e3b9ed9962d02c8b4fc6ff
Rémi Bonnet <rbonnet@qovery.com>
2022-08-22 14:52:05 +0200

    wip

--- libs/domains/organization/src/lib/slices/repository.slice.spec.ts
+++ libs/domains/organization/src/lib/slices/repository.slice.spec.ts
@@ -1,4 +1,4 @@
-import { fetchRepository, repositoryAdapter, repositoryReducer } from './repository.slice'
+import { repositoryAdapter, repositoryReducer } from './repository.slice'
 
 describe('repository reducer', () => {
   it('should handle initial state', () => {
@@ -8,37 +8,5 @@ describe('repository reducer', () => {
     })
 
     expect(repositoryReducer(undefined, { type: '' })).toEqual(expected)
-  })
-
-  it('should handle fetchRepositorys', () => {
-    let state = repositoryReducer(undefined, fetchRepository.pending(null, null))
-
-    expect(state).toEqual(
-      expect.objectContaining({
-        loadingStatus: 'loading',
-        error: null,
-        entities: {},
-      })
-    )
-
-    state = repositoryReducer(state, fetchRepository.fulfilled([{ id: 1 }], null, null))
-
-    expect(state).toEqual(
-      expect.objectContaining({
-        loadingStatus: 'loaded',
-        error: null,
-        entities: { 1: { id: 1 } },
-      })
-    )
-
-    state = repositoryReducer(state, fetchRepository.rejected(new Error('Uh oh'), null, null))
-
-    expect(state).toEqual(
-      expect.objectContaining({
-        loadingStatus: 'error',
-        error: 'Uh oh',
-        entities: { 1: { id: 1 } },
-      })
-    )
   })
 })
