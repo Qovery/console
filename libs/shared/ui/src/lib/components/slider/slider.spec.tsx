3c501d9a33d6b96b91a88c962f71b31c51e61f6d
Rémi Bonnet <rbonnet@qovery.com>
2022-08-30 12:13:34 +0200

    wip

--- libs/shared/ui/src/lib/components/slider/slider.spec.tsx
+++ libs/shared/ui/src/lib/components/slider/slider.spec.tsx
@@ -1,5 +1,5 @@
-import { act, render, screen } from '@testing-library/react'
 import { ResizeObserver } from '__tests__/utils/resize-observer'
+import { act, render, screen } from '__tests__/utils/setup-jest'
 import { Slider, SliderProps } from './slider'
 
 describe('Slider', () => {
