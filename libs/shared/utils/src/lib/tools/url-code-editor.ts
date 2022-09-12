e654c6116870150e757060b1e4ad688d43128fb5
Rémi Bonnet <rbonnet@qovery.com>
2022-09-12 09:04:20 +0200

    fix(url-code-editor): gitpod only

--- libs/shared/utils/src/lib/tools/url-code-editor.ts
+++ libs/shared/utils/src/lib/tools/url-code-editor.ts
@@ -1,19 +1,9 @@
-import { ApplicationGitRepository, GitProviderEnum } from 'qovery-typescript-axios'
+import { ApplicationGitRepository } from 'qovery-typescript-axios'
 
 export const urlCodeEditor = (git_repository?: ApplicationGitRepository) => {
   if (!git_repository) return
 
-  let editorUrl: string | undefined = ''
-
-  switch (git_repository?.provider) {
-    case GitProviderEnum.GITHUB:
-      editorUrl = git_repository.url?.replace('github.com', 'github.dev')
-      break
-    case GitProviderEnum.GITLAB:
-    case GitProviderEnum.BITBUCKET:
-      editorUrl = 'https://gitpod.io/#' + git_repository?.url
-      break
-  }
+  let editorUrl = 'https://gitpod.io/#' + git_repository?.url
 
   if (editorUrl && editorUrl.endsWith('.git')) editorUrl = editorUrl.replace('.git', '')
 
