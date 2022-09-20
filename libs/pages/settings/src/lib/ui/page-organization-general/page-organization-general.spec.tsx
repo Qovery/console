03ee049e94989530741424833c3c1b8612dc6ce6
Rémi Bonnet <rbonnet@qovery.com>
2022-09-20 11:25:01 +0200

    wip

--- libs/pages/settings/src/lib/ui/page-organization-general/page-organization-general.spec.tsx
+++ libs/pages/settings/src/lib/ui/page-organization-general/page-organization-general.spec.tsx
@@ -1,9 +1,44 @@
-import { render } from '@testing-library/react'
-import PageOrganizationGeneral from './page-organization-general'
+import { waitFor } from '@testing-library/react'
+import { render } from '__tests__/utils/setup-jest'
+import { wrapWithReactHookForm } from '__tests__/utils/wrap-with-react-hook-form'
+import PageOrganizationGeneral, { PageOrganizationGeneralProps } from './page-organization-general'
 
 describe('PageOrganizationGeneral', () => {
+  const props: PageOrganizationGeneralProps = {
+    onSubmit: jest.fn((e) => e.preventDefault()),
+    loading: false,
+    created_at: new Date().toString(),
+  }
+
+  const defaultValues = {
+    logo_url: 'https://qovery.com',
+    name: 'hello',
+    description: 'hello world',
+    website_url: 'https://qovery.com',
+    admin_emails: ['test@test.com', 'test2@test.com'],
+  }
+
   it('should render successfully', () => {
-    const { baseElement } = render(<PageOrganizationGeneral />)
+    const { baseElement } = render(
+      wrapWithReactHookForm(<PageOrganizationGeneral {...props} />, {
+        defaultValues: defaultValues,
+      })
+    )
     expect(baseElement).toBeTruthy()
+  })
+
+  it('should submit the form', async () => {
+    const { getByTestId } = render(
+      wrapWithReactHookForm(<PageOrganizationGeneral {...props} />, {
+        defaultValues: defaultValues,
+      })
+    )
+
+    const button = getByTestId('submit-button')
+
+    await waitFor(() => {
+      button.click()
+      expect(props.onSubmit).toHaveBeenCalled()
+    })
   })
 })
