fcd4751fdfd2da4b29152575e9d2fccad530a0f5
Rémi Bonnet <rbonnet@qovery.com>
2022-09-20 17:06:27 +0200

    test

--- libs/pages/settings/src/lib/feature/page-organization-general-feature/page-organization-general-feature.spec.tsx
+++ libs/pages/settings/src/lib/feature/page-organization-general-feature/page-organization-general-feature.spec.tsx
@@ -1,9 +1,80 @@
-import { render } from '__tests__/utils/setup-jest'
-import PageOrganizationGeneralFeature from './page-organization-general-feature'
+import { act, fireEvent, render } from '__tests__/utils/setup-jest'
+import { Organization } from 'qovery-typescript-axios'
+import * as storeOrganization from '@qovery/domains/organization'
+import PageOrganizationGeneralFeature, { handleSubmit } from './page-organization-general-feature'
+
+import SpyInstance = jest.SpyInstance
+
+const mockOrganization: Organization = storeOrganization.organizationFactoryMock(1)[0]
+
+jest.mock('@qovery/domains/organization', () => {
+  return {
+    ...jest.requireActual('@qovery/domains/organization'),
+    editOrganization: jest.fn(),
+    selectOrganizationById: () => {
+      const currentMockOrganization = mockOrganization
+      mockOrganization.description = 'description'
+      mockOrganization.website_url = 'https://qovery.com'
+      mockOrganization.logo_url = 'my-logo'
+      return currentMockOrganization
+    },
+  }
+})
+
+const mockDispatch = jest.fn()
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}))
+
+jest.mock('react-router', () => ({
+  ...(jest.requireActual('react-router') as any),
+  useParams: () => ({ organizationId: '0' }),
+}))
 
 describe('PageOrganizationGeneral', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<PageOrganizationGeneralFeature />)
     expect(baseElement).toBeTruthy()
+  })
+
+  it('should dispatch editOrganization if form is submitted', async () => {
+    const editOrganizationSpy: SpyInstance = jest.spyOn(storeOrganization, 'editOrganization')
+    mockDispatch.mockImplementation(() => ({
+      unwrap: () =>
+        Promise.resolve({
+          data: {},
+        }),
+    }))
+
+    const { getByTestId } = render(<PageOrganizationGeneralFeature />)
+
+    await act(() => {
+      const input = getByTestId('input-name')
+      fireEvent.input(input, { target: { value: 'hello-world' } })
+    })
+
+    expect(getByTestId('submit-button')).not.toBeDisabled()
+
+    await act(() => {
+      getByTestId('submit-button').click()
+    })
+
+    const newOrganization = {
+      name: 'hello-world',
+      website_url: 'https://qovery.com',
+      logo_url: 'my-logo',
+      description: 'description',
+      admin_emails: ['test@test.com'],
+    }
+
+    const cloneOrganization = handleSubmit(newOrganization, mockOrganization)
+
+    expect(editOrganizationSpy.mock.calls[0][0].organizationId).toBe(mockOrganization.id)
+    expect(cloneOrganization.name).toBe('hello-world')
+    expect(cloneOrganization.description).toBe('description')
+    expect(cloneOrganization.website_url).toBe('https://qovery.com')
+    expect(cloneOrganization.logo_url).toBe('my-logo')
+    expect(editOrganizationSpy.mock.calls[0][0].data).toStrictEqual(cloneOrganization)
   })
 })
