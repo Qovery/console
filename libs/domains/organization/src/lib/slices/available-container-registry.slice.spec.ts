7de585bfac9f35f6c1b315d41fd5bafaaf57ffb3
Rémi Bonnet <rbonnet@qovery.com>
2022-09-28 11:36:50 +0200

    wip

--- libs/domains/organization/src/lib/slices/available-container-registry.slice.spec.ts
+++ libs/domains/organization/src/lib/slices/available-container-registry.slice.spec.ts
@@ -1,51 +1,13 @@
-import {
-  availableContainerRegistryAdapter,
-  availableContainerRegistryReducer,
-  fetchAvailableContainerRegistry,
-} from './available-container-registry.slice'
+import { availableContainerRegistryReducer } from './available-container-registry.slice'
 
 describe('availableContainerRegistry reducer', () => {
   it('should handle initial state', () => {
-    const expected = availableContainerRegistryAdapter.getInitialState({
+    const expected = {
       loadingStatus: 'not loaded',
       error: null,
-    })
+      items: [],
+    }
 
     expect(availableContainerRegistryReducer(undefined, { type: '' })).toEqual(expected)
-  })
-
-  it('should handle fetchAvailableContainerRegistrys', () => {
-    let state = availableContainerRegistryReducer(undefined, fetchAvailableContainerRegistry.pending(null, null))
-
-    expect(state).toEqual(
-      expect.objectContaining({
-        loadingStatus: 'loading',
-        error: null,
-        entities: {},
-      })
-    )
-
-    state = availableContainerRegistryReducer(state, fetchAvailableContainerRegistry.fulfilled([{ id: 1 }], null, null))
-
-    expect(state).toEqual(
-      expect.objectContaining({
-        loadingStatus: 'loaded',
-        error: null,
-        entities: { 1: { id: 1 } },
-      })
-    )
-
-    state = availableContainerRegistryReducer(
-      state,
-      fetchAvailableContainerRegistry.rejected(new Error('Uh oh'), null, null)
-    )
-
-    expect(state).toEqual(
-      expect.objectContaining({
-        loadingStatus: 'error',
-        error: 'Uh oh',
-        entities: { 1: { id: 1 } },
-      })
-    )
   })
 })
