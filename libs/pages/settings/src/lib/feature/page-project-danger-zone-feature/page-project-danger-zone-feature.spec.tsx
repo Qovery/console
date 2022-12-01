17c39a9beabb9b868e9bd60f28a750d7a03b5f8f
Rémi Bonnet <rbonnet@qovery.com>
2022-12-01 16:50:14 +0100

    fix

--- libs/pages/settings/src/lib/feature/page-project-danger-zone-feature/page-project-danger-zone-feature.spec.tsx
+++ libs/pages/settings/src/lib/feature/page-project-danger-zone-feature/page-project-danger-zone-feature.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '__tests__/utils/setup-jest'
-import PageProjectDangerZoneFeature from './page-organization-danger-zone-feature'
+import PageProjectDangerZoneFeature from './page-project-danger-zone-feature'
 
 describe('PageProjectDangerZoneFeature', () => {
   it('should render successfully', () => {
