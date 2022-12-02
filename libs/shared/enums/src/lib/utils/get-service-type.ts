ea6c061349341f18ce650efe0e400a7db3aae002
Rémi Bonnet <rbonnet@qovery.com>
2022-12-02 17:34:14 +0100

    fix(jobs): types and get job service

--- libs/shared/enums/src/lib/utils/get-service-type.ts
+++ libs/shared/enums/src/lib/utils/get-service-type.ts
@@ -1,4 +1,3 @@
-import { JobScheduleEvent } from 'qovery-typescript-axios'
 import {
   ApplicationEntity,
   ContainerApplicationEntity,
@@ -11,15 +10,15 @@ import { ServiceTypeEnum } from '../service-type.enum'
 export const getServiceType = (data: ApplicationEntity | DatabaseEntity) => {
   let currentType: ServiceTypeEnum
 
-  const isJob = (data as JobApplicationEntity).max_nb_restart
+  const isJob = (data as JobApplicationEntity).max_nb_restart !== undefined
 
   if ((data as ContainerApplicationEntity).image_name) {
     currentType = ServiceTypeEnum.CONTAINER
   } else if (!(data as GitApplicationEntity).build_mode && !(data as ContainerApplicationEntity).image_name && !isJob) {
     currentType = ServiceTypeEnum.DATABASE
-  } else if (isJob && (data as JobApplicationEntity).schedule?.event !== JobScheduleEvent.CRON) {
+  } else if (isJob && (data as JobApplicationEntity).schedule?.cronjob) {
     currentType = ServiceTypeEnum.CRON_JOB
-  } else if (isJob && (data as JobApplicationEntity).schedule?.event === JobScheduleEvent.CRON) {
+  } else if (isJob && !(data as JobApplicationEntity).schedule?.cronjob) {
     currentType = ServiceTypeEnum.LIFECYCLE_JOB
   } else {
     currentType = ServiceTypeEnum.APPLICATION
