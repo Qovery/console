ddb40e1f6b146ecd002c4fae3d1eb3f0a66fff0a
bdebon <benjamin.debon@gmail.com>
2022-12-12 16:51:07 +0100

    fix(jest): fix jest config and removing faulty useless server wrapper (#412)

--- jest.preset.js
+++ jest.preset.js
@@ -7,7 +7,7 @@ module.exports = {
   testPathIgnorePatterns: ['./node_modules/', './.next/', './__tests__/utils/setup-jest.tsx'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
     '^.+\\.scss$': 'jest-scss-transform',
   },
   transformIgnorePatterns: [
