19b506fd8c49b34a90708eb4d92efee8b419000e
Rémi Bonnet <rbonnet@qovery.com>
2022-12-15 11:53:46 +0100

    wip

--- libs/pages/clusters/src/lib/ui/page-clusters-general/page-clusters-general.tsx
+++ libs/pages/clusters/src/lib/ui/page-clusters-general/page-clusters-general.tsx
@@ -1,5 +1,6 @@
+import { useParams } from 'react-router-dom'
 import { ClusterEntity, LoadingStatus } from '@qovery/shared/interfaces'
-import { EmptyState, HelpSection, LoaderSpinner } from '@qovery/shared/ui'
+import { Button, EmptyState, HelpSection, IconAwesomeEnum, LoaderSpinner } from '@qovery/shared/ui'
 import CardCluster from '../card-cluster/card-cluster'
 
 export interface PageClustersGeneralProps {
@@ -10,6 +11,8 @@ export interface PageClustersGeneralProps {
 export function PageClustersGeneral(props: PageClustersGeneralProps) {
   const { loading, clusters } = props
 
+  const { organizationId = '' } = useParams()
+
   return (
     <div className="flex flex-col justify-between w-full">
       <div className="p-8">
@@ -18,7 +21,14 @@ export function PageClustersGeneral(props: PageClustersGeneralProps) {
             <h1 className="h5 text-text-700 mb-2">Manage your clusters</h1>
             <p className="text-text-500 text-xs">Manage your infrastructure across different Cloud providers.</p>
           </div>
-          {/* <Button iconRight={IconAwesomeEnum.CIRCLE_PLUS}>Add Cluster</Button> */}
+          <Button
+            iconRight={IconAwesomeEnum.CIRCLE_PLUS}
+            onClick={() =>
+              window.open(`https://console.qovery.com/platform/organization/${organizationId}/settings/clusters`)
+            }
+          >
+            Add Cluster
+          </Button>
         </div>
         {(loading === 'not loaded' || loading === 'loading') && clusters?.length === 0 ? (
           <div data-testid="clusters-loader" className="flex justify-center">
