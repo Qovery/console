2bfda1359f24c798db33912f4ed449184670ee2e
Rémi Bonnet <rbonnet@qovery.com>
2022-12-19 12:22:45 +0100

    wip

--- libs/pages/clusters/src/lib/ui/card-cluster/card-cluster.spec.tsx
+++ libs/pages/clusters/src/lib/ui/card-cluster/card-cluster.spec.tsx
@@ -42,7 +42,7 @@ describe('CardCluster', () => {
   })
 
   it('should have a function to display color by status', () => {
-    expect(getColorForStatus(StateEnum.BUILDING)).toBe('text-progressing-500')
+    expect(getColorForStatus(StateEnum.DELETING)).toBe('text-progressing-500')
     expect(getColorForStatus(StateEnum.STOP_ERROR)).toBe('text-error-500')
     expect(getColorForStatus(StateEnum.READY)).toBe('text-brand-500')
   })
