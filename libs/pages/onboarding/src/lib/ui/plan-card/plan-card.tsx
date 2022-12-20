98231ec3f5009991110e43813e9949c0d6bc6de2
Rémi Bonnet <rbonnet@qovery.com>
2022-12-20 14:38:26 +0100

    responsive

--- libs/pages/onboarding/src/lib/ui/plan-card/plan-card.tsx
+++ libs/pages/onboarding/src/lib/ui/plan-card/plan-card.tsx
@@ -15,7 +15,7 @@ export function PlanCard(props: PlanCardProps) {
   const { name, title, text, price, loading, onClick, list } = props
 
   return (
-    <div className="bg-element-light-lighter-200 border border-element-light-lighter-500 px-5 py-4 rounded w-80">
+    <div className="bg-element-light-lighter-200 border border-element-light-lighter-500 px-5 py-4 rounded lg:w-80">
       <h2 className="h5 mb-1">{title}</h2>
       <p className="text-sm text-text-500 mb-2">{text}</p>
       <p className="flex items-center text-text-600 text-xs mb-4">
