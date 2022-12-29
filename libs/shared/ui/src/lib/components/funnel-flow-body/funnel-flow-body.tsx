8f12382e6d9e0f309093bc9dfc1f04ae533ab7ab
Rémi Bonnet <rbonnet@qovery.com>
2022-12-29 10:40:41 +0100

    fix(funnel-flow):  hide right section for mobile

--- libs/shared/ui/src/lib/components/funnel-flow-body/funnel-flow-body.tsx
+++ libs/shared/ui/src/lib/components/funnel-flow-body/funnel-flow-body.tsx
@@ -12,7 +12,7 @@ export function FunnelFlowBody(props: FunnelFlowBodyProps) {
         <div className="absolute h-full bg-white w-full pointer-events-none" style={{ left: '-30%' }}></div>
       )}
       <div className="flex w-full overflow-auto">
-        <section className={`bg-white pt-14 ${props.helpSection ? 'w-[70%]' : 'w-full overflow-auto'}`}>
+        <section className={`bg-white pt-14 w-full ${props.helpSection ? 'lg:w-[70%]' : 'overflow-auto'}`}>
           <div
             data-testid="funnel-body-content"
             className={`px-12 mx-auto relative pb-14 ${
@@ -23,7 +23,7 @@ export function FunnelFlowBody(props: FunnelFlowBodyProps) {
           </div>
         </section>
         {props.helpSection && (
-          <aside className="w-[30%] sticky top-0 pt-14 pl-10">
+          <aside className="w-[30%] sticky top-0 pt-14 pl-10 hidden lg:block">
             <div
               data-testid="funnel-body-help"
               className={`bg-element-light-lighter-100 p-8 border-element-light-lighter-400 border-b border-l max-w-[22.5rem] ${
