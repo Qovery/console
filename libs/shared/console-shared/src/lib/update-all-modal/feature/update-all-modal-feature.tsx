3c0526be7db47a763302e56895cf6cabf6874c22
bdebon <benjamin.debon@gmail.com>
2023-01-12 15:36:05 +0100

    wip

--- libs/shared/console-shared/src/lib/update-all-modal/feature/update-all-modal-feature.tsx
+++ libs/shared/console-shared/src/lib/update-all-modal/feature/update-all-modal-feature.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
+  applicationsLoadingStatus,
   fetchApplicationCommits,
   fetchApplications,
   selectApplicationsEntitiesByEnvId,
 } from '@qovery/domains/application'
 import { getServiceType, isApplication, isGitJob } from '@qovery/shared/enums'
-import { ApplicationEntity } from '@qovery/shared/interfaces'
+import { ApplicationEntity, LoadingStatus } from '@qovery/shared/interfaces'
 import { AppDispatch, RootState } from '@qovery/store'
 
 export interface UpdateAllModalFeatureProps {
@@ -29,17 +30,24 @@ export function UpdateAllModalFeature(props: UpdateAllModalFeatureProps) {
       return a.length === b.length
     }
   )
+  const appsLoadingStatus = useSelector<RootState, LoadingStatus | undefined>((state: RootState) =>
+    applicationsLoadingStatus(state)
+  )
 
   useEffect(() => {
-    dispatch(fetchApplications({ environmentId }))
+    if (appsLoadingStatus !== 'not loaded') {
+      dispatch(fetchApplications({ environmentId }))
+    }
   }, [dispatch, environmentId])
 
   useEffect(() => {
-    applications?.forEach((application) => {
-      if (isApplication(application) || isGitJob(application)) {
-        dispatch(fetchApplicationCommits({ applicationId: application.id, serviceType: getServiceType(application) }))
-      }
-    })
+    if (appsLoadingStatus === 'loaded') {
+      applications?.forEach((application) => {
+        if (isApplication(application) || isGitJob(application)) {
+          dispatch(fetchApplicationCommits({ applicationId: application.id, serviceType: getServiceType(application) }))
+        }
+      })
+    }
   }, [applications, dispatch])
 
   return (
