649bc27c9eb74c98907cebadc76459ebe19016c9
bdebon <benjamin.debon@gmail.com>
2023-01-12 10:15:00 +0100

    allowind import of store everywhere

--- libs/shared/ui/src/lib/components/table/table.stories.tsx
+++ libs/shared/ui/src/lib/components/table/table.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, Story } from '@storybook/react'
 import { useState } from 'react'
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { environmentFactoryMock } from '@qovery/domains/environment'
+import { environmentFactoryMock } from '@qovery/shared/factories'
 import { EnvironmentEntity } from '@qovery/shared/interfaces'
 import Button from '../buttons/button/button'
 import Icon from '../icon/icon'
