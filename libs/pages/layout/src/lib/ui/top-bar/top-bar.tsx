e89b7a53a87f36cdfe48f2faedca8defcf64264b
Rémi Bonnet <rbonnet@qovery.com>
2023-01-16 11:53:11 +0100

    fix(top-bar): remove V2 button

--- libs/pages/layout/src/lib/ui/top-bar/top-bar.tsx
+++ libs/pages/layout/src/lib/ui/top-bar/top-bar.tsx
@@ -1,4 +1,3 @@
-import { Button, ButtonSize, ButtonStyle, IconAwesomeEnum } from '@qovery/shared/ui'
 import { BreadcrumbFeature } from '../../feature/breadcrumb/breadcrumb'
 
 export function TopBar() {
@@ -6,15 +5,6 @@ export function TopBar() {
     <div className="sticky top-0 left-16 z-20 border-l border-b w-full h-navbar-height dark:border-element-light-darker-100 dark:bg-element-light-darker-400 dark:border-b-0 border-element-light-lighter-400 bg-white">
       <div className="flex px-5 justify-between items-center h-full">
         <BreadcrumbFeature />
-        <Button
-          className="dark:hidden w-[245px]"
-          style={ButtonStyle.STROKED}
-          size={ButtonSize.LARGE}
-          iconRight={IconAwesomeEnum.ARROW_RIGHT_FROM_BRACKET}
-          onClick={() => window.location.replace(`https://console.qovery.com/platform/organization/`)}
-        >
-          Back to the console V2
-        </Button>
       </div>
     </div>
   )
