684b2e3f0582f4e9cdcbc4a07dd3fabefb5ae135
Rémi Bonnet <bonnet.rem@gmail.com>
2023-01-18 09:52:44 +0100

    feat(page-cluster): settings danger zone (#448)

---
+++ libs/pages/cluster/src/lib/ui/page-settings-danger-zone/page-settings-danger-zone.tsx
@@ -0,0 +1,41 @@
+import { EnvironmentModeEnum } from 'qovery-typescript-axios'
+import { ClusterEntity } from '@qovery/shared/interfaces'
+import { BlockContentDelete, HelpSection } from '@qovery/shared/ui'
+
+export interface PageSettingsDangerZoneProps {
+  deleteCluster: () => void
+  cluster?: ClusterEntity
+}
+
+export function PageSettingsDangerZone(props: PageSettingsDangerZoneProps) {
+  const { deleteCluster, cluster } = props
+  return (
+    <div className="flex flex-col justify-between w-full">
+      <div className="p-8 max-w-content-with-navigation-left">
+        <BlockContentDelete
+          title="Uninstall cluster"
+          ctaLabel="Delete cluster"
+          callback={deleteCluster}
+          modalConfirmation={{
+            mode: EnvironmentModeEnum.PRODUCTION,
+            title: 'Uninstall cluster',
+            description: 'To confirm the deletion of your cluster, please type the name of the cluster:',
+            name: cluster?.name,
+          }}
+        />
+      </div>
+      <HelpSection
+        description="Need help? You may find these links useful"
+        links={[
+          {
+            link: 'https://hub.qovery.com/docs/using-qovery/configuration/clusters/#deleting-a-cluster',
+            linkLabel: 'How to delete my cluster',
+            external: true,
+          },
+        ]}
+      />
+    </div>
+  )
+}
+
+export default PageSettingsDangerZone
