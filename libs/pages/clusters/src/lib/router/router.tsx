862630c0847009cc53e77b7e290cfe021eb9118b
Rémi Bonnet <rbonnet@qovery.com>
2023-01-31 16:02:48 +0100

    test

--- libs/pages/clusters/src/lib/router/router.tsx
+++ libs/pages/clusters/src/lib/router/router.tsx
@@ -4,7 +4,7 @@ import {
   CLUSTERS_GENERAL_URL,
   Route,
 } from '@qovery/shared/routes'
-import PageClusterCreateFeature from '../feature/page-clusters-create-feature/page-clusters-create-feature'
+import { PageClusterCreateFeature } from '../feature/page-clusters-create-feature/page-clusters-create-feature'
 import StepGeneralFeature from '../feature/page-clusters-create-feature/step-general-feature/step-general-feature'
 import PageClustersGeneralFeature from '../feature/page-clusters-general-feature/page-clusters-general-feature'
 
