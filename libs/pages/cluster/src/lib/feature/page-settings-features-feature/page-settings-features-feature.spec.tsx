967fe51d1a5e5584980962e0bf8386788f39ef35
Rémi Bonnet <bonnet.rem@gmail.com>
2023-01-31 12:12:52 +0100

    feat(cluster-settings): features section (#469)

---
+++ libs/pages/cluster/src/lib/feature/page-settings-features-feature/page-settings-features-feature.spec.tsx
@@ -0,0 +1,26 @@
+import { render } from '__tests__/utils/setup-jest'
+import { clusterFactoryMock } from '@qovery/shared/factories'
+import { ClusterEntity } from '@qovery/shared/interfaces'
+import PageSettingsFeaturesFeature from './page-settings-features-feature'
+
+const mockCluster: ClusterEntity = clusterFactoryMock(1)[0]
+
+jest.mock('@qovery/domains/organization', () => {
+  return {
+    ...jest.requireActual('@qovery/domains/organization'),
+    selectClusterById: () => mockCluster,
+    selectClustersLoadingStatus: () => 'loaded',
+  }
+})
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useParams: () => ({ clusterId: mockCluster.id }),
+}))
+
+describe('PageSettingsFeaturesFeature', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<PageSettingsFeaturesFeature />)
+    expect(baseElement).toBeTruthy()
+  })
+})
