0af7368dca963182c14ad1cbcce74726968e0105
bdebon <benjamin.debon@gmail.com>
2023-02-15 14:51:50 +0100

    chore(nx): migrate nx to 15.7.0 (#496)

--- libs/shared/ui/.storybook/main.js
+++ libs/shared/ui/.storybook/main.js
@@ -3,8 +3,8 @@ const rootMain = require('../../../../.storybook/main')
 module.exports = {
   ...rootMain,
   core: { ...rootMain.core, builder: 'webpack5' },
-  stories: [...rootMain.stories, '../src/**/**/*.stories.mdx', '../src/**/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
+  stories: ['../src/**/**/*.stories.mdx', '../src/**/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', ...rootMain.addons, '@nrwl/react/plugins/storybook'],
   staticDirs: ['../src/lib'],
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
