7423a9dfb094c77e0792e88e830a968470546e7a
bdebon <benjamin.debon@gmail.com>
2023-03-02 14:03:28 +0100

    wip

--- __tests__/utils/setup-jest.tsx
+++ __tests__/utils/setup-jest.tsx
@@ -1,13 +1,13 @@
 import '@testing-library/jest-dom'
 import { RenderOptions, render } from '@testing-library/react'
-import React from 'react'
+import { ReactElement } from 'react'
 import { Props, Wrapper } from './providers'
 
 type CustomRenderOptions = {
   wrapperProps?: Props
 } & Omit<RenderOptions, 'wrapper'>
 
-const customRender = (ui: React.ReactElement, options?: CustomRenderOptions) =>
+const customRender = (ui: ReactElement, options?: CustomRenderOptions) =>
   render(ui, { wrapper: (props) => <Wrapper {...props} {...options?.wrapperProps} />, ...options })
 
 export * from '@testing-library/react'
