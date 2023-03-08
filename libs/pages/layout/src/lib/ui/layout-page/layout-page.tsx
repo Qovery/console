16025b3006bcd77d4497945e1ac74d65c8fb4dbf
Rémi Bonnet <rbonnet@qovery.com>
2023-03-08 10:49:09 +0100

    add

--- libs/pages/layout/src/lib/ui/layout-page/layout-page.tsx
+++ libs/pages/layout/src/lib/ui/layout-page/layout-page.tsx
@@ -13,6 +13,16 @@ export interface LayoutPageProps {
   cluster?: ClusterEntity
 }
 
+export const displayClusterBanner = (status?: StateEnum): boolean => {
+  switch (status) {
+    case StateEnum.DEPLOYMENT_QUEUED:
+    case StateEnum.DEPLOYING:
+      return true
+    default:
+      return false
+  }
+}
+
 export function LayoutPage(props: LayoutPageProps) {
   const { children, topBar = true, cluster } = props
 
@@ -22,29 +32,21 @@ export function LayoutPage(props: LayoutPageProps) {
 
   const matchLogInfraRoute = pathname.includes(INFRA_LOGS_URL(organizationId, cluster?.id))
 
-  const displayBanner = (status?: StateEnum): boolean => {
-    switch (status) {
-      case StateEnum.DEPLOYMENT_QUEUED:
-      case StateEnum.DEPLOYING:
-        return true
-      default:
-        return false
-    }
-  }
-
   const clusterIsDeployed = cluster?.extendedStatus?.status?.is_deployed
+
+  const clusterBanner = !matchLogInfraRoute && cluster && displayClusterBanner(cluster.status) && !clusterIsDeployed
 
   return (
     <>
       <WarningScreenMobile />
-      <main className="dark:bg-element-light-darker-600 dark:h-screen bg-element-light-lighter-400">
+      <main className="dark:bg-element-light-darker-600 dark:h-full bg-element-light-lighter-400">
         <div className="flex">
           <div className="h-full sticky top-0 z-20">
             <Navigation />
           </div>
           <div className="w-full">
             {topBar && <TopBar />}
-            {!matchLogInfraRoute && cluster && displayBanner(cluster.status) && !clusterIsDeployed && (
+            {clusterBanner && (
               <Banner
                 bannerStyle={BannerStyle.PRIMARY}
                 onClickButton={() => navigate(INFRA_LOGS_URL(organizationId, cluster.id))}
@@ -54,7 +56,13 @@ export function LayoutPage(props: LayoutPageProps) {
                 can follow it from logs
               </Banner>
             )}
-            <div className="relative flex flex-col min-h-page-container pt-2 px-2 dark:pt-0 dark:px-0">{children}</div>
+            <div
+              className={`relative flex flex-col pt-2 px-2 dark:pt-0 dark:px-0 ${
+                !clusterBanner ? 'min-h-page-container-wbanner' : 'max-h-page-container'
+              }`}
+            >
+              {children}
+            </div>
           </div>
         </div>
       </main>
