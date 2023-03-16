03df6d70921378cfe21edbd2d7f2615e4b2f7397
bdebon <benjamin.debon@gmail.com>
2023-03-16 13:57:40 +0100

    wip

--- libs/pages/settings/src/lib/feature/page-organization-billing-summary-feature/checkout-upgrade-modal-feature/checkout-upgrade-modal-feature.tsx
+++ libs/pages/settings/src/lib/feature/page-organization-billing-summary-feature/checkout-upgrade-modal-feature/checkout-upgrade-modal-feature.tsx
@@ -1,9 +1,18 @@
 import { PlanEnum } from 'qovery-typescript-axios'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { fetchMembers } from '@qovery/domains/organization'
-import { OrganizationEntity } from '@qovery/shared/interfaces'
-import { AppDispatch } from '@qovery/store'
+import { useEffect, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { CardImages } from 'react-payment-inputs/images'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  fetchCreditCards,
+  fetchMembers,
+  getCreditCardsState,
+  selectCreditCardsByOrganizationId,
+} from '@qovery/domains/organization'
+import { CreditCardFormValues } from '@qovery/shared/console-shared'
+import { OrganizationEntity, Value } from '@qovery/shared/interfaces'
+import { imagesCreditCart } from '@qovery/shared/ui'
+import { AppDispatch, RootState } from '@qovery/store'
 import CheckoutUpgradeModal from '../../../ui/page-organization-billing-summary/checkout-upgrade-modal/checkout-upgrade-modal'
 
 export interface CheckoutUpgradeModalFeatureProps {
@@ -14,13 +23,68 @@ export interface CheckoutUpgradeModalFeatureProps {
 export function CheckoutUpgradeModalFeature(props: CheckoutUpgradeModalFeatureProps) {
   const dispatch = useDispatch<AppDispatch>()
 
+  const methods = useForm<CreditCardFormValues>({ mode: 'all' })
+
+  const onCardSubmit = methods.handleSubmit((data) => {
+    setShowAddCardForm(false)
+  })
+
+  const creditCards = useSelector((state: RootState) =>
+    selectCreditCardsByOrganizationId(state, props.organization.id || '')
+  )
+  const creditCardLoadingStatus = useSelector<RootState, string | undefined>(
+    (state) => getCreditCardsState(state).loadingStatus
+  )
+  const [creditCardsOptions, setCreditCardsOptions] = useState<Value[]>([])
+
+  const [showAddCardForm, setShowAddCardForm] = useState(false)
+  const [selectedCard, setSelectedCard] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if (props.organization.id && creditCardLoadingStatus === 'not loaded')
+      dispatch(fetchCreditCards({ organizationId: props.organization.id }))
+  }, [props.organization.id, dispatch, creditCardLoadingStatus])
+
   useEffect(() => {
     if (!props.organization.members?.loadingStatus) {
       dispatch(fetchMembers({ organizationId: props.organization.id }))
     }
   }, [props.organization.members?.loadingStatus, dispatch, props.organization.id])
 
-  return <CheckoutUpgradeModal plan={props.plan} memberCount={props.organization.members?.items?.length || 1} />
+  useEffect(() => {
+    if (creditCards.length > 0) {
+      setCreditCardsOptions(
+        creditCards.map((card) => ({
+          label: '**** ' + card.last_digit,
+          value: card.id,
+          icon: (
+            <svg
+              data-testid="credit-card-image"
+              className="absolute -translate-x-full -ml-3 -translate-y-1/2 top-1/2 z-10 w-6 h-4"
+              {...{ children: imagesCreditCart[card.brand as keyof CardImages] }}
+            ></svg>
+          ),
+        }))
+      )
+    }
+  }, [creditCards])
+
+  return (
+    <FormProvider {...methods}>
+      <CheckoutUpgradeModal
+        toggleShowAddForm={(value: boolean) => {
+          setShowAddCardForm(value)
+        }}
+        showAddCardForm={showAddCardForm}
+        plan={props.plan}
+        memberCount={props.organization.members?.items?.length || 1}
+        onCardSubmit={onCardSubmit}
+        setSelectedCard={setSelectedCard}
+        selectedCard={selectedCard}
+        creditCardsOptions={creditCardsOptions}
+      />
+    </FormProvider>
+  )
 }
 
 export default CheckoutUpgradeModalFeature
--- libs/pages/settings/src/lib/ui/page-organization-billing-summary/checkout-upgrade-modal/checkout-upgrade-modal.tsx
+++ libs/pages/settings/src/lib/ui/page-organization-billing-summary/checkout-upgrade-modal/checkout-upgrade-modal.tsx
@@ -1,18 +1,31 @@
 import { PlanEnum } from 'qovery-typescript-axios'
+import { FormEventHandler } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CreditCardForm, CreditCardFormValues } from '@qovery/shared/console-shared'
+import { Value } from '@qovery/shared/interfaces'
+import { Button, ButtonSize, ButtonStyle, InputSelect } from '@qovery/shared/ui'
 import { upperCaseFirstLetter } from '@qovery/shared/utils'
 
 export interface CheckoutUpgradeModalProps {
   plan: PlanEnum
   memberCount: number
+  onCardSubmit: FormEventHandler<HTMLFormElement>
+  showAddCardForm: boolean
+  toggleShowAddForm: (value: boolean) => void
+  selectedCard: string | undefined
+  setSelectedCard: (value: string) => void
+  creditCardsOptions: Value[]
 }
 
 export function CheckoutUpgradeModal(props: CheckoutUpgradeModalProps) {
+  const { formState } = useFormContext<CreditCardFormValues>()
+
   return (
     <div className="p-6">
       <h2 className="h4 text-text-600 mb-2 max-w-lg mb-8">
         Confirm your change to the {upperCaseFirstLetter(props.plan)} plan for $49 per user
       </h2>
-      <div className="bg-element-light-lighter-200 border border-element-light-lighter-500 rounded px-6 py-5">
+      <div className="bg-element-light-lighter-200 border border-element-light-lighter-500 rounded px-6 py-5 mb-8">
         <div className="flex items-center justify-between font-medium text-sm text-text-600 mb-1">
           <span>Total seat</span>
           <span>{props.memberCount}</span>
@@ -29,6 +42,75 @@ export function CheckoutUpgradeModal(props: CheckoutUpgradeModalProps) {
           <span>$124,00 HT</span>
         </div>
       </div>
+
+      {!props.showAddCardForm ? (
+        <div className="bg-brand-50 rounded-lg p-6">
+          <div className="flex items-center justify-between mb-3">
+            <div className="text-text-500 font-medium">Pay with</div>
+            <div
+              className="text-brand-500 font-medium text-sm cursor-pointer"
+              onClick={() => props.toggleShowAddForm(true)}
+            >
+              Add a new card
+            </div>
+          </div>
+          <div className="">
+            <InputSelect
+              className="mb-5"
+              label="Card number"
+              options={[]}
+              portal
+              onChange={(value) => props.setSelectedCard(value as string)}
+            />
+
+            <p className="text-text-500 text-sm mb-6">
+              Price paid and delta Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+              officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+            </p>
+            <p className="text-center text-text-500 text-sm mb-6">
+              Card information are secured by <strong>Stripe</strong>
+            </p>
+
+            <Button
+              className="w-full"
+              style={ButtonStyle.BASIC}
+              size={ButtonSize.XLARGE}
+              disabled={!props.selectedCard}
+            >
+              Confirm
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <div className="bg-brand-50 rounded-lg p-6">
+          <div className="flex items-center justify-between mb-3">
+            <div className="text-text-500 font-medium">Register your credit card</div>
+            <div
+              className="text-brand-500 font-medium text-sm cursor-pointer"
+              onClick={() => props.toggleShowAddForm(false)}
+            >
+              Cancel
+            </div>
+          </div>
+          <div className="">
+            <form onSubmit={props.onCardSubmit}>
+              <CreditCardForm />
+              <p className="text-center text-text-500 text-sm my-6">
+                Card information are secured by <strong>Stripe</strong>
+              </p>
+              <Button
+                className="w-full"
+                style={ButtonStyle.BASIC}
+                size={ButtonSize.XLARGE}
+                type="submit"
+                disabled={!formState.isValid}
+              >
+                Confirm
+              </Button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
