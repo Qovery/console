7a68afdce400f7ffbc327810704ee9b451a63f91
Rémi Bonnet <rbonnet@qovery.com>
2023-03-17 14:00:25 +0100

    wip

--- libs/pages/logs/application/src/lib/page-application-logs.spec.tsx
+++ libs/pages/logs/application/src/lib/page-application-logs.spec.tsx
@@ -1,6 +1,11 @@
 import { render } from '__tests__/utils/setup-jest'
 import PageApplicationLogs from './page-application-logs'
 
+jest.mock('react-use-websocket', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}))
+
 describe('PageApplicationLogs', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<PageApplicationLogs />)
