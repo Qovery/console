dc1462f862f3a47b3068833abe2bc4cabcdc6df3
bdebon <benjamin.debon@gmail.com>
2023-03-28 17:04:11 +0200

    architecture

--- libs/shared/console-shared/src/lib/cluster-settings/feature/cluster-resources-settings-feature/utils/list-instance-type-formatter.spec.ts
+++ libs/shared/console-shared/src/lib/cluster-settings/feature/cluster-resources-settings-feature/utils/list-instance-type-formatter.spec.ts
@@ -8,31 +8,34 @@ describe('listInstanceTypeFormatter', () => {
         cpu: 1,
         ram_in_gb: 1,
         type: 't2.micro',
+        architecture: 'x86_64',
       },
       {
         name: 't2.small',
         cpu: 1,
         ram_in_gb: 2,
         type: 't2.small',
+        architecture: 'x86_64',
       },
       {
         name: 't2.medium',
         cpu: 2,
         ram_in_gb: 4,
         type: 't2.medium',
+        architecture: 'arm64',
       },
     ]
     const expected = [
       {
-        label: 't2.micro (1CPU - 1GB RAM)',
+        label: 't2.micro (1CPU - 1GB RAM - x86_64)',
         value: 't2.micro',
       },
       {
-        label: 't2.small (1CPU - 2GB RAM)',
+        label: 't2.small (1CPU - 2GB RAM - x86_64)',
         value: 't2.small',
       },
       {
-        label: 't2.medium (2CPU - 4GB RAM)',
+        label: 't2.medium (2CPU - 4GB RAM - arm64)',
         value: 't2.medium',
       },
     ]
