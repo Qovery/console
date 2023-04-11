c37e3db416563d344021a1a2bcfed3a46706afbd
Rémi Bonnet <rbonnet@qovery.com>
2023-04-11 09:53:53 +0200

    test: pod logs feature

--- libs/pages/logs/environment/src/lib/feature/pod-logs-feature/pod-logs-feature.spec.tsx
+++ libs/pages/logs/environment/src/lib/feature/pod-logs-feature/pod-logs-feature.spec.tsx
@@ -1,5 +1,11 @@
 import { render } from '__tests__/utils/setup-jest'
+import { Route, Routes } from 'react-router-dom'
 import PodLogsFeature, { PodLogsFeatureProps } from './pod-logs-feature'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router'),
+  useParams: () => ({ organizationId: '1', projectId: '2', environmentId: '3', serviceId: '4' }),
+}))
 
 describe('PodLogsFeature', () => {
   const props: PodLogsFeatureProps = {
@@ -8,7 +14,16 @@ describe('PodLogsFeature', () => {
   }
 
   it('should render successfully', () => {
-    const { baseElement } = render(<PodLogsFeature {...props} />)
+    console.warn = jest.fn()
+
+    const { baseElement } = render(
+      <Routes location="/organization/1/project/2/environment/3/logs/">
+        <Route
+          path="/organization/1/project/2/environment/3/logs/4/live-logs"
+          element={<PodLogsFeature {...props} />}
+        />
+      </Routes>
+    )
     expect(baseElement).toBeTruthy()
   })
 })
