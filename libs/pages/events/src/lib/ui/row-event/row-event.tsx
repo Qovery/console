a6c20f145dc459ec918131b22ae13f2b52fb48c6
bdebon <benjamin.debon@gmail.com>
2023-04-27 18:23:12 +0200

    wip

--- libs/pages/events/src/lib/ui/row-event/row-event.tsx
+++ libs/pages/events/src/lib/ui/row-event/row-event.tsx
@@ -1,4 +1,5 @@
 import { OrganizationEventResponse } from 'qovery-typescript-axios'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { Icon, IconAwesomeEnum } from '@qovery/shared/ui'
 import { dateYearMonthDayHourMinuteSecond } from '@qovery/shared/utils'
 
@@ -29,7 +30,17 @@ export function RowEvent(props: RowEventProps) {
       <div className="px-4">{event.change}</div>
       <div className="px-4">{event.triggered_by}</div>
       <div className="px-4">{event.origin}</div>
-      {expanded && <div className={`col-span-${nbCols} bg-red-500 -mb-5 h-6 mt-5`}>Test</div>}
+      {expanded && (
+        <div className={`col-span-${nbCols} bg-element-light-darker-100 -mb-5 mt-5 text-red-50`}>
+          <SyntaxHighlighter
+            language="json"
+            customStyle={{ padding: '1rem', borderRadius: '0.25rem', backgroundColor: 'transparent' }}
+            wrapLines
+          >
+            {JSON.stringify(JSON.parse(event.change || ''), null, 2)}
+          </SyntaxHighlighter>
+        </div>
+      )}
     </div>
   )
 }
