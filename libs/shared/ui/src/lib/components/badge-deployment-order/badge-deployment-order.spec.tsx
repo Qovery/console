7c56394c19ff0d058c6daae4c3978aee37aa2c63
Rémi Bonnet <bonnet.rem@gmail.com>
2023-05-12 13:31:59 +0200

    fix(badge-deployment-order): remove color by id (#607)

--- libs/shared/ui/src/lib/components/badge-deployment-order/badge-deployment-order.spec.tsx
+++ libs/shared/ui/src/lib/components/badge-deployment-order/badge-deployment-order.spec.tsx
@@ -1,34 +1,20 @@
 import { render } from '__tests__/utils/setup-jest'
-import BadgeDeploymentOrder, { getColorFromUID } from './badge-deployment-order'
+import BadgeDeploymentOrder from './badge-deployment-order'
 
 describe('BadgeDeploymentOrder', () => {
   it('should render successfully', () => {
-    const { baseElement, getByTestId } = render(<BadgeDeploymentOrder id="foo" />)
+    const { baseElement, getByTestId } = render(<BadgeDeploymentOrder order={3} />)
     const badge = getByTestId('badge')
     expect(badge).toBeInTheDocument()
     expect(baseElement).toBeTruthy()
   })
 
   it('should renders the badge with the default color when no order is provided', () => {
-    const { getByTestId } = render(<BadgeDeploymentOrder id="testuid" />)
+    const { getByTestId } = render(<BadgeDeploymentOrder order={3} />)
     const badge = getByTestId('badge')
     const badgeSvg = getByTestId('badge-svg')
     expect(badge).toBeInTheDocument()
     expect(badgeSvg).toBeInTheDocument()
-  })
-
-  it('should renders the badge with a color from the `defaultColors`', () => {
-    const { getByTestId } = render(<BadgeDeploymentOrder id="testuid" order={3} />)
-    const badge = getByTestId('badge')
-    expect(badge).toBeInTheDocument()
-    const badgeSvg = getByTestId('badge-svg')
-    const expectedColor = getColorFromUID('testuid', 3)
-    expect(badgeSvg).toHaveAttribute('fill', expectedColor)
-  })
-
-  it('should returns the same color for the same uid and colorArray', () => {
-    const result1 = getColorFromUID('testuid', 5)
-    const result2 = getColorFromUID('testuid', 5)
-    expect(result1).toBe(result2)
+    expect(badgeSvg).toHaveAttribute('fill', '#009EDD')
   })
 })
