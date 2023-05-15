5c8932d28a2de8ea7b483419e274abc802d6da2b
Rémi Bonnet <rbonnet@qovery.com>
2023-05-15 17:45:47 +0200

    fix(onboarding-project): add back button if organizations available

--- libs/pages/onboarding/src/lib/feature/onboarding-project/onboarding-project.tsx
+++ libs/pages/onboarding/src/lib/feature/onboarding-project/onboarding-project.tsx
@@ -1,9 +1,12 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { fetchOrganization, selectAllOrganization } from '@qovery/domains/organization'
 import { useAuth } from '@qovery/shared/auth'
 import { ONBOARDING_PRICING_URL, ONBOARDING_URL } from '@qovery/shared/routes'
 import { useDocumentTitle } from '@qovery/shared/utils'
+import { AppDispatch } from '@qovery/store'
 import { StepProject } from '../../ui/step-project/step-project'
 import { ContextOnboarding } from '../container/container'
 
@@ -13,8 +16,25 @@ export function OnboardingProject() {
   const navigate = useNavigate()
   const { authLogout } = useAuth()
   const { handleSubmit, control, setValue } = useForm()
+  const dispatch = useDispatch<AppDispatch>()
+  const organizations = useSelector(selectAllOrganization)
+  const [backButton, setBackButton] = useState<boolean | undefined>()
 
   const { organization_name, project_name, setContextValue } = useContext(ContextOnboarding)
+
+  useEffect(() => {
+    async function fetchOrganizations() {
+      if (organizations.length === 0) {
+        await dispatch(fetchOrganization())
+          .unwrap()
+          .then((result) => setBackButton(result.length > 0))
+          .catch((e) => console.log(e))
+      } else {
+        setBackButton(true)
+      }
+    }
+    fetchOrganizations()
+  }, [organizations.length, dispatch])
 
   useEffect(() => {
     setValue('organization_name', organization_name)
@@ -32,7 +52,7 @@ export function OnboardingProject() {
     }
   })
 
-  return <StepProject onSubmit={onSubmit} control={control} authLogout={authLogout} />
+  return <StepProject onSubmit={onSubmit} control={control} authLogout={authLogout} backButton={backButton} />
 }
 
 export default OnboardingProject
