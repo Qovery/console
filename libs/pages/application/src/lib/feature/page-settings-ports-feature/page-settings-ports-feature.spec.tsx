f6d431288c57895be64b8235293df6bc66668a65
Rémi Bonnet <bonnet.rem@gmail.com>
2023-06-28 16:30:10 +0200

    fix(port): remove port and associated healthchecks (#666)

--- libs/pages/application/src/lib/feature/page-settings-ports-feature/page-settings-ports-feature.spec.tsx
+++ libs/pages/application/src/lib/feature/page-settings-ports-feature/page-settings-ports-feature.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '__tests__/utils/setup-jest'
+import { PortProtocolEnum } from 'qovery-typescript-axios'
 import { applicationFactoryMock } from '@qovery/shared/factories'
-import PageSettingsPortsFeature, { deletePort } from './page-settings-ports-feature'
+import PageSettingsPortsFeature, { deletePort, removePortFromProbes } from './page-settings-ports-feature'
 
 const application = applicationFactoryMock(1)[0]
 describe('PageSettingsPortsFeature', () => {
@@ -16,10 +17,41 @@ describe('PageSettingsPortsFeature', () => {
         external_port: 30,
         internal_port: 30,
         publicly_accessible: true,
+        protocol: PortProtocolEnum.HTTP,
       },
     ]
 
     const cloneApplication = deletePort(application, '1')
     expect(cloneApplication.ports).toStrictEqual([])
+  })
+
+  it('should remove probes if port is on the healthchecks', () => {
+    application.healthchecks = {
+      liveness_probe: {
+        type: {
+          tcp: { port: 8080 },
+        },
+      },
+      readiness_probe: {
+        type: {
+          grpc: { port: 8082 },
+        },
+      },
+    }
+
+    const port = 8080
+
+    const expectedApplication = {
+      liveness_probe: undefined,
+      readiness_probe: {
+        type: {
+          grpc: { port: 8082 },
+        },
+      },
+    }
+
+    const result = removePortFromProbes(application, port)
+
+    expect(result.healthchecks).toEqual(expectedApplication)
   })
 })
--- libs/pages/application/src/lib/feature/page-settings-ports-feature/page-settings-ports-feature.tsx
+++ libs/pages/application/src/lib/feature/page-settings-ports-feature/page-settings-ports-feature.tsx
@@ -1,4 +1,4 @@
-import { ServicePort } from 'qovery-typescript-axios'
+import { Probe, ProbeType, ServicePort } from 'qovery-typescript-axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import {
@@ -7,15 +7,58 @@ import {
   postApplicationActionsRedeploy,
   selectApplicationById,
 } from '@qovery/domains/application'
-import { getServiceType } from '@qovery/shared/enums'
+import { ProbeTypeEnum, getServiceType } from '@qovery/shared/enums'
 import { ApplicationEntity, LoadingStatus } from '@qovery/shared/interfaces'
 import { useModal, useModalConfirmation } from '@qovery/shared/ui'
 import { AppDispatch, RootState } from '@qovery/store'
 import PageSettingsPorts from '../../ui/page-settings-ports/page-settings-ports'
 import CrudModalFeature from './crud-modal-feature/crud-modal-feature'
 
+export function removePortFromProbes(application?: ApplicationEntity, port?: number): ApplicationEntity {
+  const cloneApplication = { ...application } as ApplicationEntity
+
+  const removePortFromProbe = (probe?: Probe): Probe | undefined => {
+    let result = probe
+
+    if (probe && probe.type) {
+      const type: ProbeType = probe.type
+
+      type &&
+        Object.keys(type).forEach((key) => {
+          const subType = type[key as keyof ProbeType]
+
+          if (typeof subType === 'object') {
+            if (
+              key === ProbeTypeEnum.TCP.toLowerCase() ||
+              key === ProbeTypeEnum.HTTP.toLowerCase() ||
+              key === ProbeTypeEnum.GRPC.toLowerCase()
+            ) {
+              const portValue = (subType as { port?: number })?.port
+
+              if (portValue === port) {
+                result = undefined
+              }
+            }
+          }
+        })
+    }
+
+    return result
+  }
+
+  cloneApplication.healthchecks = {
+    liveness_probe: removePortFromProbe(cloneApplication.healthchecks?.liveness_probe),
+    readiness_probe: removePortFromProbe(cloneApplication.healthchecks?.readiness_probe),
+  }
+
+  return cloneApplication
+}
+
 export const deletePort = (application?: ApplicationEntity, portId?: string) => {
-  const cloneApplication = Object.assign({}, application)
+  const cloneApplication = {
+    ...removePortFromProbes(application, application?.ports?.find((port) => port.id === portId)?.internal_port),
+  }
+
   cloneApplication.ports = cloneApplication.ports?.filter((port) => port.id !== portId)
   return cloneApplication
 }
