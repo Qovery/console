1e9b6094646426b1bca7e42b60e4b4d5ef9fe20d
Rémi Bonnet <bonnet.rem@gmail.com>
2023-07-20 10:56:38 +0200

    chore(package.json): update yarn test command (#686)

--- apps/console/src/app/app.spec.tsx
+++ apps/console/src/app/app.spec.tsx
@@ -1,9 +1,11 @@
-import App from './app'
 import { render } from '__tests__/utils/setup-jest'
 import { IntercomProvider } from 'react-use-intercom'
+import App from './app'
 
 describe('App', () => {
   it('should render successfully', () => {
+    window.scrollTo = jest.fn()
+
     const { baseElement } = render(
       <IntercomProvider appId="__test__app__id__" autoBoot={false}>
         <App />
