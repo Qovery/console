496a4ca161cf60e48ea18a9b96bdc7483956d9ba
Rémi Bonnet <rbonnet@qovery.com>
2023-08-02 11:41:42 +0200

    test: decreased coverage

--- libs/shared/console-shared/src/lib/flow-create-port/ui/crud-modal/crud-modal.spec.tsx
+++ libs/shared/console-shared/src/lib/flow-create-port/ui/crud-modal/crud-modal.spec.tsx
@@ -18,77 +18,77 @@ describe('CrudModal', () => {
     expect(baseElement).toBeTruthy()
   })
 
-  // it('should render the form for AWS provider with correct options', () => {
-  //   render(wrapWithReactHookForm(<CrudModal {...props} />))
+  it('should render the form for AWS provider with correct options', () => {
+    render(wrapWithReactHookForm(<CrudModal {...props} />))
 
-  //   const protocolSelect = screen.getByRole('combobox', {
-  //     name: /protocol/i,
-  //   })
-  //   selectEvent.openMenu(protocolSelect)
+    const protocolSelect = screen.getByRole('combobox', {
+      name: /protocol/i,
+    })
+    selectEvent.openMenu(protocolSelect)
 
-  //   waitFor(() => {
-  //     screen.getByLabelText('HTTP')
-  //     screen.getByLabelText('UDP')
-  //     screen.getByLabelText('TCP')
-  //     screen.getByLabelText('GRPC')
-  //   })
-  // })
+    waitFor(() => {
+      screen.getByLabelText('HTTP')
+      screen.getByLabelText('UDP')
+      screen.getByLabelText('TCP')
+      screen.getByLabelText('GRPC')
+    })
+  })
 
-  // it('should render the form for SCW provider with correct options', () => {
-  //   props.cloudProvider = CloudProviderEnum.SCW
-  //   render(wrapWithReactHookForm(<CrudModal {...props} />))
+  it('should render the form for SCW provider with correct options', () => {
+    props.cloudProvider = CloudProviderEnum.SCW
+    render(wrapWithReactHookForm(<CrudModal {...props} />))
 
-  //   const protocolSelect = screen.getByRole('combobox', {
-  //     name: /protocol/i,
-  //   })
-  //   selectEvent.openMenu(protocolSelect)
+    const protocolSelect = screen.getByRole('combobox', {
+      name: /protocol/i,
+    })
+    selectEvent.openMenu(protocolSelect)
 
-  //   waitFor(() => {
-  //     expect(screen.queryByLabelText('UDP')).toBe(null)
-  //     screen.getByLabelText('HTTP')
-  //     screen.getByLabelText('TCP')
-  //     screen.getByLabelText('GRPC')
-  //   })
-  // })
+    waitFor(() => {
+      expect(screen.queryByLabelText('UDP')).toBe(null)
+      screen.getByLabelText('HTTP')
+      screen.getByLabelText('TCP')
+      screen.getByLabelText('GRPC')
+    })
+  })
 
-  // it('should render the form', async () => {
-  //   const { getByDisplayValue } = render(
-  //     wrapWithReactHookForm(<CrudModal {...props} />, {
-  //       defaultValues: {
-  //         internal_port: 99,
-  //         external_port: 420,
-  //         publicly_accessible: true,
-  //         protocol: PortProtocolEnum.HTTP,
-  //       },
-  //     })
-  //   )
+  it('should render the form', async () => {
+    const { getByDisplayValue } = render(
+      wrapWithReactHookForm(<CrudModal {...props} />, {
+        defaultValues: {
+          internal_port: 99,
+          external_port: 420,
+          publicly_accessible: true,
+          protocol: PortProtocolEnum.HTTP,
+        },
+      })
+    )
 
-  //   await waitFor(() => {
-  //     getByDisplayValue(99)
-  //     getByDisplayValue('true')
-  //   })
-  // })
+    await waitFor(() => {
+      getByDisplayValue(99)
+      getByDisplayValue('true')
+    })
+  })
 
-  // it('should submit the form', async () => {
-  //   const spy = jest.fn().mockImplementation((e) => e.preventDefault())
-  //   props.onSubmit = spy
-  //   const { findByTestId } = render(
-  //     wrapWithReactHookForm(<CrudModal {...props} />, {
-  //       defaultValues: {
-  //         internal_port: 99,
-  //         external_port: 420,
-  //         publicly_accessible: true,
-  //         protocol: PortProtocolEnum.HTTP,
-  //       },
-  //     })
-  //   )
+  it('should submit the form', async () => {
+    const spy = jest.fn().mockImplementation((e) => e.preventDefault())
+    props.onSubmit = spy
+    const { findByTestId } = render(
+      wrapWithReactHookForm(<CrudModal {...props} />, {
+        defaultValues: {
+          internal_port: 99,
+          external_port: 420,
+          publicly_accessible: true,
+          protocol: PortProtocolEnum.HTTP,
+        },
+      })
+    )
 
-  //   const button = await findByTestId('submit-button')
+    const button = await findByTestId('submit-button')
 
-  //   await waitFor(() => {
-  //     button.click()
-  //     expect(button).not.toBeDisabled()
-  //     expect(spy).toHaveBeenCalled()
-  //   })
-  // })
+    await waitFor(() => {
+      button.click()
+      expect(button).not.toBeDisabled()
+      expect(spy).toHaveBeenCalled()
+    })
+  })
 })
