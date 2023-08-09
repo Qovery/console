a8c0027a1f1d7a8a117b3bee2f203d03d7dc54e8
Rémi Bonnet <rbonnet@qovery.com>
2023-08-09 11:22:11 +0200

    cleanup: remove useless useEffect

--- libs/pages/logs/environment/src/lib/feature/sidebar-history-feature/sidebar-history-feature.tsx
+++ libs/pages/logs/environment/src/lib/feature/sidebar-history-feature/sidebar-history-feature.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { selectApplicationsEntitiesByEnvId } from '@qovery/domains/application'
 import { useEnvironmentDeploymentHistory } from '@qovery/domains/environment'
-import { DEPLOYMENT_LOGS_VERSION_URL, ENVIRONMENT_LOGS_URL, SERVICE_LOGS_URL } from '@qovery/shared/routes'
+import { ENVIRONMENT_LOGS_URL, SERVICE_LOGS_URL } from '@qovery/shared/routes'
 import { RootState } from '@qovery/store'
 import SidebarHistory from '../../ui/sidebar-history/sidebar-history'
 
@@ -15,28 +14,15 @@ export interface SidebarHistoryFeatureProps {
 export function SidebarHistoryFeature({ versionId, serviceId }: SidebarHistoryFeatureProps) {
   const { organizationId = '', projectId = '', environmentId = '' } = useParams()
   const { data } = useEnvironmentDeploymentHistory(projectId, environmentId)
-  const navigate = useNavigate()
   const applications = useSelector((state: RootState) => selectApplicationsEntitiesByEnvId(state, environmentId))
 
   const { pathname } = useLocation()
   const pathLogs = ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId)
   const serviceLogsPath = pathname.includes(SERVICE_LOGS_URL(serviceId))
 
-  useEffect(() => {
-    if (!versionId && !serviceLogsPath) {
-      const firstVersionId = data?.[0]?.id || ''
-      if (serviceId) {
-        navigate(
-          ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId) +
-            DEPLOYMENT_LOGS_VERSION_URL(serviceId, firstVersionId)
-        )
-      }
-    }
-  }, [organizationId, projectId, environmentId, serviceId, versionId, navigate, data, serviceLogsPath])
-
   const defaultServiceId = serviceId || applications[0]?.id
 
-  if (!data || !defaultServiceId) return
+  if (!data || !defaultServiceId || serviceLogsPath) return
 
   return <SidebarHistory data={data} versionId={versionId} serviceId={defaultServiceId} pathLogs={pathLogs} />
 }
