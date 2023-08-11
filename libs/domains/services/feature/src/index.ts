a6894568787655e2e85abfcd9fc7745ed1abfe81
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-11 16:54:54 +0200

    feat(services): add service-state-chip component

--- libs/domains/services/feature/src/index.ts
+++ libs/domains/services/feature/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/hooks/use-service-statuses/use-service-statuses'
 export * from './lib/hooks/use-services/use-services'
+export * from './lib/service-state-chip/service-state-chip'
 export * from './lib/slices/services.queries'
