7ecac56fde21546c4b3ee88862f5398fec78d9c0
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-14 17:55:48 +0200

    feat(environments): add environment-deployment-status-label component

--- libs/domains/environments/feature/src/index.ts
+++ libs/domains/environments/feature/src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/environment-deployment-status-label/environment-deployment-status-label'
 export * from './lib/environment-state-chip/environment-state-chip'
 export * from './lib/hooks/use-environment/use-environment'
---
+++ libs/domains/environments/feature/src/lib/environment-deployment-status-label/environment-deployment-status-label.tsx
@@ -0,0 +1,26 @@
+import { StatusLabel, type StatusLabelProps, Tooltip } from '@qovery/shared/ui'
+import { dateFullFormat, timeAgo } from '@qovery/shared/utils'
+import { useDeploymentStatus } from '../hooks/use-deployment-status/use-deployment-status'
+
+export interface EnvironmentDeploymentStatusLabelProps extends Omit<StatusLabelProps, 'status'> {
+  environmentId?: string
+}
+
+export function EnvironmentDeploymentStatusLabel({ environmentId, ...props }: EnvironmentDeploymentStatusLabelProps) {
+  const { data: deploymentStatus } = useDeploymentStatus({ environmentId })
+  return (
+    <div className="flex items-center gap-3 leading-7 text-text-400 text-sm">
+      <StatusLabel status={deploymentStatus?.state} {...props} />
+
+      {deploymentStatus?.last_deployment_date && (
+        <Tooltip content={dateFullFormat(deploymentStatus.last_deployment_date)}>
+          <span className="text-xs text-text-300 font-medium">
+            {timeAgo(new Date(deploymentStatus.last_deployment_date))} ago
+          </span>
+        </Tooltip>
+      )}
+    </div>
+  )
+}
+
+export default EnvironmentDeploymentStatusLabel
