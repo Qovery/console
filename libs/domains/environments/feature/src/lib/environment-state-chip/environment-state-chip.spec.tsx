b27d9e7ca76e9fb9334a9f0401c1ecc83b244c5b
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-17 10:48:29 +0200

    fixup! fixup! feat(environment): add use-environment hook

--- libs/domains/environments/feature/src/lib/environment-state-chip/environment-state-chip.spec.tsx
+++ libs/domains/environments/feature/src/lib/environment-state-chip/environment-state-chip.spec.tsx
@@ -1,5 +1,3 @@
-import { StateEnum } from 'qovery-typescript-axios'
-import { RunningState } from '@qovery/shared/enums'
 import { renderWithProviders } from '@qovery/shared/util-tests'
 import EnvironmentStateChip from './environment-state-chip'
 
@@ -8,7 +6,7 @@ jest.mock('../hooks/use-deployment-status/use-deployment-status', () => {
     ...jest.requireActual('../hooks/use-deployment-status/use-deployment-status'),
     useDeploymentStatus: () => ({
       data: {
-        state: StateEnum.BUILDING,
+        state: 'BUILDING',
         last_deployment_date: '2023-08-11T13:33:32.083371Z',
       },
     }),
@@ -20,7 +18,7 @@ jest.mock('../hooks/use-running-status/use-running-status', () => {
     ...jest.requireActual('../hooks/use-running-status/use-running-status'),
     useRunningStatus: () => ({
       data: {
-        state: RunningState.STARTING,
+        state: 'STARTING',
         last_running_date: '2023-08-11T13:33:32.083371Z',
       },
     }),
