6e9de4d23bba34fa0aa917ec99b6159614a447b7
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-17 14:09:20 +0200

    refactor(domains-services): refactor useDeploymentStatus

--- libs/domains/services/feature/src/lib/hooks/use-deployment-status/use-deployment-status.ts
+++ libs/domains/services/feature/src/lib/hooks/use-deployment-status/use-deployment-status.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { queries } from '@qovery/state/util-queries'
+import useListStatuses from '../use-list-statuses/use-list-statuses'
 
 export interface UseDeploymentStatusProps {
   environmentId?: string
@@ -7,7 +8,7 @@ export interface UseDeploymentStatusProps {
 }
 
 export function useDeploymentStatus({ environmentId, serviceId }: UseDeploymentStatusProps) {
-  return useQuery({
+  const webSocketResult = useQuery({
     // eslint-disable-next-line @typescript-eslint/no-extra-non-null-assertion
     ...queries.services.deploymentStatus(environmentId!!, serviceId!!),
     enabled: Boolean(environmentId) && Boolean(serviceId),
@@ -16,6 +17,23 @@ export function useDeploymentStatus({ environmentId, serviceId }: UseDeploymentS
     refetchOnReconnect: false,
     staleTime: Infinity,
   })
+
+  // NOTE: Deployment status by WS isn't enough because WS can something return no message.
+  // We need to fallback on service status, unfortunately we have to play with endpoints to get
+  // a service status without knowing its type (eg. application, container ...)
+  const { data: environmentStatus, ...rest } = useListStatuses({ environmentId })
+  const serviceStatuses = [
+    ...(environmentStatus?.applications ?? []),
+    ...(environmentStatus?.containers ?? []),
+    ...(environmentStatus?.databases ?? []),
+    ...(environmentStatus?.jobs ?? []),
+  ]
+  return webSocketResult.data
+    ? webSocketResult
+    : {
+        data: serviceStatuses.find(({ id }) => id === serviceId),
+        ...rest,
+      }
 }
 
 export default useDeploymentStatus
--- libs/domains/services/feature/src/lib/service-deployment-status-label/service-deployment-status-label.tsx
+++ libs/domains/services/feature/src/lib/service-deployment-status-label/service-deployment-status-label.tsx
@@ -1,7 +1,6 @@
 import { StatusLabel, type StatusLabelProps, Tooltip } from '@qovery/shared/ui'
 import { dateFullFormat, timeAgo } from '@qovery/shared/utils'
 import { useDeploymentStatus } from '../hooks/use-deployment-status/use-deployment-status'
-import { useListStatuses } from '../hooks/use-list-statuses/use-list-statuses'
 
 export interface ServiceDeploymentStatusLabelProps extends Omit<StatusLabelProps, 'status'> {
   environmentId?: string
@@ -13,18 +12,7 @@ export function ServiceDeploymentStatusLabel({
   serviceId,
   ...props
 }: ServiceDeploymentStatusLabelProps) {
-  let { data: deploymentStatus } = useDeploymentStatus({ environmentId, serviceId })
-  // NOTE: Deployment status by WS isn't enough because WS can something return no message.
-  // We need to fallback on service status, unfortunately play with endpoints to get
-  // a service status without knowing its type (eg. application, container ...)
-  const { data: environmentStatus } = useListStatuses({ environmentId })
-  const serviceStatuses = [
-    ...(environmentStatus?.applications ?? []),
-    ...(environmentStatus?.containers ?? []),
-    ...(environmentStatus?.databases ?? []),
-    ...(environmentStatus?.jobs ?? []),
-  ]
-  deploymentStatus ??= serviceStatuses.find(({ id }) => id === serviceId)
+  const { data: deploymentStatus } = useDeploymentStatus({ environmentId, serviceId })
   return (
     <div className="flex items-center gap-3 leading-7 text-neutral-350 text-sm">
       <StatusLabel status={deploymentStatus?.state} {...props} />
