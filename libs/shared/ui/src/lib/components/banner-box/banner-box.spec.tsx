4280daf51e69e620b9a2354a1971524f609f4a35
Rémi Bonnet <rbonnet@qovery.com>
2023-08-17 17:09:42 +0200

    fix: banner box test

--- libs/shared/ui/src/lib/components/banner-box/banner-box.spec.tsx
+++ libs/shared/ui/src/lib/components/banner-box/banner-box.spec.tsx
@@ -20,7 +20,7 @@ describe('BannerBox', () => {
 
     expect(box.classList.contains('border-yellow-500')).toBe(true)
     expect(box.classList.contains('bg-yellow-50')).toBe(true)
-    expect(icon?.classList.contains('text-warning-600')).toBe(true)
+    expect(icon?.classList.contains('text-yellow-600')).toBe(true)
   })
 
   it('should render with error colors', () => {
