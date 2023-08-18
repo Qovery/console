c190e4cb153456afac98cb2a4665ca8745a4e667
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-18 11:42:50 +0200

    refactor: deprecate entity `status` & `running_status` properties

--- libs/shared/interfaces/src/lib/domain/container-application.entity.ts
+++ libs/shared/interfaces/src/lib/domain/container-application.entity.ts
@@ -3,11 +3,17 @@ import { LoadingStatus } from '../types/loading-status.type'
 import { ServiceRunningStatus } from './service-running-status.interface'
 
 export interface ContainerApplicationEntity extends ContainerResponse {
+  /**
+   * @deprecated This should be not be used, use dedicated hooks instead `useDeploymentStatus`
+   */
   status?: Status
   links?: {
     loadingStatus: LoadingStatus
     items?: Link[]
   }
+  /**
+   * @deprecated This should be not be used, use dedicated hooks instead `useRunningStatus`
+   */
   running_status?: ServiceRunningStatus
   instances?: {
     loadingStatus: LoadingStatus
