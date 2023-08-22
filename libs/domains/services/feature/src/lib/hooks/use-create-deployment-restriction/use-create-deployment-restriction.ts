0ca0938caf44c5f83ef7b1315ec698d4a7a2001d
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-22 16:30:46 +0200

    feat(services): add deployment restrictions hooks

---
+++ libs/domains/services/feature/src/lib/hooks/use-create-deployment-restriction/use-create-deployment-restriction.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { type ApplicationType, mutations } from '@qovery/domains/services/data-access'
+import { toastError } from '@qovery/shared/ui'
+import { queries } from '@qovery/state/util-queries'
+
+export interface UseCreateDeploymentRestrictionProps {
+  serviceId: string
+  serviceType: ApplicationType
+}
+
+export function useCreateDeploymentRestriction({ serviceId, serviceType }: UseCreateDeploymentRestrictionProps) {
+  const queryClient = useQueryClient()
+  return useMutation(mutations.createDeploymentRestriction, {
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: queries.services.deploymentRestrictions({ serviceId, serviceType }).queryKey,
+      })
+    },
+    onError: (error: Error) => toastError(error),
+  })
+}
+
+export default useCreateDeploymentRestriction
---
+++ libs/domains/services/feature/src/lib/hooks/use-deployment-restrictions/use-deployment-restrictions.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+import { type ApplicationType } from '@qovery/domains/services/data-access'
+import { queries } from '@qovery/state/util-queries'
+
+export interface UseDeploymentRestrictionsProps {
+  serviceId: string
+  serviceType: ApplicationType
+}
+
+export function useDeploymentRestrictions({ serviceId, serviceType }: UseDeploymentRestrictionsProps) {
+  return useQuery({
+    // eslint-disable-next-line @typescript-eslint/no-extra-non-null-assertion
+    ...queries.services.deploymentRestrictions({ serviceId: serviceId!!, serviceType }),
+    enabled: Boolean(serviceId),
+  })
+}
+
+export default useDeploymentRestrictions
