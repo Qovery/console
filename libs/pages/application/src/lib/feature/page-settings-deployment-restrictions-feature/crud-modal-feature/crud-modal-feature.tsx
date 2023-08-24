1f8c41f604e6fbf7e0c7d8d9e20d2234cca4e275
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-24 15:57:33 +0200

    fixup! feat(service): add application deployment restrictions settings

--- libs/pages/application/src/lib/feature/page-settings-deployment-restrictions-feature/crud-modal-feature/crud-modal-feature.tsx
+++ libs/pages/application/src/lib/feature/page-settings-deployment-restrictions-feature/crud-modal-feature/crud-modal-feature.tsx
@@ -1,5 +1,5 @@
 import {
-  ApplicationDeploymentRestriction,
+  type ApplicationDeploymentRestriction,
   type ApplicationDeploymentRestrictionRequest,
   DeploymentRestrictionModeEnum,
   DeploymentRestrictionTypeEnum,
