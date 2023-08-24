b5d1756b753e1b59e8a15d6c3dcffa51dc54f3e6
Rémi Bonnet <rbonnet@qovery.com>
2023-08-24 17:03:00 +0200

    fix: users domain react library

--- libs/domains/users/data-access/jest.config.ts
+++ libs/domains/users/data-access/jest.config.ts
@@ -2,10 +2,10 @@
 export default {
   displayName: 'domains-user-data-access',
   preset: '../../../../jest.preset.js',
-  testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/domains/users/data-access',
 }
