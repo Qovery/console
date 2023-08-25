0e47ef2222808bb9efc2564fc42c73d6a2954e1c
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-25 15:24:14 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat(service): add application deployment restrictions settings

--- libs/pages/application/src/lib/feature/page-settings-deployment-restrictions-feature/page-settings-deployment-restrictions-feature.tsx
+++ libs/pages/application/src/lib/feature/page-settings-deployment-restrictions-feature/page-settings-deployment-restrictions-feature.tsx
@@ -12,6 +12,7 @@ import {
   HelpSection,
   IconAwesomeEnum,
   InputText,
+  LoaderSpinner,
   useModal,
   useModalConfirmation,
 } from '@qovery/shared/ui'
@@ -23,11 +24,13 @@ export function PageSettingsDeploymentRestrictionsFeature() {
     serviceId: applicationId,
     serviceType: 'APPLICATION' as const,
   }
-  const { data: deploymentRestrictions = [] } = useDeploymentRestrictions(serviceParams)
+  const { data: deploymentRestrictions = [], isLoading: isLoadingDeploymentRestrictions } =
+    useDeploymentRestrictions(serviceParams)
   const { mutate: deleteRestriction } = useDeleteDeploymentRestriction(serviceParams)
   const { openModal, closeModal } = useModal()
   const { openModalConfirmation } = useModalConfirmation()
-  const { data: environmentDeploymentRules } = useFetchEnvironmentDeploymentRule(projectId, environmentId)
+  const { data: environmentDeploymentRules, isLoading: isLoadingEnvironmentDeploymentRule } =
+    useFetchEnvironmentDeploymentRule(projectId, environmentId)
   const isAutoDeployActive = environmentDeploymentRules?.auto_deploy
 
   const handleCreate = () => {
@@ -77,66 +80,72 @@ export function PageSettingsDeploymentRestrictionsFeature() {
           </Button>
         </div>
 
-        {!isAutoDeployActive && (
-          <BannerBox
-            className="mb-5"
-            title="Auto deploy is not active"
-            message="These rules are applied only if the auto-deploy feature is activated. Activate it first on the “General” settings of your environment"
-          />
-        )}
-
-        {deploymentRestrictions?.length > 0 ? (
-          <BlockContent title="Deployment restrictions">
-            <div className="flex flex-col gap-3">
-              {deploymentRestrictions.map((deploymentRestriction) => {
-                const { id, type, mode, value } = deploymentRestriction
-                return (
-                  <div key={id} className="flex justify-between w-full items-center gap-3">
-                    <InputText
-                      name={`mode_${id}`}
-                      className="shrink-0 grow flex-1"
-                      label="Mode"
-                      value={mode}
-                      disabled
-                    />
-                    <InputText
-                      name={`type_${id}`}
-                      className="shrink-0 grow flex-1"
-                      label="Type"
-                      value={type}
-                      disabled
-                    />
-                    <InputText
-                      name={`value_${id}`}
-                      className="shrink-0 grow flex-1"
-                      label="Value"
-                      value={value}
-                      disabled
-                    />
-                    <ButtonIcon
-                      className="!bg-transparent hover:!bg-neutral-200 !w-[52px] !h-[52px]"
-                      style={ButtonIconStyle.STROKED}
-                      onClick={() => handleEdit(deploymentRestriction)}
-                      dataTestId="edit"
-                      icon={IconAwesomeEnum.WHEEL}
-                    />
-                    <ButtonIcon
-                      className="!bg-transparent hover:!bg-neutral-200 !w-[52px] !h-[52px]"
-                      onClick={() => handleDelete(deploymentRestriction)}
-                      dataTestId="remove"
-                      icon={IconAwesomeEnum.TRASH}
-                      style={ButtonIconStyle.STROKED}
-                    />
-                  </div>
-                )
-              })}
-            </div>
-          </BlockContent>
+        {isLoadingEnvironmentDeploymentRule || isLoadingDeploymentRestrictions ? (
+          <LoaderSpinner className="w-6" />
         ) : (
-          <EmptyState
-            title="No deployment restrictions are set"
-            description="Adding deployment restrictions allows you to control the auto-deploy feature and determine when a commit on your repository should be auto-deployed or not."
-          />
+          <>
+            {!isAutoDeployActive && (
+              <BannerBox
+                className="mb-5"
+                title="Auto deploy is not active"
+                message="These rules are applied only if the auto-deploy feature is activated. Activate it first on the “General” settings of your environment"
+              />
+            )}
+
+            {deploymentRestrictions?.length > 0 ? (
+              <BlockContent title="Deployment restrictions">
+                <div className="flex flex-col gap-3">
+                  {deploymentRestrictions.map((deploymentRestriction) => {
+                    const { id, type, mode, value } = deploymentRestriction
+                    return (
+                      <div key={id} className="flex justify-between w-full items-center gap-3">
+                        <InputText
+                          name={`mode_${id}`}
+                          className="shrink-0 grow flex-1"
+                          label="Mode"
+                          value={mode}
+                          disabled
+                        />
+                        <InputText
+                          name={`type_${id}`}
+                          className="shrink-0 grow flex-1"
+                          label="Type"
+                          value={type}
+                          disabled
+                        />
+                        <InputText
+                          name={`value_${id}`}
+                          className="shrink-0 grow flex-1"
+                          label="Value"
+                          value={value}
+                          disabled
+                        />
+                        <ButtonIcon
+                          className="!bg-transparent hover:!bg-neutral-200 !w-[52px] !h-[52px]"
+                          style={ButtonIconStyle.STROKED}
+                          onClick={() => handleEdit(deploymentRestriction)}
+                          dataTestId="edit"
+                          icon={IconAwesomeEnum.WHEEL}
+                        />
+                        <ButtonIcon
+                          className="!bg-transparent hover:!bg-neutral-200 !w-[52px] !h-[52px]"
+                          onClick={() => handleDelete(deploymentRestriction)}
+                          dataTestId="remove"
+                          icon={IconAwesomeEnum.TRASH}
+                          style={ButtonIconStyle.STROKED}
+                        />
+                      </div>
+                    )
+                  })}
+                </div>
+              </BlockContent>
+            ) : (
+              <EmptyState
+                title="No deployment restrictions are set"
+                description="Adding deployment restrictions allows you to control the auto-deploy feature and determine when a commit on your repository should be auto-deployed or not."
+              />
+            )}
+          </>
         )}
       </div>
       <HelpSection
