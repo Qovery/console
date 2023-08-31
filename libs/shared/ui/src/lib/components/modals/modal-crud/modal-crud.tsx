99a6c14f916d98ca848fdb13f5ba4796b70febde
Camille TJHOA <camille.tjhoa@outlook.com>
2023-08-31 14:28:56 +0200

    feat(ui): add howItWorks modal prop

--- libs/shared/ui/src/lib/components/modals/modal-crud/modal-crud.tsx
+++ libs/shared/ui/src/lib/components/modals/modal-crud/modal-crud.tsx
@@ -2,6 +2,9 @@ import { type FormEventHandler, type ReactNode, useEffect } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { type ApplicationEntity } from '@qovery/shared/interfaces'
 import { Button, ButtonSize, ButtonStyle } from '../../buttons/button/button'
+import Icon from '../../icon/icon'
+import { IconAwesomeEnum } from '../../icon/icon-awesome.enum'
+import { Popover } from '../../popover/popover'
 import Truncate from '../../truncate/truncate'
 
 export interface ModalCrudProps {
@@ -16,6 +19,7 @@ export interface ModalCrudProps {
   forService?: ApplicationEntity
   onDelete?: () => void
   deleteButtonLabel?: string
+  howItWorks?: ReactNode
 }
 
 export function ModalCrud(props: ModalCrudProps) {
@@ -31,6 +35,7 @@ export function ModalCrud(props: ModalCrudProps) {
     onDelete,
     submitLabel,
     deleteButtonLabel,
+    howItWorks = null,
   } = props
   const { formState, trigger } = useFormContext()
 
@@ -40,10 +45,10 @@ export function ModalCrud(props: ModalCrudProps) {
 
   return (
     <div className="p-6">
-      <h2 className={`h4 text-neutral-400 max-w-sm truncate ${description ? 'mb-1' : 'mb-6'}`}>{title}</h2>
-      {description && <p className="mb-6 text-neutral-350 text-sm">{description}</p>}
+      <h2 className="h4 text-neutral-400 max-w-sm truncate">{title}</h2>
+      {description && <p className="mt-1 text-neutral-350 text-sm">{description}</p>}
       {forService && (
-        <div className={`text-neutral-400 text-sm mb-6 flex justify-between items-center -mt-2`}>
+        <div className="text-neutral-400 text-sm flex justify-between items-center mt-4">
           <p>
             For{' '}
             <strong className="text-neutral-400 font-medium">
@@ -52,7 +57,23 @@ export function ModalCrud(props: ModalCrudProps) {
           </p>
         </div>
       )}
-      <form onSubmit={onSubmit}>
+      {howItWorks && (
+        // XXX: modal prop should not be necessary but it's workaround to:
+        // https://github.com/radix-ui/primitives/issues/2348
+        // https://github.com/radix-ui/primitives/issues/2180
+        <Popover.Root modal={true}>
+          <Popover.Trigger>
+            <span className="text-sm text-brand-500 font-medium">
+              Show how it works <Icon name={IconAwesomeEnum.CIRCLE_QUESTION} />
+            </span>
+          </Popover.Trigger>
+          <Popover.Content side="left" className="z-50 text-neutral-350 text-sm" style={{ width: 440 }}>
+            <h6 className="text-neutral-400 font-medium mb-2">How it works</h6>
+            {howItWorks}
+          </Popover.Content>
+        </Popover.Root>
+      )}
+      <form className="mt-6" onSubmit={onSubmit}>
         {children}
         <div className="flex gap-3 justify-end mt-6">
           {isEdit && onDelete ? (
