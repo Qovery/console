802ace2f0dc3d64e89da60f767dd973b8a5ecce8
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-06 17:01:59 +0200

    fix(metrics): preserve order

--- libs/domains/services/feature/src/lib/hooks/use-metrics/use-metrics.ts
+++ libs/domains/services/feature/src/lib/hooks/use-metrics/use-metrics.ts
@@ -10,6 +10,12 @@ export function useMetrics({ environmentId, serviceId }: UseMetricsProps) {
   return useQuery({
     // eslint-disable-next-line @typescript-eslint/no-extra-non-null-assertion
     ...queries.services.metrics(environmentId!!, serviceId!!),
+    select(data) {
+      if (!data) {
+        return data
+      }
+      return data.sort((a, b) => a.pod_name.localeCompare(b.pod_name))
+    },
     enabled: Boolean(environmentId) && Boolean(serviceId),
     refetchOnMount: false,
     refetchOnWindowFocus: false,
