88d41a9b42ca587c0578c33c722dfc11c4aa4cf8
acarranoqovery <105300721+acarranoqovery@users.noreply.github.com>
2023-09-13 17:57:22 +0200

    fix(container-registry): move dockerhub info message (#830)
    fix(container-registry): make username and password optional for github/gitlab registry

--- libs/pages/settings/src/lib/ui/page-organization-container-registries/crud-modal/crud-modal.tsx
+++ libs/pages/settings/src/lib/ui/page-organization-container-registries/crud-modal/crud-modal.tsx
@@ -123,20 +123,6 @@ export function CrudModal(props: CrudModalProps) {
               options={getOptionsContainerRegistry(props.availableContainerRegistry)}
               portal
             />
-            {watch('kind') === ContainerRegistryKindEnum.DOCKER_HUB && (
-              <p className="text-xs text-neutral-350 my-1">
-                We encourage you to set credentials for Docker Hub due to the limits on the pull rate.
-                <Link
-                  className="font-medium text-sky-500 block ml-1"
-                  size="text-xs"
-                  link="https://www.docker.com/increase-rate-limits"
-                  linkLabel="See here"
-                  iconRightClassName="text-2xs relative top-[1px]"
-                  iconRight={IconAwesomeEnum.ARROW_UP_RIGHT_FROM_SQUARE}
-                  external
-                />
-              </p>
-            )}
           </div>
         )}
       />
@@ -194,16 +180,32 @@ export function CrudModal(props: CrudModalProps) {
             name="config.password"
             control={control}
             render={({ field, fieldState: { error } }) => (
-              <InputText
-                dataTestId="input-password"
-                className="mb-5"
-                type="password"
-                name={field.name}
-                onChange={field.onChange}
-                value={field.value}
-                label="Password (optional)"
-                error={error?.message}
-              />
+              <div className="mb-5">
+                <InputText
+                  dataTestId="input-password"
+                  className="mb-5"
+                  type="password"
+                  name={field.name}
+                  onChange={field.onChange}
+                  value={field.value}
+                  label="Password (optional)"
+                  error={error?.message}
+                />
+                {watch('kind') === ContainerRegistryKindEnum.DOCKER_HUB && (
+                  <p className="text-xs text-neutral-350 my-1">
+                    We encourage you to set credentials for Docker Hub due to the limits on the pull rate.
+                    <Link
+                      className="font-medium text-sky-500 block ml-1"
+                      size="text-xs"
+                      link="https://www.docker.com/increase-rate-limits"
+                      linkLabel="See here"
+                      iconRightClassName="text-2xs relative top-[1px]"
+                      iconRight={IconAwesomeEnum.ARROW_UP_RIGHT_FROM_SQUARE}
+                      external
+                    />
+                  </p>
+                )}
+              </div>
             )}
           />
         </>
