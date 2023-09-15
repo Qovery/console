12c37b24bdb5cfd7535e320ebe68feb3c6d62059
Rémi Bonnet <rbonnet@qovery.com>
2023-09-15 15:42:53 +0200

    test: remove obsolete snapshots

--- libs/shared/ui/src/lib/components/status-label/status-label.spec.tsx
+++ libs/shared/ui/src/lib/components/status-label/status-label.spec.tsx
@@ -4,31 +4,11 @@ import StatusLabel, { type StatusLabelProps } from './status-label'
 
 describe('StatusLabel', () => {
   const props: StatusLabelProps = {
-    status: StateEnum.DEPLOYED,
+    status: StateEnum.QUEUED,
   }
 
   it('should render successfully', () => {
     const { baseElement } = renderWithProviders(<StatusLabel {...props} />)
     expect(baseElement).toBeTruthy()
-  })
-
-  it('should match snapshot for DEPLOYED status', () => {
-    const { container } = renderWithProviders(<StatusLabel status={StateEnum.DEPLOYED} />)
-    expect(container).toMatchSnapshot()
-  })
-
-  it('should match snapshot for QUEUED status', () => {
-    const { container } = renderWithProviders(<StatusLabel status={StateEnum.QUEUED} />)
-    expect(container).toMatchSnapshot()
-  })
-
-  it('should match snapshot for BUILD_ERROR status', () => {
-    const { container } = renderWithProviders(<StatusLabel status={StateEnum.BUILD_ERROR} />)
-    expect(container).toMatchSnapshot()
-  })
-
-  it('should match snapshot for UNDEFINED status', () => {
-    const { container } = renderWithProviders(<StatusLabel status={undefined} />)
-    expect(container).toMatchSnapshot()
   })
 })
