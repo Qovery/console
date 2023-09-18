15fdbb8c83ce3d4aecbbce1e4af2fcdece3bb0b5
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-18 12:18:53 +0200

    refacto(ui): use ts-pattern

--- libs/shared/ui/src/lib/components/status-chip/status-chip.tsx
+++ libs/shared/ui/src/lib/components/status-chip/status-chip.tsx
@@ -1,5 +1,6 @@
-import { StateEnum } from 'qovery-typescript-axios'
-import { RunningState } from '@qovery/shared/enums'
+import { type StateEnum } from 'qovery-typescript-axios'
+import { match } from 'ts-pattern'
+import { type RunningState } from '@qovery/shared/enums'
 import { upperCaseFirstLetter } from '@qovery/shared/util-js'
 import {
   BuildErrorIcon,
@@ -39,86 +40,36 @@ export function StatusChip(props: StatusChipProps) {
       </Tooltip>
     )
 
-  function shouldShowStatus(status: StateEnum | RunningState, allowedStates: (StateEnum | RunningState)[]): boolean {
-    return allowedStates.includes(status)
-  }
-
-  const stateMap = {
-    showDeployed: [StateEnum.DEPLOYED, RunningState.RUNNING, StateEnum.READY, RunningState.COMPLETED],
-    showError: [
-      StateEnum.DEPLOYMENT_ERROR,
-      StateEnum.STOP_ERROR,
-      StateEnum.DELETE_ERROR,
-      StateEnum.RESTART_ERROR,
-      RunningState.ERROR,
-    ],
-    showQueued: [
-      StateEnum.QUEUED,
-      StateEnum.DELETE_QUEUED,
-      StateEnum.DEPLOYMENT_QUEUED,
-      StateEnum.RESTART_QUEUED,
-      StateEnum.STOP_QUEUED,
-    ],
-    showDeploying: [StateEnum.DEPLOYING, RunningState.STARTING],
-    showRestarted: [StateEnum.RESTARTED],
-    showBuilding: [StateEnum.BUILDING],
-    showRestarting: [StateEnum.RESTARTING],
-    showStopping: [StateEnum.STOPPING],
-    showCanceling: [StateEnum.CANCELING],
-    showDeleting: [StateEnum.DELETING],
-    showStopped: [StateEnum.STOPPED],
-    showCanceled: [StateEnum.CANCELED],
-    showDeleted: [StateEnum.DELETED],
-    showUnknow: [RunningState.UNKNOWN],
-    showBuildError: [StateEnum.BUILD_ERROR],
-    showWarning: [RunningState.WARNING],
-  }
-
-  // success
-  const showDeployed: boolean = shouldShowStatus(status as StateEnum, stateMap.showDeployed)
-  const showRestarted: boolean = shouldShowStatus(status as StateEnum, stateMap.showRestarted)
-  // spinner
-  const showQueued: boolean = shouldShowStatus(status as StateEnum, stateMap.showQueued)
-  const showBuilding: boolean = shouldShowStatus(status as StateEnum, stateMap.showBuilding)
-  const showDeploying: boolean = shouldShowStatus(status as StateEnum, stateMap.showDeploying)
-  const showRestarting: boolean = shouldShowStatus(status as StateEnum, stateMap.showRestarting)
-  const showStopping: boolean = shouldShowStatus(status as StateEnum, stateMap.showStopping)
-  const showCanceling: boolean = shouldShowStatus(status as StateEnum, stateMap.showCanceling)
-  const showDeleting: boolean = shouldShowStatus(status as StateEnum, stateMap.showDeleting)
-  // stopped
-  const showStopped: boolean = shouldShowStatus(status as StateEnum, stateMap.showStopped)
-  const showCanceled: boolean = shouldShowStatus(status as StateEnum, stateMap.showCanceled)
-  const showDeleted: boolean = shouldShowStatus(status as StateEnum, stateMap.showDeleted)
-  // unknow / error / warning
-  const showUnknow: boolean = shouldShowStatus(status as StateEnum, stateMap.showUnknow)
-  const showBuildError: boolean = shouldShowStatus(status as StateEnum, stateMap.showBuildError)
-  const showError: boolean = shouldShowStatus(status as StateEnum, stateMap.showError)
-  const showWarning: boolean = shouldShowStatus(status as RunningState, stateMap.showWarning)
-
   const tooltipContent = `${upperCaseFirstLetter(status.replace('_', ' '))} ${
     appendTooltipMessage ? ' - ' + appendTooltipMessage : ''
   }`
 
+  const icon = match(status)
+    // success
+    .with('DEPLOYED', 'RUNNING', 'READY', 'COMPLETED', () => <DeployedIcon />)
+    .with('RESTARTED', () => <RestartedIcon />)
+    // spinner
+    .with('QUEUED', 'DELETE_QUEUED', 'DEPLOYMENT_QUEUED', 'RESTART_QUEUED', 'STOP_QUEUED', () => <QueuedIcon />)
+    .with('DEPLOYING', 'STARTING', () => <DeployingIcon />)
+    .with('RESTARTING', () => <RestartingIcon />)
+    .with('BUILDING', () => <BuildingIcon />)
+    .with('STOPPING', () => <StoppingIcon />)
+    .with('CANCELING', () => <CancelingIcon />)
+    .with('DELETING', () => <DeletingIcon />)
+    // stopped
+    .with('STOPPED', () => <StoppedIcon />)
+    .with('CANCELED', () => <CanceledIcon />)
+    .with('DELETED', () => <DeletedIcon />)
+    // unknow / error / warning
+    .with('UNKNOWN', () => <UnknowIcon />)
+    .with('BUILD_ERROR', () => <BuildErrorIcon />)
+    .with('WARNING', () => <WarningIcon />)
+    .with('DEPLOYMENT_ERROR', 'STOP_ERROR', 'DELETE_ERROR', 'RESTART_ERROR', 'ERROR', () => <ErrorIcon />)
+    .exhaustive()
+
   return (
     <Tooltip content={tooltipContent}>
-      <div className={className}>
-        {showDeployed && <DeployedIcon />}
-        {showRestarted && <RestartedIcon />}
-        {showQueued && <QueuedIcon />}
-        {showBuilding && <BuildingIcon />}
-        {showDeploying && <DeployingIcon />}
-        {showRestarting && <RestartingIcon />}
-        {showStopping && <StoppingIcon />}
-        {showCanceling && <CancelingIcon />}
-        {showDeleting && <DeletingIcon />}
-        {showStopped && <StoppedIcon />}
-        {showCanceled && <CanceledIcon />}
-        {showDeleted && <DeletedIcon />}
-        {showUnknow && <UnknowIcon />}
-        {showBuildError && <BuildErrorIcon />}
-        {showError && <ErrorIcon />}
-        {showWarning && <WarningIcon />}
-      </div>
+      <div className={className}>{icon}</div>
     </Tooltip>
   )
 }
