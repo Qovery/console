c3e54d934324227b13c6f1e339fdc4f8f2ef87c8
Rémi Bonnet <rbonnet@qovery.com>
2023-09-18 12:11:06 +0200

    fix: queued icon color

--- libs/shared/ui/src/lib/components/icon/icons-status/queued.tsx
+++ libs/shared/ui/src/lib/components/icon/icons-status/queued.tsx
@@ -6,7 +6,7 @@ import SpinnerIcon from './spinner'
 export const QueuedIcon = forwardRef<ElementRef<'div'>, IconStatusProps>(
   ({ className = '', width = 16, height = 16 }, forwardedRef) => {
     return (
-      <div className={twMerge('text-brand-500', className)} ref={forwardedRef}>
+      <div className={twMerge('text-brand-500 dark:text-brand-400', className)} ref={forwardedRef}>
         <SpinnerIcon width={width} height={height} />
       </div>
     )
