b915a09c07588572c08b7bf3e8fb8edf22214e70
Rémi Bonnet <rbonnet@qovery.com>
2023-09-19 11:11:23 +0200

    fix: environment state default stopped status

--- libs/domains/environments/feature/src/lib/environment-state-chip/environment-state-chip.tsx
+++ libs/domains/environments/feature/src/lib/environment-state-chip/environment-state-chip.tsx
@@ -11,14 +11,14 @@ type DeploymentStateChipProps = Omit<EnvironmentStateChipProps, 'mode'>
 
 function DeploymentStateChip({ environmentId, ...props }: DeploymentStateChipProps) {
   const { data: deploymentStatus } = useDeploymentStatus({ environmentId })
-  return <StatusChip status={deploymentStatus?.state} {...props} />
+  return <StatusChip status={deploymentStatus?.state ?? 'STOPPED'} {...props} />
 }
 
 type RunningStateChipProps = Omit<EnvironmentStateChipProps, 'mode'>
 
 function RunningStateChip({ environmentId, ...props }: RunningStateChipProps) {
   const { data: runningStatus } = useRunningStatus({ environmentId })
-  return <StatusChip status={runningStatus?.state} {...props} />
+  return <StatusChip status={runningStatus?.state ?? 'STOPPED'} {...props} />
 }
 
 export function EnvironmentStateChip({ mode, ...props }: EnvironmentStateChipProps) {
