5dec31f1bc58a19569cfdcc7e1f3447a4bf50b89
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-25 10:27:33 +0200

    feat(services): use service-details component

--- libs/pages/application/src/lib/ui/page-general/page-general.tsx
+++ libs/pages/application/src/lib/ui/page-general/page-general.tsx
@@ -1,6 +1,6 @@
 import { type ContainerRegistryResponse } from 'qovery-typescript-axios'
 import { useParams } from 'react-router-dom'
-import { PodsMetrics } from '@qovery/domains/services/feature'
+import { PodsMetrics, ServiceDetails } from '@qovery/domains/services/feature'
 import { isApplication, isGitJob, isJob } from '@qovery/shared/enums'
 import { type ApplicationEntity, type JobApplicationEntity, type LoadingStatus } from '@qovery/shared/interfaces'
 import { type BaseLink, HelpSection, Icon, Skeleton, Tooltip } from '@qovery/shared/ui'
@@ -20,84 +20,65 @@ export interface PageGeneralProps {
 
 export function PageGeneral(props: PageGeneralProps) {
   const { application, listHelpfulLinks, loadingStatus, serviceStability = 0 } = props
-  const { organizationId = '' } = useParams()
+  const { organizationId = '', environmentId = '', applicationId = '' } = useParams()
 
   return (
     <div className="mt-2 bg-white rounded flex flex-grow min-h-0">
-      <div className="flex flex-col flex-grow">
-        <div className="py-7 px-10 flex-grow overflow-y-auto min-h-0">
-          {!isJob(application) ? (
-            <>
-              <div className="flex border border-neutral-200 mb-4">
-                <div className="flex-1 border-r border-neutral-200 p-5">
-                  <Skeleton height={24} width={48} show={application?.instances?.loadingStatus === 'loading'}>
-                    <span className="text-neutral-400 font-bold">
-                      {application?.instances?.items?.length || '–'}/{application?.max_running_instances || '-'}
+      <div className="flex flex-col grow">
+        <div className="flex flex-row grow">
+          <div className="py-7 px-10 flex-grow overflow-y-auto min-h-0">
+            {!isJob(application) ? (
+              <>
+                <div className="flex border border-neutral-200 mb-4">
+                  <div className="flex-1 border-r border-neutral-200 p-5">
+                    <Skeleton height={24} width={48} show={application?.instances?.loadingStatus === 'loading'}>
+                      <span className="text-neutral-400 font-bold">
+                        {application?.instances?.items?.length || '–'}/{application?.max_running_instances || '-'}
+                      </span>
+                    </Skeleton>
+                    <span className="flex text-xs text-neutral-350 font-medium">
+                      Running instances{' '}
+                      <Tooltip side="right" content="Number of running instances">
+                        <div className="flex items-center">
+                          <Icon
+                            className="cursor-pointer ml-1 text-xs text-neutral-350"
+                            name="icon-solid-circle-info"
+                          />
+                        </div>
+                      </Tooltip>
                     </span>
-                  </Skeleton>
-                  <span className="flex text-xs text-neutral-350 font-medium">
-                    Running instances{' '}
-                    <Tooltip side="right" content="Number of running instances">
-                      <div className="flex items-center">
-                        <Icon className="cursor-pointer ml-1 text-xs text-neutral-350" name="icon-solid-circle-info" />
-                      </div>
-                    </Tooltip>
-                  </span>
+                  </div>
+                  <div className="flex-1 p-5">
+                    <div className="text-neutral-400 font-bold mb-1">{serviceStability}</div>
+                    <span className="flex text-xs text-neutral-350 font-medium">
+                      Service stability
+                      <Tooltip
+                        side="right"
+                        content="Number of application instance restarts since the last deployment due to application errors"
+                      >
+                        <div className="flex items-center">
+                          <Icon
+                            className="cursor-pointer ml-1 text-xs text-neutral-350"
+                            name="icon-solid-circle-info"
+                          />
+                        </div>
+                      </Tooltip>
+                    </span>
+                  </div>
                 </div>
-                <div className="flex-1 p-5">
-                  <div className="text-neutral-400 font-bold mb-1">{serviceStability}</div>
-                  <span className="flex text-xs text-neutral-350 font-medium">
-                    Service stability
-                    <Tooltip
-                      side="right"
-                      content="Number of application instance restarts since the last deployment due to application errors"
-                    >
-                      <div className="flex items-center">
-                        <Icon className="cursor-pointer ml-1 text-xs text-neutral-350" name="icon-solid-circle-info" />
-                      </div>
-                    </Tooltip>
-                  </span>
-                </div>
-              </div>
-              {application && application.environment && (
-                <PodsMetrics environmentId={application.environment.id} serviceId={application.id} />
-              )}
-            </>
-          ) : (
-            <JobOverview application={application as JobApplicationEntity} />
-          )}
+                {application && application.environment && (
+                  <PodsMetrics environmentId={application.environment.id} serviceId={application.id} />
+                )}
+              </>
+            ) : (
+              <JobOverview application={application as JobApplicationEntity} />
+            )}
+          </div>
+          <ServiceDetails className="w-[360px] border-l" environmentId={environmentId} serviceId={applicationId} />
         </div>
         <div className="bg-white rounded-b flex flex-col justify-end">
           <HelpSection description="Need help? You may find these links useful" links={listHelpfulLinks} />
         </div>
-      </div>
-      <div className="w-right-help-sidebar shrink-0 py-10 border-l border-neutral-200">
-        <About description={application?.description || ''} />
-
-        {application &&
-          (isApplication(application) ? (
-            <AboutGit application={application} />
-          ) : isJob(application) ? (
-            isGitJob(application) ? (
-              <AboutGit application={application} />
-            ) : (
-              <AboutContainer
-                loadingStatus={loadingStatus}
-                organizationId={organizationId}
-                currentRegistry={props.currentRegistry}
-                container={application}
-              />
-            )
-          ) : (
-            <AboutContainer
-              loadingStatus={loadingStatus}
-              organizationId={organizationId}
-              container={application}
-              currentRegistry={props.currentRegistry}
-            />
-          ))}
-
-        <AboutUpdate application={application} />
       </div>
     </div>
   )
--- libs/pages/database/src/lib/ui/page-general/page-general.tsx
+++ libs/pages/database/src/lib/ui/page-general/page-general.tsx
@@ -1,5 +1,6 @@
 import { DatabaseModeEnum, StateEnum } from 'qovery-typescript-axios'
-import { PodsMetrics } from '@qovery/domains/services/feature'
+import { useParams } from 'react-router-dom'
+import { PodsMetrics, ServiceDetails } from '@qovery/domains/services/feature'
 import { useDeploymentStatus } from '@qovery/domains/services/feature'
 import { type DatabaseEntity, type LoadingStatus } from '@qovery/shared/interfaces'
 import { type BaseLink, HelpSection, Skeleton } from '@qovery/shared/ui'
@@ -13,6 +14,7 @@ export interface PageGeneralProps {
 
 export function PageGeneral(props: PageGeneralProps) {
   const { database, listHelpfulLinks, loadingStatus } = props
+  const { environmentId = '', databaseId = '' } = useParams()
 
   const { data: deploymentStatus } = useDeploymentStatus({
     environmentId: database?.environment?.id,
@@ -21,48 +23,40 @@ export function PageGeneral(props: PageGeneralProps) {
 
   return (
     <div className="mt-2 bg-white rounded flex flex-grow min-h-0">
-      <div className="flex h-full flex-col flex-grow">
-        <div className="py-7 px-10 flex-grow overflow-auto">
-          {database?.mode && database?.mode === DatabaseModeEnum.MANAGED && (
-            <p className="text-sm text-neutral-400 font-medium mb-4">
-              Metrics for Managed Databases can be found directly on your cloud provider dashboard
-            </p>
-          )}
-          <div className="flex border border-neutral-200 mb-4">
-            <div className="flex-1 border-r border-neutral-200 p-5">
-              <Skeleton height={16} width={48} show={false}>
-                <div className="text-neutral-400 font-bold">
-                  {database?.mode === DatabaseModeEnum.MANAGED
-                    ? 'N / A'
-                    : `${deploymentStatus?.state === StateEnum.DEPLOYED ? 1 : 0} / 1`}
-                </div>
-              </Skeleton>
-              <span className="text-xs text-neutral-350 font-medium">Running instances</span>
-            </div>
-            <div className="flex-1 p-5">
-              <div className="text-neutral-400 font-bold">
-                {database?.mode === DatabaseModeEnum.MANAGED ? 'N / A' : '-' /** TODO: implem real metrics **/}
+      <div className="flex h-full flex-col grow">
+        <div className="flex flex-row grow">
+          <div className="py-7 px-10 flex-grow overflow-auto">
+            {database?.mode && database?.mode === DatabaseModeEnum.MANAGED && (
+              <p className="text-sm text-neutral-400 font-medium mb-4">
+                Metrics for Managed Databases can be found directly on your cloud provider dashboard
+              </p>
+            )}
+            <div className="flex border border-neutral-200 mb-4">
+              <div className="flex-1 border-r border-neutral-200 p-5">
+                <Skeleton height={16} width={48} show={false}>
+                  <div className="text-neutral-400 font-bold">
+                    {database?.mode === DatabaseModeEnum.MANAGED
+                      ? 'N / A'
+                      : `${deploymentStatus?.state === StateEnum.DEPLOYED ? 1 : 0} / 1`}
+                  </div>
+                </Skeleton>
+                <span className="text-xs text-neutral-350 font-medium">Running instances</span>
               </div>
-              <span className="text-xs text-neutral-350 font-medium">Service restart</span>
+              <div className="flex-1 p-5">
+                <div className="text-neutral-400 font-bold">
+                  {database?.mode === DatabaseModeEnum.MANAGED ? 'N / A' : '-' /** TODO: implem real metrics **/}
+                </div>
+                <span className="text-xs text-neutral-350 font-medium">Service restart</span>
+              </div>
             </div>
-          </div>
 
-          {database && database.environment && (
-            <PodsMetrics environmentId={database.environment.id} serviceId={database.id} />
-          )}
+            {database && database.environment && (
+              <PodsMetrics environmentId={database.environment.id} serviceId={database.id} />
+            )}
+          </div>
+          <ServiceDetails className="w-[360px] border-l" environmentId={environmentId} serviceId={databaseId} />
         </div>
         <HelpSection description="Need help? You may find these links useful" links={listHelpfulLinks}></HelpSection>
-      </div>
-      <div className="w-right-help-sidebar shrink-0 py-10 border-l border-neutral-200">
-        <About
-          description={database?.description}
-          type={database?.type}
-          version={database?.version}
-          mode={database?.mode}
-          accessibility={database?.accessibility}
-          credentials={database?.credentials?.items}
-          loadingStatus={loadingStatus}
-        />
       </div>
     </div>
   )
