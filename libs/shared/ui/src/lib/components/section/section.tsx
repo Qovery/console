22139787bb215d4e5faa56da03a88237566d6e53
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-26 18:32:08 +0200

    fixup! fixup! fixup! feat(ui): add Section and Heading components

--- libs/shared/ui/src/lib/components/section/section.tsx
+++ libs/shared/ui/src/lib/components/section/section.tsx
@@ -1,39 +1,16 @@
-import { type VariantProps, cva } from 'class-variance-authority'
 import { type ComponentPropsWithoutRef, type ElementRef, forwardRef, useContext } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { LevelContext } from '../heading/level-context'
 
-const sectionVariants = cva(['flex', 'flex-col'], {
-  variants: {
-    variant: {
-      neutral: ['text-neutral-350'],
-    },
-    level: {
-      // TODO: work on design system to standardize font-size
-      1: 'text-sm',
-      2: 'text-sm',
-      3: 'text-sm',
-      4: 'text-sm',
-      5: 'text-sm',
-      6: 'text-sm',
-    },
-  },
-  defaultVariants: {
-    variant: 'neutral',
-    level: 1,
-  },
-})
-
-export interface SectionProps extends ComponentPropsWithoutRef<'section'>, VariantProps<typeof sectionVariants> {}
+export interface SectionProps extends ComponentPropsWithoutRef<'section'> {}
 
 export const Section = forwardRef<ElementRef<'section'>, SectionProps>(function Section(
-  { className, children, variant, level: lvl, ...props },
+  { className, children, ...props },
   forwardedRef
 ) {
-  const ctxLvl = useContext(LevelContext) as 1 | 2 | 3 | 4 | 5 | 6
-  const level = lvl ?? ctxLvl
+  const level = useContext(LevelContext)
   return (
-    <section ref={forwardedRef} className={twMerge(sectionVariants({ variant, level }), className)} {...props}>
+    <section ref={forwardedRef} className={twMerge('flex flex-col', className)} {...props}>
       <LevelContext.Provider value={level + 1}>{children}</LevelContext.Provider>
     </section>
   )
