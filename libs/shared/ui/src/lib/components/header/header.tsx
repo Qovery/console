d135caf7698401ed35f4ae72171ec14df0fcb964
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-26 19:37:37 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat(ui): revamp header

--- libs/shared/ui/src/lib/components/header/header.tsx
+++ libs/shared/ui/src/lib/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 import { type IconEnum } from '@qovery/shared/enums'
 import Icon from '../icon/icon'
 import Skeleton from '../skeleton/skeleton'
@@ -7,6 +8,7 @@ import Truncate from '../truncate/truncate'
 export interface HeaderProps {
   title?: string
   icon?: IconEnum | string
+  iconClassName?: string
   buttons?: ReactNode
   copyTitle?: boolean
   copyContent?: string
@@ -14,14 +16,14 @@ export interface HeaderProps {
 }
 
 export function Header(props: HeaderProps) {
-  const { title, icon, buttons, actions } = props
+  const { title, icon, buttons, actions, iconClassName } = props
 
   return (
     <div className="flex border-b border-neutral-200 items-center justify-between bg-white rounded-t px-5 py-8 shrink-0">
       <div className="flex gap-5 ml-4 items-center">
         {icon && (
           <div className="flex items-center justify-center w-16 h-16">
-            <Icon name={icon} className="w-10" />
+            <Icon name={icon} className={twMerge('w-10', iconClassName)} />
           </div>
         )}
         <div className="flex flex-col gap-3">
