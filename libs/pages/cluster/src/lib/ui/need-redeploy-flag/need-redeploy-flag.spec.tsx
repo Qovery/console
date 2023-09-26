b9ad94802b58d247e947f318771c870a8ddd237d
Rémi Bonnet <rbonnet@qovery.com>
2023-09-26 14:33:07 +0200

    test: update need redeploy flag

--- libs/pages/cluster/src/lib/ui/need-redeploy-flag/need-redeploy-flag.spec.tsx
+++ libs/pages/cluster/src/lib/ui/need-redeploy-flag/need-redeploy-flag.spec.tsx
@@ -1,24 +1,26 @@
 import { ClusterDeploymentStatusEnum } from 'qovery-typescript-axios'
-import { clusterFactoryMock } from '@qovery/shared/factories'
-import { type ClusterEntity } from '@qovery/shared/interfaces'
 import { renderWithProviders, screen } from '@qovery/shared/util-tests'
 import NeedRedeployFlag from './need-redeploy-flag'
 
-const mockCluster: ClusterEntity = clusterFactoryMock(1)[0]
-
 describe('NeedRedeployFlag', () => {
   it('should render successfully', () => {
-    const { baseElement } = renderWithProviders(<NeedRedeployFlag deploymentStatus={mockCluster.deployment_status} />)
+    const { baseElement } = renderWithProviders(
+      <NeedRedeployFlag deploymentStatus={ClusterDeploymentStatusEnum.NEVER_DEPLOYED} onClickButton={jest.fn()} />
+    )
     expect(baseElement).toBeTruthy()
   })
 
   it('should render button with Deploy now', () => {
-    renderWithProviders(<NeedRedeployFlag deploymentStatus={ClusterDeploymentStatusEnum.NEVER_DEPLOYED} />)
+    renderWithProviders(
+      <NeedRedeployFlag deploymentStatus={ClusterDeploymentStatusEnum.NEVER_DEPLOYED} onClickButton={jest.fn()} />
+    )
     screen.getByRole('button', { name: 'Deploy now' })
   })
 
   it('should render button with Redeploy now', () => {
-    renderWithProviders(<NeedRedeployFlag deploymentStatus={ClusterDeploymentStatusEnum.OUT_OF_DATE} />)
+    renderWithProviders(
+      <NeedRedeployFlag deploymentStatus={ClusterDeploymentStatusEnum.OUT_OF_DATE} onClickButton={jest.fn()} />
+    )
     screen.getByRole('button', { name: 'Redeploy now' })
   })
 
