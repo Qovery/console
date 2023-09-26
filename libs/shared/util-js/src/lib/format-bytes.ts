2444f9a49a076766f4075d89dece5fd32707fd7f
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-26 15:09:00 +0200

    feat(util-js): add formaBytes

---
+++ libs/shared/util-js/src/lib/format-bytes.ts
@@ -0,0 +1,8 @@
+import prettyBytes from 'pretty-bytes'
+
+export function formatBytes(number?: Parameters<typeof prettyBytes>[0], options?: Parameters<typeof prettyBytes>[1]) {
+  if (number === undefined) {
+    return number
+  }
+  return prettyBytes(number, options)
+}
