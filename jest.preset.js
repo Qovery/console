1c40e000dc4746ba85aa98135a3a8d0d0d7b80a0
Camille TJHOA <camille.tjhoa@outlook.com>
2023-09-29 11:08:18 +0200

    chore: simplify jest config
    Remove params override by nx jest-preset
    https://github.com/nrwl/nx/blob/867a5a81cd9aad4799a2492fcd1874b7f6cb77e4/packages/jest/preset/jest-preset.ts

--- jest.preset.js
+++ jest.preset.js
@@ -3,13 +3,7 @@ const nxPreset = require('@nx/jest/preset').default
 module.exports = {
   setupFilesAfterEnv: ['./__tests__/mocks.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
-  testMatch: ['./libs/**/__tests__/**/*.{js,jsx,ts,tsx}', './libs/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   testPathIgnorePatterns: ['./node_modules/', './.next/', './__tests__/utils/setup-jest.tsx'],
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': ['babel-jest', { presets: ['@nx/react/babel'] }],
-    '^.+\\.scss$': 'jest-scss-transform',
-  },
   transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
     '^.+\\.module\\.(css|sass|scss)$',
