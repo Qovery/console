40e812aba598f815342ee92ef9fa7ae9a08c949b
Rémi Bonnet <rbonnet@qovery.com>
2023-09-29 14:30:42 +0200

    fix: test service links popover

--- libs/domains/services/feature/src/lib/service-links-popover/service-links-popover.spec.tsx
+++ libs/domains/services/feature/src/lib/service-links-popover/service-links-popover.spec.tsx
@@ -1,14 +1,14 @@
 import { renderWithProviders } from '@qovery/shared/util-tests'
-import ServiceLinksDropdown from './service-links-dropdown'
+import ServiceLinksPopover from './service-links-popover'
 
-describe('ServiceLinksDropdown', () => {
+describe('ServiceLinksPopover', () => {
   it('should render successfully', () => {
-    const { baseElement } = renderWithProviders(<ServiceLinksDropdown links={[]} />)
+    const { baseElement } = renderWithProviders(<ServiceLinksPopover links={[]} />)
     expect(baseElement).toBeTruthy()
   })
   it('should match snapshot with button links', async () => {
     const { container } = renderWithProviders(
-      <ServiceLinksDropdown
+      <ServiceLinksPopover
         links={[
           {
             url: 'https://qovery.com',
