d65e518f6a5d56aa06ba12872ef8156a03dd21cb
acarranoqovery <105300721+acarranoqovery@users.noreply.github.com>
2023-10-11 10:37:00 +0200

    chore: added hint for image tag field (#892)

--- libs/shared/console-shared/src/lib/general-container-settings/ui/general-container-settings.tsx
+++ libs/shared/console-shared/src/lib/general-container-settings/ui/general-container-settings.tsx
@@ -31,63 +31,70 @@ export function GeneralContainerSettings({ organization, className }: GeneralCon
 
   return (
     <div className={twMerge('mb-6 flex flex-col gap-3', className)}>
-      <Controller
-        name="registry"
-        control={control}
-        rules={{
-          required: 'Please select a registry.',
-        }}
-        render={({ field, fieldState: { error } }) => (
-          <InputSelect
-            dataTestId="input-select-registry"
-            className="mb-0.5"
-            onChange={field.onChange}
-            value={field.value}
-            options={availableRegistiesOptions}
-            label="Registry"
-            error={error?.message}
-          />
-        )}
-      />
-      <p className="text-right">
-        <Link to={SETTINGS_URL(organization?.id) + SETTINGS_CONTAINER_REGISTRIES_URL} className="text-ssm">
-          Don't see your registry?
-        </Link>
-      </p>
-      <Controller
-        name="image_name"
-        control={control}
-        rules={{
-          required: 'Please type a value.',
-        }}
-        render={({ field, fieldState: { error } }) => (
-          <InputText
-            dataTestId="input-text-image-name"
-            name="image_name"
-            onChange={field.onChange}
-            value={field.value}
-            label="Image name"
-            error={error?.message}
-          />
-        )}
-      />
-      <Controller
-        name="image_tag"
-        control={control}
-        rules={{
-          required: 'Please type a value.',
-        }}
-        render={({ field, fieldState: { error } }) => (
-          <InputText
-            dataTestId="input-text-image-tag"
-            name="image_tag"
-            onChange={field.onChange}
-            value={field.value}
-            label="Image tag"
-            error={error?.message}
-          />
-        )}
-      />
+      <div>
+        <Controller
+          name="registry"
+          control={control}
+          rules={{
+            required: 'Please select a registry.',
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <InputSelect
+              dataTestId="input-select-registry"
+              className="mb-0.5"
+              onChange={field.onChange}
+              value={field.value}
+              options={availableRegistiesOptions}
+              label="Registry"
+              error={error?.message}
+            />
+          )}
+        />
+        <p className="text-right">
+          <Link to={SETTINGS_URL(organization?.id) + SETTINGS_CONTAINER_REGISTRIES_URL} className="text-ssm">
+            Don't see your registry?
+          </Link>
+        </p>
+      </div>
+      <div>
+        <Controller
+          name="image_name"
+          control={control}
+          rules={{
+            required: 'Please type a value.',
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <InputText
+              dataTestId="input-text-image-name"
+              name="image_name"
+              onChange={field.onChange}
+              value={field.value}
+              label="Image name"
+              error={error?.message}
+            />
+          )}
+        />
+      </div>
+      <div>
+        <Controller
+          name="image_tag"
+          control={control}
+          rules={{
+            required: 'Please type a value.',
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <InputText
+              dataTestId="input-text-image-tag"
+              name="image_tag"
+              onChange={field.onChange}
+              value={field.value}
+              label="Image tag"
+              error={error?.message}
+            />
+          )}
+        />
+        <p className="text-xs ml-4 mt-1 text-neutral-350">Image tag shall be unique (no ‘main’, ‘dev’, ‘master’)</p>
+      </div>
     </div>
   )
 }
