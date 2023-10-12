e67668b5f2c8d714ae1ae206ab86d864bb7f5fe6
Carrano <acarrano@qovery.com>
2023-10-12 15:00:31 +0200

    fix(delete-cluster):updated text based on selected mode

--- libs/domains/clusters/feature/src/lib/cluster-delete-modal/cluster-delete-modal.tsx
+++ libs/domains/clusters/feature/src/lib/cluster-delete-modal/cluster-delete-modal.tsx
@@ -49,11 +49,27 @@ export function ClusterDeleteModal({ organizationId, clusterId, name }: ClusterD
           value={clusterDeleteMode}
         />
         <p>This operation will delete:</p>
+
         <ul className="list-disc pl-5">
-          <li>
-            <b>Cloud provider</b>: any resource created by Qovery on your cloud provider account to run this cluster
-            will be deleted, including any environment attached to it.{' '}
-          </li>
+          {clusterDeleteMode == ClusterDeleteMode.DEFAULT && (
+            <li>
+              <b>Cloud provider</b>: any resource created by Qovery on your cloud provider account to run this cluster
+              will be deleted, including any application running on it.{' '}
+            </li>
+          )}
+          {clusterDeleteMode == ClusterDeleteMode.DELETE_CLUSTER_AND_QOVERY_CONFIG && (
+            <li>
+              <b>Cloud provider</b>: any resource created by Qovery on your cloud provider account to run this cluster
+              will be deleted, including any application running on it. Any cloud provider managed service deployed via
+              Qovery will NOT be deleted.{' '}
+            </li>
+          )}
+          {clusterDeleteMode == ClusterDeleteMode.DELETE_QOVERY_CONFIG && (
+            <li>
+              <b>Cloud provider</b>: nothing will be deleted on your cloud provider account. You will have to manually
+              delete any resource created by Qovery directly from your cloud provider console.{' '}
+            </li>
+          )}
           <li>
             <b>Qovery organization</b>: the configuration of this cluster and any linked environment.
           </li>
@@ -67,7 +83,7 @@ export function ClusterDeleteModal({ organizationId, clusterId, name }: ClusterD
               <>
                 <p>Please note that you will have to manually delete on your cloud account:</p>
                 <ul className="list-disc pl-3">
-                  <li>the S3 bucket named xxxxxx-xxxxxx-xxxxxx</li>
+                  <li>the S3 bucket created during the cluster installation</li>
                   <li>the image registry linked to this cluster</li>
                   {clusterDeleteMode === ClusterDeleteMode.DELETE_CLUSTER_AND_QOVERY_CONFIG && (
                     <li>any managed database that was created via Qovery.</li>
