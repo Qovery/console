aced048ae46b9ffa8d98dda7cb712e148e74177f
Rémi Bonnet <bonnet.rem@gmail.com>
2023-10-17 09:28:46 +0200

    fix(build-git-url): includes http (#908)

--- libs/shared/util-js/src/lib/build-git-repo-url.ts
+++ libs/shared/util-js/src/lib/build-git-repo-url.ts
@@ -1,5 +1,5 @@
 export const buildGitRepoUrl = (provider: string, branch: string): string => {
-  if (branch.includes('http')) return branch
+  if (branch.includes('http://') || branch.includes('https://')) return branch
   const authProvider = provider.toLowerCase()
   return `https://${authProvider}.com/${branch}.git`
 }
