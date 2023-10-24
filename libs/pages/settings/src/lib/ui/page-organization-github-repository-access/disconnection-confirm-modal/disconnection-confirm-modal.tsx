1c00b534ae715fb41942274b677610d9758c7dd6
Camille TJHOA <camille.tjhoa@outlook.com>
2023-10-24 11:59:22 +0200

    refactor(organization): replace callout in disconnection-confirm-modal

--- libs/pages/settings/src/lib/ui/page-organization-github-repository-access/disconnection-confirm-modal/disconnection-confirm-modal.tsx
+++ libs/pages/settings/src/lib/ui/page-organization-github-repository-access/disconnection-confirm-modal/disconnection-confirm-modal.tsx
@@ -1,11 +1,4 @@
-import {
-  BannerBox,
-  BannerBoxEnum,
-  ButtonLegacy,
-  ButtonLegacySize,
-  ButtonLegacyStyle,
-  IconAwesomeEnum,
-} from '@qovery/shared/ui'
+import { ButtonLegacy, ButtonLegacySize, ButtonLegacyStyle, Callout, Icon, IconAwesomeEnum } from '@qovery/shared/ui'
 
 export interface DisconnectionConfirmModalProps {
   onSubmit: (force: boolean) => void
@@ -16,13 +9,19 @@ export function DisconnectionConfirmModal(props: DisconnectionConfirmModalProps)
   return (
     <div className="p-6">
       <h2 className="h4 text-neutral-400 max-w-sm truncate mb-6">Disconnect the Qovery Github App</h2>
-      <BannerBox
-        className="mb-5"
-        type={BannerBoxEnum.WARNING}
-        title="This action might affect your future deployment"
-        message="Removing the Qovery Github App will reset the permission access to your repositories. After the removal, Qovery will use your Github account to access them. Make sure that your Github account has the access permissions to all the repositories shown in the “Authorised Repositories” section."
-        icon={IconAwesomeEnum.CIRCLE_INFO}
-      />
+      <Callout.Root className="mb-5" color="yellow">
+        <Callout.Icon>
+          <Icon name={IconAwesomeEnum.CIRCLE_INFO} />
+        </Callout.Icon>
+        <Callout.Text>
+          <Callout.TextHeading>This action might affect your future deployment</Callout.TextHeading>
+          <Callout.TextDescription className="text-xs">
+            Removing the Qovery Github App will reset the permission access to your repositories. After the removal,
+            Qovery will use your Github account to access them. Make sure that your Github account has the access
+            permissions to all the repositories shown in the “Authorised Repositories” section.
+          </Callout.TextDescription>
+        </Callout.Text>
+      </Callout.Root>
       <div className="flex gap-3 justify-end mt-6">
         <ButtonLegacy
           dataTestId="cancel-button"
