3d67095b48774c7c8b4a386aafad50e2584eedb8
Rémi Bonnet <rbonnet@qovery.com>
2023-11-21 18:59:37 +0100

    test: add snapshots settings

--- libs/pages/services/src/lib/feature/page-helm-create-feature/step-general-feature/step-general-feature.spec.tsx
+++ libs/pages/services/src/lib/feature/page-helm-create-feature/step-general-feature/step-general-feature.spec.tsx
@@ -1,25 +1,29 @@
-import { render } from '__tests__/utils/setup-jest'
-import { ServiceTypeEnum } from '@qovery/shared/enums'
-import { ApplicationContainerCreateContext } from '../page-application-create-feature'
+import { renderWithProviders } from '@qovery/shared/util-tests'
+import { HelmCreateContext } from '../page-helm-create-feature'
 import StepGeneralFeature from './step-general-feature'
 
 describe('PageApplicationCreateGeneralFeature', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <ApplicationContainerCreateContext.Provider
+    const { baseElement } = renderWithProviders(
+      <HelmCreateContext.Provider
         value={{
           currentStep: 1,
           setCurrentStep: jest.fn(),
-          generalData: { name: 'test', serviceType: ServiceTypeEnum.APPLICATION },
-          setGeneralData: jest.fn(),
-          resourcesData: undefined,
-          setResourcesData: jest.fn(),
-          portData: undefined,
-          setPortData: jest.fn(),
+          generalForm: {
+            name: 'my-helm-app',
+            description: 'hello',
+            auto_preview: true,
+            timeout_sec: 60,
+            arguments: ['--wait'],
+            source_provider: 'HELM_REPOSITORY',
+            repository: 'https://charts.bitnami.com/bitnami',
+            chart_name: 'nginx',
+            chart_version: '8.9.0',
+          },
         }}
       >
         <StepGeneralFeature />
-      </ApplicationContainerCreateContext.Provider>
+      </HelmCreateContext.Provider>
     )
     expect(baseElement).toBeTruthy()
   })
