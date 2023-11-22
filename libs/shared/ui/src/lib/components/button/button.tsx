5b0084698dd1f61e5d859d640a96c14e8f0ea558
Rémi Bonnet <rbonnet@qovery.com>
2023-11-22 15:48:26 +0100

    add feedback

--- libs/shared/ui/src/lib/components/button/button.tsx
+++ libs/shared/ui/src/lib/components/button/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(['inline-flex', 'items-center', 'shrink-0', 'font-med
       outline: [],
     },
     color: {
-      brand: ['disabled:text-brand-300', 'disabled:bg-brand-100', 'disabled:border-none'],
+      brand: [],
       neutral: [],
       green: [],
     },
@@ -55,7 +55,14 @@ const buttonVariants = cva(['inline-flex', 'items-center', 'shrink-0', 'font-med
     ...['brand' as const, 'neutral' as const, 'green' as const].map((color) => ({
       variant: 'solid' as const,
       color,
-      className: [`bg-${color}-500`, `hover:bg-${color}-600`, 'text-white'],
+      className: [
+        `bg-${color}-500`,
+        `hover:bg-${color}-600`,
+        'text-white',
+        `disabled:text-${color}-300`,
+        `disabled:bg-${color}-100`,
+        `disabled:border-none`,
+      ],
     })),
   ],
   defaultVariants: {
