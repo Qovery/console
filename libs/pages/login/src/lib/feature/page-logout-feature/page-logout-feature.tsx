746b5309c99e11eab454b697b572215dc1081b3a
Rémi Bonnet <bonnet.rem@gmail.com>
2023-11-22 11:05:17 +0100

    fix(use-redirect-if-logged): check if organizations is fetched (#977)
    * fix(use-redirect-if-logged): check if organizations is fetched
    
    * fix: add default value use organizations hook

--- libs/pages/login/src/lib/feature/page-logout-feature/page-logout-feature.tsx
+++ libs/pages/login/src/lib/feature/page-logout-feature/page-logout-feature.tsx
@@ -1,20 +1,18 @@
 import posthog from 'posthog-js'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useAuth } from '@qovery/shared/auth'
-import { LOGIN_URL } from '@qovery/shared/routes'
 import { LoadingScreen } from '@qovery/shared/ui'
 
 export function PageLogoutFeature() {
   const { authLogout } = useAuth()
-  const navigate = useNavigate()
 
   useEffect(() => {
-    authLogout().then(() => {
+    async function redirectLogout() {
+      await authLogout()
       posthog.reset()
-      navigate(LOGIN_URL)
-    })
-  }, [authLogout, navigate])
+    }
+    redirectLogout()
+  }, [authLogout])
 
   return <LoadingScreen />
 }
