b4b9b3d43e67969ddec057d0ecef9be6dda98539
Camille TJHOA <camille.tjhoa@outlook.com>
2023-11-28 10:54:35 +0100

    fix(ui): fix z-index conflicts (#992)

--- libs/shared/ui/src/lib/components/modal/modal.tsx
+++ libs/shared/ui/src/lib/components/modal/modal.tsx
@@ -89,7 +89,7 @@ export const Modal = (props: ModalProps) => {
             event.preventDefault()
           }}
           style={{ width: `${width}px` }}
-          className={`modal__content fixed top-[84px] left-1/2 bg-white rounded-md shadow-[0_0_32px_rgba(0,0,0,0.08)] z-[1] ${className}`}
+          className={`modal__content fixed top-[84px] left-1/2 bg-white rounded-md shadow-[0_0_32px_rgba(0,0,0,0.08)] ${className}`}
         >
           <div className="max-h-[80vh] overflow-auto">
             {cloneElement(children, {
