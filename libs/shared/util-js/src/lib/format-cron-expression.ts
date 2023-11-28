b726822ef1fc0f03c326f4f3364a4b334f41bceb
Carrano <acarrano@qovery.com>
2023-11-28 18:39:36 +0100

    fix

--- libs/shared/util-js/src/lib/format-cron-expression.ts
+++ libs/shared/util-js/src/lib/format-cron-expression.ts
@@ -5,9 +5,10 @@ export function formatCronExpression(expression?: string) {
   if (expression === undefined) {
     return undefined
   }
-  const res = cronstrue.toString(expression, {
-    throwExceptionOnParseError: false,
-    tzOffset: (new Date().getTimezoneOffset() / 60) * -1,
-  })
+  const res =
+    cronstrue.toString(expression, {
+      throwExceptionOnParseError: false,
+      tzOffset: (new Date().getTimezoneOffset() / 60) * -1,
+    }) + ' (browser timezone)'
   return res.indexOf('An error') === -1 ? res : undefined
 }
