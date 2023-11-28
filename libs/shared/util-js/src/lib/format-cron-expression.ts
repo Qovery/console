418edce37c34b7aa53eedd5206b7d754e7597243
Carrano <acarrano@qovery.com>
2023-11-28 18:28:46 +0100

    fix: show scheduling with local browser timezone

--- libs/shared/util-js/src/lib/format-cron-expression.ts
+++ libs/shared/util-js/src/lib/format-cron-expression.ts
@@ -5,6 +5,9 @@ export function formatCronExpression(expression?: string) {
   if (expression === undefined) {
     return undefined
   }
-  const res = cronstrue.toString(expression, { throwExceptionOnParseError: false })
+  const res = cronstrue.toString(expression, {
+    throwExceptionOnParseError: false,
+    tzOffset: (new Date().getTimezoneOffset() / 60) * -1,
+  })
   return res.indexOf('An error') === -1 ? res : undefined
 }
