1723302b1ba220c9d8f3e89b480d4d2156c1228c
Camille TJHOA <camille.tjhoa@outlook.com>
2023-11-29 17:57:34 +0100

    refactor(clusters): move logs cluster card to react-query

--- libs/pages/logs/infra/src/lib/feature/card-cluster-feature/card-cluster-feature.tsx
+++ libs/pages/logs/infra/src/lib/feature/card-cluster-feature/card-cluster-feature.tsx
@@ -1,25 +1,10 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { fetchClusterStatus, selectClusterById } from '@qovery/domains/organization'
 import { CardCluster } from '@qovery/shared/console-shared'
-import { type AppDispatch, type RootState } from '@qovery/state/store'
 
 export function CardClusterFeature() {
   const { organizationId = '', clusterId = '' } = useParams()
-  const dispatch = useDispatch<AppDispatch>()
 
-  useEffect(() => {
-    const fetchLogsAndClusterStatusByInterval = setInterval(() => {
-      dispatch(fetchClusterStatus({ organizationId, clusterId }))
-    }, 3000)
-    dispatch(fetchClusterStatus({ organizationId, clusterId }))
-    return () => clearInterval(fetchLogsAndClusterStatusByInterval)
-  }, [dispatch, organizationId, clusterId])
-
-  const cluster = useSelector((state: RootState) => selectClusterById(state, clusterId))
-
-  return <CardCluster cluster={cluster} organizationId={organizationId} />
+  return <CardCluster clusterId={clusterId} organizationId={organizationId} />
 }
 
 export default CardClusterFeature
