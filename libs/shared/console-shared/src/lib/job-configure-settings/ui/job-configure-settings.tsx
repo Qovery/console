f186dcd416bd2c90a0ec994a3046933739031be7
Carrano <acarrano@qovery.com>
2023-11-29 10:05:42 +0100

    fix

--- libs/shared/console-shared/src/lib/job-configure-settings/ui/job-configure-settings.tsx
+++ libs/shared/console-shared/src/lib/job-configure-settings/ui/job-configure-settings.tsx
@@ -50,7 +50,9 @@ export function JobConfigureSettings(props: JobConfigureSettingsProps) {
             )}
           />
           <div className="mb-3 flex justify-between">
-            <p className="text-neutral-400 text-xs">{formatCronExpression(watchSchedule)}</p>
+            <p className="text-neutral-400 text-xs">
+              {formatCronExpression(watchSchedule) ? formatCronExpression(watchSchedule) + ' (UTC)' : ''}
+            </p>
             <ExternalLink href="https://crontab.guru" size="xs" className="text-neutral-350">
               CRON expression builder
             </ExternalLink>
