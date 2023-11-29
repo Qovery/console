78dd25de352e88217f585a39f9f4fb63b971bf50
Camille TJHOA <camille.tjhoa@outlook.com>
2023-11-29 15:17:46 +0100

    feat(clusters): add data-access queries

--- libs/domains/clusters/data-access/src/lib/domains-clusters-data-access.ts
+++ libs/domains/clusters/data-access/src/lib/domains-clusters-data-access.ts
@@ -1,8 +1,33 @@
+import { createQueryKeys } from '@lukemorales/query-key-factory'
 import { type ClusterDeleteMode, ClustersApi } from 'qovery-typescript-axios'
 
 const clusterApi = new ClustersApi()
 
-export interface DeleteClusterProps {
+export const clusters = createQueryKeys('clusters', {
+  list: ({ organizationId }: { organizationId: string }) => ({
+    queryKey: [organizationId],
+    async queryFn() {
+      const response = await clusterApi.listOrganizationCluster(organizationId)
+      return response.data.results
+    },
+  }),
+  listStatuses: ({ organizationId }: { organizationId: string }) => ({
+    queryKey: [organizationId],
+    async queryFn() {
+      const response = await clusterApi.getOrganizationClusterStatus(organizationId)
+      return response.data.results
+    },
+  }),
+  status: ({ organizationId, clusterId }: { organizationId: string; clusterId: string }) => ({
+    queryKey: [organizationId, clusterId],
+    async queryFn() {
+      const response = await clusterApi.getClusterStatus(organizationId, clusterId)
+      return response.data
+    },
+  }),
+})
+
+interface DeleteClusterProps {
   organizationId: string
   clusterId: string
   clusterDeleteMode: ClusterDeleteMode
--- libs/state/util-queries/src/lib/queries/queries.ts
+++ libs/state/util-queries/src/lib/queries/queries.ts
@@ -1,4 +1,5 @@
 import { type inferQueryKeyStore, mergeQueryKeys } from '@lukemorales/query-key-factory'
+import { clusters } from '@qovery/domains/clusters/data-access'
 import { environments } from '@qovery/domains/environments/data-access'
 import { organizations } from '@qovery/domains/organizations/data-access'
 import { projects } from '@qovery/domains/projects/data-access'
@@ -7,6 +8,15 @@ import { services } from '@qovery/domains/services/data-access'
 import { variables } from '@qovery/domains/variables/data-access'
 import { user } from '@qovery/shared/iam/data-access'
 
-export const queries = mergeQueryKeys(environments, services, user, organizations, projects, variables, serviceHelm)
+export const queries = mergeQueryKeys(
+  clusters,
+  environments,
+  organizations,
+  projects,
+  serviceHelm,
+  services,
+  user,
+  variables
+)
 
 export type QueryKeys = inferQueryKeyStore<typeof queries>
