1ffc9ef70de98d4ba1e7e48495fad8d059648d77
Camille TJHOA <camille.tjhoa@outlook.com>
2023-11-30 17:01:33 +0100

    refactor(clusters): move create deployment rule to react-query

--- libs/pages/environments/src/lib/feature/page-create-deployment-rule-feature/page-create-deployment-rule-feature.spec.tsx
+++ libs/pages/environments/src/lib/feature/page-create-deployment-rule-feature/page-create-deployment-rule-feature.spec.tsx
@@ -1,6 +1,12 @@
 import { render } from '__tests__/utils/setup-jest'
-
+import { clusterFactoryMock } from '@qovery/shared/factories'
 import PageCreateDeploymentRuleFeature from './page-create-deployment-rule-feature'
+
+const mockClusters = clusterFactoryMock(3)
+jest.mock('@qovery/domains/clusters/feature', () => ({
+  ...jest.requireActual('@qovery/domains/clusters/feature'),
+  useClusters: () => ({ data: mockClusters }),
+}))
 
 describe('PageCreateDeploymentRuleFeature', () => {
   it('should render successfully', () => {
