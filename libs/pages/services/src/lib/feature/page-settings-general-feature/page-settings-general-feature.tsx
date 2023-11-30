d3d126182e29c7d1fa4ec195ab7e92059eb44937
Camille TJHOA <camille.tjhoa@outlook.com>
2023-11-30 17:43:21 +0100

    refactor(clusters): move service settings to react-query

--- libs/pages/services/src/lib/feature/page-settings-general-feature/page-settings-general-feature.tsx
+++ libs/pages/services/src/lib/feature/page-settings-general-feature/page-settings-general-feature.tsx
@@ -1,25 +1,19 @@
-import { type Cluster } from 'qovery-typescript-axios'
 import { useEffect, useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
-import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { useClusters } from '@qovery/domains/clusters/feature'
 import { useEditEnvironment, useFetchEnvironment } from '@qovery/domains/environment'
-import { fetchClusters, selectClustersEntitiesByOrganizationId } from '@qovery/domains/organization'
 import { useDocumentTitle } from '@qovery/shared/util-hooks'
-import { type AppDispatch, type RootState } from '@qovery/state/store'
 import PageSettingsGeneral from '../../ui/page-settings-general/page-settings-general'
 
 export function PageSettingsGeneralFeature() {
   const { organizationId = '', projectId = '', environmentId = '' } = useParams()
   useDocumentTitle('Environment General - Settings - Qovery')
-  const dispatch = useDispatch<AppDispatch>()
   const methods = useForm({
     mode: 'onChange',
   })
 
-  const clusters = useSelector<RootState, Cluster[]>((state) =>
-    selectClustersEntitiesByOrganizationId(state, organizationId)
-  )
+  const { data: clusters = [] } = useClusters({ organizationId })
 
   const { data: environment } = useFetchEnvironment(projectId, environmentId)
   const editEnvironment = useEditEnvironment(projectId, () => setLoading(false))
@@ -34,10 +28,6 @@ export function PageSettingsGeneralFeature() {
       editEnvironment.mutate({ environmentId, data })
     }
   })
-
-  useEffect(() => {
-    dispatch(fetchClusters({ organizationId }))
-  }, [dispatch, organizationId])
 
   useEffect(() => {
     methods.setValue('name', environment?.name)
