5a5f58d4faea7f1a600bdf18ad69ed6d4655b1b0
Rémi Bonnet <rbonnet@qovery.com>
2023-11-30 12:05:23 +0100

    feedback

--- libs/shared/routes/src/lib/sub-router/helm.router.ts
+++ libs/shared/routes/src/lib/sub-router/helm.router.ts
@@ -1,6 +1,6 @@
 export const SERVICES_HELM_CREATION_URL = '/create/helm'
 
-// subrouter for job steps /create/general /create/settings etc...
+// subrouter for helm steps /create/general /create/settings etc...
 export const SERVICES_HELM_CREATION_GENERAL_URL = '/general'
 export const SERVICES_HELM_CREATION_VALUES_STEP_1_URL = '/values-override/repository-and-yaml'
 export const SERVICES_HELM_CREATION_SUMMARY_URL = '/summary'
