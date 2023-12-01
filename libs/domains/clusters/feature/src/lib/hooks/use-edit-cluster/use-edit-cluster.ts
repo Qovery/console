31cfa5da135338d219c2d2091213bed9d6df0a0f
Camille TJHOA <camille.tjhoa@outlook.com>
2023-12-01 16:09:37 +0100

    feat(clusters): add edit cluster mutation

---
+++ libs/domains/clusters/feature/src/lib/hooks/use-edit-cluster/use-edit-cluster.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { mutations } from '@qovery/domains/clusters/data-access'
+import { queries } from '@qovery/state/util-queries'
+import { useDeployCluster } from '../use-deploy-cluster/use-deploy-cluster'
+
+export function useEditCluster() {
+  const queryClient = useQueryClient()
+  const { mutateAsync: deployCluster } = useDeployCluster()
+
+  return useMutation(mutations.editCluster, {
+    onSuccess(_, { organizationId }) {
+      queryClient.invalidateQueries({
+        queryKey: queries.clusters.list({ organizationId }).queryKey,
+      })
+    },
+    meta: {
+      notifyOnSuccess(_: unknown, variables: unknown) {
+        const { organizationId, clusterId } = variables as Parameters<typeof mutations.editCluster>[0]
+        return {
+          title: 'Cluster updated',
+          description: 'You must update to apply the settings',
+          callback() {
+            deployCluster({ organizationId, clusterId })
+          },
+          labelAction: 'Update',
+        }
+      },
+      notifyOnError: true,
+    },
+  })
+}
+
+export default useEditCluster
