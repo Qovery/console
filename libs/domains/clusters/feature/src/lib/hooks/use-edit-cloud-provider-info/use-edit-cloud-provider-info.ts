51660f95eacf53435725f80a07b69372d800b4c9
Camille TJHOA <camille.tjhoa@outlook.com>
2023-12-05 18:40:49 +0100

    feat(clusters): add silently option to useEditCloudProviderInfo

--- libs/domains/clusters/feature/src/lib/hooks/use-edit-cloud-provider-info/use-edit-cloud-provider-info.ts
+++ libs/domains/clusters/feature/src/lib/hooks/use-edit-cloud-provider-info/use-edit-cloud-provider-info.ts
@@ -3,7 +3,7 @@ import { mutations } from '@qovery/domains/clusters/data-access'
 import { queries } from '@qovery/state/util-queries'
 import { useDeployCluster } from '../use-deploy-cluster/use-deploy-cluster'
 
-export function useEditCloudProviderInfo() {
+export function useEditCloudProviderInfo({ silently = false } = {}) {
   const queryClient = useQueryClient()
   const { mutateAsync: deployCluster } = useDeployCluster()
 
@@ -13,20 +13,24 @@ export function useEditCloudProviderInfo() {
         queryKey: queries.clusters.cloudProviderInfo({ organizationId, clusterId }).queryKey,
       })
     },
-    meta: {
-      notifyOnSuccess(_: unknown, variables: unknown) {
-        const { organizationId, clusterId } = variables as Parameters<typeof mutations.editCloudProviderInfo>[0]
-        return {
-          title: 'Credentials updated',
-          description: 'You must update your cluster to apply the settings',
-          callback() {
-            deployCluster({ organizationId, clusterId })
+    ...(silently
+      ? {}
+      : {
+          meta: {
+            notifyOnSuccess(_: unknown, variables: unknown) {
+              const { organizationId, clusterId } = variables as Parameters<typeof mutations.editCloudProviderInfo>[0]
+              return {
+                title: 'Credentials updated',
+                description: 'You must update your cluster to apply the settings',
+                callback() {
+                  deployCluster({ organizationId, clusterId })
+                },
+                labelAction: 'Update cluster',
+              }
+            },
+            notifyOnError: true,
           },
-          labelAction: 'Update cluster',
-        }
-      },
-      notifyOnError: true,
-    },
+        }),
   })
 }
 
