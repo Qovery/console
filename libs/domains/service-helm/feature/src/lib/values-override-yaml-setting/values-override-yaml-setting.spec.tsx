4e4e5bd9462aec569e66222a52d644f037b14f0f
Rémi Bonnet <rbonnet@qovery.com>
2023-12-05 10:12:42 +0100

    fix: tests values yaml override

--- libs/domains/service-helm/feature/src/lib/values-override-yaml-setting/values-override-yaml-setting.spec.tsx
+++ libs/domains/service-helm/feature/src/lib/values-override-yaml-setting/values-override-yaml-setting.spec.tsx
@@ -1,10 +1,10 @@
 import { wrapWithReactHookForm } from '__tests__/utils/wrap-with-react-hook-form'
 import { renderWithProviders } from '@qovery/shared/util-tests'
-import { ValuesOverrideAsYamlSetting } from './values-override-as-yaml-setting'
+import { ValuesOverrideYamlSetting } from './values-override-yaml-setting'
 
 describe('ValuesOverrideAsYamlSetting', () => {
   it('should match snapshot', () => {
-    const { baseElement } = renderWithProviders(wrapWithReactHookForm(<ValuesOverrideAsYamlSetting />))
+    const { baseElement } = renderWithProviders(wrapWithReactHookForm(<ValuesOverrideYamlSetting />))
     expect(baseElement).toMatchSnapshot()
   })
 })
