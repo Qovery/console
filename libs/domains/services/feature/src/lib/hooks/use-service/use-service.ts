7a0668eca420a6d76697c5615675e2cce6fba4d4
Camille TJHOA <camille.tjhoa@outlook.com>
2023-12-13 14:36:22 +0100

    fixup! refactor(services): improve use-service hook with generics

--- libs/domains/services/feature/src/lib/hooks/use-service/use-service.ts
+++ libs/domains/services/feature/src/lib/hooks/use-service/use-service.ts
@@ -26,6 +26,20 @@ export type UseServiceProps =
       serviceType: ServiceType
     }
 
+/**
+ * Here we use TS function overload to allow us to call the hook in 2 different ways.
+ * The first one to get a service which you don't know the type of ahead.
+ * ```ts
+ * const { data: service } = useService({ environmentId: '123', serviceId: '456' })
+ * ```
+ * The seconde one to get a service which you already know the type of.
+ * ```ts
+ * const { data: database } = useService({ serviceId: '456', serviceType: 'DATABASE' })
+ * ```
+ *
+ * https://dev.to/zirkelc/how-to-return-different-types-from-functions-in-typescript-2a2h
+ * https://www.typescriptlang.org/docs/handbook/2/functions.html#function-overloads
+ **/
 export function useService(props: { environmentId?: string; serviceId?: string }): UseQueryResult<AnyService>
 export function useService<
   T extends ServiceType,
