230e343fefcf325cb36d3ff502c2ed3ae1069d80
Remi Bonnet <bonnet.rem@gmail.com>
2023-12-20 15:39:42 +0100

    fix: functions menus

--- libs/domains/services/feature/src/lib/service-action-toolbar/service-action-toolbar.tsx
+++ libs/domains/services/feature/src/lib/service-action-toolbar/service-action-toolbar.tsx
@@ -84,104 +84,112 @@ export function ServiceActionToolbar({ serviceId }: { serviceId: string }) {
     })
   }
 
-  const menuManageDeployment = (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <ActionToolbar.Button aria-label="Manage Deployment">
-          <Tooltip content="Manage Deployment">
-            <div className="flex items-center w-full h-full">
-              <Icon name={IconAwesomeEnum.PLAY} className="mr-3" />
-              <Icon name={IconAwesomeEnum.ANGLE_DOWN} />
-            </div>
-          </Tooltip>
-        </ActionToolbar.Button>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Content>
-        {isDeployAvailable(deploymentStatus.state) && (
-          <DropdownMenu.Item icon={<Icon name={IconAwesomeEnum.PLAY} />} onClick={mutationDeploy}>
-            Deploy
-          </DropdownMenu.Item>
-        )}
-        {isRedeployAvailable(deploymentStatus.state) && (
-          <DropdownMenu.Item icon={<Icon name={IconAwesomeEnum.ROTATE_RIGHT} />} onClick={mutationRedeploy}>
-            Redeploy
-          </DropdownMenu.Item>
-        )}
-        {isStopAvailable(deploymentStatus.state) && (
-          <DropdownMenu.Item icon={<Icon name={IconAwesomeEnum.CIRCLE_STOP} />} onClick={mutationStop}>
-            Stop
-          </DropdownMenu.Item>
-        )}
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-  )
+  function MenuManageDeployment() {
+    if (!deploymentStatus) return null
 
-  const menuOtherActions = (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <ActionToolbar.Button aria-label="Other actions">
-          <Tooltip content="Other actions">
-            <div className="flex items-center w-full h-full">
-              <Icon name={IconAwesomeEnum.ELLIPSIS_V} />
-            </div>
-          </Tooltip>
-        </ActionToolbar.Button>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Content>
-        <DropdownMenu.Item
-          icon={<Icon name={IconAwesomeEnum.SCROLL} />}
-          onClick={() =>
-            navigate(ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId) + SERVICE_LOGS_URL(serviceId))
-          }
-        >
-          Logs
-        </DropdownMenu.Item>
-        <DropdownMenu.Item
-          icon={<Icon name={IconAwesomeEnum.COPY} />}
-          onClick={() =>
-            navigate(
-              AUDIT_LOGS_PARAMS_URL(organizationId, {
-                targetId: serviceId,
-                targetType: service.serviceType,
-                projectId,
-                environmentId,
-              })
-            )
-          }
-        >
-          See audit logs
-        </DropdownMenu.Item>
-        <DropdownMenu.Item
-          icon={<Icon name={IconAwesomeEnum.CLOCK_ROTATE_LEFT} />}
-          onClick={() => copyToClipboard(copyContent)}
-        >
-          Copy identifiers
-        </DropdownMenu.Item>
-        <DropdownMenu.Item
-          icon={<Icon name={IconAwesomeEnum.WHEEL} />}
-          onClick={() =>
-            navigate(
-              `${APPLICATION_URL(organizationId, projectId, environmentId, serviceId)}${APPLICATION_SETTINGS_URL}`
-            ) + APPLICATION_SETTINGS_GENERAL_URL
-          }
-        >
-          Open settings
-        </DropdownMenu.Item>
-        {isDeleteAvailable(deploymentStatus.state) && (
-          <>
-            <DropdownMenu.Separator />
-            <DropdownMenu.Item color="red" icon={<Icon name={IconAwesomeEnum.TRASH} />} onClick={mutationDelete}>
-              Delete service
+    return (
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <ActionToolbar.Button aria-label="Manage Deployment">
+            <Tooltip content="Manage Deployment">
+              <div className="flex items-center w-full h-full">
+                <Icon name={IconAwesomeEnum.PLAY} className="mr-3" />
+                <Icon name={IconAwesomeEnum.ANGLE_DOWN} />
+              </div>
+            </Tooltip>
+          </ActionToolbar.Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          {isDeployAvailable(deploymentStatus.state) && (
+            <DropdownMenu.Item icon={<Icon name={IconAwesomeEnum.PLAY} />} onClick={mutationDeploy}>
+              Deploy
             </DropdownMenu.Item>
-          </>
-        )}
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
-  )
+          )}
+          {isRedeployAvailable(deploymentStatus.state) && (
+            <DropdownMenu.Item icon={<Icon name={IconAwesomeEnum.ROTATE_RIGHT} />} onClick={mutationRedeploy}>
+              Redeploy
+            </DropdownMenu.Item>
+          )}
+          {isStopAvailable(deploymentStatus.state) && (
+            <DropdownMenu.Item icon={<Icon name={IconAwesomeEnum.CIRCLE_STOP} />} onClick={mutationStop}>
+              Stop
+            </DropdownMenu.Item>
+          )}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    )
+  }
+
+  function MenuOtherActions() {
+    if (!service || !deploymentStatus) return null
+
+    return (
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <ActionToolbar.Button aria-label="Other actions">
+            <Tooltip content="Other actions">
+              <div className="flex items-center w-full h-full">
+                <Icon name={IconAwesomeEnum.ELLIPSIS_V} />
+              </div>
+            </Tooltip>
+          </ActionToolbar.Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item
+            icon={<Icon name={IconAwesomeEnum.SCROLL} />}
+            onClick={() =>
+              navigate(ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId) + SERVICE_LOGS_URL(serviceId))
+            }
+          >
+            Logs
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            icon={<Icon name={IconAwesomeEnum.COPY} />}
+            onClick={() =>
+              navigate(
+                AUDIT_LOGS_PARAMS_URL(organizationId, {
+                  targetId: serviceId,
+                  targetType: service.serviceType,
+                  projectId,
+                  environmentId,
+                })
+              )
+            }
+          >
+            See audit logs
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            icon={<Icon name={IconAwesomeEnum.CLOCK_ROTATE_LEFT} />}
+            onClick={() => copyToClipboard(copyContent)}
+          >
+            Copy identifiers
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            icon={<Icon name={IconAwesomeEnum.WHEEL} />}
+            onClick={() =>
+              navigate(
+                `${APPLICATION_URL(organizationId, projectId, environmentId, serviceId)}${APPLICATION_SETTINGS_URL}`
+              ) + APPLICATION_SETTINGS_GENERAL_URL
+            }
+          >
+            Open settings
+          </DropdownMenu.Item>
+          {isDeleteAvailable(deploymentStatus.state) && (
+            <>
+              <DropdownMenu.Separator />
+              <DropdownMenu.Item color="red" icon={<Icon name={IconAwesomeEnum.TRASH} />} onClick={mutationDelete}>
+                Delete service
+              </DropdownMenu.Item>
+            </>
+          )}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    )
+  }
 
   return (
     <ActionToolbar.Root>
-      {menuManageDeployment}
+      {MenuManageDeployment()}
       <Tooltip content="Logs">
         <ActionToolbar.Button
           onClick={() =>
@@ -191,7 +199,7 @@ export function ServiceActionToolbar({ serviceId }: { serviceId: string }) {
           <Icon name={IconAwesomeEnum.SCROLL} />
         </ActionToolbar.Button>
       </Tooltip>
-      {menuOtherActions}
+      {MenuOtherActions()}
     </ActionToolbar.Root>
   )
 }
