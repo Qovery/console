5d93b7559c5ccb1b3115aeb2b5e0542677869c6c
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-02 15:38:47 +0100

    fix(services): fix .env file import with secret
    Boolean value aren't trimmable so we must add fallback

--- libs/pages/application/src/lib/feature/import-environment-variable-modal-feature/utils/handle-submit.tsx
+++ libs/pages/application/src/lib/feature/import-environment-variable-modal-feature/utils/handle-submit.tsx
@@ -8,7 +8,7 @@ export function formatData(data: { [key: string]: string }, keys: string[]) {
         name: data[key + '_key'].trim(),
         value: data[key + '_value'].trim(),
         scope: data[key + '_scope'].trim() as APIVariableScopeEnum,
-        is_secret: data[key + '_secret'] ? JSON.parse(data[key + '_secret'].trim()) : false,
+        is_secret: data[key + '_secret'] ? JSON.parse(data[key + '_secret'].trim?.() ?? data[key + '_secret']) : false,
       })
     })
   }
