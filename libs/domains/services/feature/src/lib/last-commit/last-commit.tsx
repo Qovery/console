51d0e85d4960d9b43a57a84b8e7591823fed674f
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-15 10:45:55 +0100

    refactor(services): use new CopyToClipboard component

--- libs/domains/services/feature/src/lib/last-commit/last-commit.tsx
+++ libs/domains/services/feature/src/lib/last-commit/last-commit.tsx
@@ -1,8 +1,6 @@
 import { type ApplicationGitRepository } from 'qovery-typescript-axios'
-import { useState } from 'react'
 import { type ServiceType } from '@qovery/domains/services/data-access'
-import { Button, Icon, IconAwesomeEnum, Tooltip, Truncate } from '@qovery/shared/ui'
-import { useCopyToClipboard } from '@qovery/shared/util-hooks'
+import { Button, CopyToClipboard, Icon, IconAwesomeEnum, Tooltip, Truncate } from '@qovery/shared/ui'
 import { twMerge } from '@qovery/shared/util-js'
 import { useLastDeployedCommit } from '../hooks/use-last-deployed-commit/use-last-deployed-commit'
 
@@ -14,36 +12,15 @@ export interface LastCommitProps {
 }
 
 export function LastCommit({ className, gitRepository, serviceId, serviceType }: LastCommitProps) {
-  const [, copyToClipboard] = useCopyToClipboard()
-  const [copied, setCopied] = useState(false)
   const {
     data: { deployedCommit, delta },
   } = useLastDeployedCommit({ gitRepository, serviceId, serviceType })
-
-  const onClickCopyToClipboard = (content: string) => {
-    copyToClipboard(content)
-    setCopied(true)
-    setTimeout(() => {
-      setCopied(false)
-    }, 1000)
-  }
 
   if (deployedCommit === undefined) {
     return null
   }
 
-  return copied ? (
-    <Button
-      type="button"
-      variant="solid"
-      color="green"
-      size="xs"
-      className="shrink-0 border border-green-500 max-w-max"
-    >
-      <Icon name={IconAwesomeEnum.CHECK} className="mr-1" />
-      <span>Copied</span>
-    </Button>
-  ) : (
+  return (
     <Tooltip
       content={
         <>
@@ -55,24 +32,25 @@ export function LastCommit({ className, gitRepository, serviceId, serviceType }:
       }
     >
       <span className="relative">
-        <Button
-          type="button"
-          variant="surface"
-          color="neutral"
-          size="xs"
-          className={twMerge(`group justify-between ${delta > 0 ? 'w-[98px] pr-[26px]' : 'gap-1'}`, className)}
-          onClick={() => onClickCopyToClipboard(deployedCommit.git_commit_id)}
-        >
-          <Icon name={IconAwesomeEnum.COPY} className="hidden group-hover:inline w-4" />
-          <Icon name={IconAwesomeEnum.CODE_COMMIT} className="group-hover:hidden w-4" />
-          {deployedCommit.git_commit_id.substring(0, 7)}
-          {delta > 0 ? (
-            // TODO: improve inset
-            <span className="absolute right-0 inset-y-0 bg-orange-500 text-white px-1 rounded-tr rounded-br flex items-center min-w-[22px] justify-center">
-              {delta}
-            </span>
-          ) : null}
-        </Button>
+        <CopyToClipboard text={deployedCommit.git_commit_id}>
+          <Button
+            type="button"
+            variant="surface"
+            color="neutral"
+            size="xs"
+            className={twMerge(`group justify-between ${delta > 0 ? 'w-[98px] pr-[26px]' : 'gap-1'}`, className)}
+          >
+            <Icon name={IconAwesomeEnum.COPY} className="hidden group-hover:inline w-4" />
+            <Icon name={IconAwesomeEnum.CODE_COMMIT} className="group-hover:hidden w-4" />
+            {deployedCommit.git_commit_id.substring(0, 7)}
+            {delta > 0 ? (
+              // TODO: improve inset
+              <span className="absolute right-0 inset-y-0 bg-orange-500 text-white px-1 rounded-tr rounded-br flex items-center min-w-[22px] justify-center">
+                {delta}
+              </span>
+            ) : null}
+          </Button>
+        </CopyToClipboard>
       </span>
     </Tooltip>
   )
