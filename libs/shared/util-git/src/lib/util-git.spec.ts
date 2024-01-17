0d576712321411051098453dffda25f5118c1644
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-17 11:10:05 +0100

    feat(services): add repo w/ branch link (#1130)

--- libs/shared/util-git/src/lib/util-git.spec.ts
+++ libs/shared/util-git/src/lib/util-git.spec.ts
@@ -1,4 +1,4 @@
-import { getGitTokenValue, guessGitProvider } from './util-git'
+import { buildGitProviderUrl, getGitTokenValue, guessGitProvider } from './util-git'
 
 describe('util-git', () => {
   it('should parse a valid token value', () => {
@@ -26,5 +26,26 @@ describe('util-git', () => {
   it('should return BITBUCKET for a valid Bitbucket URL', () => {
     const url = 'https://bitbucket.org/user/my-repo'
     expect(guessGitProvider(url)).toBe('BITBUCKET')
+  })
+
+  it('should return branch url for a valid Github URL', () => {
+    const url = 'https://github.com/user/my-repo'
+    expect(buildGitProviderUrl(url, 'my_branch')).toBe('https://github.com/user/my-repo/tree/my_branch/')
+    const url2 = 'https://github.com/user/my-repo.git'
+    expect(buildGitProviderUrl(url2, 'my_branch')).toBe('https://github.com/user/my-repo/tree/my_branch/')
+  })
+
+  it('should return branch url for a valid Gitlab URL', () => {
+    const url = 'https://gitlab.com/user/my-repo'
+    expect(buildGitProviderUrl(url, 'my_branch')).toBe('https://gitlab.com/user/my-repo/tree/my_branch/')
+    const url2 = 'https://gitlab.com/user/my-repo.git'
+    expect(buildGitProviderUrl(url2, 'my_branch')).toBe('https://gitlab.com/user/my-repo/tree/my_branch/')
+  })
+
+  it('should return branch url for a valid Bitbucket URL', () => {
+    const url = 'https://bitbucket.org/user/my-repo'
+    expect(buildGitProviderUrl(url, 'my_branch')).toBe('https://bitbucket.org/user/my-repo/src/my_branch/')
+    const url2 = 'https://bitbucket.org/user/my-repo.git'
+    expect(buildGitProviderUrl(url2, 'my_branch')).toBe('https://bitbucket.org/user/my-repo/src/my_branch/')
   })
 })
--- libs/shared/util-git/src/lib/util-git.ts
+++ libs/shared/util-git/src/lib/util-git.ts
@@ -1,4 +1,5 @@
 import { type GitProviderEnum } from 'qovery-typescript-axios'
+import { match } from 'ts-pattern'
 
 export const getGitTokenValue = (value: string) => {
   if (value?.includes('TOKEN')) return { type: value.split('_')[1] as GitProviderEnum, id: value.split('_')[2] }
@@ -19,4 +20,15 @@ export const guessGitProvider = (url: string) => {
   }
 
   return undefined
+}
+
+export function buildGitProviderUrl(url: string, branch?: string) {
+  const gitProvider = guessGitProvider(url)
+  if (!gitProvider || !branch) {
+    return url.trim().replace(/.git$/, '')
+  }
+  return match(gitProvider)
+    .with('BITBUCKET', () => `${url.trim().replace(/.git$/, '')}/src/${branch.trim()}/`)
+    .with('GITHUB', 'GITLAB', () => `${url.trim().replace(/.git$/, '')}/tree/${branch.trim()}/`)
+    .exhaustive()
 }
