4a04400851f62400699aa2d03c00427f628d6b42
Remi Bonnet <bonnet.rem@gmail.com>
2024-01-17 10:19:07 +0100

    feedback

--- libs/domains/services/feature/src/lib/timezone-setting/timezone-setting.tsx
+++ libs/domains/services/feature/src/lib/timezone-setting/timezone-setting.tsx
@@ -1,40 +1,34 @@
 import { Controller, useFormContext } from 'react-hook-form'
-import { InputSelect, LoaderSpinner } from '@qovery/shared/ui'
+import { InputSelect } from '@qovery/shared/ui'
 import { useListTimezone } from '../hooks/use-list-timezone/use-list-timezone'
 
 export function TimezoneSetting({ className = '' }: { className?: string }) {
   const { control } = useFormContext()
-  const { data = [], isLoading } = useListTimezone()
+  const { data = [] } = useListTimezone()
 
   return (
     <div className={className}>
-      {isLoading ? (
-        <div className="py-2">
-          <LoaderSpinner className="w-5" />
-        </div>
-      ) : (
-        <Controller
-          name="timezone"
-          control={control}
-          defaultValue="Etc/UTC"
-          rules={{
-            required: 'Please select a timezone.',
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <InputSelect
-              isSearchable
-              label="Timezone"
-              onChange={field.onChange}
-              options={data.map((tz) => ({
-                label: tz,
-                value: tz,
-              }))}
-              value={field.value}
-              error={error?.message}
-            />
-          )}
-        />
-      )}
+      <Controller
+        name="timezone"
+        control={control}
+        defaultValue="Etc/UTC"
+        rules={{
+          required: 'Please select a timezone.',
+        }}
+        render={({ field, fieldState: { error } }) => (
+          <InputSelect
+            isSearchable
+            label="Timezone"
+            onChange={field.onChange}
+            options={data.map((tz) => ({
+              label: tz,
+              value: tz,
+            }))}
+            value={field.value}
+            error={error?.message}
+          />
+        )}
+      />
     </div>
   )
 }
