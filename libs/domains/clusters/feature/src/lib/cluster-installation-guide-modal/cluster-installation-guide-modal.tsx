175ac90ed2f0fb1ea503aaf130633dd4da83420e
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-23 12:23:40 +0100

    feat(clusters): revamp installation guide modal

--- libs/domains/clusters/feature/src/lib/cluster-installation-guide-modal/cluster-installation-guide-modal.tsx
+++ libs/domains/clusters/feature/src/lib/cluster-installation-guide-modal/cluster-installation-guide-modal.tsx
@@ -23,24 +23,26 @@ export function ClusterInstallationGuideModal({
     <div className="p-6 flex flex-col gap-6">
       <h2 className="h4 text-neutral-400">Installation guide</h2>
 
-      <ol className="ml-4 list-outside list-decimal" type="1">
+      <ol className="ml-4 list-outside list-decimal text-neutral-400" type="1">
         <li className="mb-6 text-sm font-medium">
           <span>Save the following yaml, it contains the Qovery configuration assigned to your cluster.</span>
           <br />
           <span className="inline-block mt-2">
             <Button
-              size="lg"
-              variant="outline"
-              color="neutral"
+              size="xs"
+              variant="solid"
+              color="brand"
               onClick={downloadInstallationValues}
               loading={isLoading}
+              className="gap-1"
             >
-              Click here to download
+              Download configuration
+              <Icon name={IconAwesomeEnum.DOWNLOAD} />
             </Button>
           </span>
         </li>
         <li className="text-sm font-medium">
-          <span>Follow the instruction described in this documentation</span>
+          <span>Make sure you meet the requirements described here</span>
           <br />
           <ExternalLink
             className="mt-2"
@@ -55,7 +57,7 @@ export function ClusterInstallationGuideModal({
         <Callout.Icon>
           <Icon name={IconAwesomeEnum.CIRCLE_INFO} />
         </Callout.Icon>
-        <Callout.Text>
+        <Callout.Text className="text-xs">
           Note: You can access again this installation guide and the configuration file by opening the “Installation
           guide” section from the cluster menu
         </Callout.Text>
