720c6d402db88d86a03c303d1ced4aa9ad67f137
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-25 15:37:27 +0100

    feat(environments): add deploy all services mutation

--- libs/domains/environments/data-access/src/lib/domains-environments-data-access.ts
+++ libs/domains/environments/data-access/src/lib/domains-environments-data-access.ts
@@ -2,6 +2,7 @@ import { createQueryKeys, type inferQueryKeys } from '@lukemorales/query-key-fac
 import {
   type CloneEnvironmentRequest,
   type CreateEnvironmentRequest,
+  type DeployAllRequest,
   EnvironmentActionsApi,
   EnvironmentExportApi,
   EnvironmentMainCallsApi,
@@ -64,6 +65,10 @@ export const mutations = {
   },
   async deployEnvironment({ environmentId }: { environmentId: string }) {
     const result = await environmentActionApi.deployEnvironment(environmentId)
+    return result.data
+  },
+  async deployAllServices({ environmentId, payload }: { environmentId: string; payload: DeployAllRequest }) {
+    const result = await environmentActionApi.deployAllServices(environmentId, payload)
     return result.data
   },
   async stopEnvironment({ environmentId }: { environmentId: string }) {
