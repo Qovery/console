18998ee640b03a054dff2418cabf71bfaedf7283
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-26 09:55:25 +0100

    feat(environments): replace action toolbar

--- libs/domains/environments/feature/src/index.ts
+++ libs/domains/environments/feature/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/environment-deployment-status-label/environment-deployment-
 export * from './lib/environment-mode/environment-mode'
 export * from './lib/environment-state-chip/environment-state-chip'
 export * from './lib/terraform-export-modal/terraform-export-modal'
+export * from './lib/environment-action-toolbar/environment-action-toolbar'
 export * from './lib/hooks/use-deployment-status/use-deployment-status'
 export * from './lib/hooks/use-environment/use-environment'
 export * from './lib/hooks/use-list-statuses/use-list-statuses'
