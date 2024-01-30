00d299bf280602dd168c3e50a4364970a55f3535
Camille TJHOA <camille.tjhoa@outlook.com>
2024-01-30 19:09:42 +0100

    feat(environments): add info to useEnvironments

--- libs/domains/environments/feature/src/lib/hooks/use-environments/use-environments.ts
+++ libs/domains/environments/feature/src/lib/hooks/use-environments/use-environments.ts
@@ -1,4 +1,7 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQueries, useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import { P, match } from 'ts-pattern'
+import { upperCaseFirstLetter } from '@qovery/shared/util-js'
 import { queries } from '@qovery/state/util-queries'
 
 export interface UseEnvironmentsProps {
@@ -6,9 +9,76 @@ export interface UseEnvironmentsProps {
 }
 
 export function useEnvironments({ projectId }: UseEnvironmentsProps) {
-  return useQuery({
+  const { data: environments, isLoading: isEnvironmentsLoading } = useQuery({
     ...queries.environments.list({ projectId }),
   })
+
+  const runningStatusResults = useQueries({
+    queries: (environments ?? []).map(({ id }) => ({
+      ...queries.environments.runningStatus(id),
+    })),
+  })
+
+  const deploymentStatusResults = useQueries({
+    queries: (environments ?? []).map(({ id }) => ({
+      ...queries.environments.deploymentStatus(id),
+    })),
+  })
+
+  const data = useMemo(
+    () =>
+      (environments ?? []).map((environment, index) => {
+        const runningStatus = runningStatusResults[index].data
+        const deploymentStatus = deploymentStatusResults[index].data
+
+        const runningStatusLabel = upperCaseFirstLetter(runningStatus?.state.replace('_', ' ') ?? 'STOPPED')
+        const deploymentStatusLabel = upperCaseFirstLetter(
+          (deploymentStatus?.state === 'READY' ? 'NEVER_DEPLOYED' : deploymentStatus?.state)?.replace('_', ' ') ??
+            'STOPPED'
+        )
+
+        const runningStatusOverride = match({ runningStatus })
+          .with({ runningStatus: P.nullish }, () => ({
+            state: undefined,
+            stateLabel: 'Stopped',
+          }))
+          .with({ runningStatus: P.not(P.nullish) }, ({ runningStatus }) => ({
+            ...runningStatus,
+            stateLabel: runningStatusLabel,
+          }))
+          .exhaustive()
+
+        return {
+          ...environment,
+          runningStatus: runningStatusOverride,
+          ...(deploymentStatus
+            ? {
+                deploymentStatus: {
+                  ...deploymentStatus,
+                  stateLabel: deploymentStatusLabel,
+                },
+              }
+            : {}),
+        }
+      }),
+    [
+      environments,
+      // https://github.com/TanStack/query/issues/5137
+      // As we currently use tanstack query V4, we cannot use combine to avoid infinite renders
+      // So we need to use service "state" to memoize data for usage like tanstack table.
+      // As useMemo cannot have variable params length, we need JSON.stringify to not be bound by the services length
+      JSON.stringify([
+        ...runningStatusResults.map(({ data }) => data?.state),
+        ...deploymentStatusResults.map(({ data }) => data?.state),
+      ]),
+    ]
+  )
+
+  return {
+    data,
+    isLoading:
+      [...runningStatusResults, ...deploymentStatusResults].some(({ isLoading }) => isLoading) || isEnvironmentsLoading,
+  }
 }
 
 export default useEnvironments
