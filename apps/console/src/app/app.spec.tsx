ef2416d2f3a925815afd347b5777dad2a0ba5940
Remi Bonnet <bonnet.rem@gmail.com>
2024-02-14 13:16:59 +0100

    fix: test

--- apps/console/src/app/app.spec.tsx
+++ apps/console/src/app/app.spec.tsx
@@ -2,6 +2,7 @@ import { type Organization } from 'qovery-typescript-axios'
 import { IntercomProvider } from 'react-use-intercom'
 import * as organizationsDomain from '@qovery/domains/organizations/feature'
 import { organizationFactoryMock } from '@qovery/shared/factories'
+import { MyHistoryProvider } from '@qovery/shared/util-hooks'
 import { renderWithProviders } from '@qovery/shared/util-tests'
 import App from './app'
 
@@ -21,9 +22,11 @@ describe('App', () => {
     window.scrollTo = jest.fn()
 
     const { baseElement } = renderWithProviders(
-      <IntercomProvider appId="__test__app__id__" autoBoot={false}>
-        <App />
-      </IntercomProvider>
+      <MyHistoryProvider>
+        <IntercomProvider appId="__test__app__id__" autoBoot={false}>
+          <App />
+        </IntercomProvider>
+      </MyHistoryProvider>
     )
 
     expect(baseElement).toBeTruthy()
