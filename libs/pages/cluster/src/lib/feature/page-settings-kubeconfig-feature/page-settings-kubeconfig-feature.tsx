bb2893e8bced1be5907a451c6b3a5cd53d72a414
Camille TJHOA <camille.tjhoa@outlook.com>
2024-02-16 10:14:40 +0100

    feat(clusters): use the right edit kubeconfig endpoint

--- libs/pages/cluster/src/lib/feature/page-settings-kubeconfig-feature/page-settings-kubeconfig-feature.tsx
+++ libs/pages/cluster/src/lib/feature/page-settings-kubeconfig-feature/page-settings-kubeconfig-feature.tsx
@@ -1,22 +1,19 @@
 import { type Cluster } from 'qovery-typescript-axios'
 import { useParams } from 'react-router-dom'
-import { useCluster, useEditCluster } from '@qovery/domains/clusters/feature'
+import { useCluster, useEditClusterKubeconfig } from '@qovery/domains/clusters/feature'
 import { PageSettingsKubeconfig } from '../../ui/page-settings-kubeconfig/page-settings-kubeconfig'
 
 export function PageSettingsKubeconfigFeature() {
   const { organizationId = '', clusterId = '' } = useParams()
 
   const { data: cluster } = useCluster({ organizationId, clusterId })
-  const { mutate: editCluster } = useEditCluster()
+  const { mutate: editClusterKubeconfig } = useEditClusterKubeconfig()
 
   const onSubmit = (cluster: Cluster, kubeconfig: string) => {
-    editCluster({
+    editClusterKubeconfig({
       clusterId: cluster.id,
       organizationId: organizationId,
-      clusterRequest: {
-        ...cluster,
-        kubeconfig,
-      },
+      payload: kubeconfig,
     })
   }
 
