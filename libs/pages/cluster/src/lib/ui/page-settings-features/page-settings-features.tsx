2f8214928ad6ad1f97458007f6d37b65ba842db9
Remi Bonnet <bonnet.rem@gmail.com>
2024-02-26 19:30:46 +0100

    feat(cluster-detail): feature VPC

--- libs/pages/cluster/src/lib/ui/page-settings-features/page-settings-features.tsx
+++ libs/pages/cluster/src/lib/ui/page-settings-features/page-settings-features.tsx
@@ -1,6 +1,7 @@
 import { type CloudProviderEnum, type ClusterFeature } from 'qovery-typescript-axios'
 import { CardClusterFeature } from '@qovery/shared/console-shared'
-import { BlockContent, Heading, HelpSection, LoaderSpinner, Section } from '@qovery/shared/ui'
+import { IconEnum } from '@qovery/shared/enums'
+import { BlockContent, Heading, HelpSection, Icon, InputText, LoaderSpinner, Section } from '@qovery/shared/ui'
 
 export interface PageSettingsFeaturesProps {
   loading: boolean
@@ -15,6 +16,104 @@ export function PageSettingsFeatures(props: PageSettingsFeaturesProps) {
     <div className="flex flex-col justify-between w-full">
       <Section className="p-8 max-w-content-with-navigation-left">
         <Heading className="mb-8">Features</Heading>
+        <div className="flex flex-col justify-between p-4 rounded border bg-neutral-100 border-neutral-250">
+          <h4 className="text-neutral-400 text-sm font-medium mb-1">Deploy on an existing VPC</h4>
+          <p className="text-neutral-350 text-sm mb-4">In your VPC settings, you have enable the DNS hostnames.</p>
+          <InputText name="vpc_id" label="VPC ID" value="subnet-06aa1f94d55e81677" className="mb-4" disabled />
+          <ul className="grid grid-cols-3 gap-4">
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">
+                <Icon name={IconEnum.HELM} width="16" className="mr-2" />
+                EKS subnet IDs A:
+              </span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone B:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone C:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">
+                <Icon name={IconEnum.MONGODB} width="16" className="mr-2" />
+                MongoDB subnet IDs A:
+              </span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone B:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone C:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">
+                <Icon name={IconEnum.MYSQL} width="16" className="mr-2" />
+                MySQL subnet IDs A:
+              </span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone B:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone C:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">
+                <Icon name={IconEnum.POSTGRESQL} width="16" className="mr-2" />
+                PostgreSQL subnet IDs A:
+              </span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone B:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone C:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">
+                <Icon name={IconEnum.REDIS} width="16" className="mr-2" />
+                Redis subnet IDs A:
+              </span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone B:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+            <li className="flex flex-col text-neutral-400 text-xs gap-1">
+              <span className="flex items-center font-medium">zone C:</span>
+              <span>subnet-06aa1f94d55e81677</span>
+              <span>subnet-06aa1f94d55e81677</span>
+            </li>
+          </ul>
+        </div>
+
         <BlockContent title="Configured features" classNameContent="p-0">
           {loading && (
             <div className="flex justify-center p-5">
