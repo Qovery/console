277a24604e119b7c7deb4a8b1da401e1ea4d3506
Rémi Bonnet <bonnet.rem@gmail.com>
2024-02-28 16:48:26 +0100

    feat(cluster-detail): feature VPC (#1204)

--- libs/pages/cluster/src/lib/ui/page-settings-features/page-settings-features.spec.tsx
+++ libs/pages/cluster/src/lib/ui/page-settings-features/page-settings-features.spec.tsx
@@ -1,6 +1,6 @@
-import { render } from '__tests__/utils/setup-jest'
-import { CloudProviderEnum } from 'qovery-typescript-axios'
+import { CloudProviderEnum, type ClusterFeature } from 'qovery-typescript-axios'
 import { clusterFactoryMock } from '@qovery/shared/factories'
+import { renderWithProviders, screen } from '@qovery/shared/util-tests'
 import PageSettingsFeatures, { type PageSettingsFeaturesProps } from './page-settings-features'
 
 const cluster = clusterFactoryMock(1)[0]
@@ -13,14 +13,14 @@ describe('PageSettingsFeatures', () => {
   }
 
   it('should render successfully', () => {
-    const { baseElement } = render(<PageSettingsFeatures {...props} />)
+    const { baseElement } = renderWithProviders(<PageSettingsFeatures {...props} />)
     expect(baseElement).toBeTruthy()
   })
 
   it('should render a loader spinner', () => {
     props.loading = true
-    const { getByTestId } = render(<PageSettingsFeatures {...props} />)
-    getByTestId('spinner')
+    renderWithProviders(<PageSettingsFeatures {...props} />)
+    screen.getByTestId('spinner')
   })
 
   it('should render a list of features', () => {
@@ -40,8 +40,45 @@ describe('PageSettingsFeatures', () => {
       },
     ]
 
-    const { getAllByTestId } = render(<PageSettingsFeatures {...props} />)
+    renderWithProviders(<PageSettingsFeatures {...props} />)
+    screen.getAllByTestId('feature')
+  })
 
-    getAllByTestId('feature')
+  it('renders with existing VPC data', () => {
+    const mockFeatures: ClusterFeature[] = [
+      {
+        value: {
+          aws_vpc_eks_id: 'vpc-id',
+          eks_subnets_zone_a_ids: ['subnet-eks-zone-a'],
+          eks_subnets_zone_b_ids: ['subnet-eks-zone-b'],
+          eks_subnets_zone_c_ids: ['subnet-eks-zone-c'],
+          documentdb_subnets_zone_a_ids: ['subnet-documentdb-zone-a'],
+          documentdb_subnets_zone_b_ids: ['subnet-documentdb-zone-b'],
+          documentdb_subnets_zone_c_ids: ['subnet-documentdb-zone-c'],
+          elasticache_subnets_zone_a_ids: ['subnet-elasticache-zone-a'],
+          elasticache_subnets_zone_b_ids: ['subnet-elasticache-zone-b'],
+          elasticache_subnets_zone_c_ids: ['subnet-elasticache-zone-c'],
+          rds_subnets_zone_a_ids: ['subnet-rds-zone-a'],
+          rds_subnets_zone_b_ids: ['subnet-rds-zone-b'],
+          rds_subnets_zone_c_ids: ['subnet-rds-zone-c'],
+        },
+        id: 'EXISTING_VPC',
+        accepted_values: [],
+        title: 'Install cluster on an existing VPC',
+        description:
+          'Install a cluster on an existing VPC. You are responsible to create all the network plan and get the routing right on your own.',
+        cost_per_month_in_cents: 0,
+        cost_per_month: 0,
+        is_value_updatable: false,
+      },
+    ]
+
+    renderWithProviders(<PageSettingsFeatures loading={false} features={mockFeatures} cloudProvider="AWS" />)
+
+    expect(screen.getByText('Deploy on an existing VPC')).toBeInTheDocument()
+    expect(screen.getByText('EKS subnet IDs')).toBeInTheDocument()
+    expect(screen.getByText('MongoDB subnet IDs')).toBeInTheDocument()
+    expect(screen.getByText('Redis subnet IDs')).toBeInTheDocument()
+    expect(screen.getByText('MySQL/PostgreSQL subnet IDs')).toBeInTheDocument()
   })
 })
--- libs/pages/cluster/src/lib/ui/page-settings-features/page-settings-features.tsx
+++ libs/pages/cluster/src/lib/ui/page-settings-features/page-settings-features.tsx
@@ -1,6 +1,7 @@
-import { type CloudProviderEnum, type ClusterFeature } from 'qovery-typescript-axios'
+import { type CloudProviderEnum, type ClusterFeature, type ClusterFeatureAwsExistingVpc } from 'qovery-typescript-axios'
 import { CardClusterFeature } from '@qovery/shared/console-shared'
-import { BlockContent, Heading, HelpSection, LoaderSpinner, Section } from '@qovery/shared/ui'
+import { IconEnum } from '@qovery/shared/enums'
+import { BlockContent, Heading, HelpSection, Icon, InputText, LoaderSpinner, Section } from '@qovery/shared/ui'
 
 export interface PageSettingsFeaturesProps {
   loading: boolean
@@ -8,28 +9,103 @@ export interface PageSettingsFeaturesProps {
   cloudProvider?: CloudProviderEnum
 }
 
+function Subnets({ title, value }: { title: string; value?: string[] | null }) {
+  return (
+    <li className="flex flex-col text-xs gap-1">
+      <span className="font-medium">{title}</span>
+      {value ? (
+        value.map((subnetId: string, index: number) => <span key={index}>{subnetId}</span>)
+      ) : (
+        <span>Not defined</span>
+      )}
+    </li>
+  )
+}
+
 export function PageSettingsFeatures(props: PageSettingsFeaturesProps) {
   const { loading, features, cloudProvider } = props
+
+  const featureExistingVpc = features?.find(({ id }) => id === 'EXISTING_VPC')
+  const featureExistingVpcValue = featureExistingVpc?.value as ClusterFeatureAwsExistingVpc
 
   return (
     <div className="flex flex-col justify-between w-full">
       <Section className="p-8 max-w-content-with-navigation-left">
         <Heading className="mb-8">Features</Heading>
-        <BlockContent title="Configured features" classNameContent="p-0">
-          {loading && (
-            <div className="flex justify-center p-5">
-              <LoaderSpinner className="w-4" />
-            </div>
-          )}
-          {
-            // TODO: hide existing VPC feature for now
-            features
+
+        {!loading && featureExistingVpcValue && (
+          <div className="flex flex-col justify-between p-4 rounded border bg-neutral-100 border-neutral-250 mb-5">
+            <h4 className="text-neutral-400 text-sm font-medium mb-1">Deploy on an existing VPC</h4>
+            <p className="text-neutral-350 text-sm mb-4">In your VPC settings, you have enable the DNS hostnames.</p>
+            <InputText
+              name="vpc_id"
+              label="VPC ID"
+              value={featureExistingVpcValue.aws_vpc_eks_id}
+              className="mb-4"
+              disabled
+            />
+            <ul className="grid gap-4">
+              <li className="text-neutral-400">
+                <span className="flex items-center font-medium text-xs mb-1">
+                  <Icon name={IconEnum.EKS} width="16" className="mr-2" />
+                  EKS subnet IDs
+                </span>
+                <ul className="grid grid-cols-3 gap-4">
+                  <Subnets title="Zone A:" value={featureExistingVpcValue.eks_subnets_zone_a_ids} />
+                  <Subnets title="Zone B:" value={featureExistingVpcValue.eks_subnets_zone_b_ids} />
+                  <Subnets title="Zone C:" value={featureExistingVpcValue.eks_subnets_zone_c_ids} />
+                </ul>
+              </li>
+              <li className="text-neutral-400">
+                <span className="flex items-center font-medium text-xs mb-1">
+                  <Icon name={IconEnum.MONGODB} width="16" className="mr-2" />
+                  MongoDB subnet IDs
+                </span>
+                <ul className="grid grid-cols-3 gap-4">
+                  <Subnets title="Zone A:" value={featureExistingVpcValue.documentdb_subnets_zone_a_ids} />
+                  <Subnets title="Zone B:" value={featureExistingVpcValue.documentdb_subnets_zone_b_ids} />
+                  <Subnets title="Zone C:" value={featureExistingVpcValue.documentdb_subnets_zone_c_ids} />
+                </ul>
+              </li>
+
+              <li className="text-neutral-400">
+                <span className="flex items-center font-medium text-xs mb-1">
+                  <Icon name={IconEnum.REDIS} width="16" className="mr-2" />
+                  Redis subnet IDs
+                </span>
+                <ul className="grid grid-cols-3 gap-4">
+                  <Subnets title="Zone A:" value={featureExistingVpcValue.elasticache_subnets_zone_a_ids} />
+                  <Subnets title="Zone B:" value={featureExistingVpcValue.elasticache_subnets_zone_b_ids} />
+                  <Subnets title="Zone C:" value={featureExistingVpcValue.elasticache_subnets_zone_c_ids} />
+                </ul>
+              </li>
+
+              <li className="text-neutral-400">
+                <span className="flex items-center font-medium text-xs mb-1">MySQL/PostgreSQL subnet IDs</span>
+                <ul className="grid grid-cols-3 gap-4">
+                  <Subnets title="Zone A:" value={featureExistingVpcValue.rds_subnets_zone_a_ids} />
+                  <Subnets title="Zone B:" value={featureExistingVpcValue.rds_subnets_zone_b_ids} />
+                  <Subnets title="Zone C:" value={featureExistingVpcValue.rds_subnets_zone_c_ids} />
+                </ul>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {!featureExistingVpc?.value && (
+          <BlockContent title="Configured features" classNameContent="p-0">
+            {loading && (
+              <div className="flex justify-center p-5">
+                <LoaderSpinner className="w-4" />
+              </div>
+            )}
+            {features
               ?.filter(({ id }) => id !== 'EXISTING_VPC')
               .map((feature: ClusterFeature) => (
                 <CardClusterFeature key={feature.id} feature={feature} cloudProvider={cloudProvider} disabled />
-              ))
-          }
-        </BlockContent>
+              ))}
+          </BlockContent>
+        )}
       </Section>
       <HelpSection
         description="Need help? You may find these links useful"
