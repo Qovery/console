d6993319c295addd21a729e5ee9382aec3c0a923
Camille TJHOA <camille.tjhoa@outlook.com>
2024-03-05 17:41:51 +0100

    feat(environments): remove deadcode (#1229)

--- libs/domains/environment/src/lib/slices/environment.actions.ts
+++ libs/domains/environment/src/lib/slices/environment.actions.ts
@@ -1,10 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import {
-  type DeployAllRequest,
-  type Environment,
-  EnvironmentActionsApi,
-  EnvironmentMainCallsApi,
-} from 'qovery-typescript-axios'
+import { type Environment, EnvironmentActionsApi, EnvironmentMainCallsApi } from 'qovery-typescript-axios'
 
 const environmentActionApi = new EnvironmentActionsApi()
 const environmentMainCallsApi = new EnvironmentMainCallsApi()
@@ -46,109 +41,6 @@ export const useActionRedeployEnvironment = (
     }
   )
 }
-
-/*
- * @deprecated use `useDeployEnvironment` from `@qovery/domains/environments/feature` instead of `useActionRedeployEnvironment`
- */
-export const useActionDeployEnvironment = (
-  projectId: string,
-  environmentId: string,
-  withDeployments?: boolean,
-  onSettledCallback?: () => void,
-  callback?: () => void
-) => {
-  const queryClient = useQueryClient()
-
-  return useMutation(
-    async () => {
-      const response = await environmentActionApi.deployEnvironment(environmentId)
-      return response.data
-    },
-    {
-      onSuccess: () => {
-        // refetch environmentsStatus requests
-        queryClient.invalidateQueries(['environmentsStatus', projectId])
-
-        if (withDeployments)
-          queryClient.invalidateQueries(['project', projectId, 'environments', environmentId, 'deploymentHistory'])
-      },
-      onSettled: () => onSettledCallback && onSettledCallback(),
-      meta: {
-        notifyOnSuccess: {
-          title: 'Your environment is deploying',
-          labelAction: 'See Deployment Logs',
-          callback: () => callback && callback(),
-        },
-        notifyOnError: true,
-      },
-    }
-  )
-}
-
-export const useActionDeployAllEnvironment = (
-  environmentId: string,
-  onSuccessCallback?: () => void,
-  callback?: () => void
-) => {
-  return useMutation(
-    async (deployRequest?: DeployAllRequest) => {
-      const response = await environmentActionApi.deployAllServices(environmentId, deployRequest)
-      return response.data
-    },
-    {
-      onSuccess: () => {
-        onSuccessCallback && onSuccessCallback()
-      },
-      meta: {
-        notifyOnSuccess: {
-          title: 'Your environment are being updated',
-          labelAction: 'See Deployment Logs',
-          callback: () => callback && callback(),
-        },
-        notifyOnError: true,
-      },
-    }
-  )
-}
-
-/*
- * @deprecated use `useStopEnvironment` from `@qovery/domains/environments/feature` instead of `useActionStopEnvironment`
- */
-export const useActionStopEnvironment = (
-  projectId: string,
-  environmentId: string,
-  withDeployments?: boolean,
-  onSettledCallback?: () => void,
-  callback?: () => void
-) => {
-  const queryClient = useQueryClient()
-
-  return useMutation(
-    async () => {
-      const response = await environmentActionApi.stopEnvironment(environmentId)
-      return response.data
-    },
-    {
-      onSuccess: () => {
-        // refetch environmentsStatus requests
-        queryClient.invalidateQueries(['environmentsStatus', projectId])
-
-        if (withDeployments)
-          queryClient.invalidateQueries(['project', projectId, 'environments', environmentId, 'deploymentHistory'])
-      },
-      onSettled: () => onSettledCallback && onSettledCallback(),
-      meta: {
-        notifyOnSuccess: {
-          title: 'Your environment is stopping',
-          labelAction: 'See Deployment Logs',
-          callback: () => callback && callback(),
-        },
-        notifyOnError: true,
-      },
-    }
-  )
-}
-
 /*
  * @deprecated use `useCancelEnvironment` from `@qovery/domains/environments/feature` instead of `useActionCancelEnvironment`
  */
