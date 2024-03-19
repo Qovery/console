cd7158053fd8fb8b0d0767217e62ca42a61c3196
Camille TJHOA <camille.tjhoa@outlook.com>
2024-03-19 17:44:13 +0100

    docs: add TODO comment

--- libs/shared/util-js/src/lib/build-git-repo-url.ts
+++ libs/shared/util-js/src/lib/build-git-repo-url.ts
@@ -1,3 +1,4 @@
+// TODO: move to '@qovery/shared/util-git'
 export const buildGitRepoUrl = (provider: string, repository: string): string => {
   if (repository.includes('http://') || repository.includes('https://')) return repository
   const authProvider = provider.toLowerCase()
