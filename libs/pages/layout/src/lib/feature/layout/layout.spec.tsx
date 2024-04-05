c8abb2770755665723cc1c065337144e121b4ea1
Camille TJHOA <camille.tjhoa@outlook.com>
2024-04-05 15:53:52 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! feat: add spotlight

--- libs/pages/layout/src/lib/feature/layout/layout.spec.tsx
+++ libs/pages/layout/src/lib/feature/layout/layout.spec.tsx
@@ -1,5 +1,6 @@
 import { render } from '__tests__/utils/setup-jest'
 import { createElement } from 'react'
+import { IntercomProvider } from 'react-use-intercom'
 import { setCurrentOrganizationIdOnStorage, setCurrentProjectIdOnStorage } from '../../utils/utils'
 import Layout, { type LayoutProps } from './layout'
 
@@ -15,7 +16,11 @@ describe('Layout', () => {
   })
 
   it('should render successfully', () => {
-    const { baseElement } = render(<Layout {...props} />)
+    const { baseElement } = render(
+      <IntercomProvider appId="__test__app__id__" autoBoot={false}>
+        <Layout {...props} />
+      </IntercomProvider>
+    )
     expect(baseElement).toBeTruthy()
   })
 
@@ -24,14 +29,22 @@ describe('Layout', () => {
       typeof setCurrentOrganizationIdOnStorage
     >
     mockSetOrganizationId.mockImplementation()
-    render(<Layout {...props} />)
+    render(
+      <IntercomProvider appId="__test__app__id__" autoBoot={false}>
+        <Layout {...props} />
+      </IntercomProvider>
+    )
     expect(mockSetOrganizationId).toBeCalled()
   })
 
   it('should save the current project id on local storage', () => {
     const mockSetProjectId = setCurrentProjectIdOnStorage as jest.Mock<typeof setCurrentProjectIdOnStorage>
     mockSetProjectId.mockImplementation()
-    render(<Layout {...props} />)
+    render(
+      <IntercomProvider appId="__test__app__id__" autoBoot={false}>
+        <Layout {...props} />
+      </IntercomProvider>
+    )
     expect(mockSetProjectId).toBeCalled()
   })
 })
