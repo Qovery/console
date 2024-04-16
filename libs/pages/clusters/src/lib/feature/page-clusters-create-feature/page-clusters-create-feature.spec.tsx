044d48e01bedf99aadfd69064657e20e324d23b6
Camille TJHOA <camille.tjhoa@outlook.com>
2024-04-16 18:13:39 +0200

    fixup! chore: add shared/util-node-env lib

--- libs/pages/clusters/src/lib/feature/page-clusters-create-feature/page-clusters-create-feature.spec.tsx
+++ libs/pages/clusters/src/lib/feature/page-clusters-create-feature/page-clusters-create-feature.spec.tsx
@@ -1,5 +1,6 @@
-import { render } from '__tests__/utils/setup-jest'
 import { Route, Routes } from 'react-router-dom'
+import { IntercomProvider } from 'react-use-intercom'
+import { renderWithProviders } from '@qovery/shared/util-tests'
 import PageClustersCreateFeature from './page-clusters-create-feature'
 
 jest.mock('react-router-dom', () => ({
@@ -9,10 +10,12 @@ jest.mock('react-router-dom', () => ({
 
 describe('PageClustersCreateFeature', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <Routes location="/organization/1/clusters/create/general">
-        <Route path="/organization/1/clusters/create/*" element={<PageClustersCreateFeature />} />
-      </Routes>
+    const { baseElement } = renderWithProviders(
+      <IntercomProvider appId="__test__app__id__" autoBoot={false}>
+        <Routes location="/organization/1/clusters/create/general">
+          <Route path="/organization/1/clusters/create/*" element={<PageClustersCreateFeature />} />
+        </Routes>
+      </IntercomProvider>
     )
     expect(baseElement).toBeTruthy()
   })
