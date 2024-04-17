853aa92e5f460f10ef493bbacf35e55d404452b5
Remi Bonnet <bonnet.rem@gmail.com>
2024-04-17 14:31:20 +0200

    fix: page-settings-resources spec

--- libs/pages/application/src/lib/feature/page-settings-resources-feature/page-settings-resources-feature.tsx
+++ libs/pages/application/src/lib/feature/page-settings-resources-feature/page-settings-resources-feature.tsx
@@ -17,8 +17,7 @@ export function SettingsResourcesFeature({ service, environment }: SettingsResou
   const { mutate: editService, isLoading: isLoadingService } = useEditService({ environmentId: environment.id })
 
   const defaultInstances = match(service)
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    .with({ serviceType: 'JOB' }, () => {})
+    .with({ serviceType: 'JOB' }, () => ({}))
     .otherwise((s) => ({
       min_running_instances: s.min_running_instances || 1,
       max_running_instances: s.max_running_instances || 1,
