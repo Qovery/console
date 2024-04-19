5f79e53b6b7727f8ac0e2c3a8c095684fcebe579
Remi Bonnet <bonnet.rem@gmail.com>
2024-04-19 17:45:25 +0200

    feat: update skake ease

--- libs/shared/assistant/feature/src/lib/assistant-trigger/assistant-trigger.tsx
+++ libs/shared/assistant/feature/src/lib/assistant-trigger/assistant-trigger.tsx
@@ -20,7 +20,7 @@ export function AssistantTrigger({ defaultOpen = false }: AssistantTriggerProps)
         <Icon
           iconStyle="light"
           iconName="question"
-          className="text-2xl group-hover:animate-[shake_0.6s_cubic-bezier(.36,.07,.19,.97)_both]"
+          className="text-2xl group-hover:animate-[shake_0.6s_cubic-bezier(0.36,0.07,0.19,0.97)_both]"
         />
         <span className="group-hover:opacity-20 group-hover:animate-[shineAssistantTrigger_1s_linear_forwards] pointer-events-none opacity-0 absolute left-0 top-0 w-[150px] h-[150px] bg-gradient-to-b from-transparent from-0% via-neutral-50 via-50% to-transparent to-100%"></span>
       </button>
