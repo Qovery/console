3006add3fdec35b17052d05dcb1e911e2b9981f4
Remi Bonnet <bonnet.rem@gmail.com>
2024-04-22 11:13:56 +0200

    feat(dark-mode): hide transition during navigation

--- libs/pages/layout/src/lib/feature/dark-mode-enabler/dark-mode-enabler.tsx
+++ libs/pages/layout/src/lib/feature/dark-mode-enabler/dark-mode-enabler.tsx
@@ -8,11 +8,34 @@ export interface DarkModeEnablerProps {
 export function DarkModeEnabler(props: DarkModeEnablerProps) {
   const { isDarkMode = false, children } = props
 
+  const hideElementTransitionDuringNavigation = () => {
+    const css = document.createElement('style')
+    css.appendChild(
+      document.createTextNode(
+        `* {
+          -webkit-transition: none !important;
+          -moz-transition: none !important;
+          -o-transition: none !important;
+          -ms-transition: none !important;
+          transition: none !important;
+        }`
+      )
+    )
+    document.head.appendChild(css)
+    setTimeout(() => document.head.removeChild(css), 1000)
+  }
+
   useEffect(() => {
-    if (isDarkMode) document.documentElement.classList.add('dark')
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+      hideElementTransitionDuringNavigation()
+    }
 
     return () => {
-      if (isDarkMode) document.documentElement.classList.remove('dark')
+      if (isDarkMode) {
+        document.documentElement.classList.remove('dark')
+        hideElementTransitionDuringNavigation()
+      }
     }
   }, [isDarkMode])
 
