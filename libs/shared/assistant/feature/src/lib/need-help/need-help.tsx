337906554221af4e3ee364314d4c89425450389e
Camille TJHOA <camille.tjhoa@outlook.com>
2024-05-03 17:31:08 +0200

    feat(assistant): add need-help button

---
+++ libs/shared/assistant/feature/src/lib/need-help/need-help.tsx
@@ -0,0 +1,26 @@
+import { useContext } from 'react'
+import { Button, Icon } from '@qovery/shared/ui'
+import { twMerge } from '@qovery/shared/util-js'
+import { AssistantContext } from '../assistant-context/assistant-context'
+
+export interface NeedHelpProps {
+  className?: string
+}
+
+export function NeedHelp({ className }: NeedHelpProps) {
+  const { assistantOpen, setAssistantOpen } = useContext(AssistantContext)
+  return (
+    <Button
+      size="xs"
+      color="brand"
+      variant="surface"
+      className={twMerge('w-fit items-center gap-1', className)}
+      onClick={() => setAssistantOpen(!assistantOpen)}
+    >
+      <Icon iconName="circle-question" iconStyle="regular" className="text-xs p-0.5" />
+      <span>Need help here?</span>
+    </Button>
+  )
+}
+
+export default NeedHelp
