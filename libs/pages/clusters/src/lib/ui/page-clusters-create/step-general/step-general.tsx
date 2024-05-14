72a57c7f30d12a93d2de00d34bbc27a369f9d61f
Remi Bonnet <bonnet.rem@gmail.com>
2024-05-14 13:49:36 +0200

    fix: font-size

--- libs/pages/clusters/src/lib/ui/page-clusters-create/step-general/step-general.tsx
+++ libs/pages/clusters/src/lib/ui/page-clusters-create/step-general/step-general.tsx
@@ -268,7 +268,7 @@ export function StepGeneral(props: StepGeneralProps) {
                 <p className="font-normal  text-neutral-350 mb-2">
                   Run the following command from your terminal and follow the instructions.
                 </p>
-                <pre className="flex items-center justify-between bg-neutral-150 text-neutral-400 p-3 rounded-sm text-base font-mono">
+                <pre className="flex items-center justify-between bg-neutral-150 text-neutral-400 p-3 rounded-sm font-mono">
                   $ qovery cluster install <CopyButton content="qovery cluster install" />
                 </pre>
               </li>
