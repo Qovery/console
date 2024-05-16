a6e942e79d41cd4126a1c6477d8a566258f34106
Rémi Bonnet <bonnet.rem@gmail.com>
2024-05-16 16:35:52 +0200

    feat(cluster): self-managed advanced settings (#1352)

--- libs/pages/cluster/src/lib/feature/page-settings-feature/page-settings-feature.spec.tsx
+++ libs/pages/cluster/src/lib/feature/page-settings-feature/page-settings-feature.spec.tsx
@@ -1,10 +1,10 @@
-import { render } from '__tests__/utils/setup-jest'
 import { Route, Routes } from 'react-router-dom'
+import { renderWithProviders } from '@qovery/shared/util-tests'
 import PageSettingsFeature from './page-settings-feature'
 
 describe('PageSettingsFeature', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
+    const { baseElement } = renderWithProviders(
       <Routes location="/organization/1/cluster/2/general">
         <Route path="/organization/1/cluster/2/*" element={<PageSettingsFeature />} />
       </Routes>
