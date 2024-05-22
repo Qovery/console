511af68411bf342940931d0849532227b1da7321
Remi Bonnet <bonnet.rem@gmail.com>
2024-05-22 17:25:18 +0200

    feat: update ui not found page

--- libs/pages/layout/src/lib/feature/not-found-page/not-found-page.tsx
+++ libs/pages/layout/src/lib/feature/not-found-page/not-found-page.tsx
@@ -1,13 +1,26 @@
+import { Heading, Icon, Link, Section } from '@qovery/shared/ui'
+
 export function NotFoundPage({ error }: { error?: unknown }) {
   const errorTyped = error as Error
 
   return (
-    <div className="bg-white rounded-sm flex-grow items-center justify-center">
-      <div className="text-center">
-        <h1>{errorTyped?.name ?? '404'}</h1>
-        <p>{errorTyped?.message ?? 'Not found'}</p>
+    <Section className="bg-white rounded-sm flex-grow items-center justify-center">
+      <div className="text-center flex flex-col gap-3 items-center justify-center w-[500px] -mt-[100px]">
+        <img
+          className="pointer-events-none user-none w-[170px]"
+          src="/assets/images/event-placeholder-light.svg"
+          alt="Event placeholder"
+        />
+        <div>
+          <Heading className="text-neutral-400 font-medium">{errorTyped?.name ?? '404'}</Heading>
+          <p className="text-sm text-neutral-350">{errorTyped?.message ?? 'Not found'}</p>
+        </div>
+        <Link as="button" to="/" size="md" className="flex gap-2">
+          Go to correct URL
+          <Icon iconName="arrow-right" />
+        </Link>
       </div>
-    </div>
+    </Section>
   )
 }
 
