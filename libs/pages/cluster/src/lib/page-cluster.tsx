bc19e81b850ba1c683c113402096d28f0f4d4f9d
Remi Bonnet <bonnet.rem@gmail.com>
2024-05-23 10:09:04 +0200

    feat: cluster 404

--- libs/pages/cluster/src/lib/page-cluster.tsx
+++ libs/pages/cluster/src/lib/page-cluster.tsx
@@ -1,36 +1,41 @@
-import { Navigate, Route, Routes, useParams } from 'react-router-dom'
+import { Route, Routes, useParams } from 'react-router-dom'
 import { useCluster, useDeployCluster } from '@qovery/domains/clusters/feature'
-import { CLUSTER_SETTINGS_URL, CLUSTER_URL } from '@qovery/shared/routes'
+import { NotFoundPage } from '@qovery/pages/layout'
 import { ROUTER_CLUSTER } from './router/router'
 import Container from './ui/container/container'
 
 export function PagesCluster() {
   const { organizationId = '', clusterId = '' } = useParams()
 
-  const { data: cluster } = useCluster({ organizationId, clusterId })
+  const { data: cluster, isFetched } = useCluster({ organizationId, clusterId })
   const { mutate: deployCluster } = useDeployCluster()
 
-  return (
-    <Container
-      cluster={cluster}
-      deployCluster={() =>
-        deployCluster({
-          organizationId,
-          clusterId,
-        })
-      }
-    >
-      <Routes>
-        {ROUTER_CLUSTER.map((route) => (
-          <Route key={route.path} path={route.path} element={route.component} />
-        ))}
-        <Route
-          path="*"
-          element={<Navigate replace to={CLUSTER_URL(organizationId, clusterId) + CLUSTER_SETTINGS_URL} />}
-        />
-      </Routes>
-    </Container>
-  )
+  if (!cluster && isFetched) {
+    return <NotFoundPage />
+  }
+
+  if (cluster) {
+    return (
+      <Container
+        cluster={cluster}
+        deployCluster={() =>
+          deployCluster({
+            organizationId,
+            clusterId,
+          })
+        }
+      >
+        <Routes>
+          {ROUTER_CLUSTER.map((route) => (
+            <Route key={route.path} path={route.path} element={route.component} />
+          ))}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Container>
+    )
+  } else {
+    return null
+  }
 }
 
 export default PagesCluster
