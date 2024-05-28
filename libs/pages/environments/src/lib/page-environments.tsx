efdd532fecc1cecb2379ece24ead7e2990928430
Remi Bonnet <bonnet.rem@gmail.com>
2024-05-28 14:54:07 +0200

    fix(page-services): 404page

--- libs/pages/environments/src/lib/page-environments.tsx
+++ libs/pages/environments/src/lib/page-environments.tsx
@@ -1,22 +1,25 @@
-import { Route, Routes, useParams } from 'react-router-dom'
+import { Navigate, Route, Routes, useParams } from 'react-router-dom'
 import { useEnvironments } from '@qovery/domains/environments/feature'
 import { NotFoundPage } from '@qovery/pages/layout'
+import { ENVIRONMENTS_GENERAL_URL, ENVIRONMENTS_URL } from '@qovery/shared/routes'
 import { ROUTER_ENVIRONMENTS } from './router/router'
 
 export function PageEnvironments() {
-  const { projectId = '' } = useParams()
+  const { organizationId = '', projectId = '' } = useParams()
   const { error } = useEnvironments({ projectId })
 
   if (error) {
     return <NotFoundPage error={error} />
   }
 
+  const path = ENVIRONMENTS_URL(organizationId, projectId)
+
   return (
     <Routes>
       {ROUTER_ENVIRONMENTS.map((route) => (
         <Route key={route.path} path={route.path} element={route.component} />
       ))}
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate replace to={path + ENVIRONMENTS_GENERAL_URL} />} />
     </Routes>
   )
 }
