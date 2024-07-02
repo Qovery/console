0a0ee10a08f8d9b8957566b5ff36f3dfd614a9d5
Remi Bonnet <bonnet.rem@gmail.com>
2024-07-02 16:09:47 +0200

    fix: type import

--- libs/domains/services/feature/src/lib/service-badge/service-badge.tsx
+++ libs/domains/services/feature/src/lib/service-badge/service-badge.tsx
@@ -1,6 +1,6 @@
 import { type VariantProps, cva } from 'class-variance-authority'
 import { type JobLifecycleTypeEnum } from 'qovery-typescript-axios'
-import { ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
+import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
 import { type IconEnum } from '@qovery/shared/enums'
 import { Icon, Tooltip } from '@qovery/shared/ui'
 import { twMerge, upperCaseFirstLetter } from '@qovery/shared/util-js'
