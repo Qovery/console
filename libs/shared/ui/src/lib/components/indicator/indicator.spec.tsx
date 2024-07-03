b4419c6077f388bb8f368893621aa904dcef87fe
Remi Bonnet <bonnet.rem@gmail.com>
2024-07-03 11:52:36 +0200

    feat(indicator): add new wrapper component

---
+++ libs/shared/ui/src/lib/components/indicator/indicator.spec.tsx
@@ -0,0 +1,17 @@
+import { renderWithProviders } from '@qovery/shared/util-tests'
+import Indicator, { type IndicatorProps } from './indicator'
+
+const props: IndicatorProps = {
+  content: <div className="h-3 w-3 rounded-full bg-red-500" />,
+}
+
+describe('Indicator', () => {
+  it('should match snapshot top end', () => {
+    const { baseElement } = renderWithProviders(<Indicator side="top" align="end" {...props} />)
+    expect(baseElement).toMatchSnapshot()
+  })
+  it('should match snapshot bottom end', () => {
+    const { baseElement } = renderWithProviders(<Indicator side="bottom" align="end" {...props} />)
+    expect(baseElement).toMatchSnapshot()
+  })
+})
---
+++ libs/shared/ui/src/lib/components/indicator/indicator.tsx
@@ -0,0 +1,58 @@
+import { type VariantProps, cva } from 'class-variance-authority'
+import { ComponentPropsWithoutRef, type ElementRef, ReactNode, forwardRef } from 'react'
+import { twMerge } from '@qovery/shared/util-js'
+
+const indicatorVariants = cva('absolute', {
+  variants: {
+    side: {
+      top: ['top-0'],
+      right: ['right-0'],
+      bottom: ['bottom-0'],
+      left: ['left-0'],
+    },
+    align: {
+      center: ['left-1/2', 'translate-x-1/2'],
+      start: ['left-0'],
+      end: ['right-0'],
+    },
+  },
+  compoundVariants: [
+    { side: 'top', align: 'center', className: 'left-1/2 -translate-x-1/2 -translate-y-1/2' },
+    { side: 'top', align: 'start', className: 'left-0 -translate-x-1/2 -translate-y-1/2' },
+    { side: 'top', align: 'end', className: '- right-0 -translate-y-1/2 translate-x-1/2' },
+
+    { side: 'right', align: 'center', className: 'left-auto top-2/4 -translate-y-1/2 translate-x-1/2' },
+    { side: 'right', align: 'start', className: 'left-auto top-0 -translate-y-1/2 translate-x-1/2' },
+    { side: 'right', align: 'end', className: 'bottom-0 translate-x-1/2 translate-y-1/2' },
+
+    { side: 'bottom', align: 'center', className: 'left-1/2 -translate-x-1/2 translate-y-1/2 transform' },
+    { side: 'bottom', align: 'start', className: 'left-0 -translate-x-1/2 translate-y-1/2' },
+    { side: 'bottom', align: 'end', className: 'right-0 translate-x-1/2 translate-y-1/2' },
+
+    { side: 'left', align: 'center', className: 'left-0 right-auto top-2/4 -translate-x-1/2 -translate-y-1/2' },
+    { side: 'left', align: 'start', className: 'top-0  -translate-x-1/2 -translate-y-1/2' },
+    { side: 'left', align: 'end', className: 'bottom-0 right-auto -translate-x-1/2 translate-y-1/2' },
+  ],
+})
+
+export interface IndicatorProps
+  extends Omit<ComponentPropsWithoutRef<'span'>, 'content'>,
+    VariantProps<typeof indicatorVariants> {
+  content: ReactNode
+}
+
+export const Indicator = forwardRef<ElementRef<'span'>, IndicatorProps>(function Indicator(
+  { className, side = 'top', align = 'end', children, content, ...indicatorProps },
+  forwardedRef
+) {
+  return (
+    <span ref={forwardedRef} className="relative inline-block">
+      <span {...indicatorProps} className={twMerge(indicatorVariants({ side, align }), className)}>
+        {content}
+      </span>
+      {children}
+    </span>
+  )
+})
+
+export default Indicator
