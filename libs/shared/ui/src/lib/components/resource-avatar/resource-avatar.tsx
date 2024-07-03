16090adf46fc50b7495266ab90fbbe391a593064
Remi Bonnet <bonnet.rem@gmail.com>
2024-07-03 15:48:34 +0200

    feat: add component resource avatar

---
+++ libs/shared/ui/src/lib/components/resource-avatar/resource-avatar.tsx
@@ -0,0 +1,61 @@
+import * as AvatarPrimitive from '@radix-ui/react-avatar'
+import { VariantProps, cva } from 'class-variance-authority'
+import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
+import { IconEnum } from '@qovery/shared/enums'
+import { twMerge } from '@qovery/shared/util-js'
+import Icon from '../icon/icon'
+
+const resourceAvatarVariants = cva(
+  ['relative', 'flex', 'items-center', 'justify-center', 'rounded-full', 'border', 'border-neutral-200'],
+  {
+    variants: {
+      size: {
+        md: ['h-16', 'w-16'],
+        xs: ['h-8', 'w-8'],
+      },
+    },
+  }
+)
+
+export interface ResourceAvatarProps
+  extends Omit<ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>, 'size'>,
+    VariantProps<typeof resourceAvatarVariants> {}
+
+export const ResourceAvatar = forwardRef<ElementRef<typeof AvatarPrimitive.Root>, ResourceAvatarProps>(function Avatar(
+  { className, ...props },
+  forwardedRef
+) {
+  return (
+    <AvatarPrimitive.Root
+      ref={forwardedRef}
+      className={twMerge('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
+      {...props}
+    />
+  )
+})
+
+const resourceAvatarIconVariants = cva('', {
+  variants: {
+    size: {
+      md: ['h-10', 'w-10'],
+      xs: ['h-6', 'w-6'],
+    },
+  },
+})
+
+export interface ResourceAvatarIconProps
+  extends Omit<ComponentPropsWithoutRef<'span'>, 'size'>,
+    VariantProps<typeof resourceAvatarIconVariants> {
+  icon: keyof typeof IconEnum
+}
+
+export const ResourceAvatarIcon = forwardRef<ElementRef<'span'>, ResourceAvatarIconProps>(function AvatarIcon(
+  { className, size, icon, ...props },
+  forwardedRef
+) {
+  return (
+    <span ref={forwardedRef} {...props} className={twMerge(resourceAvatarIconVariants({ size }))}>
+      <Icon className={twMerge(resourceAvatarIconVariants({ size }))} name={icon} />
+    </span>
+  )
+})
