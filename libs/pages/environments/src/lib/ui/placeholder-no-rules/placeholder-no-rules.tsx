8ddb018e94bc8f56071145dc2aaf1fafca8e3fab
Camille TJHOA <camille.tjhoa@outlook.com>
2024-07-17 13:17:26 +0200

    feat(projects): disable add deployment rule button if no cluster avai… (#1488)
    * feat(projects): disable add deployment rule button if no cluster available
    
    * fixup! feat(projects): disable add deployment rule button if no cluster available

--- libs/pages/environments/src/lib/ui/placeholder-no-rules/placeholder-no-rules.tsx
+++ libs/pages/environments/src/lib/ui/placeholder-no-rules/placeholder-no-rules.tsx
@@ -1,31 +1,37 @@
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@qovery/shared/ui'
+import { CLUSTERS_CREATION_GENERAL_URL, CLUSTERS_CREATION_URL, CLUSTERS_URL } from '@qovery/shared/routes'
+import { Button, EmptyState } from '@qovery/shared/ui'
 
 export interface PlaceholderNoRulesProps {
+  organizationId: string
   linkNewRule: string
+  clusterAvailable: boolean
 }
 
-export function PlaceholderNoRules({ linkNewRule }: PlaceholderNoRulesProps) {
+export function PlaceholderNoRules({ clusterAvailable, linkNewRule, organizationId }: PlaceholderNoRulesProps) {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-grow overflow-y-auto">
-      <div className="flex flex-grow items-center justify-center">
-        <div className="flex flex-col items-center">
-          <img
-            className="user-none pointer-events-none mb-5"
-            src="/assets/images/event-placeholder-light.svg"
-            alt="Event placeholder"
-          />
-          <h2 className="mb-5 text-base font-medium text-neutral-400">
-            Create your first Deployment Rules <span role="img">🕹</span>
-          </h2>
-          <Button size="lg" onClick={() => navigate(linkNewRule)}>
-            Create Deployment Rule
-          </Button>
-        </div>
-      </div>
-    </div>
+    <EmptyState
+      title={`${clusterAvailable ? 'Create your first Deployment Rules 🕹' : 'Create your Cluster first 💫'}`}
+      description={
+        clusterAvailable
+          ? undefined
+          : 'Deploying a cluster is necessary to start using Qovery and create your first Deployment Rules'
+      }
+    >
+      <Button
+        className="mt-5"
+        size="lg"
+        onClick={() =>
+          clusterAvailable
+            ? navigate(linkNewRule)
+            : navigate(CLUSTERS_URL(organizationId) + CLUSTERS_CREATION_URL + CLUSTERS_CREATION_GENERAL_URL)
+        }
+      >
+        {clusterAvailable ? 'Create Deployment Rule' : 'Create a Cluster'}
+      </Button>
+    </EmptyState>
   )
 }
 
