d37c0cc3fa9a2fc331a787fe60fd02013799ba38
Remi Bonnet <bonnet.rem@gmail.com>
2024-07-18 17:43:24 +0200

    feat: add new design

--- libs/pages/services/src/lib/feature/page-job-create-feature/step-introduction-feature/step-introduction-feature.tsx
+++ libs/pages/services/src/lib/feature/page-job-create-feature/step-introduction-feature/step-introduction-feature.tsx
@@ -1,11 +1,37 @@
-import { useEffect, useState } from 'react'
+import { type PropsWithChildren, type ReactNode, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { SERVICES_JOB_CREATION_DOCKERFILE_URL, SERVICES_URL } from '@qovery/shared/routes'
-import { Button, Checkbox, ExternalLink, FunnelFlowBody, Heading, Section } from '@qovery/shared/ui'
+import { Button, Checkbox, ExternalLink, FunnelFlowBody, Heading, Icon, Section, Tooltip } from '@qovery/shared/ui'
 import { useDocumentTitle } from '@qovery/shared/util-hooks'
 import { useJobContainerCreateContext } from '../page-job-create-feature'
-import imageLifecycleIntroduction from './lifecycle-introduction.png'
+import imageBuild from './images/build.svg'
+import imageDeploy from './images/deploy.svg'
+import imageOutput from './images/output.svg'
+import imageRun from './images/run.svg'
 import { setLocalStorageStepIntroduction } from './util-localstorage-step'
+
+interface CardProps extends PropsWithChildren {
+  title: string
+  content: ReactNode
+}
+
+function Card({ title, content, children }: CardProps) {
+  return (
+    <div className="w-full overflow-hidden rounded-[16px] bg-gradient-to-b from-[#B160F0] via-[#7366FF] to-[#B160F0] p-0.5 shadow-md sm:max-w-[196px]">
+      <div className="flex h-full w-full flex-col gap-[18px] rounded-[14px] bg-white p-3">
+        <h1 className="flex text-[28px] text-brand-500">
+          {title}
+          <Tooltip classNameContent="max-w-[196px]" classNameTrigger="ml-2 relative -top-[2px]" content={content}>
+            <span>
+              <Icon iconName="info-circle" iconStyle="regular" className="mr-4 text-lg" />
+            </span>
+          </Tooltip>
+        </h1>
+        {children}
+      </div>
+    </div>
+  )
+}
 
 export function StepIntroductionFeature() {
   useDocumentTitle('Introduction - Create Job')
@@ -26,62 +52,85 @@ export function StepIntroductionFeature() {
   }
 
   return (
-    <FunnelFlowBody customContentWidth="max-w-[1280px]">
-      <div className="flex items-center gap-16">
-        <Section className="mt-10 flex max-w-[480px] flex-col gap-8">
-          <Heading className="text-[32px]">Lifecycle Jobs</Heading>
-          <div className="flex flex-col gap-4 text-neutral-350">
-            <p>
-              Lifecycle jobs allow to execute custom code based on the event triggered on the environment
-              (start/stop/delete). This can be useful to manage external resources or trigger specific actions based on
-              the environment events. More in details:
+    <FunnelFlowBody customContentWidth="max-w-[924px]">
+      <div className="mx-auto flex flex-col items-center justify-center gap-16">
+        <Section className="flex flex-col gap-8">
+          <div>
+            <Heading className="mb-4 text-[32px]">Deploying `lifecycle_type` manifest</Heading>
+            <p className="text-neutral-350">
+              Qovery packages your `lifecycle_type` manifest, inputs and `Lifecicle_type` CLI as a containerized
+              application. It is then executed on your Kubernetes cluster as job (Qovery Lifecycle job), allowing you to
+              create/update/destroy your resources based on the events on your environment (start/stop/delete).
             </p>
-            <ul className="list-decimal pl-4">
-              <li>
-                When a deployment on your environment is triggered, an image is built from your code source (deploying
-                from a container registry is supported)
-              </li>
-              <li>
-                Depending on the action triggered on your environment (start/stop/delete), a specific command can be
-                execute at runtime. Example: on env start run “/bin/sh start.sh”
-              </li>
-              <li>
-                Your code can generate an output file /qovery-output/qovery-output.json. The content of this file is
-                automatically fetched by Qovery and stored as environment variables.
-              </li>
-              <li>Any other service within the same environment can then access these environment variables.</li>
-            </ul>
           </div>
-          <div className="flex flex-col items-center gap-5">
-            <ExternalLink className="ml-4" href="https://hub.qovery.com/docs/using-qovery/configuration/lifecycle-job/">
-              Learn more
-            </ExternalLink>
+          <div className="flex flex-col items-center gap-6 md:flex-row md:items-start md:gap-1">
+            <Card
+              title="Build"
+              content={
+                <ul className="list-disc pl-3">
+                  <li>Your manifest and inputs are packaged as a containerised application via a Dockerfile.</li>
+                  <li>
+                    The Dockerfile defines the execution environment: Terraform CLI version, commands to run during the
+                    job execution (e.g: command “start” corresponds to “cloudformation deploy..”).
+                  </li>
+                  <li>
+                    Qovery provides you with a pre-configured Dockerfile that you can customize to match your needs.
+                  </li>
+                </ul>
+              }
+            >
+              <img src={imageBuild} alt="Build - Lifecycle job" className="pointer-events-none w-full select-none" />
+            </Card>
+            <Icon iconName="angles-right" className="mt-8 hidden text-brand-500 md:block" />
+            <Card
+              title="Deploy"
+              content="Depending on the action triggered on your environment (start/stop/delete), a dedicated command defined within your Dockerfile will be executed to create/destroy the resource. Example: on “environment start” execute “start”"
+            >
+              <img src={imageDeploy} alt="Deploy - Lifecycle job" className="pointer-events-none w-full select-none" />
+            </Card>
+            <Icon iconName="angles-right" className="mt-8 hidden text-brand-500 md:block" />
+            <Card
+              title="Run"
+              content="The container is executed as a Kubernetes Job on your cluster and it runs the command chosen from the previous step. At this step it will create/destroy the resource (DB, queues ..)."
+            >
+              <img src={imageRun} alt="Run - Lifecycle job" className="pointer-events-none w-full select-none" />
+            </Card>
+            <Icon iconName="angles-right" className="mt-8 hidden text-brand-500 md:block" />
+            <Card
+              title="Output"
+              content="If a resource is created, the job can write the references of the external resouces within an “output file” (format and path to be respected). Qovery automatically  injects its content as environment variable on any application within the same environment, allowing them to access the resource."
+            >
+              <img src={imageOutput} alt="Output - Lifecycle job" className="pointer-events-none w-full select-none" />
+            </Card>
+          </div>
+          <div className="mx-auto flex w-full flex-col items-center gap-5 md:w-[480px]">
+            <div className="flex w-full justify-between">
+              <div className="flex items-center">
+                <Checkbox
+                  id="dont-show-again"
+                  className="mr-3 h-4 w-4"
+                  checked={dontShowAgain}
+                  onCheckedChange={(checked: boolean) => {
+                    setDontShowAgain(checked)
+                    setLocalStorageStepIntroduction(checked)
+                  }}
+                />
+                <label className="text-sm font-medium text-neutral-400" htmlFor="dont-show-again">
+                  Don’t show it again
+                </label>
+              </div>
+              <ExternalLink
+                className="ml-4"
+                href="https://hub.qovery.com/docs/using-qovery/configuration/lifecycle-job/"
+              >
+                Learn more
+              </ExternalLink>
+            </div>
             <Button size="lg" onClick={onSubmit} className="w-full justify-center">
               Get started
             </Button>
-            <div className="flex items-center">
-              <Checkbox
-                id="dont-show-again"
-                className="mr-3 h-4 w-4"
-                checked={dontShowAgain}
-                onCheckedChange={(checked: boolean) => {
-                  setDontShowAgain(checked)
-                  setLocalStorageStepIntroduction(checked)
-                }}
-              />
-              <label className="text-sm font-medium text-neutral-400" htmlFor="dont-show-again">
-                Don’t show it again
-              </label>
-            </div>
           </div>
         </Section>
-        <div>
-          <img
-            src={imageLifecycleIntroduction}
-            alt="Lifecycle Jobs"
-            className="pointer-events-none w-full select-none"
-          />
-        </div>
       </div>
     </FunnelFlowBody>
   )
