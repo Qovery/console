f08532b1aaba3ef2aed6ba6fd889b5323bf1103d
Remi Bonnet <bonnet.rem@gmail.com>
2024-07-23 14:16:11 +0200

    feat(audit-logs): add last entry placeholder

--- libs/pages/events/src/lib/ui/page-general/page-general.tsx
+++ libs/pages/events/src/lib/ui/page-general/page-general.tsx
@@ -5,7 +5,16 @@ import {
   OrganizationEventType,
 } from 'qovery-typescript-axios'
 import { type Dispatch, type SetStateAction } from 'react'
-import { Icon, Pagination, Section, Table, type TableFilterProps, type TableHeadProps } from '@qovery/shared/ui'
+import { useIntercom } from 'react-use-intercom'
+import {
+  Icon,
+  Pagination,
+  Section,
+  Skeleton,
+  Table,
+  type TableFilterProps,
+  type TableHeadProps,
+} from '@qovery/shared/ui'
 import CustomFilterFeature from '../../feature/custom-filter-feature/custom-filter-feature'
 import RowEventFeature from '../../feature/row-event-feature/row-event-feature'
 
@@ -25,6 +34,56 @@ export interface PageGeneralProps {
   organization?: Organization
 }
 
+const dataHead: TableHeadProps<OrganizationEventResponse>[] = [
+  {
+    title: 'Timestamp',
+    className: 'pl-9',
+  },
+  {
+    title: 'Event',
+    filter: [
+      {
+        title: 'Filter by event',
+        key: 'event_type',
+        itemsCustom: Object.keys(OrganizationEventType).map((item) => item),
+        hideFilterNumber: true,
+      },
+    ],
+  },
+  {
+    title: 'Target type',
+  },
+  {
+    title: 'Target',
+  },
+  {
+    title: 'Change',
+  },
+  {
+    title: 'User',
+    filter: [
+      {
+        title: 'Filter by user',
+        key: 'triggered_by',
+        hideFilterNumber: true,
+      },
+    ],
+  },
+  {
+    title: 'Source',
+    filter: [
+      {
+        title: 'Filter by source',
+        key: 'origin',
+        itemsCustom: Object.keys(OrganizationEventOrigin).map((item) => item),
+        hideFilterNumber: true,
+      },
+    ],
+  },
+]
+
+const columnsWidth = '14% 14% 12% 15% 10% 22% 11%'
+
 export function PageGeneral({
   isLoading,
   events,
@@ -40,55 +99,19 @@ export function PageGeneral({
   handleClearFilter,
   organization,
 }: PageGeneralProps) {
-  const dataHead: TableHeadProps<OrganizationEventResponse>[] = [
-    {
-      title: 'Timestamp',
-      className: 'pl-9',
-    },
-    {
-      title: 'Event',
-      filter: [
-        {
-          title: 'Filter by event',
-          key: 'event_type',
-          itemsCustom: Object.keys(OrganizationEventType).map((item) => item),
-          hideFilterNumber: true,
-        },
-      ],
-    },
-    {
-      title: 'Target type',
-    },
-    {
-      title: 'Target',
-    },
-    {
-      title: 'Change',
-    },
-    {
-      title: 'User',
-      filter: [
-        {
-          title: 'Filter by user',
-          key: 'triggered_by',
-          hideFilterNumber: true,
-        },
-      ],
-    },
-    {
-      title: 'Source',
-      filter: [
-        {
-          title: 'Filter by source',
-          key: 'origin',
-          itemsCustom: Object.keys(OrganizationEventOrigin).map((item) => item),
-          hideFilterNumber: true,
-        },
-      ],
-    },
-  ]
+  const { show: showIntercom } = useIntercom()
 
-  const columnsWidth = '14% 14% 12% 15% 10% 22% 11%'
+  const auditLogsRetentionInDays = organization?.organization_plan?.audit_logs_retention_in_days ?? 30
+  const currentDate = new Date().getTime()
+  const retentionLimitDate = currentDate - auditLogsRetentionInDays * 24 * 60 * 60 * 1000
+
+  const filteredEvents =
+    events?.filter((event) => {
+      if (!event.timestamp) return null
+      return new Date(event.timestamp).getTime() >= retentionLimitDate
+    }) || []
+
+  const checkIfEventsAreFiltered = filteredEvents.length !== events?.length
 
   return (
     <Section className="grow p-8">
@@ -110,19 +133,51 @@ export function PageGeneral({
             placeholderEvents?.map((event) => (
               <RowEventFeature key={event.timestamp} event={event} columnsWidth={columnsWidth} isPlaceholder />
             ))
-          ) : events && events.length === 0 ? (
+          ) : !checkIfEventsAreFiltered && events.length === 0 ? (
             <div className="flex h-[30vh] items-center justify-center px-5 py-4 text-center">
               <div>
                 <Icon iconName="wave-pulse" className="text-neutral-350" />
                 <p className="mt-1 text-xs font-medium text-neutral-350" data-testid="empty-result">
-                  No events found, we retain logs for a maximum of{' '}
-                  {organization?.organization_plan?.audit_logs_retention_in_days ?? 30} days <br /> Try to change your
-                  filters.
+                  No events found, we retain logs for a maximum of {auditLogsRetentionInDays} days <br /> Try to change
+                  your filters.
                 </p>
               </div>
             </div>
+          ) : checkIfEventsAreFiltered ? (
+            <div>
+              {filteredEvents?.map((event) => (
+                <RowEventFeature key={event.timestamp} event={event} columnsWidth={columnsWidth} />
+              ))}
+              <div className="flex h-14 items-center justify-center border-b border-neutral-200">
+                <p className="flex gap-1 text-sm text-neutral-400">
+                  {auditLogsRetentionInDays} days limit reached.
+                  {/* TODO: add a real button */}
+                  <span
+                    className="cursor-pointer font-medium text-sky-500 transition-colors hover:text-sky-600"
+                    onClick={showIntercom}
+                  >
+                    Upgrade your plan to see more
+                  </span>
+                </p>
+              </div>
+              {[...Array(3)].map((_, index) => (
+                <div
+                  key={index}
+                  className="flex h-14 items-center justify-between border-b border-neutral-200 px-5 last:border-0"
+                >
+                  {[...Array(6)].map((_, index) => (
+                    <div className="flex items-center gap-4">
+                      {index === 0 ? <Icon iconName="lock-keyhole" className="text-sm text-neutral-350" /> : null}
+                      <Skeleton key={index} height={16} width={116} />
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
           ) : (
-            events?.map((event) => <RowEventFeature key={event.timestamp} event={event} columnsWidth={columnsWidth} />)
+            filteredEvents?.map((event) => (
+              <RowEventFeature key={event.timestamp} event={event} columnsWidth={columnsWidth} />
+            ))
           )}
         </div>
       </Table>
