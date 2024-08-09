f01775a3268a2d73c700931e87205b99e01ef316
Remi Bonnet <bonnet.rem@gmail.com>
2024-08-09 17:17:59 +0200

    fix(not-found-page): redirect link and status

--- libs/pages/layout/src/lib/feature/not-found-page/not-found-page.tsx
+++ libs/pages/layout/src/lib/feature/not-found-page/not-found-page.tsx
@@ -1,7 +1,12 @@
+import { useOrganizations } from '@qovery/domains/organizations/feature'
+import { ORGANIZATION_URL, USER_URL } from '@qovery/shared/routes'
 import { Heading, Icon, Link, Section } from '@qovery/shared/ui'
+import { type SerializedError } from '@qovery/shared/utils'
 
 export function NotFoundPage({ error }: { error?: unknown }) {
-  const errorTyped = error as Error
+  const errorTyped = error as SerializedError
+
+  const { data: organizations = [] } = useOrganizations()
 
   return (
     <Section className="flex-grow items-center justify-center rounded-sm bg-white">
@@ -12,10 +17,15 @@ export function NotFoundPage({ error }: { error?: unknown }) {
           alt="Event placeholder"
         />
         <div>
-          <Heading className="font-medium text-neutral-400">{errorTyped?.name ?? '404'}</Heading>
+          <Heading className="font-medium text-neutral-400">{errorTyped?.name ?? errorTyped?.code}</Heading>
           <p className="text-sm text-neutral-350">{errorTyped?.message ?? 'Not found'}</p>
         </div>
-        <Link as="button" to="/" size="md" className="flex gap-2">
+        <Link
+          as="button"
+          to={organizations?.length > 0 ? ORGANIZATION_URL(organizations[0].id) : USER_URL}
+          size="md"
+          className="flex gap-2"
+        >
           Go to correct URL
           <Icon iconName="arrow-right" />
         </Link>
