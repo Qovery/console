c81e0340fda002ac86318ce3e323cf55397c62b7
Camille TJHOA <camille.tjhoa@outlook.com>
2024-08-19 18:36:55 +0200

    feat(assistant): add paperclip customization

---
+++ libs/shared/assistant/feature/src/lib/assistant-icon-switcher/assistant-icon-switcher.tsx
@@ -0,0 +1,68 @@
+import { clsx } from 'clsx'
+import { match } from 'ts-pattern'
+import { Icon, Popover } from '@qovery/shared/ui'
+import { useLocalStorage } from '@qovery/shared/util-hooks'
+import { type AssistantIcon, AssistantIconKey } from '../assistant-icon/assistant-icon'
+
+export function AssistantIconSwitcher() {
+  const [assistantIcon, setAssistantIcon] = useLocalStorage<AssistantIcon>(AssistantIconKey, 'QUESTION_MARK')
+  const isQuestionMarkSelected = assistantIcon === 'QUESTION_MARK'
+  const isPaperclipSelected = assistantIcon === 'PAPERCLIP'
+  return (
+    <Popover.Root>
+      <Popover.Trigger>
+        <button className="relative rounded-full transition hover:bg-neutral-100" type="button">
+          {match(assistantIcon)
+            .with('PAPERCLIP', () => (
+              <div className="flex h-6 w-6 items-center justify-center overflow-hidden rounded-full bg-brand-500 text-neutral-50 shadow-2xl outline-brand-600 transition">
+                <Icon iconStyle="light" iconName="question" className="text-xs" />
+              </div>
+            ))
+
+            .with('QUESTION_MARK', () => (
+              <div className="flex h-6 w-6 items-center justify-center overflow-hidden rounded-full bg-brand-500 text-neutral-50 shadow-2xl outline-brand-600 transition">
+                <Icon iconStyle="light" iconName="question" className="text-xs" />
+              </div>
+            ))
+            .exhaustive()}
+          <Icon
+            iconName="angle-down"
+            className="absolute bottom-0 right-0 h-2.5 w-2.5 rounded-full bg-white p-0.5 text-[8px] text-neutral-300"
+          />
+        </button>
+      </Popover.Trigger>
+      <Popover.Content side="bottom" className="grid grid-cols-2 gap-2 text-sm text-neutral-350" style={{ width: 212 }}>
+        <Popover.Close>
+          <div
+            className={clsx(
+              'max-h-24 max-w-24 cursor-pointer select-none rounded border p-2',
+              isQuestionMarkSelected && 'border-brand-500 bg-brand-50',
+              !isQuestionMarkSelected && 'border-neutral-250 transition hover:border-neutral-300'
+            )}
+            onClick={() => setAssistantIcon('QUESTION_MARK')}
+          >
+            <div className="flex h-16 w-16 items-center justify-center overflow-hidden rounded-full bg-brand-500 text-neutral-50 shadow-2xl outline-brand-600 ">
+              <Icon iconStyle="light" iconName="question" className="text-2xl" />
+            </div>
+          </div>
+        </Popover.Close>
+        <Popover.Close>
+          <img
+            className={clsx(
+              'max-h-24 max-w-24 cursor-pointer select-none rounded border p-2',
+              isPaperclipSelected && 'border-brand-500 bg-brand-50',
+              !isPaperclipSelected && 'border-neutral-250 transition hover:border-neutral-300'
+            )}
+            width="100%"
+            height="100%"
+            src="/assets/images/paperclip_emoji.png"
+            alt="paperclip"
+            onClick={() => setAssistantIcon('PAPERCLIP')}
+          />
+        </Popover.Close>
+      </Popover.Content>
+    </Popover.Root>
+  )
+}
+
+export default AssistantIconSwitcher
--- libs/shared/assistant/feature/src/lib/assistant-trigger/assistant-trigger.tsx
+++ libs/shared/assistant/feature/src/lib/assistant-trigger/assistant-trigger.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
+import { match } from 'ts-pattern'
 import { Icon } from '@qovery/shared/ui'
+import { useLocalStorage } from '@qovery/shared/util-hooks'
 import { AssistantContext } from '../assistant-context/assistant-context'
+import { type AssistantIcon, AssistantIconKey } from '../assistant-icon/assistant-icon'
 import { AssistantPanel } from '../assistant-panel/assistant-panel'
 
 export interface AssistantTriggerProps {
@@ -9,20 +12,39 @@ export interface AssistantTriggerProps {
 
 export function AssistantTrigger({ defaultOpen = false }: AssistantTriggerProps) {
   const { assistantOpen, setAssistantOpen } = useContext(AssistantContext)
+  const [assistantIcon] = useLocalStorage<AssistantIcon>(AssistantIconKey, 'QUESTION_MARK')
 
   return (
     <>
-      <button
-        className="group fixed bottom-8 right-8 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full bg-brand-500 text-neutral-50 shadow-2xl outline-brand-600 transition hover:animate-[showAssistantTrigger_0.2s_cubic-bezier(0.21,1.02,0.73,1)_forwards] hover:bg-brand-600"
-        onClick={() => setAssistantOpen(!assistantOpen)}
-      >
-        <Icon
-          iconStyle="light"
-          iconName="question"
-          className="text-2xl group-hover:animate-[shake_0.6s_cubic-bezier(0.36,0.07,0.19,0.97)_both]"
-        />
-        <span className="pointer-events-none absolute left-0 top-0 h-[150px] w-[150px] bg-gradient-to-b from-transparent from-0% via-neutral-50 via-50% to-transparent to-100% opacity-0 group-hover:animate-[shineAssistantTrigger_1s_linear_forwards] group-hover:opacity-20"></span>
-      </button>
+      {match(assistantIcon)
+        .with('QUESTION_MARK', () => (
+          <button
+            className="group fixed bottom-8 right-8 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full bg-brand-500 text-neutral-50 shadow-2xl outline-brand-600 transition hover:animate-[showAssistantTrigger_0.2s_cubic-bezier(0.21,1.02,0.73,1)_forwards] hover:bg-brand-600"
+            onClick={() => setAssistantOpen(!assistantOpen)}
+          >
+            <Icon
+              iconStyle="light"
+              iconName="question"
+              className="text-2xl group-hover:animate-[shake_0.6s_cubic-bezier(0.36,0.07,0.19,0.97)_both]"
+            />
+            <span className="pointer-events-none absolute left-0 top-0 h-[150px] w-[150px] bg-gradient-to-b from-transparent from-0% via-neutral-50 via-50% to-transparent to-100% opacity-0 group-hover:animate-[shineAssistantTrigger_1s_linear_forwards] group-hover:opacity-20"></span>
+          </button>
+        ))
+        .with('PAPERCLIP', () => (
+          <button
+            className="group fixed bottom-8 right-8 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full text-neutral-50 outline-brand-600 drop-shadow-2xl transition hover:animate-[showAssistantTrigger_0.2s_cubic-bezier(0.21,1.02,0.73,1)_forwards]"
+            onClick={() => setAssistantOpen(!assistantOpen)}
+          >
+            <img
+              src="/assets/images/paperclip_emoji.png"
+              alt="assistant-button"
+              className="group-hover:animate-[shake_0.6s_cubic-bezier(0.36,0.07,0.19,0.97)_both]"
+            />
+            <span className="pointer-events-none absolute left-0 top-0 h-[150px] w-[150px] bg-gradient-to-b from-transparent from-0% via-neutral-50 via-50% to-transparent to-100% opacity-0 group-hover:animate-[shineAssistantTrigger_1s_linear_forwards] group-hover:opacity-20"></span>
+          </button>
+        ))
+        .exhaustive()}
+
       {/* XXX: rely on defaultOpen boolean for `smaller` prop as all funnel flows require smaller panel */}
       {assistantOpen && <AssistantPanel onClose={() => setAssistantOpen(false)} smaller={defaultOpen} />}
     </>
