6d9daf18a3128f1a2e5ba4f262c250487478c720
Camille TJHOA <camille.tjhoa@outlook.com>
2024-08-26 10:37:33 +0200

    feat(services): improve service icons (#1589)
    * feat(services): improve service icons
    
    * fixup! feat(services): improve service icons

--- libs/domains/services/feature/src/lib/service-avatar-switcher/service-avatar-switcher.tsx
+++ libs/domains/services/feature/src/lib/service-avatar-switcher/service-avatar-switcher.tsx
@@ -1,6 +1,7 @@
 import { clsx } from 'clsx'
+import { useState } from 'react'
 import { type AnyService } from '@qovery/domains/services/data-access'
-import { Icon, Popover } from '@qovery/shared/ui'
+import { Icon, InputSearch, Popover, Tooltip } from '@qovery/shared/ui'
 import { ServiceAvatar } from '../service-avatar/service-avatar'
 import { type IconURI, ServiceIcons } from '../service-icon/service-icon'
 import { ServiceTemplateIndicator } from '../service-template-indicator/service-template-indicator'
@@ -16,9 +17,13 @@ export interface ServiceAvatarSwitcherProps {
 }
 
 export function ServiceAvatarSwitcher({ onChange, service }: ServiceAvatarSwitcherProps) {
+  const [searchTerm, setSearchTerm] = useState('')
   const handleClick = (newUri: IconURI) => {
     onChange(newUri)
   }
+  const filteredIcons = icons.filter(({ title }) =>
+    searchTerm ? title.toLowerCase().includes(searchTerm.toLowerCase()) : true
+  )
 
   return (
     <Popover.Root>
@@ -33,30 +38,43 @@ export function ServiceAvatarSwitcher({ onChange, service }: ServiceAvatarSwitch
           />
         </button>
       </Popover.Trigger>
-      <Popover.Content side="bottom" className="grid grid-cols-8 gap-2 text-sm text-neutral-350" style={{ width: 400 }}>
-        {icons.map(({ icon, title, uri }) => {
-          const isSelected = uri === service.icon_uri
-          // XXX: corner case as application and container have the same icon, we want to hide one of them.
-          if (uri === 'app://qovery-console/container') {
-            return null
-          }
-          return (
-            <Popover.Close key={title}>
-              <img
-                className={clsx(
-                  'max-h-[38px] max-w-[38px] cursor-pointer select-none rounded border p-2',
-                  isSelected && 'border-brand-500 bg-brand-50',
-                  !isSelected && 'border-neutral-250 transition hover:border-neutral-300'
-                )}
-                width="100%"
-                height="100%"
-                src={icon}
-                alt={title}
-                onClick={() => handleClick(uri)}
-              />
-            </Popover.Close>
-          )
-        })}
+      <Popover.Content side="bottom" className="flex flex-col gap-2" style={{ width: 400 }}>
+        <span className="text-sm text-neutral-350">Change icon</span>
+        <InputSearch placeholder="Search..." className="mb-1" onChange={(value) => setSearchTerm(value)} autofocus />
+        {filteredIcons.length > 0 ? (
+          <div className="grid grid-cols-8 gap-2 text-sm text-neutral-350">
+            {filteredIcons.map(({ icon, title, uri }) => {
+              const isSelected = uri === service.icon_uri
+              // XXX: corner case as application and container have the same icon, we want to hide one of them.
+              if (uri === 'app://qovery-console/container') {
+                return null
+              }
+              return (
+                <Popover.Close key={title}>
+                  <Tooltip content={title}>
+                    <img
+                      className={clsx(
+                        'max-h-[38px] max-w-[38px] cursor-pointer select-none rounded border p-2',
+                        isSelected && 'border-brand-500 bg-brand-50',
+                        !isSelected && 'border-neutral-250 transition hover:border-neutral-300'
+                      )}
+                      width="100%"
+                      height="100%"
+                      src={icon}
+                      alt={title}
+                      onClick={() => handleClick(uri)}
+                    />
+                  </Tooltip>
+                </Popover.Close>
+              )
+            })}
+          </div>
+        ) : (
+          <div className="px-3 py-6 text-center text-neutral-350">
+            <Icon iconName="wave-pulse" />
+            <p className="mt-1 text-xs font-medium">No result for this search</p>
+          </div>
+        )}
       </Popover.Content>
     </Popover.Root>
   )
