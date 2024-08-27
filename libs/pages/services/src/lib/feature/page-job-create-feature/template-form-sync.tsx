43f7d21e7713444b73c86ce4ee03d6daa3a03259
Rémi Bonnet <bonnet.rem@gmail.com>
2024-08-27 15:54:47 +0200

    feat(lifecycle-template): add flag enable interpolation creation flow (#1600)

--- libs/pages/services/src/lib/feature/page-job-create-feature/template-form-sync.tsx
+++ libs/pages/services/src/lib/feature/page-job-create-feature/template-form-sync.tsx
@@ -100,7 +100,6 @@ export function TemplateFormSync({
       }))
 
       // Variables
-
       setVariableData((variableData) => ({
         ...(variableData ?? {}),
         variables: template.variables.map((v) => ({
