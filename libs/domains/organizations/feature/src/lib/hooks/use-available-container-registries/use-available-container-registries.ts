53d76f99634d5cac860354f47872186b6c3b1bce
Camille TJHOA <camille.tjhoa@outlook.com>
2024-09-04 17:03:34 +0200

    feat(ui): sort container registries

--- libs/domains/organizations/feature/src/lib/hooks/use-available-container-registries/use-available-container-registries.ts
+++ libs/domains/organizations/feature/src/lib/hooks/use-available-container-registries/use-available-container-registries.ts
@@ -1,11 +1,40 @@
 import { useQuery } from '@tanstack/react-query'
+import { type ContainerRegistryKindEnum } from 'qovery-typescript-axios'
 import { queries } from '@qovery/state/util-queries'
 
 export function useAvailableContainerRegistries() {
   return useQuery({
     ...queries.organizations.availableContainerRegistries,
     select(registries) {
-      return registries
+      const sortOrder: (keyof typeof ContainerRegistryKindEnum)[] = [
+        'ECR',
+        'GCP_ARTIFACT_REGISTRY',
+        'SCALEWAY_CR',
+        'DOCKER_HUB',
+        'GENERIC_CR',
+        'GITHUB_CR',
+        'GITLAB_CR',
+        'PUBLIC_ECR',
+        'DOCR',
+      ]
+
+      return registries?.sort((a, b) => {
+        const indexA = sortOrder.indexOf(a.kind)
+        const indexB = sortOrder.indexOf(b.kind)
+
+        // If both kinds are found, sort by their order in sortOrder array
+        if (indexA !== -1 && indexB !== -1) {
+          return indexA - indexB
+        }
+
+        // If a.kind is not found in sortOrder, place it after b.kind
+        if (indexA === -1) return 1
+
+        // If b.kind is not found in sortOrder, place it after a.kind
+        if (indexB === -1) return -1
+
+        return 0
+      })
     },
   })
 }
