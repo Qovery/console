e343905930f25d71e39db6b7f6e91ba980d4cd3b
Camille TJHOA <camille.tjhoa@outlook.com>
2024-09-09 11:57:18 +0200

    feat(clusters): add mutation & hook for upgrade cluster

---
+++ libs/domains/clusters/feature/src/lib/hooks/use-upgrade-cluster/use-upgrade-cluster.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { mutations } from '@qovery/domains/clusters/data-access'
+import { queries } from '@qovery/state/util-queries'
+
+export interface UseUpgradeClusterProps {
+  organizationId: string
+}
+
+export function useUpgradeCluster({ organizationId }: UseUpgradeClusterProps) {
+  const queryClient = useQueryClient()
+
+  return useMutation(mutations.upgradeCluster, {
+    onSuccess(_, { clusterId }) {
+      queryClient.invalidateQueries({
+        queryKey: queries.clusters.status({ organizationId, clusterId }).queryKey,
+      })
+    },
+  })
+}
+
+export default useUpgradeCluster
