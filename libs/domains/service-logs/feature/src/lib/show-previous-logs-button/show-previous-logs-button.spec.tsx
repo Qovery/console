245b163ea943afbfd68c4b5cb6501b9241c3508c
Remi Bonnet <bonnet.rem@gmail.com>
2024-09-17 16:01:01 +0200

    fix: test show previous logs button

--- libs/domains/service-logs/feature/src/lib/show-previous-logs-button/show-previous-logs-button.spec.tsx
+++ libs/domains/service-logs/feature/src/lib/show-previous-logs-button/show-previous-logs-button.spec.tsx
@@ -4,19 +4,25 @@ import { ShowPreviousLogsButton } from './show-previous-logs-button'
 describe('ShowPreviousLogsButton', () => {
   it('should render successfully', () => {
     const { baseElement } = renderWithProviders(
-      <ShowPreviousLogsButton showPreviousLogs={true} setShowPreviousLogs={jest.fn()} />
+      <ShowPreviousLogsButton showPreviousLogs={false} setShowPreviousLogs={jest.fn()} setPauseLogs={jest.fn()} />
     )
     expect(baseElement).toBeTruthy()
   })
 
-  it('calls setShowPreviousLogs with true when button is clicked', async () => {
+  it('calls setShowPreviousLogs and setPauseLogs when button is clicked', async () => {
     const setShowPreviousLogs = jest.fn()
+    const setPauseLogs = jest.fn()
     const { userEvent } = renderWithProviders(
-      <ShowPreviousLogsButton showPreviousLogs={false} setShowPreviousLogs={setShowPreviousLogs} />
+      <ShowPreviousLogsButton
+        showPreviousLogs={false}
+        setShowPreviousLogs={setShowPreviousLogs}
+        setPauseLogs={setPauseLogs}
+      />
     )
 
     const button = screen.getByRole('button', { name: /load previous logs/i })
     await userEvent.click(button)
     expect(setShowPreviousLogs).toHaveBeenCalledWith(true)
+    expect(setPauseLogs).toHaveBeenCalledWith(false)
   })
 })
