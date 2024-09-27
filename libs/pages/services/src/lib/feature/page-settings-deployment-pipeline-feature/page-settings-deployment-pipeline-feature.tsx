7c088745b2edbf714c703174b6cd753c0db2f4d8
Camille TJHOA <ctjhoa@qovery.com>
2024-09-27 10:35:13 +0200

    feat(ui) remove legacy react beautiful dnd deployment pipeline part 2 (#1692)
    * feat(ui): add board component
    
    * refactor(ui): replace react-beautiful-dnd in deployment rules settings
    
    * chore: remove react-beautiful-dnd
    
    * refactor(ui): add comment
    
    * fixup! feat(ui): add board component
    
    * fixup! fixup! feat(ui): add board component

--- libs/pages/services/src/lib/feature/page-settings-deployment-pipeline-feature/page-settings-deployment-pipeline-feature.tsx
+++ libs/pages/services/src/lib/feature/page-settings-deployment-pipeline-feature/page-settings-deployment-pipeline-feature.tsx
@@ -1,18 +1,12 @@
-import { type CloudProviderEnum, type DeploymentStageResponse } from 'qovery-typescript-axios'
+import { type DeploymentStageResponse } from 'qovery-typescript-axios'
 import { useEffect, useState } from 'react'
 import { toast as toastAction } from 'react-hot-toast'
 import { useParams } from 'react-router-dom'
-import {
-  useAttachServiceToDeploymentStage,
-  useDeleteDeploymentStage,
-  useEnvironment,
-  useListDeploymentStages,
-} from '@qovery/domains/environments/feature'
+import { useAttachServiceToDeploymentStage, useListDeploymentStages } from '@qovery/domains/environments/feature'
 import { useServices } from '@qovery/domains/services/feature'
-import { Icon, useModal, useModalConfirmation } from '@qovery/shared/ui'
+import { useModal } from '@qovery/shared/ui'
 import PageSettingsDeploymentPipeline from '../../ui/page-settings-deployment-pipeline/page-settings-deployment-pipeline'
 import StageModalFeature from './stage-modal-feature/stage-modal-feature'
-import StageOrderModalFeature from './stage-order-modal-feature/stage-order-modal-feature'
 
 export interface StageRequest {
   stageId: string
@@ -22,17 +16,14 @@ export interface StageRequest {
 export function PageSettingsDeploymentPipelineFeature() {
   const { environmentId = '' } = useParams()
 
-  const { data: environment } = useEnvironment({ environmentId })
   const { data: services } = useServices({ environmentId })
 
   const { openModal, closeModal } = useModal()
-  const { openModalConfirmation } = useModalConfirmation()
 
   const [stages, setStages] = useState<DeploymentStageResponse[] | undefined>()
 
   const { data: deploymentStageList } = useListDeploymentStages({ environmentId })
   const { mutate: addServiceToDeploymentStage } = useAttachServiceToDeploymentStage()
-  const { mutate: deleteEnvironmentDeploymentStage } = useDeleteDeploymentStage({ environmentId })
 
   useEffect(() => {
     if (deploymentStageList) {
@@ -53,58 +44,17 @@ export function PageSettingsDeploymentPipelineFeature() {
     }
   }
 
-  const menuStage = (stage: DeploymentStageResponse, stages?: DeploymentStageResponse[]) => [
-    {
-      items: [
-        {
-          name: 'Edit stage',
-          onClick: () =>
-            openModal({
-              content: <StageModalFeature onClose={closeModal} environmentId={environmentId} stage={stage} />,
-            }),
-          contentLeft: <Icon iconName="pen" className="text-sm text-brand-500" />,
-        },
-        {
-          name: 'Edit order',
-          onClick: () =>
-            openModal({
-              content: <StageOrderModalFeature onClose={closeModal} stages={stages} />,
-            }),
-          contentLeft: <Icon iconName="arrow-down-1-9" className="text-sm text-brand-500" />,
-        },
-      ],
-    },
-    {
-      items: [
-        {
-          name: 'Delete stage',
-          onClick: () =>
-            openModalConfirmation({
-              title: 'Delete this stage',
-              isDelete: true,
-              name: stage.name,
-              action: () => deleteEnvironmentDeploymentStage({ stageId: stage.id }),
-            }),
-          contentLeft: <Icon iconName="trash" className="text-sm text-red-600" />,
-          containerClassName: 'text-red-600',
-        },
-      ],
-    },
-  ]
-
   return (
     <PageSettingsDeploymentPipeline
       stages={stages}
       setStages={setStages}
       onSubmit={onSubmit}
       services={services}
-      cloudProvider={environment?.cloud_provider.provider as CloudProviderEnum}
       onAddStage={() => {
         openModal({
           content: <StageModalFeature onClose={closeModal} environmentId={environmentId} />,
         })
       }}
-      menuStage={menuStage}
     />
   )
 }
--- libs/pages/services/src/lib/ui/page-settings-deployment-pipeline/page-settings-deployment-pipeline.tsx
+++ libs/pages/services/src/lib/ui/page-settings-deployment-pipeline/page-settings-deployment-pipeline.tsx
@@ -1,80 +1,57 @@
-import {
-  type CloudProviderEnum,
-  type DeploymentStageResponse,
-  type DeploymentStageServiceResponse,
-} from 'qovery-typescript-axios'
-import { type Dispatch, Fragment, type SetStateAction } from 'react'
-import { DragDropContext, Draggable, type DropResult, Droppable } from 'react-beautiful-dnd'
+import { type DeploymentStageResponse } from 'qovery-typescript-axios'
+import { type Dispatch, type SetStateAction, useMemo } from 'react'
+import { useParams } from 'react-router-dom'
+import { useDeleteDeploymentStage } from '@qovery/domains/environments/feature'
 import { type AnyService } from '@qovery/domains/services/data-access'
+import { ServiceAvatar } from '@qovery/domains/services/feature'
 import { SettingsHeading } from '@qovery/shared/console-shared'
 import {
   BadgeDeploymentOrder,
+  Board,
+  type BoardCard,
   Button,
+  DropdownMenu,
   Icon,
   LoaderSpinner,
-  Menu,
-  MenuAlign,
-  type MenuData,
   Section,
   Tooltip,
   Truncate,
+  useModal,
+  useModalConfirmation,
 } from '@qovery/shared/ui'
+import { upperCaseFirstLetter } from '@qovery/shared/util-js'
 import { type StageRequest } from '../../feature/page-settings-deployment-pipeline-feature/page-settings-deployment-pipeline-feature'
-import { move, reorderService } from '../../feature/page-settings-deployment-pipeline-feature/utils/utils'
-import DraggableItem from './draggable-item/draggable-item'
+import { StageModalFeature } from '../../feature/page-settings-deployment-pipeline-feature/stage-modal-feature/stage-modal-feature'
+import { StageOrderModalFeature } from '../../feature/page-settings-deployment-pipeline-feature/stage-order-modal-feature/stage-order-modal-feature'
 
 export interface PageSettingsDeploymentPipelineProps {
   onSubmit: (newStage: StageRequest, prevStage: StageRequest) => void
   setStages: Dispatch<SetStateAction<DeploymentStageResponse[] | undefined>>
-  menuStage: (stage: DeploymentStageResponse, stages?: DeploymentStageResponse[]) => MenuData
   onAddStage: () => void
   stages?: DeploymentStageResponse[]
   services?: AnyService[]
-  cloudProvider?: CloudProviderEnum
 }
 
 export function PageSettingsDeploymentPipeline(props: PageSettingsDeploymentPipelineProps) {
-  const { stages, setStages, onSubmit, services, cloudProvider, onAddStage, menuStage } = props
+  const { stages, setStages, onSubmit, services, onAddStage } = props
 
-  const onDragEnd = (result: DropResult) => {
-    const { source, destination, draggableId } = result
+  const { openModal, closeModal } = useModal()
+  const { openModalConfirmation } = useModalConfirmation()
+  const { environmentId = '' } = useParams()
+  const { mutate: deleteEnvironmentDeploymentStage } = useDeleteDeploymentStage({ environmentId })
 
-    if (!destination || !stages) {
-      return
-    }
-    const sourceIndex = +source.droppableId
-    const destinationIndex = +destination.droppableId
-
-    if (sourceIndex === destinationIndex) {
-      // reorder service inside the group
-      const newStages = reorderService(stages, destinationIndex, source.index, destination.index)
-      setStages(newStages)
-    } else {
-      // move to another group
-      const newStages = move(stages, source, destination) as DeploymentStageResponse[]
-      setStages(newStages)
-
-      // new stage for final request
-      const newStageRequest: StageRequest = {
-        stageId: stages[destinationIndex].id,
-        serviceId: draggableId,
-      }
-
-      // previous stage for final request if undo
-      const prevStageRequest: StageRequest = {
-        stageId: stages[sourceIndex].id,
-        serviceId: draggableId,
-      }
-
-      // submit change
-      onSubmit(newStageRequest, prevStageRequest)
-    }
-  }
-
-  const classNameGroup = (isDraggingOver: boolean) =>
-    `grid gap-1 p-1 border border-neutral-250 border-t-0 rounded-b ${
-      isDraggingOver ? 'bg-green-100' : 'bg-neutral-200'
-    }`
+  const cards = useMemo(
+    () =>
+      (stages?.flatMap(
+        ({ id: stageId, services: stageServices }) =>
+          stageServices?.map((s) => ({
+            ...s,
+            columnId: stageId,
+            cardId: s.id,
+          })) ?? []
+      ) ?? []) satisfies BoardCard[],
+    [stages]
+  )
 
   return (
     <div className="w-[calc(100vw-368px)]">
@@ -105,100 +82,177 @@ export function PageSettingsDeploymentPipeline(props: PageSettingsDeploymentPipe
           </Button>
         </SettingsHeading>
         <div className="h-full overflow-x-auto">
-          {!stages && !services ? (
+          {!cards || cards.length === 0 || !services || services.length === 0 || !stages || stages.length === 0 ? (
             <div data-testid="stages-loader" className="flex max-w-4xl justify-center">
               <LoaderSpinner className="mt-5 w-4" />
             </div>
           ) : (
             <div className="flex pb-5">
-              <DragDropContext onDragEnd={onDragEnd}>
-                {stages?.map((stage, index) => (
-                  <Fragment key={stage.id}>
-                    {index !== 0 && (
-                      <svg
-                        data-testid={`arrow-${index}`}
-                        className="shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="43"
-                        fill="none"
-                        viewBox="0 0 16 43"
-                      >
-                        <path fill="#C6D3E7" d="M16 21.5l-7.5-4.33v8.66L16 21.5zm-16 .75h9.25v-1.5H0v1.5z"></path>
-                      </svg>
-                    )}
-                    <div className="w-60 shrink-0 rounded">
-                      <div className="flex h-11 items-center justify-between rounded-t border border-neutral-250 bg-neutral-100 px-3 py-2">
-                        <div className="flex items-center">
-                          <BadgeDeploymentOrder order={stage.deployment_order} />
-                          <span className="block truncate text-2xs font-bold text-neutral-400">
-                            <Truncate truncateLimit={28} text={stage.name || ''} />
-                          </span>
-                          {stage.description && (
-                            <Tooltip content={stage.description}>
-                              <div>
-                                <Icon
-                                  iconName="circle-info"
-                                  iconStyle="regular"
-                                  className="relative -top-[2px] ml-1 text-xs text-neutral-350"
-                                />
-                              </div>
-                            </Tooltip>
-                          )}
-                        </div>
-                        <Menu
-                          key={JSON.stringify(stages)}
-                          width={256}
-                          trigger={
-                            <Button data-testid="btn-more-menu" color="neutral" variant="plain">
-                              <Icon iconName="ellipsis" />
-                            </Button>
+              <Board.Root>
+                {stages?.map((stage, index) => {
+                  const { id: stageId, deployment_order, name, description, services: stageServices } = stage
+                  const isLast = index !== stages.length - 1
+                  return (
+                    <div className="relative mr-4" key={stageId}>
+                      {isLast && (
+                        <svg
+                          data-testid={`arrow-${stageId}`}
+                          className="absolute left-full shrink-0"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="43"
+                          fill="none"
+                          viewBox="0 0 16 43"
+                        >
+                          <path fill="#C6D3E7" d="M16 21.5l-7.5-4.33v8.66L16 21.5zm-16 .75h9.25v-1.5H0v1.5z"></path>
+                        </svg>
+                      )}
+                      <Board.Column
+                        data={cards}
+                        setData={({ sourceColumnId, targetColumnId, sourceCardId, newData }) => {
+                          setStages((stages) =>
+                            stages?.map((stage) => ({
+                              ...stage,
+                              services: newData.filter(({ columnId }) => columnId === stage.id),
+                            }))
+                          )
+
+                          const serviceId = stages
+                            ?.flatMap((stage) => stage.services ?? [])
+                            .find((service) => service.id === sourceCardId)?.service_id
+
+                          if (serviceId) {
+                            // new stage for final request
+                            const newStageRequest: StageRequest = {
+                              stageId: targetColumnId,
+                              serviceId,
+                            }
+                            // previous stage for final request if undo
+                            const prevStageRequest: StageRequest = {
+                              stageId: sourceColumnId,
+                              serviceId,
+                            }
+                            // submit change
+                            onSubmit(newStageRequest, prevStageRequest)
                           }
-                          menus={menuStage(stage, stages)}
-                          arrowAlign={MenuAlign.END}
-                        />
-                      </div>
-                      <Droppable key={index} droppableId={`${index}`}>
-                        {(provided, snapshot) => (
-                          <div
-                            ref={provided.innerRef}
-                            className={classNameGroup(snapshot.isDraggingOver)}
-                            {...provided.droppableProps}
-                          >
-                            {stage.services?.map((item: DeploymentStageServiceResponse, index: number) => (
-                              <Draggable key={item.service_id} draggableId={item.service_id || ''} index={index}>
-                                {(provided, snapshot) => {
-                                  if (services && cloudProvider) {
-                                    return (
-                                      <DraggableItem
-                                        serviceId={item.service_id || ''}
-                                        provided={provided}
-                                        snapshot={snapshot}
-                                        services={services}
-                                      />
-                                    )
-                                  }
-                                  return <div />
-                                }}
-                              </Draggable>
-                            ))}
-                            {provided.placeholder}
-                            {stage.services?.length === 0 && !snapshot.isDraggingOver && (
-                              <div data-testid="placeholder-stage" className="px-3 py-6 text-center">
-                                <Icon iconName="wave-pulse" className="text-neutral-350" />
-                                <p className="mt-1 text-xs font-medium text-neutral-350">
-                                  No service for this stage. <br /> Please drag and drop a service.
-                                </p>
-                              </div>
+                        }}
+                        columnId={stageId}
+                        shouldHighlightIndicator={false}
+                        heading={
+                          <div className="flex grow items-center">
+                            <BadgeDeploymentOrder order={deployment_order} />
+                            <span className="block truncate text-2xs font-bold text-neutral-400">
+                              <Truncate truncateLimit={28} text={name || ''} />
+                            </span>
+                            {description && (
+                              <Tooltip content={description}>
+                                <div>
+                                  <Icon
+                                    iconName="circle-info"
+                                    iconStyle="regular"
+                                    className="relative -top-[2px] ml-1 cursor-default text-xs text-neutral-350"
+                                  />
+                                </div>
+                              </Tooltip>
                             )}
+                            <DropdownMenu.Root>
+                              <DropdownMenu.Trigger asChild>
+                                <Button data-testid="btn-more-menu" color="neutral" variant="plain" className="ml-auto">
+                                  <Icon iconName="ellipsis" />
+                                </Button>
+                              </DropdownMenu.Trigger>
+                              <DropdownMenu.Content>
+                                <DropdownMenu.Item
+                                  data-testid="menuItem"
+                                  icon={<Icon iconName="pen" />}
+                                  onSelect={() =>
+                                    openModal({
+                                      content: (
+                                        <StageModalFeature
+                                          onClose={closeModal}
+                                          environmentId={environmentId}
+                                          stage={stage}
+                                        />
+                                      ),
+                                    })
+                                  }
+                                >
+                                  Edit stage
+                                </DropdownMenu.Item>
+                                <DropdownMenu.Item
+                                  data-testid="menuItem"
+                                  icon={<Icon iconName="arrow-down-1-9" />}
+                                  onSelect={() =>
+                                    openModal({
+                                      content: <StageOrderModalFeature onClose={closeModal} stages={stages} />,
+                                    })
+                                  }
+                                >
+                                  Edit order
+                                </DropdownMenu.Item>
+                                <DropdownMenu.Separator />
+                                <DropdownMenu.Item
+                                  data-testid="menuItem"
+                                  color="red"
+                                  icon={<Icon iconName="trash" />}
+                                  onSelect={() =>
+                                    openModalConfirmation({
+                                      title: 'Delete this stage',
+                                      isDelete: true,
+                                      name,
+                                      action: () => deleteEnvironmentDeploymentStage({ stageId: stageId }),
+                                    })
+                                  }
+                                >
+                                  Delete stage
+                                </DropdownMenu.Item>
+                              </DropdownMenu.Content>
+                            </DropdownMenu.Root>
                           </div>
+                        }
+                      >
+                        {!stageServices || stageServices.length === 0 ? (
+                          <div className="px-3 py-6 text-center" data-testid="placeholder-stage">
+                            <Icon iconName="wave-pulse" className="text-neutral-350" />
+                            <p className="mt-1 text-xs font-medium text-neutral-350">
+                              No service for this stage. <br /> Please drag and drop a service.
+                            </p>
+                          </div>
+                        ) : (
+                          cards
+                            .filter(({ columnId }) => columnId === stage.id)
+                            .map(({ id, service_id }) => {
+                              const service = services?.find(({ id: serviceId }) => serviceId === service_id)
+                              const contentWithParams = service?.serviceType === 'DATABASE'
+                              return (
+                                <Board.Card key={id} cardId={id} columnId={stage.id}>
+                                  <div className="flex flex-row items-center">
+                                    {service && (
+                                      <ServiceAvatar className="mr-2" service={service} size="sm" border="solid" />
+                                    )}
+                                    <div
+                                      className={`font-medium text-neutral-400 ${contentWithParams ? 'text-xs' : 'text-ssm'}`}
+                                    >
+                                      <Truncate
+                                        truncateLimit={contentWithParams ? 32 : 27}
+                                        text={service?.name || ''}
+                                      />
+                                      {contentWithParams && (
+                                        <div data-testid="draggable-item-subtitle" className="text-2xs font-normal">
+                                          {upperCaseFirstLetter(service.type)} - {upperCaseFirstLetter(service.mode)}
+                                        </div>
+                                      )}
+                                    </div>
+                                  </div>
+                                </Board.Card>
+                              )
+                            })
                         )}
-                      </Droppable>
+                      </Board.Column>
                     </div>
-                    {index === stages.length - 1 && <div className="block w-5 shrink-0"></div>}
-                  </Fragment>
-                ))}
-              </DragDropContext>
+                  )
+                })}
+              </Board.Root>
             </div>
           )}
         </div>
