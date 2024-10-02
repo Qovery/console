aad1cdd1ed6efb9a1c344892930fdd586db6adfe
Remi Bonnet <bonnet.rem@gmail.com>
2024-10-02 14:43:21 +0200

    feat: add deployment history breadcrumb

--- libs/domains/service-logs/feature/src/lib/breadcrumb-deployment-history/breadcrumb-deployment-history.tsx
+++ libs/domains/service-logs/feature/src/lib/breadcrumb-deployment-history/breadcrumb-deployment-history.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useMatch, useParams } from 'react-router-dom'
 import { DEPLOYMENT_LOGS_VERSION_URL, ENVIRONMENT_LOGS_URL } from '@qovery/shared/routes'
 import { Button, DropdownMenu, Icon, StatusChip, Tooltip } from '@qovery/shared/ui'
 import { dateFullFormat } from '@qovery/shared/util-dates'
@@ -8,12 +8,18 @@ import { useDeploymentHistory } from '../hooks/use-deployment-history/use-deploy
 
 export interface BreadcrumbDeploymentHistoryProps {
   serviceId: string
-  versionId?: string
 }
 
-export function BreadcrumbDeploymentHistory({ serviceId, versionId }: BreadcrumbDeploymentHistoryProps) {
+export function BreadcrumbDeploymentHistory({ serviceId }: BreadcrumbDeploymentHistoryProps) {
   const { organizationId = '', projectId = '', environmentId = '' } = useParams()
   const { data: deploymentHistory = [], isFetched: isFetchedDeloymentHistory } = useDeploymentHistory({ environmentId })
+
+  const matchDeploymentLogs = useMatch({
+    path: ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId) + DEPLOYMENT_LOGS_VERSION_URL(serviceId),
+    end: false,
+  })
+
+  const versionIdUrl = matchDeploymentLogs?.params['versionId']
 
   if (!isFetchedDeloymentHistory) return null
 
@@ -23,33 +29,33 @@ export function BreadcrumbDeploymentHistory({ serviceId, versionId }: Breadcrumb
         <span className="ml-2 text-xs font-medium text-neutral-350 dark:text-neutral-300">History</span>
         <div className="flex items-center gap-1">
           <DropdownMenu.Root>
-            <span className="flex h-6 items-center px-2">
-              {!versionId || versionId === deploymentHistory[0]?.id ? (
-                <Tooltip content={dateFullFormat(deploymentHistory[0].created_at)} side="bottom">
-                  <span className="mr-2 flex h-5 items-center gap-1 rounded bg-purple-500 px-1 text-sm font-medium text-neutral-50">
-                    Latest
-                    <Icon iconName="circle-info" iconStyle="regular" className="text-xs" />
+            <DropdownMenu.Trigger asChild className="h-6 px-2">
+              <span className="flex items-center">
+                {!versionIdUrl || versionIdUrl === deploymentHistory[0]?.id ? (
+                  <Tooltip content={dateFullFormat(deploymentHistory[0].created_at)} side="bottom">
+                    <span className="mr-2 flex h-4 items-center gap-0.5 rounded bg-purple-500 px-1 text-xs font-medium text-neutral-50">
+                      Latest
+                      <Icon iconName="circle-info" className="text-2xs" />
+                    </span>
+                  </Tooltip>
+                ) : (
+                  <span className="mr-2 text-sm font-medium text-neutral-50">
+                    {dateFullFormat(deploymentHistory.find((h) => h.id === versionIdUrl)?.created_at ?? 0)}
                   </span>
-                </Tooltip>
-              ) : (
-                <span className="mr-2 text-sm font-medium text-neutral-50">
-                  {dateFullFormat(deploymentHistory.find((h) => h.id === versionId)?.created_at ?? 0)}
-                </span>
-              )}
-              <DropdownMenu.Trigger asChild>
+                )}
                 <Button type="button" variant="plain" radius="full">
                   <Icon iconName="angle-down" />
                 </Button>
-              </DropdownMenu.Trigger>
-            </span>
-            <DropdownMenu.Content className="-ml-2.5 max-h-64 w-80 overflow-y-auto">
-              <span className="mb-1 block px-2 text-sm font-medium text-neutral-250">Deployment History</span>
+              </span>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content className="max-h-64 w-80 overflow-y-auto">
+              <span className="mb-1 block px-2 text-sm text-neutral-250">Deployment History</span>
               {deploymentHistory.map((history) => (
                 <DropdownMenu.Item
                   asChild
                   key={history.id}
                   className={clsx('min-h-9', {
-                    'bg-neutral-400': (versionId || deploymentHistory[0]?.id) === history.id,
+                    'bg-neutral-550': (versionIdUrl || deploymentHistory[0]?.id) === history.id,
                   })}
                 >
                   <Link
