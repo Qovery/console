c58d7959117ac2c3666c3e23bb9914404d5a7669
Remi Bonnet <bonnet.rem@gmail.com>
2024-10-31 15:17:19 +0100

    network indicator

--- libs/domains/service-logs/feature/src/lib/service-logs-placeholder/service-logs-placeholder.tsx
+++ libs/domains/service-logs/feature/src/lib/service-logs-placeholder/service-logs-placeholder.tsx
@@ -5,6 +5,7 @@ import { P, match } from 'ts-pattern'
 import { useDeploymentStatus } from '@qovery/domains/services/feature'
 import { DEPLOYMENT_LOGS_URL, ENVIRONMENT_LOGS_URL } from '@qovery/shared/routes'
 import { Icon, Link, LoaderSpinner } from '@qovery/shared/ui'
+import { useNetworkState } from '@qovery/shared/util-hooks'
 
 export function LoaderPlaceholder() {
   return (
@@ -25,6 +26,7 @@ export function ServiceLogsPlaceholder({ serviceName, databaseMode, itemsLength 
   const { data: deploymentStatus } = useDeploymentStatus({ environmentId, serviceId })
   const { state: deploymentState } = deploymentStatus ?? {}
   const [showPlaceholder, setShowPlaceholder] = useState(false)
+  const { effectiveType } = useNetworkState()
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -43,34 +45,44 @@ export function ServiceLogsPlaceholder({ serviceName, databaseMode, itemsLength 
       () => (
         <div>
           <LoaderPlaceholder />
-          {showPlaceholder && (
-            <p className="mt-4 text-center text-sm font-medium text-neutral-300">
-              Processing is taking more than 10s <br /> No logs available, please check the service configuration
-            </p>
-          )}
+          <div className="mt-4 flex flex-col gap-1">
+            {showPlaceholder && (
+              <p className="text-center text-sm font-medium text-neutral-300">
+                Processing is taking more than 10s <br /> No logs available, please check the service configuration
+              </p>
+            )}
+            {effectiveType !== undefined && effectiveType !== '4g' && (
+              <p className="text-center text-sm font-medium text-neutral-350">Your connection is slow.</p>
+            )}
+          </div>
         </div>
       )
     )
-    .with({ itemsLength: 0, deploymentState: P.when((state) => state === 'READY' || state === 'STOPPED') }, () =>
-      showPlaceholder ? (
-        <div className="text-center">
-          <p className="mb-1 font-medium text-neutral-50">
-            No logs available for <span className="text-brand-400">{serviceName}</span>.
-          </p>
-          <p className="mb-4 text-sm font-normal text-neutral-300">Please check if the service is up and running.</p>
-          <Link
-            as="button"
-            size="sm"
-            color="neutral"
-            to={ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId) + DEPLOYMENT_LOGS_URL(serviceId)}
-          >
-            Go to latest deployment
-            <Icon iconName="arrow-right" className="ml-1" />
-          </Link>
-        </div>
-      ) : (
-        <LoaderPlaceholder />
-      )
+    .with(
+      {
+        itemsLength: 0,
+        deploymentState: P.when((state) => state === 'READY' || state === 'STOPPED'),
+      },
+      () =>
+        showPlaceholder ? (
+          <div className="text-center">
+            <p className="mb-1 font-medium text-neutral-50">
+              No logs available for <span className="text-brand-400">{serviceName}</span>.
+            </p>
+            <p className="mb-4 text-sm font-normal text-neutral-300">Please check if the service is up and running.</p>
+            <Link
+              as="button"
+              size="sm"
+              color="neutral"
+              to={ENVIRONMENT_LOGS_URL(organizationId, projectId, environmentId) + DEPLOYMENT_LOGS_URL(serviceId)}
+            >
+              Go to latest deployment
+              <Icon iconName="arrow-right" className="ml-1" />
+            </Link>
+          </div>
+        ) : (
+          <LoaderPlaceholder />
+        )
     )
     .otherwise(() => (
       <div className="text-center">
