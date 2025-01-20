32b975d71d20ba9fbc96817896fc72ff27785a4a
Julien Dan <jdan@qovery.com>
2025-01-20 11:33:51 +0100

    FRT-1413 - Add Triggered label for jobs/cronjobs

--- libs/domains/services/feature/src/lib/service-list/service-list.tsx
+++ libs/domains/services/feature/src/lib/service-list/service-list.tsx
@@ -183,14 +183,16 @@ function ServiceNameCell({
               .with(
                 { job_type: 'CRON' },
                 ({ schedule }) =>
-                  `${formatCronExpression(schedule.cronjob?.scheduled_at)} (${schedule.cronjob?.timezone})`
+                  `Triggered: ${formatCronExpression(schedule.cronjob?.scheduled_at)} (${schedule.cronjob?.timezone})`
               )
               .with(
                 { job_type: 'LIFECYCLE' },
-                ({ schedule }) =>
-                  [schedule.on_start && 'Deploy', schedule.on_stop && 'Stop', schedule.on_delete && 'Delete']
+                ({ schedule }) => {
+                  const actions = [schedule.on_start && 'Deploy', schedule.on_stop && 'Stop', schedule.on_delete && 'Delete']
                     .filter(Boolean)
-                    .join(' - ') || undefined
+                    .join(' - ');
+                  return actions ? `Triggered on: ${actions}` : undefined;
+                }
               )
               .exhaustive()
 
