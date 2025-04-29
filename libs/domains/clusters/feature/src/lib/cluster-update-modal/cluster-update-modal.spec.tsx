787516596043cc3d0e9f9f8480d22c6df38f152a
Romain Billard <romainbillard@mac-1.home>
2025-04-29 12:17:58 +0200

    feat(cluster): redirect to logs when selecting dry-run

--- libs/domains/clusters/feature/src/lib/cluster-update-modal/cluster-update-modal.spec.tsx
+++ libs/domains/clusters/feature/src/lib/cluster-update-modal/cluster-update-modal.spec.tsx
@@ -1,8 +1,15 @@
+import { INFRA_LOGS_URL } from '@qovery/shared/routes'
 import { renderWithProviders, screen } from '@qovery/shared/util-tests'
 import * as useDeployCluster from '../hooks/use-deploy-cluster/use-deploy-cluster'
 import { ClusterUpdateModal } from './cluster-update-modal'
 
 const useDeployClusterMockSpy = jest.spyOn(useDeployCluster, 'useDeployCluster') as jest.Mock
+
+const mockNavigate = jest.fn()
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}))
 
 describe('ClusterUpdateModal', () => {
   const mockCluster = {
@@ -48,5 +55,31 @@ describe('ClusterUpdateModal', () => {
 
     await userEvent.click(checkbox)
     expect(checkbox).toBeChecked()
+  })
+
+  it('should not redirect to cluster logs page when dry-run is not selected', async () => {
+    const { userEvent } = renderWithProviders(<ClusterUpdateModal cluster={mockCluster} />)
+
+    const submitButton = screen.getByTestId('submit-button')
+    await userEvent.click(submitButton)
+
+    expect(mockNavigate).not.toHaveBeenCalled()
+  })
+
+  it('should redirect to cluster logs page when dry-run is selected', async () => {
+    const { userEvent } = renderWithProviders(<ClusterUpdateModal cluster={mockCluster} />)
+
+    const input = screen.getByTestId('input-value')
+    await userEvent.type(input, 'Test Cluster')
+
+    const checkbox = screen.getByRole('checkbox', { name: /dry-run/i })
+    await userEvent.click(checkbox)
+
+    const submitButton = screen.getByTestId('submit-button')
+    await userEvent.click(submitButton)
+
+    expect(mockNavigate).toHaveBeenCalledWith(INFRA_LOGS_URL(mockCluster.organization.id, mockCluster.id), {
+      state: { prevUrl: '/' },
+    })
   })
 })
