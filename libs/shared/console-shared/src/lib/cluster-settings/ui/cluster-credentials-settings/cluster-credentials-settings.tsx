b9b22e77bcf6b7291465691c712398fd631e2e8c
Rémi Bonnet <bonnet.rem@gmail.com>
2025-05-05 14:49:22 +0200

    feat(cluster): add warning if credentials switch (#1888)

--- libs/shared/console-shared/src/lib/cluster-settings/ui/cluster-credentials-settings/cluster-credentials-settings.tsx
+++ libs/shared/console-shared/src/lib/cluster-settings/ui/cluster-credentials-settings/cluster-credentials-settings.tsx
@@ -1,6 +1,8 @@
 import { type ClusterCredentials } from 'qovery-typescript-axios'
 import { Controller, useFormContext } from 'react-hook-form'
-import { Icon, InputSelect, LoaderSpinner } from '@qovery/shared/ui'
+import { useParams } from 'react-router-dom'
+import { CLUSTER_SETTINGS_IMAGE_REGISTRY_URL, CLUSTER_SETTINGS_URL, CLUSTER_URL } from '@qovery/shared/routes'
+import { Callout, ExternalLink, Icon, InputSelect, LoaderSpinner } from '@qovery/shared/ui'
 
 export interface ClusterCredentialsSettingsProps {
   credentials?: ClusterCredentials[]
@@ -9,8 +11,9 @@ export interface ClusterCredentialsSettingsProps {
 }
 
 export function ClusterCredentialsSettings(props: ClusterCredentialsSettingsProps) {
+  const { organizationId = '', clusterId } = useParams()
   const { credentials, openCredentialsModal, loading } = props
-  const { control } = useFormContext()
+  const { control, formState } = useFormContext()
 
   const buildCredentials = credentials?.map((item: ClusterCredentials) => ({
     label: `${item.name}${'access_key_id' in item ? ` (${item.access_key_id})` : ''}`,
@@ -25,31 +28,56 @@ export function ClusterCredentialsSettings(props: ClusterCredentialsSettingsProp
           <LoaderSpinner className="w-4" />
         </div>
       ) : (
-        <Controller
-          name="credentials"
-          control={control}
-          rules={{
-            required: 'Please select a credential.',
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <InputSelect
-              dataTestId="input-credentials"
-              label="Credentials"
-              className="mb-3"
-              options={buildCredentials || []}
-              onChange={field.onChange}
-              value={field.value}
-              error={error?.message}
-              isSearchable
-              menuListButton={{
-                title: 'Select credential',
-                label: 'New credential',
-                icon: <Icon iconName="circle-plus" className="text-brand-500" />,
-                onClick: () => openCredentialsModal(undefined, field.onChange),
-              }}
-            />
+        <>
+          <Controller
+            name="credentials"
+            control={control}
+            rules={{
+              required: 'Please select a credential.',
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <InputSelect
+                dataTestId="input-credentials"
+                label="Credentials"
+                className="mb-3"
+                options={buildCredentials || []}
+                onChange={field.onChange}
+                value={field.value}
+                error={error?.message}
+                isSearchable
+                menuListButton={{
+                  title: 'Select credential',
+                  label: 'New credential',
+                  icon: <Icon iconName="circle-plus" className="text-brand-500" />,
+                  onClick: () => openCredentialsModal(undefined, field.onChange),
+                }}
+              />
+            )}
+          />
+          {formState.isDirty && (
+            <Callout.Root color="yellow">
+              <Callout.Icon>
+                <Icon iconName="circle-exclamation" iconStyle="regular" />
+              </Callout.Icon>
+              <Callout.Text>
+                <Callout.TextDescription className="flex flex-col gap-1">
+                  The credential change won't be applied to the mirroring registry of this cluster. Make sure to update
+                  the credentials properly in this cluster's mirroring registry section.
+                  <ExternalLink
+                    className="items-center"
+                    href={
+                      CLUSTER_URL(organizationId, clusterId) +
+                      CLUSTER_SETTINGS_URL +
+                      CLUSTER_SETTINGS_IMAGE_REGISTRY_URL
+                    }
+                  >
+                    Go to mirroring registry section
+                  </ExternalLink>
+                </Callout.TextDescription>
+              </Callout.Text>
+            </Callout.Root>
           )}
-        />
+        </>
       )}
     </div>
   )
