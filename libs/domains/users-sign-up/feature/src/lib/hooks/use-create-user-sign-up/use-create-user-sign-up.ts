fabce6231abc8dc25bd1069a1cb10903f52b23b0
Rémi Bonnet <bonnet.rem@gmail.com>
2025-05-07 09:38:44 +0200

    chore(users-sign-up): integrate Cargo API for user sign-up data submission (#1891)
    * chore(users-sign-up): integrate Cargo API for user sign-up data submission
    
    - Added useSignUpCargo hook to handle posting user sign-up data to Cargo API.
    - Updated useCreateUserSignUp to format and send data to Cargo upon successful user sign-up.
    - Implemented error handling for data formatting and API requests.

--- libs/domains/users-sign-up/feature/src/lib/hooks/use-create-user-sign-up/use-create-user-sign-up.ts
+++ libs/domains/users-sign-up/feature/src/lib/hooks/use-create-user-sign-up/use-create-user-sign-up.ts
@@ -1,15 +1,27 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { mutations } from '@qovery/domains/users-sign-up/data-access'
 import { queries } from '@qovery/state/util-queries'
+import { type CargoSignupPayload, useSignUpCargo } from '../use-signup-cargo/use-signup-cargo'
 
 export function useCreateUserSignUp() {
   const queryClient = useQueryClient()
+  const { mutate: signUpCargo } = useSignUpCargo()
 
   return useMutation(mutations.createUserSignup, {
-    onSuccess() {
+    onSuccess(_, variables) {
       queryClient.invalidateQueries({
         queryKey: queries.usersSignUp.get.queryKey,
       })
+
+      const cargoPayload: CargoSignupPayload = {
+        email: variables.user_email,
+        first_name: variables.first_name,
+        last_name: variables.last_name,
+        company: variables.company_name || '',
+        job_title: variables.user_role || '',
+        signup_source: 'Console',
+      }
+      signUpCargo(cargoPayload)
     },
     meta: {
       notifyOnError: true,
