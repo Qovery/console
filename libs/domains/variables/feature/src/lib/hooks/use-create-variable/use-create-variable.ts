32f3cf1d01ce09acbbf59be9bd566473a7d23a94
Romain Billard <billard.rom@gmail.com>
2025-05-09 18:12:13 +0200

    feat: show a toast with a redirection in case of 409

--- libs/domains/variables/feature/src/lib/hooks/use-create-variable/use-create-variable.ts
+++ libs/domains/variables/feature/src/lib/hooks/use-create-variable/use-create-variable.ts
@@ -1,5 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
 import { mutations } from '@qovery/domains/variables/data-access'
+import { SERVICES_URL, SERVICES_VARIABLES_URL } from '@qovery/shared/routes'
+import { toastError } from '@qovery/shared/ui'
 import { queries } from '@qovery/state/util-queries'
 
 export function useCreateVariable() {
@@ -10,8 +13,32 @@ export function useCreateVariable() {
         queryKey: queries.variables.list._def,
       })
     },
+    onError(
+      error: AxiosError<{ existing_variable: { organization_id: string; project_id: string; environment_id: string } }>,
+      variables,
+      context
+    ) {
+      if (error.code === '409') {
+        if (error.response?.data) {
+          const {
+            organization_id: orgId,
+            project_id: projectId,
+            environment_id: envId,
+          } = error.response.data.existing_variable
+          toastError(
+            error,
+            'Conflict',
+            error.message,
+            undefined,
+            undefined,
+            'Go to the conflicting variable',
+            `${SERVICES_URL(orgId, projectId, envId)}${SERVICES_VARIABLES_URL}`
+          )
+        }
+      }
+    },
     meta: {
-      notifyOnError: true,
+      notifyOnError: false,
     },
   })
 }
