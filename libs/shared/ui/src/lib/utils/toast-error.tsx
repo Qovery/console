0196c830325d7305b46d602b3875d87659b446bf
Romain Billard <billard.rom@gmail.com>
2025-05-09 18:10:31 +0200

    feat: updated toastError to handle extra params

--- libs/shared/ui/src/lib/utils/toast-error.tsx
+++ libs/shared/ui/src/lib/utils/toast-error.tsx
@@ -1,6 +1,23 @@
+import { type IconName } from '@fortawesome/fontawesome-common-types'
 import { type SerializedError } from '@qovery/shared/utils'
 import toast, { ToastEnum } from './toast'
 
-export function toastError(error: SerializedError | Error, title?: string, message?: string): void {
-  toast(ToastEnum.ERROR, title || error.name || 'Error', message || error.message || 'No message found')
+export function toastError(
+  error: SerializedError | Error,
+  title?: string,
+  description?: string,
+  callback?: () => void,
+  iconAction?: IconName,
+  labelAction?: string,
+  externalLink?: string
+): void {
+  toast(
+    ToastEnum.ERROR,
+    title || error.name || 'Error',
+    description || error.message || 'No message found',
+    callback,
+    iconAction,
+    labelAction,
+    externalLink
+  )
 }
