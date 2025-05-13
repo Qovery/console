5e9dc1329ff4897134e758f73d7e984bc65c0560
Remi Bonnet <bonnet.rem@gmail.com>
2025-05-13 14:37:39 +0200

    Add mock for libraries

--- libs/shared/devops-copilot/feature/jest.config.ts
+++ libs/shared/devops-copilot/feature/jest.config.ts
@@ -6,7 +6,6 @@ export default {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
-  transformIgnorePatterns: ['node_modules/(?!mermaid)'], // XXX: We need to transform the ESM module 'mermaid' because Jest ignores node_modules by default
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/shared/devops-copilot/feature',
 }
