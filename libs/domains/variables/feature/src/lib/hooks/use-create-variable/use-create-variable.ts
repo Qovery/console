5cd293f26482bff76f7b136c24b5aefca6dc87ba
Romain Billard <billard.rom@gmail.com>
2025-05-14 11:13:39 +0200

    fix: corrected a bug in the redirection URL

--- libs/domains/variables/feature/src/lib/hooks/use-create-variable/use-create-variable.ts
+++ libs/domains/variables/feature/src/lib/hooks/use-create-variable/use-create-variable.ts
@@ -1,7 +1,14 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import type { AxiosError } from 'axios'
 import { mutations } from '@qovery/domains/variables/data-access'
-import { SERVICES_URL, SERVICES_VARIABLES_URL } from '@qovery/shared/routes'
+import {
+  APPLICATION_URL,
+  APPLICATION_VARIABLES_URL,
+  ENVIRONMENTS_URL,
+  ENVIRONMENTS_VARIABLES_URL,
+  SERVICES_URL,
+  SERVICES_VARIABLES_URL,
+} from '@qovery/shared/routes'
 import { toastError } from '@qovery/shared/ui'
 import { queries } from '@qovery/state/util-queries'
 
@@ -14,7 +21,9 @@ export function useCreateVariable() {
       })
     },
     onError(
-      error: AxiosError<{ existing_variable: { organization_id: string; project_id: string; environment_id: string } }>,
+      error: AxiosError<{
+        existing_variable: { organization_id: string; project_id: string; environment_id: string; service_id: string }
+      }>,
       variables,
       context
     ) {
@@ -24,16 +33,14 @@ export function useCreateVariable() {
             organization_id: orgId,
             project_id: projectId,
             environment_id: envId,
+            service_id: serviceId,
           } = error.response.data.existing_variable
-          toastError(
-            error,
-            'Conflict',
-            error.message,
-            undefined,
-            undefined,
-            'Go to the conflicting variable',
-            `${SERVICES_URL(orgId, projectId, envId)}${SERVICES_VARIABLES_URL}`
-          )
+          const url = serviceId
+            ? `${APPLICATION_URL(orgId, projectId, envId, serviceId)}${APPLICATION_VARIABLES_URL}`
+            : envId
+              ? `${SERVICES_URL(orgId, projectId, envId)}${SERVICES_VARIABLES_URL}`
+              : `${ENVIRONMENTS_URL(orgId, projectId)}${ENVIRONMENTS_VARIABLES_URL}`
+          toastError(error, 'Conflict', error.message, undefined, undefined, 'Go to the conflicting variable', url)
         }
       }
     },
