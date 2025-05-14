def006b15e70ea577f6748334f8826eba34fda50
Romain Billard <billard.rom@gmail.com>
2025-05-14 19:24:25 +0200

    chore: brought back QOVERY_FORUM_URL variable

--- libs/shared/util-const/src/lib/shared-util-const.ts
+++ libs/shared/util-const/src/lib/shared-util-const.ts
@@ -1,3 +1,4 @@
+export const QOVERY_FORUM_URL = 'https://discuss.qovery.com'
 export const QOVERY_STATUS_URL = 'https://status.qovery.com'
 export const QOVERY_DOCS_URL = 'https://hub.qovery.com'
 export const QOVERY_FEEDBACK_URL = 'https://roadmap.qovery.com'
