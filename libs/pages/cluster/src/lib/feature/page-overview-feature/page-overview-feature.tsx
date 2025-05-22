c04fcc606d96c6cea4977dc30c8e72d5de087677
Remi Bonnet <bonnet.rem@gmail.com>
2025-05-22 15:04:25 +0200

    Add placeholder if no running status available

--- libs/pages/cluster/src/lib/feature/page-overview-feature/page-overview-feature.tsx
+++ libs/pages/cluster/src/lib/feature/page-overview-feature/page-overview-feature.tsx
@@ -1,13 +1,24 @@
 import { useParams } from 'react-router-dom'
 import { ClusterCardNodeUsage, ClusterCardResources, ClusterCardSetup } from '@qovery/domains/cluster-metrics/feature'
-import { useClusterRunningStatusSocket } from '@qovery/domains/clusters/feature'
+import { useClusterRunningStatus } from '@qovery/domains/clusters/feature'
+import { Icon } from '@qovery/shared/ui'
 import { useDocumentTitle } from '@qovery/shared/util-hooks'
 
 export function PageOverviewFeature() {
-  const { organizationId = '', clusterId = '' } = useParams()
-  useClusterRunningStatusSocket({ organizationId, clusterId })
-
   useDocumentTitle('Cluster - Overview')
+  const { organizationId = '', clusterId = '' } = useParams()
+  const { data: runningStatus } = useClusterRunningStatus({ organizationId, clusterId })
+
+  if (typeof runningStatus === 'string') {
+    return (
+      <div className="h-80 p-8">
+        <div className="flex h-full flex-col items-center justify-center gap-1 rounded border border-neutral-200 bg-neutral-100 py-10 text-sm text-neutral-350">
+          <Icon className="text-xl text-neutral-300" iconName="circle-info" iconStyle="regular" />
+          <span className="font-medium">No metrics available because the running status is unavailable.</span>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="grid gap-6 p-8 md:grid-cols-3">
