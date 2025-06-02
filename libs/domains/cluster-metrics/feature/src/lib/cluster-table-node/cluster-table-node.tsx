20807505bbcce10aec975578d601819d3a60e2d7
Remi Bonnet <bonnet.rem@gmail.com>
2025-06-02 13:59:52 +0200

    Fix color reserved progress bar

--- libs/domains/cluster-metrics/feature/src/lib/cluster-table-node/cluster-table-node.tsx
+++ libs/domains/cluster-metrics/feature/src/lib/cluster-table-node/cluster-table-node.tsx
@@ -48,7 +48,12 @@ function MetricProgressBar({ type, used, reserved, total, unit, isPressure = fal
             <div className="flex flex-col gap-1 px-2.5 py-1.5">
               <div className="flex w-full items-center gap-1.5">
                 <span className="flex items-center gap-2">
-                  <span className="bg-purple-20 h-2 w-2 rounded-full bg-purple-200" />
+                  <span
+                    className={clsx('h-2 w-2 rounded-full', {
+                      'bg-purple-200': usedPercentage <= reservedPercentage,
+                      'bg-purple-500': usedPercentage > reservedPercentage,
+                    })}
+                  />
                   Reserved
                 </span>
                 <span className="ml-auto block font-semibold">
