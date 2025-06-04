f7f988c462d081b706ca9c99184784ebbb6013dd
Romain Billard <billard.rom@gmail.com>
2025-06-04 10:36:45 +0200

    chore: clean ValuesOverrideArguments component

--- libs/domains/service-terraform/feature/src/values-override-arguments-setting/values-override-arguments-setting.tsx
+++ libs/domains/service-terraform/feature/src/values-override-arguments-setting/values-override-arguments-setting.tsx
@@ -9,7 +9,6 @@ import {
 } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
 import { FieldVariableSuggestion } from '@qovery/domains/variables/feature'
-import { HELM_DEFAULT_VALUES } from '@qovery/shared/routes'
 import { Button, Heading, Icon, InputTextSmall, Section } from '@qovery/shared/ui'
 
 export interface TerraformValuesArgumentsData {
@@ -26,9 +25,9 @@ export interface ValuesOverrideArgumentsSettingProps extends PropsWithChildren {
   onSubmit: () => void
 }
 
-function Row({ index, remove }: { index: number; remove: UseFieldArrayRemove }) {
+function VarRow({ index, remove }: { index: number; remove: UseFieldArrayRemove }) {
   const { environmentId = '' } = useParams()
-  const { watch, control } = useFormContext()
+  const { control } = useFormContext()
 
   return (
     <li className="mb-3 last:mb-0">
@@ -83,17 +82,53 @@ function Row({ index, remove }: { index: number; remove: UseFieldArrayRemove }) 
   )
 }
 
-export function ValuesOverrideArgumentsSetting({
-  methods,
-  children,
-  onSubmit,
-  source,
-}: ValuesOverrideArgumentsSettingProps) {
-  const { environmentId = '' } = useParams()
+function PathRow({
+  field,
+  index,
+  tfPathsRemove,
+}: {
+  field: Record<'id', string>
+  index: number
+  tfPathsRemove: (index: number) => void
+}) {
+  const { control } = useFormContext()
 
-  console.log('methods', methods)
-  console.log('values', methods.getValues())
+  return (
+    <li key={`${field.id}-${index}`} className="mb-3 last:mb-0">
+      <div className="flex items-center gap-x-2">
+        <Controller
+          name={`tf_var_file_paths.${index}`}
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field, fieldState: { error } }) => (
+            <InputTextSmall
+              name={field.name}
+              value={field.value}
+              onChange={field.onChange}
+              error={error?.message}
+              className="w-full"
+            />
+          )}
+        />
 
+        <Button
+          size="md"
+          color="neutral"
+          variant="outline"
+          type="button"
+          className="h-[36px]"
+          onClick={() => tfPathsRemove(index)}
+        >
+          <Icon iconName="trash" className="text-neutral-400" />
+        </Button>
+      </div>
+    </li>
+  )
+}
+
+export function ValuesOverrideArgumentsSetting({ methods, children, onSubmit }: ValuesOverrideArgumentsSettingProps) {
   const {
     fields: tfVars,
     append: tfVarsAppend,
@@ -109,16 +144,6 @@ export function ValuesOverrideArgumentsSetting({
   } = useFieldArray({
     name: 'tf_var_file_paths',
   })
-
-  const createHelmDefaultValuesMutation = () => {
-    const payload = {
-      environmentId,
-      helmDefaultValuesRequest: {
-        source,
-      },
-    }
-    window.open(`${HELM_DEFAULT_VALUES}?payload=${encodeURIComponent(JSON.stringify(payload))}`, '_blank')
-  }
 
   return (
     <Section className="items-start">
@@ -149,13 +174,12 @@ export function ValuesOverrideArgumentsSetting({
             {tfVars.length > 0 && (
               <ul>
                 <li className="mb-3 grid grid-cols-[6fr_6fr_1fr] gap-x-2">
-                  {/* <span className="text-sm font-medium text-neutral-400">Override type</span> */}
                   <span className="text-sm font-medium text-neutral-400">Key</span>
                   <span className="text-sm font-medium text-neutral-400">Value</span>
                   <span></span>
                 </li>
                 {tfVars.map((field, index) => (
-                  <Row key={field.id} index={index} remove={tfVarsRemove} />
+                  <VarRow key={field.id} index={index} remove={tfVarsRemove} />
                 ))}
               </ul>
             )}
@@ -179,38 +203,7 @@ export function ValuesOverrideArgumentsSetting({
                   <span className="text-sm font-medium text-neutral-400">Path</span>
                 </li>
                 {tfPaths.map((field, index) => (
-                  <li key={`${field.id}-${index}`} className="mb-3 last:mb-0">
-                    <div className="flex items-center gap-x-2">
-                      <Controller
-                        name={`tf_var_file_paths.${index}`}
-                        control={methods.control}
-                        rules={{
-                          required: true,
-                        }}
-                        render={({ field, fieldState: { error } }) => (
-                          <InputTextSmall
-                            name={field.name}
-                            value={field.value}
-                            onChange={field.onChange}
-                            error={error?.message}
-                            className="w-full"
-                          />
-                        )}
-                      />
-
-                      <Button
-                        size="md"
-                        color="neutral"
-                        variant="outline"
-                        type="button"
-                        className="h-[36px]"
-                        onClick={() => tfPathsRemove(index)}
-                      >
-                        <Icon iconName="trash" className="text-neutral-400" />
-                      </Button>
-                    </div>
-                  </li>
-                  // <Row key={field.id} index={index} remove={tfVarsRemove} />
+                  <PathRow key={field.id} field={field} index={index} tfPathsRemove={tfPathsRemove} />
                 ))}
               </ul>
             )}
