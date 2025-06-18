189ad5de9ad0086617f2a50bfa2c626f8733ab00
Remi Bonnet <bonnet.rem@gmail.com>
2025-06-18 16:05:34 +0200

    feat(cluster-overview): first version

--- libs/domains/cluster-metrics/feature/src/lib/cluster-card-resources/cluster-card-resources.tsx
+++ libs/domains/cluster-metrics/feature/src/lib/cluster-card-resources/cluster-card-resources.tsx
@@ -21,21 +21,25 @@ export function ClusterCardResources({ organizationId, clusterId }: ClusterCardR
     label: string
     icon: IconName
     value: ResourcesProps
+    isPercentage?: boolean
   }[] = [
     {
       label: 'CPU reserved',
       icon: 'microchip',
       value: clusterResources.cpu,
+      isPercentage: true,
     },
     {
       label: 'Memory reserved',
       icon: 'memory',
       value: clusterResources.memory,
+      isPercentage: true,
     },
     {
       label: 'Disk usage',
       icon: 'hard-drive',
       value: clusterResources.disk,
+      isPercentage: false,
     },
   ]
 
@@ -43,26 +47,41 @@ export function ClusterCardResources({ organizationId, clusterId }: ClusterCardR
     <div className="flex flex-col gap-2.5 rounded border border-neutral-250 p-4">
       <p className="text-sm text-neutral-350">Total cluster resources</p>
       <ul className="flex w-full flex-col text-sm text-neutral-400">
-        {resources.map(({ label, icon, value }) => (
-          <li key={label} className="grid h-8 w-full grid-cols-[1fr_auto] items-center gap-1 p-1.5">
-            <span className="flex max-w-[140px] items-center gap-2.5 overflow-hidden">
-              <Icon className="shrink-0 text-base text-neutral-300" iconName={icon} iconStyle="regular" />
-              <span className="truncate whitespace-nowrap">{label}</span>
-            </span>
-            <Skeleton width={160} height={20} show={typeof metrics !== 'object' ? true : false}>
-              <p className="flex items-center gap-1 text-right text-neutral-400">
-                <span className="font-medium">{value.used}</span>
-                <span className="flex items-center gap-1.5 text-neutral-350">
-                  <span>
-                    /{value.total} {value.unit}
+        {resources.map(({ label, icon, value, isPercentage }) =>
+          isPercentage ? (
+            <li key={label} className="grid h-8 w-full grid-cols-[1fr_auto] items-center gap-1 p-1.5">
+              <span className="flex max-w-[140px] items-center gap-2.5 overflow-hidden">
+                <Icon className="shrink-0 text-base text-neutral-300" iconName={icon} iconStyle="regular" />
+                <span className="truncate whitespace-nowrap">{label}</span>
+              </span>
+              <Skeleton width={160} height={20} show={typeof metrics !== 'object' ? true : false}>
+                <p className="flex items-center gap-1 text-right text-neutral-400">
+                  <span className="font-medium">{value.used}</span>
+                  <span className="flex items-center gap-1.5 text-neutral-350">
+                    <span>
+                      /{value.total} {value.unit}
+                    </span>
+                    <span className="block h-[3px] w-[3px] rounded-full bg-neutral-300" />
+                    {value.percent}%
                   </span>
-                  <span className="block h-[3px] w-[3px] rounded-full bg-neutral-300" />
-                  {value.percent}%
-                </span>
-              </p>
-            </Skeleton>
-          </li>
-        ))}
+                </p>
+              </Skeleton>
+            </li>
+          ) : (
+            <li key={label} className="grid h-8 w-full grid-cols-[1fr_auto] items-center gap-1 p-1.5">
+              <span className="flex max-w-[140px] items-center gap-2.5 overflow-hidden">
+                <Icon className="shrink-0 text-base text-neutral-300" iconName={icon} iconStyle="regular" />
+                <span className="truncate whitespace-nowrap">{label}</span>
+              </span>
+              <Skeleton width={160} height={20} show={typeof metrics !== 'object' ? true : false}>
+                <p className="flex items-center gap-1 text-right text-neutral-400">
+                  <span className="font-medium">{value.total}</span>
+                  <span className="text-neutral-350">{value.unit}</span>
+                </p>
+              </Skeleton>
+            </li>
+          )
+        )}
       </ul>
     </div>
   )
