730ab281b0c10f7a1863ab704aa9a47a383450cf
Remi Bonnet <bonnet.rem@gmail.com>
2025-07-07 10:07:28 +0200

    chore: imp configuration devops copilot data-access

--- libs/shared/devops-copilot/data-access/jest.config.ts
+++ libs/shared/devops-copilot/data-access/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'data-access',
+  displayName: 'shared-devops-copilot-data-access',
   preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
