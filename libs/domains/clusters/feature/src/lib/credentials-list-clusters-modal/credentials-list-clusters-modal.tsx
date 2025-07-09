d09b38a43ac04eb25da2f23503d99ae58b804563
Romain Billard <billard.rom@gmail.com>
2025-07-09 10:50:56 +0200

    impr: remove edit button from credential dropdown

--- libs/domains/clusters/feature/src/lib/credentials-list-clusters-modal/credentials-list-clusters-modal.tsx
+++ libs/domains/clusters/feature/src/lib/credentials-list-clusters-modal/credentials-list-clusters-modal.tsx
@@ -2,7 +2,7 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { type ClusterCredentials, type CredentialCluster } from 'qovery-typescript-axios'
 import { Link } from 'react-router-dom'
 import { CLUSTER_SETTINGS_CREDENTIALS_URL, CLUSTER_SETTINGS_URL, CLUSTER_URL } from '@qovery/shared/routes'
-import { Callout, Heading, Icon, Section } from '@qovery/shared/ui'
+import { Heading, Section } from '@qovery/shared/ui'
 import { pluralize } from '@qovery/shared/util-js'
 import { ClusterAvatar } from '../cluster-avatar/cluster-avatar'
 
@@ -26,32 +26,20 @@ export function CredentialsListClustersModal({
         </Heading>
       </Dialog.Title>
       <Dialog.Description className="mb-6 text-sm text-neutral-350">Credential: {credential.name}</Dialog.Description>
-      <div className="flex flex-col gap-y-4">
-        <Callout.Root color="yellow">
-          <Callout.Icon>
-            <Icon iconName="circle-info" iconStyle="regular" />
-          </Callout.Icon>
-          <Callout.Text>
-            The credential change won't be applied to the mirroring registry of this cluster. Make sure to update the
-            credentials properly in this cluster's mirroring registry section.
-          </Callout.Text>
-        </Callout.Root>
-
-        <div className="flex flex-col gap-y-1 rounded border border-neutral-250 bg-neutral-100 p-2">
-          {clusters.map((cluster) => (
-            <Link
-              key={cluster.id}
-              to={`${CLUSTER_URL(organizationId, cluster.id)}${CLUSTER_SETTINGS_URL}${CLUSTER_SETTINGS_CREDENTIALS_URL}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex items-center gap-1.5 text-sm font-medium text-brand-500 hover:text-brand-600">
-                <ClusterAvatar cloudProvider={cluster.cloud_provider} size="sm" />
-                {cluster.name}
-              </div>
-            </Link>
-          ))}
-        </div>
+      <div className="flex flex-col gap-y-1 rounded border border-neutral-250 bg-neutral-100 p-2">
+        {clusters.map((cluster) => (
+          <Link
+            key={cluster.id}
+            to={`${CLUSTER_URL(organizationId, cluster.id)}${CLUSTER_SETTINGS_URL}${CLUSTER_SETTINGS_CREDENTIALS_URL}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="flex items-center gap-1.5 text-sm font-medium text-brand-500 hover:text-brand-600">
+              <ClusterAvatar cloudProvider={cluster.cloud_provider} size="sm" />
+              {cluster.name}
+            </div>
+          </Link>
+        ))}
       </div>
     </Section>
   )
--- libs/pages/cluster/src/lib/ui/page-settings-credentials/page-settings-credentials.tsx
+++ libs/pages/cluster/src/lib/ui/page-settings-credentials/page-settings-credentials.tsx
@@ -1,10 +1,8 @@
 import { type CloudProviderEnum } from 'qovery-typescript-axios'
 import { type FormEventHandler } from 'react'
 import { useFormContext } from 'react-hook-form'
-import { useParams } from 'react-router-dom'
 import { ClusterCredentialsSettingsFeature, SettingsHeading } from '@qovery/shared/console-shared'
-import { SETTINGS_CREDENTIALS_URL, SETTINGS_URL } from '@qovery/shared/routes'
-import { BlockContent, Button, Icon, Link, Section } from '@qovery/shared/ui'
+import { BlockContent, Button, Section } from '@qovery/shared/ui'
 
 export interface PageSettingsCredentialsProps {
   onSubmit: FormEventHandler<HTMLFormElement>
@@ -13,26 +11,13 @@ export interface PageSettingsCredentialsProps {
 }
 
 export function PageSettingsCredentials(props: PageSettingsCredentialsProps) {
-  const { organizationId = '' } = useParams()
   const { onSubmit, loading, cloudProvider } = props
   const { formState } = useFormContext()
 
   return (
     <div className="flex w-full flex-col justify-between">
       <Section className="max-w-content-with-navigation-left p-8">
-        <SettingsHeading
-          title="Credentials"
-          description={
-            <Link
-              color="current"
-              to={`${SETTINGS_URL(organizationId)}${SETTINGS_CREDENTIALS_URL}`}
-              className="flex gap-1 text-brand-500"
-            >
-              See all my cloud credentials
-              <Icon iconName="key" iconStyle="regular" />
-            </Link>
-          }
-        />
+        <SettingsHeading title="Credentials" />
         <form onSubmit={onSubmit}>
           <BlockContent title="Configured credentials">
             <ClusterCredentialsSettingsFeature cloudProvider={cloudProvider} isSetting={true} />
