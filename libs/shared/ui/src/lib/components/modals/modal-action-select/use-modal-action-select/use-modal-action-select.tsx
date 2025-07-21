ae2b5fd8bf44aed55e8323fcae792bd8cec3bcba
Romain Billard <billard.rom@gmail.com>
2025-07-21 18:04:10 +0200

    feat: add new service removal modal

---
+++ libs/shared/ui/src/lib/components/modals/modal-action-select/use-modal-action-select/use-modal-action-select.tsx
@@ -0,0 +1,49 @@
+import { type IconName } from '@fortawesome/fontawesome-common-types'
+import { type EnvironmentModeEnum } from 'qovery-typescript-axios'
+import { type ReactNode, useEffect, useState } from 'react'
+import useModal from '../../../modal/use-modal/use-modal'
+import { ModalActionSelect } from '../modal-action-select'
+
+export interface ActionSelectItem {
+  id: string // Also used as the text the user has to type to confirm
+  title: string
+  callback: () => void
+  description?: ReactNode
+  icon?: IconName
+  color?: 'brand' | 'red' | 'yellow' | 'green' | 'neutral'
+}
+
+export interface UseModalActionSelectProps {
+  title: string
+  actions: ActionSelectItem[]
+  description?: ReactNode
+  name?: string
+  mode?: keyof typeof EnvironmentModeEnum | string | undefined
+  warning?: ReactNode
+  isDelete?: boolean
+}
+
+export function useModalActionSelect() {
+  const [modalActionSelect, openModalActionSelect] = useState<UseModalActionSelectProps>()
+  const { openModal } = useModal()
+
+  useEffect(() => {
+    if (modalActionSelect) {
+      openModal({
+        content: (
+          <ModalActionSelect
+            title={modalActionSelect.title}
+            actions={modalActionSelect.actions}
+            description={modalActionSelect.description}
+            name={modalActionSelect.name}
+            warning={modalActionSelect.warning}
+          />
+        ),
+      })
+    }
+  }, [modalActionSelect, openModal])
+
+  return { openModalActionSelect }
+}
+
+export default useModalActionSelect
