899e1aa167c0d001d607eff19904b600fc057896
Romain Billard <rmnbrd@users.noreply.github.com>
2025-08-21 10:15:41 +0200

    feat: add banner for overdue invoices (#1992)
    * feat: add banner for overdue invoices
    
    * impr: post-review fixes

---
+++ libs/domains/organizations/feature/src/lib/invoice-banner/invoice-banner.tsx
@@ -0,0 +1,36 @@
+import { InvoiceStatusEnum } from 'qovery-typescript-axios'
+import { useMemo } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useUserRole } from '@qovery/shared/iam/feature'
+import { SETTINGS_BILLING_SUMMARY_URL, SETTINGS_URL } from '@qovery/shared/routes'
+import { Banner } from '@qovery/shared/ui'
+import useInvoices from '../hooks/use-invoices/use-invoices'
+
+export const InvoiceBanner = () => {
+  const navigate = useNavigate()
+  const { organizationId = '' } = useParams()
+  const { roles } = useUserRole()
+  const { data: invoices, isLoading } = useInvoices({ organizationId })
+  const unpaidInvoices = invoices?.filter((invoice) => invoice.status === InvoiceStatusEnum.NOT_PAID)
+
+  const isAdminOrOwnerOfCompany = useMemo(
+    () =>
+      roles.some((role) => role.includes(`organization:${organizationId}:admin`)) ||
+      roles.some((role) => role.includes(`organization:${organizationId}:owner`)),
+    [roles, organizationId]
+  )
+
+  const onClick = () => {
+    navigate(SETTINGS_URL(organizationId) + SETTINGS_BILLING_SUMMARY_URL)
+  }
+
+  if (unpaidInvoices?.length === 0 || isLoading || !isAdminOrOwnerOfCompany) {
+    return null
+  }
+
+  return (
+    <Banner color="yellow" buttonIconRight="arrow-right" buttonLabel="View invoices" onClickButton={onClick}>
+      <p>You have overdue invoices. Please update your payment method to avoid service interruption</p>
+    </Banner>
+  )
+}
