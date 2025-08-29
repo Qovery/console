846b622cf109822d01bc7d5cb99f9a0875c97338
Romain Billard <billard.rom@gmail.com>
2025-08-29 16:17:19 +0200

    fix: remove instance_type from header

--- libs/pages/cluster/src/lib/ui/container/container.tsx
+++ libs/pages/cluster/src/lib/ui/container/container.tsx
@@ -79,7 +79,7 @@ export function Container({ children }: PropsWithChildren) {
               )}
             </Skeleton>
             <Skeleton width={120} height={22} show={!cluster}>
-              {cluster?.instance_type && (
+              {cluster?.instance_type && cluster.kubernetes !== 'PARTIALLY_MANAGED' && (
                 <Badge color="neutral" variant="surface">
                   {cluster?.instance_type?.toLowerCase().replace('_', '.')}
                 </Badge>
