2bbf0edbd19e624e8173b8d3968a3845a8c89bf4
Romain Billard <billard.rom@gmail.com>
2025-09-16 16:23:21 +0200

    feat(terraform): add "contact us" to Terraform card

--- libs/pages/services/src/lib/feature/page-new-feature/page-new-feature.spec.tsx
+++ libs/pages/services/src/lib/feature/page-new-feature/page-new-feature.spec.tsx
@@ -1,9 +1,14 @@
+import { IntercomProvider } from 'react-use-intercom'
 import { renderWithProviders } from '@qovery/shared/util-tests'
 import PageNewFeature from './page-new-feature'
 
 describe('General', () => {
   it('should render successfully', () => {
-    const { baseElement } = renderWithProviders(<PageNewFeature />)
+    const { baseElement } = renderWithProviders(
+      <IntercomProvider appId="__test__app__id__" autoBoot={false}>
+        <PageNewFeature />
+      </IntercomProvider>
+    )
     expect(baseElement).toBeTruthy()
   })
 })
