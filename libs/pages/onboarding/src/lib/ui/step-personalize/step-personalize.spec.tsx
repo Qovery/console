7212f743b9f7302a532ebb3dc656f01d787e1f45
RemiBonnet <bonnet.rem@gmail.com>
2025-10-14 16:14:05 +0200

    fix: test

--- libs/pages/onboarding/src/lib/ui/step-personalize/step-personalize.spec.tsx
+++ libs/pages/onboarding/src/lib/ui/step-personalize/step-personalize.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '__tests__/utils/setup-jest'
+import { wrapWithReactHookForm } from '__tests__/utils/wrap-with-react-hook-form'
 import { type TypeOfUseEnum } from 'qovery-typescript-axios'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import StepPersonalize, { type StepPersonalizeProps } from './step-personalize'
 
 describe('StepPersonalize', () => {
@@ -15,24 +16,36 @@ describe('StepPersonalize', () => {
     }
 
     const Wrapper = () => {
-      const { control } = useForm<{
+      const methods = useForm<{
         first_name: string
         last_name: string
         user_email: string
         type_of_use: TypeOfUseEnum
         infrastructure_hosting: string
-      }>()
+      }>({ mode: 'all' })
 
-      props.control = control
-
-      return <StepPersonalize {...(props as StepPersonalizeProps)} />
+      return (
+        <FormProvider {...methods}>
+          <StepPersonalize {...(props as StepPersonalizeProps)} />
+        </FormProvider>
+      )
     }
 
     render(<Wrapper />)
   })
 
   it('should render successfully', () => {
-    const { baseElement } = render(<StepPersonalize {...(props as StepPersonalizeProps)} />)
+    const { baseElement } = render(
+      wrapWithReactHookForm(<StepPersonalize {...(props as StepPersonalizeProps)} />, {
+        defaultValues: {
+          first_name: '',
+          last_name: '',
+          user_email: '',
+          type_of_use: undefined,
+          infrastructure_hosting: '',
+        },
+      })
+    )
     expect(baseElement).toBeTruthy()
   })
 })
