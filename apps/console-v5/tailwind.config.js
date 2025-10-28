3beb571650cdb3e175f676d315955da88374f86a
RemiBonnet <bonnet.rem@gmail.com>
2025-10-28 09:44:59 +0100

    Add toggle theme and new app console-v5

---
+++ apps/console-v5/tailwind.config.js
@@ -0,0 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind')
+const { join } = require('path')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
