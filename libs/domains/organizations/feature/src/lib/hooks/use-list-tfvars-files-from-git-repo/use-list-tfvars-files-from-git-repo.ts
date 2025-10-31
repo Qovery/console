ba04ffe9b98e691322ce09f44d87cbbc0b6c0219
Romain Billard <billard.rom@gmail.com>
2025-10-31 13:46:59 +0100

    feat: fetch tfVars from git repo

---
+++ libs/domains/organizations/feature/src/lib/hooks/use-list-tfvars-files-from-git-repo/use-list-tfvars-files-from-git-repo.ts
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query'
+import { type ApplicationGitRepositoryRequest, type TfVarsDiscoveryMode } from 'qovery-typescript-axios'
+import { queries } from '@qovery/state/util-queries'
+
+export interface UseListTfVarsFilesFromGitRepoProps {
+  organizationId: string
+  repository: ApplicationGitRepositoryRequest
+  mode: TfVarsDiscoveryMode
+  enabled?: boolean
+}
+
+export function useListTfVarsFilesFromGitRepo({
+  organizationId,
+  repository,
+  mode,
+  enabled = true,
+}: UseListTfVarsFilesFromGitRepoProps) {
+  return useQuery({
+    ...queries.organizations.listTfVarsFilesFromGitRepo({ organizationId, repository, mode }),
+    enabled,
+    meta: {
+      notifyOnError: true,
+    },
+    refetchOnWindowFocus: false,
+    staleTime: 0,
+  })
+}
