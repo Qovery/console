783773524434b0a512930a98254b28da8e054395
Romain Billard <billard.rom@gmail.com>
2025-11-05 15:07:00 +0100

    impr: moved components to new file

--- libs/domains/service-terraform/feature/src/index.ts
+++ libs/domains/service-terraform/feature/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/terraform-configuration-settings/terraform-configuration-settings'
 export * from './source-setting/source-setting'
+export * from './lib/terraform-variables-settings/terraform-variables-settings'
