6b89f865f968293ba519fd649f2532b9caee00af
Σrebe - Romain GERARD <erebe@erebe.eu>
2025-11-13 15:14:56 +0100

    feat(terraform): Add auto-deploy support

--- libs/domains/services/feature/src/lib/auto-deploy-setting/auto-deploy-setting.tsx
+++ libs/domains/services/feature/src/lib/auto-deploy-setting/auto-deploy-setting.tsx
@@ -1,8 +1,9 @@
 import { Controller, useFormContext } from 'react-hook-form'
+import { match } from 'ts-pattern'
 import { InputToggle } from '@qovery/shared/ui'
 
 export interface AutoDeploySettingProps {
-  source: 'CONTAINER_REGISTRY' | 'GIT'
+  source: 'CONTAINER_REGISTRY' | 'GIT' | 'TERRAFORM'
   className?: string
 }
 
@@ -13,21 +14,30 @@ export function AutoDeploySetting({ source, className = '' }: AutoDeploySettingP
     <Controller
       name="auto_deploy"
       control={control}
-      render={({ field }) => (
-        <InputToggle
-          className={className}
-          value={field.value}
-          onChange={field.onChange}
-          title="Auto-deploy"
-          description={
-            source === 'CONTAINER_REGISTRY'
-              ? 'The service will be automatically updated if Qovery is notified on the API that a new image tag is available.'
-              : 'The service will be automatically updated on every new commit on the branch.'
-          }
-          forceAlignTop
-          small
-        />
-      )}
+      render={({ field }) => {
+        return (
+          <InputToggle
+            className={className}
+            value={field.value}
+            onChange={field.onChange}
+            title="Auto-deploy"
+            description={match(source)
+              .with(
+                'TERRAFORM',
+                () => 'A terraform plan will be automatically triggered on every new commit on the branch.'
+              )
+              .with('GIT', () => 'The service will be automatically updated on every new commit on the branch.')
+              .with(
+                'CONTAINER_REGISTRY',
+                () =>
+                  'The service will be automatically updated if Qovery is notified on the API that a new image tag is available.'
+              )
+              .exhaustive()}
+            forceAlignTop
+            small
+          />
+        )
+      }}
     />
   )
 }
