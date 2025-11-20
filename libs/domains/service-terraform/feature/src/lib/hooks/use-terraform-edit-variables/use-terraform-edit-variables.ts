4796a1f417b9a58876c24a4e432f72150e021d28
Romain Billard <billard.rom@gmail.com>
2025-11-20 16:14:44 +0100

    impr: remove useless log statements

--- libs/domains/service-terraform/feature/src/lib/hooks/use-terraform-edit-variables/use-terraform-edit-variables.ts
+++ libs/domains/service-terraform/feature/src/lib/hooks/use-terraform-edit-variables/use-terraform-edit-variables.ts
@@ -3,12 +3,6 @@ import { mutations } from '@qovery/domains/services/data-access'
 
 export function useTerraformEditVariables() {
   return useMutation(mutations.replaceAllTerraformVariables, {
-    onSuccess(data) {
-      console.log('🚀 ~ onSuccess ~ data:', data)
-    },
-    onError(error) {
-      console.error('🚀 ~ onError ~ error:', error)
-    },
     meta: {
       notifyOnSuccess: false,
       notifyOnError: false,
