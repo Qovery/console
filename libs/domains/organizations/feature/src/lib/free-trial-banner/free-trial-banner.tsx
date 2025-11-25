b1b2710bb520c9caf4e7b9927a509f0c457334d8
Théo Grandin <theo.grandin74@gmail.com>
2025-11-25 16:06:52 +0100

    Correct free trial banner to be shown when no credit card are saved

--- libs/domains/organizations/feature/src/lib/free-trial-banner/free-trial-banner.tsx
+++ libs/domains/organizations/feature/src/lib/free-trial-banner/free-trial-banner.tsx
@@ -1,6 +1,7 @@
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
-import { SETTINGS_BILLING_SUMMARY_URL, SETTINGS_URL } from '@qovery/shared/routes'
+import { SETTINGS_BILLING_SUMMARY_URL, SETTINGS_BILLING_URL, SETTINGS_URL } from '@qovery/shared/routes'
 import { Banner } from '@qovery/shared/ui'
+import useCreditCards from '../hooks/use-credit-cards/use-credit-cards'
 import useCurrentCost from '../hooks/use-current-cost/use-current-cost'
 
 export function FreeTrialBanner() {
@@ -8,19 +9,23 @@ export function FreeTrialBanner() {
   const { pathname } = useLocation()
   const navigate = useNavigate()
   const { data: currentCost } = useCurrentCost({ organizationId })
+  const { data: creditCards = [], isLoading: isLoadingCreditCards } = useCreditCards({ organizationId })
 
+  const hasCreditCard = creditCards.length > 0
   const remainingTrialDays = currentCost?.remaining_trial_day
 
   const isOnOrganizationBillingSummaryPage = pathname.includes(
     SETTINGS_URL(organizationId) + SETTINGS_BILLING_SUMMARY_URL
   )
 
-  if (
-    remainingTrialDays === undefined ||
-    remainingTrialDays < 0 ||
-    remainingTrialDays > 2 ||
-    isOnOrganizationBillingSummaryPage
-  ) {
+  if (remainingTrialDays === undefined || remainingTrialDays < 0 || isOnOrganizationBillingSummaryPage) {
+    return null
+  }
+
+  const showForCreditCard = hasCreditCard && remainingTrialDays <= 2
+  const showForNoCreditCard = !hasCreditCard
+
+  if (!showForCreditCard && !showForNoCreditCard) {
     return null
   }
 
@@ -28,15 +33,30 @@ export function FreeTrialBanner() {
 
   const onClick = () => {
     if (!organizationId) return
-    navigate(SETTINGS_URL(organizationId) + SETTINGS_BILLING_SUMMARY_URL)
+    const target = hasCreditCard
+      ? SETTINGS_URL(organizationId) + SETTINGS_BILLING_SUMMARY_URL
+      : SETTINGS_URL(organizationId) + SETTINGS_BILLING_URL
+    navigate(target)
   }
 
   return (
-    <Banner color="yellow" buttonIconRight="arrow-right" buttonLabel="Manage billing" onClickButton={onClick}>
-      <p>
-        Your free trial plan expires {remainingTrialDays} {dayLabel} from now. Your subscription will start
-        automatically at the end of your trial.
-      </p>
+    <Banner
+      color={hasCreditCard ? 'yellow' : 'red'}
+      buttonIconRight="arrow-right"
+      buttonLabel={hasCreditCard ? 'Manage billing' : 'Add credit card'}
+      onClickButton={onClick}
+    >
+      {hasCreditCard || isLoadingCreditCards ? (
+        <p>
+          Your free trial plan expires {remainingTrialDays} {dayLabel} from now. Your subscription will start
+          automatically at the end of your trial.
+        </p>
+      ) : (
+        <p>
+          No credit card registered, your account will be blocked at the end your trial in {remainingTrialDays}{' '}
+          {dayLabel}
+        </p>
+      )}
     </Banner>
   )
 }
