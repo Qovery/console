72414d4f90c7470dfe77188d74a2b74a5f9b935a
Rémi Bonnet <bonnet.rem@gmail.com>
2025-11-26 17:14:32 +0100

    feat(alerting): add issues view (#2172)

--- libs/domains/observability/feature/src/lib/hooks/use-alerts/use-alerts.tsx
+++ libs/domains/observability/feature/src/lib/hooks/use-alerts/use-alerts.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import { observability } from '@qovery/domains/observability/data-access'
 
-export function useAlertRules({ organizationId, serviceId }: { organizationId: string; serviceId?: string }) {
+export function useAlerts({ organizationId }: { organizationId: string }) {
   return useQuery({
     ...observability.alertRules({ organizationId }),
-    select: (data) => (serviceId ? data.filter((rule) => rule.target.target_id === serviceId) : data),
+    select: (data) => data.filter((rule) => rule.state === 'NOTIFIED'),
     enabled: Boolean(organizationId),
   })
 }
 
-export default useAlertRules
+export default useAlerts
--- libs/shared/routes/src/lib/sub-router/alerting.router.ts
+++ libs/shared/routes/src/lib/sub-router/alerting.router.ts
@@ -1,2 +1,3 @@
 export const ALERTING_URL = (organizationId = ':organizationId') => `/organization/${organizationId}/alerting`
+export const ALERTING_ISSUES_URL = '/issues'
 export const ALERTING_NOTIFICATION_CHANNEL_URL = '/notification-channel'
