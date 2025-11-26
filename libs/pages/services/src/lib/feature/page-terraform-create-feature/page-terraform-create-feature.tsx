8c2efbaca92f80b518e47cb36f686789fa730ad2
Romain Billard <billard.rom@gmail.com>
2025-11-26 13:59:19 +0100

    feat: add backend selection to TF config view

--- libs/pages/services/src/lib/feature/page-terraform-create-feature/page-terraform-create-feature.tsx
+++ libs/pages/services/src/lib/feature/page-terraform-create-feature/page-terraform-create-feature.tsx
@@ -66,6 +66,9 @@ export function PageTerraformCreateFeature() {
       timeout_sec: '3600',
       use_cluster_credentials: true,
       engine: 'TERRAFORM',
+      backend: {
+        kubernetes: {},
+      },
     },
   })
 
