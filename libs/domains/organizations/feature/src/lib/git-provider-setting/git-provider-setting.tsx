f6f25eff338de968dca78a71696ec22a1eb055d3
Rémi Bonnet <bonnet.rem@gmail.com>
2025-12-12 18:21:55 +0100

    fix(git): popover documentation link (#2224)
    fix: documentation

--- libs/domains/organizations/feature/src/lib/git-provider-setting/git-provider-setting.tsx
+++ libs/domains/organizations/feature/src/lib/git-provider-setting/git-provider-setting.tsx
@@ -132,6 +132,9 @@ export function GitProviderSetting({ disabled }: GitProviderSettingProps) {
                         }}
                       />
                     ),
+                    options: {
+                      fakeModal: true,
+                    },
                   })
                 },
               }}
