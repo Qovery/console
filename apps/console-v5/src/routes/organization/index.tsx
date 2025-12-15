86531262a1974931b050400e4f05fc83c28f1cd4
Romain Billard <rmnbrd@users.noreply.github.com>
2025-12-15 16:03:13 +0100

    fix(new-nav): update router and its layouts (#2227)
    feat: add login route

--- apps/console-v5/src/routes/organization/index.tsx
+++ apps/console-v5/src/routes/organization/index.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Outlet, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/organization/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/organization/"!</div>
+  return (
+    <div className="bg-background h-full min-h-dvh w-full">
+      <main className="!h-full">
+        <div className="m-auto mt-6 h-full w-full max-w-7xl">
+          {/* EMPTY FOR NOW */}
+          {/* TODO: Add organization list or something */}
+        </div>
+      </main>
+    </div>
+  )
 }
