563c6de6d5b70c6654fffef68147342bfa90a02e
Rémi Bonnet <bonnet.rem@gmail.com>
2025-12-15 17:40:14 +0100

    feat(organization-overview): add webflow changelog section (#2230)
    * Init organization overview
    
    * Add webflow libraries

--- apps/console-v5/src/routes/organization/$orgId/overview.tsx
+++ apps/console-v5/src/routes/organization/$orgId/overview.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute, useParams } from '@tanstack/react-router'
-import { Button, DropdownMenu } from '../../../../../../libs/shared/ui/src'
-import { useTheme } from '../../../app/components/theme-provider/theme-provider'
+import { OrganizationOverview } from '@qovery/domains/organizations/feature'
 
 export const Route = createFileRoute('/organization/$orgId/overview')({
   component: RouteComponent,
@@ -8,25 +7,10 @@ export const Route = createFileRoute('/organization/$orgId/overview')({
 
 function RouteComponent() {
   const { orgId } = useParams({ strict: false })
-  const { setTheme } = useTheme()
 
-  return (
-    <div className="flex flex-col gap-4">
-      <p className="text-neutral">Overview page for organization {orgId}</p>
+  if (!orgId) {
+    return null
+  }
 
-      <div>
-        <h1 className="text-3xl font-bold text-brand">Console V5</h1>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger asChild>
-            <Button variant="outline">Toggle theme</Button>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content align="end">
-            <DropdownMenu.Item onClick={() => setTheme('light')}>Light</DropdownMenu.Item>
-            <DropdownMenu.Item onClick={() => setTheme('dark')}>Dark</DropdownMenu.Item>
-            <DropdownMenu.Item onClick={() => setTheme('system')}>System</DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      </div>
-    </div>
-  )
+  return <OrganizationOverview />
 }
