40b64a368cba5c4256b3fa076a1f7818511a0ded
Romain Billard <billard.rom@gmail.com>
2025-12-17 16:25:58 +0100

    fix(new-nav): auth context

--- apps/console-v5/src/routes/__root.tsx
+++ apps/console-v5/src/routes/__root.tsx
@@ -1,5 +1,12 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { type QueryClient } from '@tanstack/react-query'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { type Auth0ContextType } from '../auth/auth0'
+
+interface RouterContext {
+  auth: Auth0ContextType
+  queryClient: QueryClient
+}
 
 const RootLayout = () => {
   return (
@@ -10,4 +17,4 @@ const RootLayout = () => {
   )
 }
 
-export const Route = createRootRoute({ component: RootLayout })
+export const Route = createRootRouteWithContext<RouterContext>()({ component: RootLayout })
