5e224f525ac48853d2bbdb558b66eceb79391083
Romain Billard <rmnbrd@users.noreply.github.com>
2025-12-17 13:05:30 +0100

    impr(new-nav): add logout button and better redirection (#2237)
    impr: add logout button and better redirection

--- apps/console-v5/src/routes/index.tsx
+++ apps/console-v5/src/routes/index.tsx
@@ -1,10 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Navigate, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const { isAuthenticated } = useAuth0()
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" search={{ redirect: '/' }} />
+  }
+
   return (
     <div className="p-2">
       <h3 className="text-neutral">Welcome Home!</h3>
