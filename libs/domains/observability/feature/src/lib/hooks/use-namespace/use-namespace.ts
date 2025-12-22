3a09bc1cdf41e6c78c9680ba222bc9f7882c48e2
Pierre GB <pgerbelot@qovery.com>
2025-12-22 12:49:27 +0100

    fix(service-observability): load monitoring board for application with volume (#2250)
    fix(obs): load monitoring board for application with volume

--- libs/domains/observability/feature/src/lib/hooks/use-namespace/use-namespace.ts
+++ libs/domains/observability/feature/src/lib/hooks/use-namespace/use-namespace.ts
@@ -4,15 +4,23 @@ import { observability } from '@qovery/domains/observability/data-access'
 export interface UseNamespaceProps {
   clusterId: string
   serviceId: string
+  resourceType?: 'deployment' | 'statefulset'
   startDate: string
   endDate: string
   enabled?: boolean
 }
 
 // Retrieves the namespace name associated with a specific service
-export function useNamespace({ clusterId, serviceId, enabled = true, startDate, endDate }: UseNamespaceProps) {
+export function useNamespace({
+  clusterId,
+  serviceId,
+  resourceType,
+  enabled = true,
+  startDate,
+  endDate,
+}: UseNamespaceProps) {
   return useQuery({
-    ...observability.namespace({ clusterId, serviceId, startDate, endDate }),
+    ...observability.namespace({ clusterId, serviceId, resourceType, startDate, endDate }),
     enabled: enabled && Boolean(clusterId && serviceId),
   })
 }
