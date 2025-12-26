de56afeece9d31ffa7b8002dbc098e8b8c82a221
RemiBonnet <bonnet.rem@gmail.com>
2025-12-26 14:33:12 +0100

    fix(cluster-logs): adjust scroll behavior and layout improvements
    - Updated scroll behavior to include additional padding for better visibility of logs.
    - Enhanced layout structure by modifying the component hierarchy for improved responsiveness.
    - Adjusted styling for the log header and container to ensure consistent appearance across different states.

--- apps/console-v5/src/routes/_authenticated/organization/$organizationId/cluster/$clusterId/cluster-logs.tsx
+++ apps/console-v5/src/routes/_authenticated/organization/$organizationId/cluster/$clusterId/cluster-logs.tsx
@@ -32,7 +32,7 @@ function RouteComponent() {
 
   // `useEffect` used to scroll to the bottom of the logs when we add data
   useEffect(() => {
-    refScrollSection.current?.scroll(0, refScrollSection.current.scrollHeight)
+    refScrollSection.current?.scroll(0, refScrollSection.current.scrollHeight + 40) // 40px is the padding-bottom
   }, [logs])
 
   if (!cluster || !clusterStatus) {
@@ -42,24 +42,26 @@ function RouteComponent() {
   const firstDate = logs.length > 0 && logs[0].timestamp ? new Date(logs[0].timestamp) : undefined
 
   return (
-    <div className="flex h-[calc(100dvh-108px)] flex-col items-center overflow-hidden">
+    <div className="flex h-[calc(100dvh-108px)] w-full flex-col overflow-hidden">
       {isLogsLoading && !isLogsFetched ? (
         <div className="flex h-full flex-1 flex-col items-center justify-center">
           <LoaderDots />
         </div>
       ) : isLogsFetched && logs.length > 0 ? (
-        <div ref={refScrollSection} className="flex flex-col overflow-y-scroll pb-10">
-          <div className="sticky top-0 flex h-11 min-h-11 w-full items-center justify-end border-b border-neutral bg-surface-neutral-component px-4">
+        <>
+          <div className="flex h-11 min-h-11 w-full items-center justify-end border-b border-neutral bg-surface-neutral-subtle px-1">
             <ClusterHeaderLogs data={logs} refScrollSection={refScrollSection} />
           </div>
-          <div className="flex flex-col">
-            {logs.map((log, index) => (
-              <ClusterLogRow key={index} data={log} index={index} firstDate={firstDate} />
-            ))}
+          <div ref={refScrollSection} className="w-full flex-1 overflow-y-auto pb-10">
+            <div className="flex flex-col">
+              {logs.map((log, index) => (
+                <ClusterLogRow key={index} data={log} index={index} firstDate={firstDate} />
+              ))}
+            </div>
           </div>
-        </div>
+        </>
       ) : (
-        <div className="flex h-full  w-1/2 flex-col items-center justify-center">
+        <div className="flex h-full w-full flex-col items-center justify-center">
           <EmptyState
             className="border-none bg-transparent"
             title="No logs found"
