04a45b5e8d9e574871f1cb9252c7ffadcc41561b
Rémi Bonnet <bonnet.rem@gmail.com>
2025-12-26 14:35:09 +0100

    feat(cluster-logs): add new view (#2260)
    * feat(cluster-logs): init new row with token
    
    * feat(routes): update route tree and add layout configuration
    
    - Refactored route imports in `routeTree.gen.ts` to include new routes and improve organization.
    - Introduced `route-layout-config.ts` to manage layout options for routes.
    - Enhanced `OrganizationRoute` to conditionally apply full-width layout based on route context.
    - Updated `ClusterLogs` component to include scrolling behavior and improved layout handling.
    - Added new components for cluster log rows and headers with associated tests.
    
    * fix(cluster-logs): adjust scroll behavior and layout improvements
    
    - Updated scroll behavior to include additional padding for better visibility of logs.
    - Enhanced layout structure by modifying the component hierarchy for improved responsiveness.
    - Adjusted styling for the log header and container to ensure consistent appearance across different states.

---
+++ libs/domains/clusters/feature/src/lib/cluster-logs/cluster-header-logs/cluster-header-logs.tsx
@@ -0,0 +1,56 @@
+import download from 'downloadjs'
+import { type ClusterLogs } from 'qovery-typescript-axios'
+import { type RefObject } from 'react'
+import { Button, Icon } from '@qovery/shared/ui'
+
+export interface ClusterHeaderLogsProps {
+  refScrollSection: RefObject<HTMLDivElement>
+  data: ClusterLogs[]
+}
+
+export function ClusterHeaderLogs(props: ClusterHeaderLogsProps) {
+  const { refScrollSection, data } = props
+
+  const downloadJSON = () => {
+    download(JSON.stringify(data), `data-${Date.now()}.json`, 'text/json;charset=utf-8')
+  }
+
+  const forcedScroll = (down?: boolean) => {
+    const section = refScrollSection.current
+    if (!section) return
+
+    if (down) {
+      section.scroll(0, section.scrollHeight)
+    } else {
+      section.scroll(0, 0)
+    }
+  }
+
+  return (
+    <>
+      <Button
+        data-testid="scroll-up-button"
+        className="ml-2 mr-px w-10 justify-center !rounded-br-none !rounded-tr-none"
+        type="button"
+        color="neutral"
+        onClick={() => forcedScroll()}
+      >
+        <Icon iconName="arrow-up-to-line" />
+      </Button>
+      <Button
+        data-testid="scroll-down-button"
+        className="mr-2 w-10 justify-center !rounded-bl-none !rounded-tl-none"
+        type="button"
+        color="neutral"
+        onClick={() => forcedScroll(true)}
+      >
+        <Icon iconName="arrow-down-to-line" />
+      </Button>
+      <Button type="button" color="neutral" className="w-10 justify-center" onClick={() => downloadJSON()}>
+        <Icon iconName="cloud-arrow-down" />
+      </Button>
+    </>
+  )
+}
+
+export default ClusterHeaderLogs
