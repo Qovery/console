57b0364517ddf00b966d37578da336fdecf9b844
RemiBonnet <bonnet.rem@gmail.com>
2025-12-31 16:26:47 +0100

    feat(cluster): add danger-zone settings

--- apps/console-v5/src/routes/_authenticated/organization/$organizationId/cluster/$clusterId/settings/danger-zone.tsx
+++ apps/console-v5/src/routes/_authenticated/organization/$organizationId/cluster/$clusterId/settings/danger-zone.tsx
@@ -1,4 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from '@tanstack/react-router'
+import { type Cluster } from 'qovery-typescript-axios'
+import { ClusterDeleteModal, useCluster } from '@qovery/domains/clusters/feature'
+import { BlockContentDelete, useModal } from '@qovery/shared/ui'
 
 export const Route = createFileRoute(
   '/_authenticated/organization/$organizationId/cluster/$clusterId/settings/danger-zone'
@@ -7,5 +10,30 @@ export const Route = createFileRoute(
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/organization/$organizationId/cluster/$clusterId/settings/danger-zone"!</div>
+  const { organizationId = '', clusterId = '' } = useParams({ strict: false })
+
+  const { data: cluster } = useCluster({ organizationId, clusterId })
+  const { openModal } = useModal()
+
+  const deleteCluster = (cluster: Cluster) => {
+    openModal({
+      content: <ClusterDeleteModal cluster={cluster} />,
+    })
+  }
+
+  if (!cluster) {
+    return null
+  }
+
+  return (
+    <div className="flex w-full flex-col justify-between">
+      <div className="max-w-content-with-navigation-left p-8">
+        <BlockContentDelete
+          title="Uninstall cluster"
+          ctaLabel="Delete cluster"
+          customModalConfirmation={() => deleteCluster(cluster)}
+        />
+      </div>
+    </div>
+  )
 }
