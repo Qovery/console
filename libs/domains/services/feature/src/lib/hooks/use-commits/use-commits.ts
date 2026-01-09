9d19ee743bbeb629da488de2f3c5af1c1cef073d
Romain Billard <billard.rom@gmail.com>
2026-01-09 10:47:03 +0100

    fix(commits): apply staleTime, retry and refetchOnWindowFocus params to commit API call

--- libs/domains/services/feature/src/lib/hooks/use-commits/use-commits.ts
+++ libs/domains/services/feature/src/lib/hooks/use-commits/use-commits.ts
@@ -6,6 +6,9 @@ export type UseCommitsProps = Parameters<typeof queries.services.listCommits>[0]
 export function useCommits(props: UseCommitsProps) {
   return useQuery({
     ...queries.services.listCommits(props),
+    staleTime: 3 * 60 * 1000, // 3 minutes - commit history doesn't change frequently
+    retry: false,
+    refetchOnWindowFocus: false,
   })
 }
 
