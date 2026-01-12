0979ce084bc28e308c2856dd1cd5f50fabfbb9c8
Romain Billard <billard.rom@gmail.com>
2026-01-12 18:23:34 +0100

    impr: rework orgId/clusters data fetching

--- apps/console-v5/src/routes/_authenticated/organization/$organizationId/clusters.tsx
+++ apps/console-v5/src/routes/_authenticated/organization/$organizationId/clusters.tsx
@@ -1,21 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
 import { createFileRoute, useParams } from '@tanstack/react-router'
-import { ClusterCard, useClusterStatuses, useClusters } from '@qovery/domains/clusters/feature'
+import { Suspense } from 'react'
+import { ClusterCard } from '@qovery/domains/clusters/feature'
 import { EmptyState, Heading, Icon, Link, LoaderSpinner, Section } from '@qovery/shared/ui'
 import { useDocumentTitle } from '@qovery/shared/util-hooks'
+import { queries } from '@qovery/state/util-queries'
 
 export const Route = createFileRoute('/_authenticated/organization/$organizationId/clusters')({
   component: RouteComponent,
 })
 
+const Clusters = () => {
+  const { organizationId = '' } = useParams({ strict: false })
+  const { data: clusters = [] } = useQuery({ ...queries.clusters.list({ organizationId }), suspense: true })
+  const { data: clusterStatuses = [] } = useQuery({
+    ...queries.clusters.listStatuses({ organizationId }),
+    suspense: true,
+  })
+
+  if (clusters.length === 0) {
+    return (
+      <EmptyState title="Create your first cluster">
+        <Link to="/" as="button" className="mt-4 items-center gap-2" size="md">
+          New Cluster
+          <Icon iconName="circle-plus" iconStyle="regular" />
+        </Link>
+      </EmptyState>
+    )
+  }
+
+  return (
+    <div className="grid gap-5 lg:grid-cols-2 2xl:grid-cols-3">
+      {clusters.map((cluster) => (
+        <ClusterCard
+          key={cluster.id}
+          cluster={cluster}
+          clusterDeploymentStatus={clusterStatuses.find((c) => c.cluster_id === cluster.id)}
+        />
+      ))}
+    </div>
+  )
+}
+
 function RouteComponent() {
   useDocumentTitle('Clusters - Manage your clusters')
   const { organizationId = '' } = useParams({ strict: false })
-  const { data: clusters = [], isLoading: isLoadingCluster } = useClusters({ organizationId: organizationId })
-  const { data: clusterStatuses = [], isLoading: isLoadingClusterStatus } = useClusterStatuses({
-    organizationId: organizationId,
-  })
-
-  const isLoading = isLoadingCluster || isLoadingClusterStatus
 
   return (
     <div className="container mx-auto mt-6 pb-10">
@@ -37,31 +66,15 @@ function RouteComponent() {
           <hr className="w-full border-neutral" />
         </div>
         <div>
-          {isLoading && clusters?.length === 0 ? (
-            <div data-testid="clusters-loader" className="flex justify-center">
-              <LoaderSpinner className="w-6" />
-            </div>
-          ) : clusters && clusters.length > 0 ? (
-            <div className="grid gap-5 lg:grid-cols-2 2xl:grid-cols-3">
-              {clusters.map((cluster) => (
-                <ClusterCard
-                  key={cluster.id}
-                  cluster={cluster}
-                  clusterDeploymentStatus={clusterStatuses.find((c) => c.cluster_id === cluster.id)}
-                />
-              ))}
-            </div>
-          ) : (
-            !isLoading &&
-            clusters?.length === 0 && (
-              <EmptyState title="Create your first cluster">
-                <Link to="/" as="button" className="mt-4 items-center gap-2" size="md">
-                  New Cluster
-                  <Icon iconName="circle-plus" iconStyle="regular" />
-                </Link>
-              </EmptyState>
-            )
-          )}
+          <Suspense
+            fallback={
+              <div data-testid="clusters-loader" className="flex justify-center">
+                <LoaderSpinner className="w-6" />
+              </div>
+            }
+          >
+            <Clusters />
+          </Suspense>
         </div>
       </Section>
     </div>
