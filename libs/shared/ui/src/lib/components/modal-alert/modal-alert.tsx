8f206a90598449da986bb3f40fb887866b4a10c9
Romain Billard <billard.rom@gmail.com>
2026-01-19 16:32:26 +0100

    impr: UI tweaks for 'Dismiss' modal

--- libs/shared/ui/src/lib/components/modal-alert/modal-alert.tsx
+++ libs/shared/ui/src/lib/components/modal-alert/modal-alert.tsx
@@ -16,11 +16,11 @@ export function ModalAlert(props: ModalAlertProps) {
         <Dialog.Content
           data-testid="modal-alert"
           style={{ width: `470px`, top: '200px' }}
-          className="modal__content fixed left-1/2 top-[84px] z-modal rounded-md bg-white shadow-[0_0_32px_rgba(0,0,0,0.08)]"
+          className="modal__content fixed left-1/2 top-[84px] z-modal rounded-md border border-neutral bg-background shadow-[0_0_32px_rgba(0,0,0,0.08)]"
         >
           <div className="max-h-[80vh] overflow-auto p-6">
-            <h2 className="h4 mb-2 text-neutral-400">Discard changes?</h2>
-            <p className="mb-6 text-sm text-neutral-350">Are you sure you want to discard your changes?</p>
+            <h2 className="h4 mb-2 text-neutral">Discard changes?</h2>
+            <p className="mb-6 text-sm text-neutral-subtle">Are you sure you want to discard your changes?</p>
 
             <div className="flex justify-end gap-4">
               <Button
