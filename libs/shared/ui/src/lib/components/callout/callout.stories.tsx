20b4d949141043e2ee64a8fd843682cf945b6aa8
Théo Grandin <theo.grandin74@gmail.com>
2026-01-26 10:09:27 +0100

    feat(callout, description-list): enhance styling and add color options for Callout component

--- libs/shared/ui/src/lib/components/callout/callout.stories.tsx
+++ libs/shared/ui/src/lib/components/callout/callout.stories.tsx
@@ -1,14 +1,22 @@
 import type { Meta } from '@storybook/react-webpack5'
 import { Button } from '../button/button'
 import { Icon } from '../icon/icon'
-import { Callout } from './callout'
+import { Callout, type CalloutRootProps } from './callout'
+
+const COLOR_OPTIONS = ['green', 'red', 'sky', 'yellow', 'neutral', 'purple'] as const
 
 const Story: Meta<typeof Callout.Root> = {
   component: Callout.Root,
   title: 'Callout',
+  argTypes: {
+    color: {
+      options: COLOR_OPTIONS,
+      control: { type: 'select' },
+    },
+  },
   decorators: [
     (Story) => (
-      <div style={{ background: 'white', padding: '3em' }}>
+      <div style={{ background: 'bg-background', padding: '3em' }}>
         <Story />
       </div>
     ),
@@ -17,8 +25,11 @@ const Story: Meta<typeof Callout.Root> = {
 export default Story
 
 export const Primary = {
-  render: () => (
-    <Callout.Root color="green">
+  args: {
+    color: 'green',
+  },
+  render: (props: CalloutRootProps) => (
+    <Callout.Root {...props}>
       <Callout.Icon>
         <Icon iconName="triangle-exclamation" iconStyle="regular" />
       </Callout.Icon>
@@ -26,9 +37,7 @@ export const Primary = {
         <Callout.TextHeading>Lorem ipsum</Callout.TextHeading>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
+        consequat. laborum.
       </Callout.Text>
     </Callout.Root>
   ),
