fe25a311cfb812337888b35e51491af33e150360
Guillaume Da Silva <gdasilva@qovery.com>
2026-01-30 13:37:47 +0100

    fix: wrap ApplyImmediatelyCheckbox tests in FormProvider
    The component uses useFormContext() which requires a FormProvider to be present in tests.

--- libs/shared/console-shared/src/lib/apply-immediately-checkbox/apply-immediately-checkbox.spec.tsx
+++ libs/shared/console-shared/src/lib/apply-immediately-checkbox/apply-immediately-checkbox.spec.tsx
@@ -1,14 +1,34 @@
+import { FormProvider, useForm } from 'react-hook-form'
 import { renderWithProviders, screen } from '@qovery/shared/util-tests'
 import { ApplyImmediatelyCheckbox } from './apply-immediately-checkbox'
 
+function Wrapper({ children, defaultValues = {} }: { children: React.ReactNode; defaultValues?: any }) {
+  const methods = useForm({
+    defaultValues: {
+      apply_immediately: false,
+      ...defaultValues,
+    },
+  })
+
+  return <FormProvider {...methods}>{children}</FormProvider>
+}
+
 describe('ApplyImmediatelyCheckbox', () => {
   it('should render successfully', () => {
-    const { baseElement } = renderWithProviders(<ApplyImmediatelyCheckbox />)
+    const { baseElement } = renderWithProviders(
+      <Wrapper>
+        <ApplyImmediatelyCheckbox />
+      </Wrapper>
+    )
     expect(baseElement).toBeTruthy()
   })
 
   it('should display warning when checkbox is checked', async () => {
-    const { userEvent } = renderWithProviders(<ApplyImmediatelyCheckbox />)
+    const { userEvent } = renderWithProviders(
+      <Wrapper>
+        <ApplyImmediatelyCheckbox />
+      </Wrapper>
+    )
 
     const checkbox = screen.getByRole('checkbox')
     await userEvent.click(checkbox)
@@ -17,13 +37,21 @@ describe('ApplyImmediatelyCheckbox', () => {
   })
 
   it('should not display warning when checkbox is unchecked', () => {
-    renderWithProviders(<ApplyImmediatelyCheckbox />)
+    renderWithProviders(
+      <Wrapper>
+        <ApplyImmediatelyCheckbox />
+      </Wrapper>
+    )
 
     expect(screen.queryByTestId('apply-immediately-warning')).not.toBeInTheDocument()
   })
 
   it('should be disabled when disabled prop is true', () => {
-    renderWithProviders(<ApplyImmediatelyCheckbox disabled />)
+    renderWithProviders(
+      <Wrapper>
+        <ApplyImmediatelyCheckbox disabled />
+      </Wrapper>
+    )
 
     const checkbox = screen.getByRole('checkbox')
     expect(checkbox).toBeDisabled()
