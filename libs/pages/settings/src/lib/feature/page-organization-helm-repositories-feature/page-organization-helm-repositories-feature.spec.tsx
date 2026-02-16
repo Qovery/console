2c67b4b2c994c4ee26fd5be8c3b5a2d813a02276
Théo Grandin <theo.grandin74@gmail.com>
2026-02-16 10:32:45 +0100

    feat(helm-repositories): implement helm repositories settings page and update related components

--- libs/pages/settings/src/lib/feature/page-organization-helm-repositories-feature/page-organization-helm-repositories-feature.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { renderWithProviders } from '@qovery/shared/util-tests'
-import { PageOrganizationHelmRepositoriesFeature } from './page-organization-helm-repositories-feature'
-
-describe('PageOrganizationHelmRepositoriesFeature', () => {
-  it('should render successfully', () => {
-    const { baseElement } = renderWithProviders(<PageOrganizationHelmRepositoriesFeature />)
-    expect(baseElement).toBeTruthy()
-  })
-})
