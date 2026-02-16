4e21788900548bd0fe724ff92f2beeef066a4de1
RemiBonnet <bonnet.rem@gmail.com>
2026-02-16 18:22:56 +0100

    fix(user-menu): integrate UserSettingsModal import into user menu component

--- apps/console-v5/src/app/components/header/user-menu/user-menu.tsx
+++ apps/console-v5/src/app/components/header/user-menu/user-menu.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
 import { useState } from 'react'
-import { useUserAccount } from '@qovery/shared/iam/feature'
+import { UserSettingsModal, useUserAccount } from '@qovery/shared/iam/feature'
 import { Avatar, DropdownMenu, Icon } from '@qovery/shared/ui'
 import { useModal } from '@qovery/shared/ui'
 import { type Theme, useTheme } from '../../theme-provider/theme-provider'
