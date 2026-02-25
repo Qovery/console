39f7fcd7aabacc451378311a8efb38072e0817ad
Théo Grandin <theo.grandin74@gmail.com>
2026-02-25 16:25:49 +0100

    feat(members): adding members to console v5 and tokenization (#2413)
    * feat(members): adding members to console v5 and tokenization
    
    * refactor(members): clean up and optimize member-related components and tests
    
    * fix(route):  revert routeTree changes
    
    * chore(snapshots): update snapshots for various components to reflect recent changes in styles and structure
    
    * feat(create-modal):  fix CI red
    
    * feat(row-member): enhance role selection with dropdown and state management
    
    * fix(row-member): : format check
    
    * test(row-member): update role selection test to use dropdown interaction
    
    * test(settings-members): update role selection test to use button interaction

--- libs/shared/ui/src/lib/components/inputs/input-select-small/input-select-small.tsx
+++ libs/shared/ui/src/lib/components/inputs/input-select-small/input-select-small.tsx
@@ -13,6 +13,7 @@ export interface InputSelectSmallProps {
   onChange?: (item: string | undefined) => void
   defaultValue?: string
   inputClassName?: string
+  disabled?: boolean
 }
 
 export function InputSelectSmall(props: InputSelectSmallProps) {
@@ -26,6 +27,7 @@ export function InputSelectSmall(props: InputSelectSmallProps) {
     getValue,
     dataTestId,
     inputClassName = '',
+    disabled = false,
   } = props
 
   const [value, setValue] = useState(defaultValue)
@@ -53,8 +55,9 @@ export function InputSelectSmall(props: InputSelectSmallProps) {
         data-testid={dataTestId || 'input-select-small'}
         name={name}
         value={value}
+        disabled={disabled}
         className={twMerge(
-          'h-9 w-full cursor-pointer appearance-none rounded border border-neutral bg-surface-neutral px-3 pr-6 text-sm text-neutral transition-colors hover:bg-surface-neutral-component focus:outline-none',
+          'h-9 w-full cursor-pointer appearance-none rounded border border-neutral bg-surface-neutral px-3 pr-6 text-sm text-neutral transition-colors hover:bg-surface-neutral-component focus:outline-none disabled:cursor-not-allowed disabled:bg-surface-neutral disabled:text-neutral-subtle disabled:hover:bg-surface-neutral',
           inputClassName
         )}
         onChange={(e) => onClickItem(e.target.value)}
@@ -67,7 +70,7 @@ export function InputSelectSmall(props: InputSelectSmallProps) {
       </select>
       <Icon
         iconName="chevron-down"
-        className="pointer-events-none absolute right-4 top-2.5 translate-y-0.5 text-xs leading-3 text-neutral-subtle group-hover:text-neutral"
+        className="pointer-events-none absolute right-3 top-2.5 translate-y-0.5 text-xs leading-3 text-neutral-subtle group-hover:text-neutral"
       />
     </div>
   )
