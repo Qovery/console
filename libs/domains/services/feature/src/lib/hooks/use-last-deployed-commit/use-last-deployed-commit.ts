d0fbb5198e517e205b7058589fb05934aa4d53e6
RemiBonnet <bonnet.rem@gmail.com>
2026-02-25 12:41:10 +0100

    refactor(use-last-deployed-commit): simplify props and enhance commit retrieval logic
    - Refactored the `useLastDeployedCommit` hook to streamline props by removing `serviceId` and `serviceType`, relying solely on `gitRepository`.
    - Improved the logic for retrieving the last deployed commit, ensuring a more concise and efficient implementation.
    - Updated default commit information handling to directly reference properties from `gitRepository` without optional chaining.

--- libs/domains/services/feature/src/lib/hooks/use-last-deployed-commit/use-last-deployed-commit.ts
+++ libs/domains/services/feature/src/lib/hooks/use-last-deployed-commit/use-last-deployed-commit.ts
@@ -1,33 +1,22 @@
 import { type ApplicationGitRepository, type Commit } from 'qovery-typescript-axios'
-import { type ServiceType } from '@qovery/domains/services/data-access'
 import { type UseCommitsProps, useCommits } from '../use-commits/use-commits'
 
 export type UseLastDeployedCommitProps = {
-  serviceId: string
-  serviceType: ServiceType
-  gitRepository?: ApplicationGitRepository | null
-} & Omit<UseCommitsProps, 'serviceId' | 'serviceType'>
+  gitRepository: ApplicationGitRepository
+} & UseCommitsProps
 
-export function useLastDeployedCommit({ gitRepository, serviceId, serviceType, ...rest }: UseLastDeployedCommitProps) {
-  const enabled = Boolean(gitRepository && serviceId && serviceType)
-  const { data: commits, ...props } = useCommits({
-    serviceId: serviceId ?? '',
-    serviceType: (serviceType ?? 'APPLICATION') as Extract<
-      ServiceType,
-      'APPLICATION' | 'JOB' | 'CRON_JOB' | 'LIFECYCLE_JOB' | 'HELM' | 'TERRAFORM'
-    >,
-    ...rest,
-    enabled,
-  })
+export function useLastDeployedCommit({ gitRepository, ...useCommitsProps }: UseLastDeployedCommitProps) {
+  const { data: commits, ...props } = useCommits(useCommitsProps)
 
   const delta = commits?.findIndex(({ git_commit_id }) => git_commit_id === gitRepository?.deployed_commit_id) ?? 0
   const defaultCommitInfo: Commit = {
-    created_at: gitRepository?.deployed_commit_date ?? 'unknown',
-    git_commit_id: gitRepository?.deployed_commit_id ?? 'unknown',
-    tag: gitRepository?.deployed_commit_tag ?? 'unknown',
+    created_at: gitRepository.deployed_commit_date ?? 'unknown',
+    git_commit_id: gitRepository.deployed_commit_id ?? 'unknown',
+    tag: gitRepository.deployed_commit_tag ?? 'unknown',
     message: 'unknown',
-    author_name: gitRepository?.owner ?? gitRepository?.deployed_commit_contributor ?? 'unknown',
+    author_name: gitRepository.deployed_commit_contributor ?? 'unknown',
   }
+
   return {
     data: {
       deployedCommit: commits?.[delta] ?? defaultCommitInfo,
