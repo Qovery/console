62c21f417173f1e88ac51a2fd5312ec60676ff5e
Romain Billard <billard.rom@gmail.com>
2026-03-04 14:49:19 +0100

    Bringing back the old look of the "live" button

--- libs/domains/service-logs/feature/src/lib/list-service-logs/header-service-logs/header-service-logs.tsx
+++ libs/domains/service-logs/feature/src/lib/list-service-logs/header-service-logs/header-service-logs.tsx
@@ -104,7 +104,7 @@ export function HeaderServiceLogs({ logs, isLiveMode, refetchHistoryLogs }: Head
       <div className="sticky top-[93px] z-header flex h-[60px] w-full items-center justify-between gap-2 border-b border-neutral bg-background px-4 py-2.5">
         <div className="flex w-full items-center gap-2">
           <Button
-            variant="outline"
+            variant="surface"
             color={isLiveMode ? 'brand' : 'neutral'}
             size="md"
             className={clsx('gap-1.5 pl-2.5', {
