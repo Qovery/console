7ab03f04a75c0cbf7bf7396da20cf02e0aa32902
RemiBonnet <bonnet.rem@gmail.com>
2026-03-27 13:32:10 +0100

    feat(cluster-header-logs): add execution_id, version and cluster_id

--- libs/domains/clusters/feature/src/lib/cluster-logs/cluster-header-logs/cluster-header-logs.tsx
+++ libs/domains/clusters/feature/src/lib/cluster-logs/cluster-header-logs/cluster-header-logs.tsx
@@ -1,16 +1,17 @@
 import download from 'downloadjs'
-import { type ClusterLogs } from 'qovery-typescript-axios'
+import { type Cluster, type ClusterLogs, type ClusterStatus } from 'qovery-typescript-axios'
 import { type RefObject } from 'react'
-import { Button, Icon } from '@qovery/shared/ui'
+import { Badge, Button, Icon, Tooltip } from '@qovery/shared/ui'
+import { trimId } from '@qovery/shared/util-js'
 
 export interface ClusterHeaderLogsProps {
+  cluster: Cluster
+  clusterStatus: ClusterStatus
   refScrollSection: RefObject<HTMLDivElement>
   data: ClusterLogs[]
 }
 
-export function ClusterHeaderLogs(props: ClusterHeaderLogsProps) {
-  const { refScrollSection, data } = props
-
+export function ClusterHeaderLogs({ cluster, clusterStatus, refScrollSection, data }: ClusterHeaderLogsProps) {
   const downloadJSON = () => {
     download(JSON.stringify(data), `data-${Date.now()}.json`, 'text/json;charset=utf-8')
   }
@@ -26,44 +27,69 @@ export function ClusterHeaderLogs(props: ClusterHeaderLogsProps) {
     }
   }
 
+  const lastExecutionId = clusterStatus.last_execution_id ?? ''
+
   return (
-    <>
-      <Button
-        data-testid="scroll-up-button"
-        className="rounded-br-none rounded-tr-none border-r-0"
-        type="button"
-        variant="outline"
-        color="neutral"
-        size="sm"
-        iconOnly
-        onClick={() => forcedScroll()}
-      >
-        <Icon iconName="arrow-up-to-line" />
-      </Button>
-      <Button
-        data-testid="scroll-down-button"
-        className="mr-2 rounded-bl-none rounded-tl-none"
-        variant="outline"
-        type="button"
-        color="neutral"
-        size="sm"
-        iconOnly
-        onClick={() => forcedScroll(true)}
-      >
-        <Icon iconName="arrow-down-to-line" />
-      </Button>
-      <Button
-        type="button"
-        variant="outline"
-        color="neutral"
-        size="sm"
-        className="mr-1"
-        iconOnly
-        onClick={() => downloadJSON()}
-      >
-        <Icon iconName="cloud-arrow-down" />
-      </Button>
-    </>
+    <div className="flex w-full items-center justify-between gap-2 pl-5 pr-3 text-sm">
+      <div className="flex items-center gap-2">
+        <Tooltip
+          side="bottom"
+          content={
+            <span>
+              Cluster version: {cluster.version} <br />
+              Cluster ID: {cluster.id}
+            </span>
+          }
+        >
+          <Badge variant="surface" className="max-w-full whitespace-nowrap">
+            {cluster.version}
+          </Badge>
+        </Tooltip>
+        <Tooltip side="bottom" content={<span>Execution id: {lastExecutionId}</span>}>
+          <span className="flex items-center gap-1.5 truncate">
+            <Icon iconName="code" iconStyle="regular" className="text-base text-neutral-subtle" />
+            <span className="font-normal text-neutral">{trimId(lastExecutionId)}</span>
+          </span>
+        </Tooltip>
+      </div>
+      <div>
+        <Button
+          data-testid="scroll-up-button"
+          className="rounded-br-none rounded-tr-none border-r-0"
+          type="button"
+          variant="outline"
+          color="neutral"
+          size="sm"
+          iconOnly
+          onClick={() => forcedScroll()}
+        >
+          <Icon iconName="arrow-up-to-line" />
+        </Button>
+        <Button
+          data-testid="scroll-down-button"
+          className="mr-2 rounded-bl-none rounded-tl-none"
+          variant="outline"
+          type="button"
+          color="neutral"
+          size="sm"
+          iconOnly
+          onClick={() => forcedScroll(true)}
+        >
+          <Icon iconName="arrow-down-to-line" />
+        </Button>
+        <Button
+          type="button"
+          variant="outline"
+          color="neutral"
+          size="sm"
+          className="mr-1"
+          iconOnly
+          onClick={() => downloadJSON()}
+        >
+          <Icon iconName="cloud-arrow-down" />
+        </Button>
+      </div>
+    </div>
   )
 }
 
