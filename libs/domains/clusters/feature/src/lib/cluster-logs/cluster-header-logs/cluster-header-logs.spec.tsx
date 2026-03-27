f1f7125da370f2ccfb5ecbeb4f3a7641782afc86
RemiBonnet <bonnet.rem@gmail.com>
2026-03-27 14:12:36 +0100

    refactor(cluster-header-logs): update test props to include cluster status and remove redundant definitions

--- libs/domains/clusters/feature/src/lib/cluster-logs/cluster-header-logs/cluster-header-logs.spec.tsx
+++ libs/domains/clusters/feature/src/lib/cluster-logs/cluster-header-logs/cluster-header-logs.spec.tsx
@@ -1,6 +1,7 @@
 import download from 'downloadjs'
+import { ClusterStateEnum } from 'qovery-typescript-axios'
 import { type RefObject } from 'react'
-import { clusterLogFactoryMock } from '@qovery/shared/factories'
+import { clusterFactoryMock, clusterLogFactoryMock } from '@qovery/shared/factories'
 import { renderWithProviders, screen } from '@qovery/shared/util-tests'
 import ClusterHeaderLogs, { type ClusterHeaderLogsProps } from './cluster-header-logs'
 
@@ -15,24 +16,28 @@ const refScrollSection: RefObject<HTMLDivElement> = {
 describe('ClusterHeaderLogs', () => {
   const baseData = clusterLogFactoryMock(2, false)
 
+  const props: ClusterHeaderLogsProps = {
+    cluster: clusterFactoryMock(1)[0],
+    clusterStatus: {
+      cluster_id: '1',
+      status: ClusterStateEnum.DEPLOYED,
+      last_execution_id: '1',
+      is_deployed: true,
+    },
+    refScrollSection: refScrollSection,
+    data: baseData,
+  }
+
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
   it('should render successfully', () => {
-    const props: ClusterHeaderLogsProps = {
-      refScrollSection: refScrollSection,
-      data: baseData,
-    }
     const { baseElement } = renderWithProviders(<ClusterHeaderLogs {...props} />)
     expect(baseElement).toBeTruthy()
   })
 
   it('should trigger scroll up on click', async () => {
-    const props: ClusterHeaderLogsProps = {
-      refScrollSection: refScrollSection,
-      data: baseData,
-    }
     const { userEvent } = renderWithProviders(<ClusterHeaderLogs {...props} />)
     const scrollUpButton = screen.getByTestId('scroll-up-button')
 
@@ -41,10 +46,6 @@ describe('ClusterHeaderLogs', () => {
   })
 
   it('should trigger scroll down on click', async () => {
-    const props: ClusterHeaderLogsProps = {
-      refScrollSection: refScrollSection,
-      data: baseData,
-    }
     const { userEvent } = renderWithProviders(<ClusterHeaderLogs {...props} />)
     const scrollDownButton = screen.getByTestId('scroll-down-button')
 
@@ -53,10 +54,6 @@ describe('ClusterHeaderLogs', () => {
   })
 
   it('should trigger download on click', async () => {
-    const props: ClusterHeaderLogsProps = {
-      refScrollSection: refScrollSection,
-      data: baseData,
-    }
     const { userEvent } = renderWithProviders(<ClusterHeaderLogs {...props} />)
     const buttons = screen.getAllByRole('button')
     const downloadButton = buttons[2]
