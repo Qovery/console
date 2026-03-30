ac55446432c83695bd41f508cef7ab2ce30da98f
RemiBonnet <bonnet.rem@gmail.com>
2026-03-30 14:37:46 +0200

    fix(console-v5): type preview code navigation state

--- apps/console-v5/src/router-types.d.ts
+++ apps/console-v5/src/router-types.d.ts
@@ -1,6 +1,12 @@
 import { type createRouter } from '@tanstack/react-router'
 import { type routeTree } from './routeTree.gen'
 
+declare module '@tanstack/history' {
+  interface HistoryState {
+    code?: string
+  }
+}
+
 declare module '@tanstack/react-router' {
   interface Register {
     router: ReturnType<typeof createRouter<typeof routeTree>>
