bc5f756caaa2ee585c06753ebab903779a1981ba
Romain Billard <billard.rom@gmail.com>
2026-03-31 19:11:42 +0200

    Revert error icon

--- libs/shared/ui/src/lib/components/icon/icons-status/error.tsx
+++ libs/shared/ui/src/lib/components/icon/icons-status/error.tsx
@@ -1,10 +1,27 @@
-import { type LegacyRef, forwardRef } from 'react'
+import { forwardRef } from 'react'
 import { twMerge } from '@qovery/shared/util-js'
-import Icon, { type IconSVGProps } from '../icon'
+import { type IconSVGProps } from '../icon'
 
-export const ErrorIcon = forwardRef<SVGSVGElement, IconSVGProps>(function ErrorIcon({ className = '', ...props }) {
+export const ErrorIcon = forwardRef<SVGSVGElement, IconSVGProps>(function ErrorIcon(
+  { className = '', ...props },
+  forwardedRef
+) {
   return (
-    <Icon iconName="circle-exclamation" iconStyle="solid" className={twMerge('text-negative', className)} {...props} />
+    <svg
+      className={twMerge('text-negative', className)}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 24 24"
+      ref={forwardedRef}
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M12 24a12 12 0 100-24 12 12 0 000 24zm1.125-18v7.5h-2.25V6h2.25zm-2.25 11.25V15h2.25v2.25h-2.25z"
+      ></path>
+    </svg>
   )
 })
 
