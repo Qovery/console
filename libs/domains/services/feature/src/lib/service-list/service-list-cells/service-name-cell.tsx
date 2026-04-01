dd43b20043b274c8a952fb9c164e1259a53c1694
Romain Billard <billard.rom@gmail.com>
2026-04-01 16:45:37 +0200

    Remove all deployment related info on the service-name-cell

--- libs/domains/services/feature/src/lib/service-list/service-list-cells/service-name-cell.tsx
+++ libs/domains/services/feature/src/lib/service-list/service-list-cells/service-name-cell.tsx
@@ -1,59 +1,11 @@
 import { type Environment } from 'qovery-typescript-axios'
 import { match } from 'ts-pattern'
 import { type AnyService } from '@qovery/domains/services/data-access'
-import { AnimatedGradientText, Button, Icon, Link, Tooltip } from '@qovery/shared/ui'
-import { formatCronExpression, upperCaseFirstLetter } from '@qovery/shared/util-js'
-import useDeploymentStatus from '../../hooks/use-deployment-status/use-deployment-status'
+import { Button, Icon, Link, Tooltip } from '@qovery/shared/ui'
+import { formatCronExpression } from '@qovery/shared/util-js'
 import { ServiceAvatar } from '../../service-avatar/service-avatar'
 import ServiceLinksPopover from '../../service-links-popover/service-links-popover'
 import ServiceTemplateIndicator from '../../service-template-indicator/service-template-indicator'
-
-const LinkDeploymentStatus = ({ environmentId, serviceId }: { environmentId: string; serviceId: string }) => {
-  const { data: deploymentStatus } = useDeploymentStatus({ environmentId, serviceId })
-
-  // const environmentLog = ENVIRONMENT_LOGS_URL(environment.organization.id, environment.project.id, environment.id)
-  // const deploymentLog = DEPLOYMENT_LOGS_VERSION_URL(service.id, deploymentStatus?.execution_id)
-  // const precheckLog = ENVIRONMENT_PRE_CHECK_LOGS_URL(deploymentStatus?.execution_id ?? '')
-
-  return match(deploymentStatus?.state)
-    .with('DEPLOYMENT_QUEUED', 'DELETE_QUEUED', 'STOP_QUEUED', 'RESTART_QUEUED', (s) => (
-      <span className="text-ssm font-normal text-neutral-subtle">{upperCaseFirstLetter(s).replace('_', ' ')}...</span>
-    ))
-    .with('CANCELED', () => <span className="text-ssm font-normal text-neutral-subtle">Last deployment aborted</span>)
-    .with('DEPLOYING', 'RESTARTING', 'BUILDING', 'DELETING', 'CANCELING', 'STOPPING', (s) => (
-      // TODO new-nav : Route not yet created
-      // <Link
-      //   to=""
-      //   color="brand"
-      //   underline
-      //   size="ssm"
-      //   className="group flex truncate"
-      //   onClick={(e) => e.stopPropagation()}
-      // >
-      <div className="group flex truncate">
-        <AnimatedGradientText shimmerWidth={80} className="group-hover:text-brand">
-          <span className="flex items-center gap-0.5">
-            {upperCaseFirstLetter(s)}... <Icon iconName="arrow-up-right" />
-          </span>
-        </AnimatedGradientText>
-      </div>
-      // </Link>
-    ))
-    .with('DEPLOYMENT_ERROR', 'DELETE_ERROR', 'STOP_ERROR', 'RESTART_ERROR', 'BUILD_ERROR', () => (
-      <button
-        className="flex cursor-pointer items-center gap-0.5 truncate text-ssm font-medium text-negative underline-offset-2 hover:text-negative-hover hover:underline"
-        onClick={(e) => {
-          e.stopPropagation()
-          // TODO new-nav : Route not yet created
-          // navigate({ to: (environmentLog + deploymentLog) as never })
-        }}
-      >
-        Last deployment failed
-        <Icon iconName="arrow-up-right" />
-      </button>
-    ))
-    .otherwise(() => null)
-}
 
 export function ServiceNameCell({ service, environment }: { service: AnyService; environment: Environment }) {
   return (
@@ -85,7 +37,6 @@ export function ServiceNameCell({ service, environment }: { service: AnyService;
                       </Link>
                     </Tooltip>
                   </span>
-                  <LinkDeploymentStatus environmentId={environment.id} serviceId={service.id} />
                 </span>
               )
             })
@@ -128,12 +79,11 @@ export function ServiceNameCell({ service, environment }: { service: AnyService;
                       </Link>
                     </Tooltip>
                     <Tooltip content={schedule}>
-                      <span className="truncate text-sm font-normal">
+                      <span className="truncate text-sm font-normal text-neutral-subtle">
                         <Icon iconName="info-circle" iconStyle="regular" />
                       </span>
                     </Tooltip>
                   </span>
-                  <LinkDeploymentStatus environmentId={environment.id} serviceId={service.id} />
                 </span>
               )
             })
@@ -155,7 +105,6 @@ export function ServiceNameCell({ service, environment }: { service: AnyService;
                     {service.name}
                   </Link>
                 </Tooltip>
-                <LinkDeploymentStatus environmentId={environment.id} serviceId={service.id} />
               </span>
             ))}
         </div>
