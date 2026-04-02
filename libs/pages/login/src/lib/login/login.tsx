058edf8de772cfeaf83a2e66047a113ca76a67ff
Théo Grandin <theo.grandin74@gmail.com>
2026-04-02 17:52:28 +0200

    style(login): format check

--- libs/pages/login/src/lib/login/login.tsx
+++ libs/pages/login/src/lib/login/login.tsx
@@ -287,7 +287,7 @@ export function Login() {
 
               {!displayInvitation ? (
                 <h1
-                  className={`font-brand text-center text-2xl font-normal leading-[32px] text-neutral sm:text-[28px] sm:leading-[36px] ${ssoFormVisible ? 'mb-2' : 'mb-8'}`}
+                  className={`text-center font-brand text-2xl font-normal leading-[32px] text-neutral sm:text-[28px] sm:leading-[36px] ${ssoFormVisible ? 'mb-2' : 'mb-8'}`}
                 >
                   {ssoFormVisible ? 'Enterprise single sign-on' : 'Connect to your workspace'}
                 </h1>
