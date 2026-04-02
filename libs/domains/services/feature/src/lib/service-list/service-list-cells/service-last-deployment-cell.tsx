3d71fc4d49f9e79a634d37045272a8dc6a89dfcb
Romain Billard <billard.rom@gmail.com>
2026-04-02 15:35:38 +0200

    Fix last deployment cell

--- libs/domains/services/feature/src/lib/service-list/service-list-cells/service-last-deployment-cell.tsx
+++ libs/domains/services/feature/src/lib/service-list/service-list-cells/service-last-deployment-cell.tsx
@@ -1,4 +1,5 @@
 import { type Environment } from 'qovery-typescript-axios'
+import { PropsWithChildren, useCallback } from 'react'
 import { type AnyService } from '@qovery/domains/services/data-access'
 import { DevopsCopilotTroubleshootTrigger } from '@qovery/shared/devops-copilot/feature'
 import { DeploymentAction, Link, StatusChip } from '@qovery/shared/ui'
@@ -16,27 +17,40 @@ export function ServiceLastDeploymentCell({ service, environment }: ServiceLastD
   const subAction = deploymentStatus?.status_details?.sub_action
   const triggerAction = subAction !== 'NONE' ? subAction : deploymentStatus?.status_details?.action
 
+  const WrappingLink = useCallback(
+    ({ children }: PropsWithChildren) => {
+      return (
+        <Link
+          to="/organization/$organizationId/project/$projectId/environment/$environmentId/service/$serviceId/deployments/logs/$executionId"
+          params={{
+            organizationId: environment.organization.id,
+            projectId: environment.project.id,
+            environmentId: environment.id,
+            serviceId: service.id,
+            executionId: deploymentStatus?.execution_id ?? '',
+          }}
+          underline
+          color="neutral"
+          className="flex h-full items-center justify-between py-1 font-normal hover:text-neutral"
+          onClick={(e) => {
+            e.stopPropagation()
+          }}
+        >
+          {children}
+        </Link>
+      )
+    },
+    [environment, service, deploymentStatus?.execution_id]
+  )
+
   return deploymentStatus?.state === 'READY' ? (
     <span className="text-sm font-normal text-neutral-subtle">Never been deployed</span>
   ) : (
-    <Link
-      to="/organization/$organizationId/project/$projectId/environment/$environmentId/service/$serviceId/deployments/logs/$executionId"
-      params={{
-        organizationId: environment.organization.id,
-        projectId: environment.project.id,
-        environmentId: environment.id,
-        serviceId: service.id,
-        executionId: deploymentStatus?.execution_id ?? '',
-      }}
-      underline
-      color="neutral"
-      className="flex h-full w-full items-center justify-between font-normal hover:text-neutral"
-      onClick={(e) => {
-        e.stopPropagation()
-      }}
-    >
-      <DeploymentAction status={triggerAction} />
-      <div className="flex items-center justify-between gap-4">
+    <div className="flex w-full items-center justify-between gap-4">
+      <WrappingLink>
+        <DeploymentAction status={triggerAction} />
+      </WrappingLink>
+      <div>
         <div className="flex items-center gap-2">
           {deploymentStatus?.status_details?.status === 'ERROR' && (
             <div onClick={(e) => e.stopPropagation()} onKeyDown={(e) => e.stopPropagation()}>
@@ -51,9 +65,11 @@ export function ServiceLastDeploymentCell({ service, environment }: ServiceLastD
               />
             </div>
           )}
-          <StatusChip status={deploymentStatus?.status_details?.status} variant="monochrome" />
+          <WrappingLink>
+            <StatusChip status={deploymentStatus?.status_details?.status} variant="monochrome" />
+          </WrappingLink>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
