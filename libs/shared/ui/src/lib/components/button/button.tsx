c92088eb65c8757fe94d02dcab96893bb299579a
Romain Billard <billard.rom@gmail.com>
2026-04-09 15:00:24 +0200

    Fix condition for button's loading state

--- libs/shared/ui/src/lib/components/button/button.tsx
+++ libs/shared/ui/src/lib/components/button/button.tsx
@@ -15,10 +15,10 @@ export const Button = forwardRef<ElementRef<typeof ButtonPrimitive>, ButtonProps
     <ButtonPrimitive
       {...props}
       ref={forwardedRef}
-      className={twMerge(loading ? 'pointer-events-none relative text-transparent' : '', className)}
+      className={twMerge(loading ? 'pointer-events-none relative !text-transparent' : '', className)}
     >
       {loading && <LoaderSpinner className="absolute left-0 right-0 m-auto" />}
-      <div className={loading ? 'opacity-0' : ''}>{props.children}</div>
+      {props.children}
     </ButtonPrimitive>
   )
 })
