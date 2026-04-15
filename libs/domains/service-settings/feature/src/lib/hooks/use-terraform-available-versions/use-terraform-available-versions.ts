8bc08ec7d8bdc96e1366780bb39a8d448054e6e7
Romain Billard <billard.rom@gmail.com>
2026-04-15 16:44:18 +0200

    Remove useless files

--- libs/domains/service-settings/feature/src/lib/hooks/use-terraform-available-versions/use-terraform-available-versions.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { queries } from '@qovery/state/util-queries'
-
-export function useTerraformAvailableVersions() {
-  return useQuery({
-    ...queries.serviceTerraform.listAvailableVersions(),
-  })
-}
