52161209bb4b038d01d1dcd2f62fadc0b363e818
RemiBonnet <bonnet.rem@gmail.com>
2026-04-27 11:06:26 +0200

    fix(console-shared): remove deprecated flow-create-port components

--- libs/shared/console-shared/src/index.ts
+++ libs/shared/console-shared/src/index.ts
@@ -1,7 +1,4 @@
 // This library is deprecated and will be removed in the future
 // Please use util or domain libraries instead
 export * from './lib/entrypoint-cmd-inputs/ui/entrypoint-cmd-inputs'
-export * from './lib/flow-create-port/ui/flow-create-port/flow-create-port'
-export * from './lib/flow-create-port/ui/crud-modal/crud-modal'
-export * from './lib/flow-create-port/utils/port-healthcheck'
 export * from './lib/settings-heading/settings-heading'
