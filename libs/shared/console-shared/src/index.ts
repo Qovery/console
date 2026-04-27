ca606ae3403ec667498c1576c797824c997f6090
RemiBonnet <bonnet.rem@gmail.com>
2026-04-27 10:39:50 +0200

    fix(console-shared): remove deprecated CreditCardForm component

--- libs/shared/console-shared/src/index.ts
+++ libs/shared/console-shared/src/index.ts
@@ -1,6 +1,5 @@
 // This library is deprecated and will be removed in the future
 // Please use util or domain libraries instead
-export * from './lib/credit-card-form/ui/credit-card-form'
 export * from './lib/force-run-modal/feature/force-run-modal-feature'
 export * from './lib/force-run-modal/ui/force-run-modal'
 export * from './lib/entrypoint-cmd-inputs/ui/entrypoint-cmd-inputs'
