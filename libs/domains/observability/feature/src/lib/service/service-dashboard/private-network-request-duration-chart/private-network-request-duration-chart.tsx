ec22882f4faa52112de519c0f41ae0c1757d8a24
RemiBonnet <bonnet.rem@gmail.com>
2026-04-29 14:36:14 +0200

    fix(styles): update stroke colors to use CSS variables for improved consistency in request duration and size charts

--- libs/domains/observability/feature/src/lib/service/service-dashboard/private-network-request-duration-chart/private-network-request-duration-chart.tsx
+++ libs/domains/observability/feature/src/lib/service/service-dashboard/private-network-request-duration-chart/private-network-request-duration-chart.tsx
@@ -136,7 +136,7 @@ export function PrivateNetworkRequestDurationChart({
         key="50th-percentile"
         dataKey="p50"
         type="linear"
-        stroke="#CB87F6"
+        stroke="var(--purple-8)"
         strokeWidth={2}
         dot={false}
         connectNulls={false}
@@ -147,7 +147,7 @@ export function PrivateNetworkRequestDurationChart({
         key="95th-percentile"
         dataKey="p95"
         type="linear"
-        stroke="#847AE6"
+        stroke="var(--purple-9)"
         strokeWidth={2}
         dot={false}
         connectNulls={false}
@@ -158,7 +158,7 @@ export function PrivateNetworkRequestDurationChart({
         key="99th-percentile"
         dataKey="p99"
         type="linear"
-        stroke="#8B46CE"
+        stroke="var(--purple-10)"
         strokeWidth={2}
         dot={false}
         connectNulls={false}
