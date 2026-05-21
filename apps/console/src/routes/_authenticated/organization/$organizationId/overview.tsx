6f08d6f176d09020bf7b86ecde01f590430d5f3d
RemiBonnet <bonnet.rem@gmail.com>
2026-05-21 18:21:43 +0200

    feat(overview): conditionally render ProjectList based on cluster availability

--- apps/console/src/routes/_authenticated/organization/$organizationId/overview.tsx
+++ apps/console/src/routes/_authenticated/organization/$organizationId/overview.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { SectionProductionHealth } from '@qovery/domains/clusters/feature'
+import { createFileRoute, useParams } from '@tanstack/react-router'
+import { SectionProductionHealth, useClusters } from '@qovery/domains/clusters/feature'
 import { OrganizationOverview } from '@qovery/domains/organizations/feature'
 import { ProjectList } from '@qovery/domains/projects/feature'
 import { useDocumentTitle } from '@qovery/shared/util-hooks'
@@ -10,12 +10,15 @@ export const Route = createFileRoute('/_authenticated/organization/$organization
 
 function RouteComponent() {
   useDocumentTitle('Organization - Overview')
+  const { organizationId = '' } = useParams({ strict: false })
+  const { data: clusters = [] } = useClusters({ organizationId, suspense: true })
+  const hasClusters = clusters.length > 0
 
   return (
     <div className="mt-6">
       <OrganizationOverview>
         <SectionProductionHealth />
-        <ProjectList />
+        {hasClusters ? <ProjectList /> : null}
       </OrganizationOverview>
     </div>
   )
