3247d5d7e5725aa6eb55b943169907e7bac4462d
Melvin Zottola <37779145+mzottola@users.noreply.github.com>
2026-06-11 09:15:35 +0200

    fix(external-secret): Handle FILE_EXTERNAL_SECRET (#2754)
    * fix(external-secret) Handle FILE_EXTERNAL_SECRET
    
    * fixup! fix(external-secret) Handle FILE_EXTERNAL_SECRET

--- libs/domains/variables/util/src/lib/is-external-secret-variable/is-external-secret-variable.ts
+++ libs/domains/variables/util/src/lib/is-external-secret-variable/is-external-secret-variable.ts
@@ -1,5 +1,5 @@
 import { type VariableResponse } from 'qovery-typescript-axios'
 
 export function isExternalSecretVariable(variable: VariableResponse): boolean {
-  return variable.variable_type === 'EXTERNAL_SECRET'
+  return variable.variable_type === 'EXTERNAL_SECRET' || variable.variable_type === 'FILE_EXTERNAL_SECRET'
 }
