640bcb764a951db8e3204c1c2ed8b4a8db78c64f
Romain Billard <rmnbrd@users.noreply.github.com>
2026-07-01 15:06:14 +0200

    feat(blueprints): creation flow improvements (#2787)
    * Add icons for blueprints
    
    * Improve blueprint-based service creation flow loading behavior
    
    This is done by listening to a new WS API endpoint
    
    * Reuse ServiceAvatar component
    
    * Remove aria-hidden wrappers
    
    * Refactor WS subscription and submit logic
    
    * Rename onInvalidateOperation to onQueryInvalidated
    
    * Update success toast display strategy
    
    * Add timeout fallback whem WS is not received

--- libs/domains/services/feature/src/lib/service-new/blueprint-card/blueprint-card.tsx
+++ libs/domains/services/feature/src/lib/service-new/blueprint-card/blueprint-card.tsx
@@ -1,6 +1,7 @@
 import { useParams } from '@tanstack/react-router'
 import { type BlueprintItem } from 'qovery-typescript-axios'
 import { Button, Link } from '@qovery/shared/ui'
+import { ServiceAvatar } from '../../service-avatar/service-avatar'
 
 export function BlueprintCard({
   blueprint,
@@ -14,7 +15,13 @@ export function BlueprintCard({
   return (
     <section className="flex h-full flex-col gap-4 rounded-lg border border-neutral bg-surface-neutral p-4 [box-shadow:0px_0px_4px_0px_rgba(0,0,0,0.01),0px_2px_3px_0px_rgba(0,0,0,0.02)]">
       <div className="flex flex-1 flex-col gap-3">
-        <img className="h-9 w-9 rounded" src={blueprint.icon} alt={blueprint.name} aria-hidden="true" />
+        <ServiceAvatar
+          className="h-9 w-9"
+          radius="none"
+          service={{ icon_uri: blueprint.icon, serviceType: 'APPLICATION' }}
+          serviceAvatarRadius="sm"
+          size="custom"
+        />
         <div className="flex flex-col gap-1">
           <h3 className="text-sm font-medium leading-5 text-neutral">{blueprint.name}</h3>
           <p className="text-sm leading-5 text-neutral-subtle">{blueprint.description}</p>
