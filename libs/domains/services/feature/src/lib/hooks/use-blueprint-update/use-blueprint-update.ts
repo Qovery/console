ff2639884d804243c02f3628cc683f51bc7a5806
Romain Billard <billard.rom@gmail.com>
2026-07-02 08:47:44 +0200

    Made useBlueprintUpdate's blueprintId param mandatory

--- libs/domains/services/feature/src/lib/hooks/use-blueprint-update/use-blueprint-update.ts
+++ libs/domains/services/feature/src/lib/hooks/use-blueprint-update/use-blueprint-update.ts
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query'
 import { queries } from '@qovery/state/util-queries'
 
 export interface UseBlueprintUpdateProps {
-  blueprintId?: string | null
+  blueprintId: string
   enabled?: boolean
   suspense?: boolean
 }
 
 export function useBlueprintUpdate({ blueprintId, enabled = true, suspense = false }: UseBlueprintUpdateProps) {
   return useQuery({
-    ...queries.services.blueprintUpdate({ blueprintId: blueprintId ?? '' }),
-    enabled: enabled && Boolean(blueprintId),
+    ...queries.services.blueprintUpdate({ blueprintId }),
+    enabled,
     suspense,
   })
 }
