847dc0954c83431ace17cef91a903e48c1a9cddd
RemiBonnet <rbonnet@qovery.com>
2026-07-06 18:40:43 +0200

    fix(organizations): allow custom git branch selection

--- libs/domains/organizations/feature/src/lib/git-branch-settings/git-branch-settings.spec.tsx
+++ libs/domains/organizations/feature/src/lib/git-branch-settings/git-branch-settings.spec.tsx
@@ -1,6 +1,12 @@
 import { wrapWithReactHookForm } from '__tests__/utils/wrap-with-react-hook-form'
-import { renderWithProviders } from '@qovery/shared/util-tests'
+import { renderWithProviders, screen } from '@qovery/shared/util-tests'
 import { GitBranchSettings } from './git-branch-settings'
+
+let mockBranches: { name: string }[] = [
+  {
+    name: 'main',
+  },
+]
 
 jest.mock('../hooks/use-branches/use-branches', () => {
   return {
@@ -8,20 +14,42 @@ jest.mock('../hooks/use-branches/use-branches', () => {
     useBranches: () => ({
       isLoading: false,
       isRefetching: false,
-      data: [
-        {
-          name: 'main',
-        },
-      ],
+      data: mockBranches,
     }),
   }
 })
 
 describe('GitBranchSettings', () => {
+  beforeEach(() => {
+    mockBranches = [
+      {
+        name: 'main',
+      },
+    ]
+  })
+
   it('should match snapshot', () => {
     const { baseElement } = renderWithProviders(
       wrapWithReactHookForm(<GitBranchSettings organizationId="org-1" gitProvider="GITHUB" />)
     )
     expect(baseElement).toMatchSnapshot()
+  })
+
+  it('should display the selected branch when it is not part of returned branches', () => {
+    mockBranches = [
+      {
+        name: 'develop',
+      },
+    ]
+
+    renderWithProviders(
+      wrapWithReactHookForm(<GitBranchSettings organizationId="org-1" gitProvider="GITHUB" />, {
+        defaultValues: {
+          branch: 'main',
+        },
+      })
+    )
+
+    expect(screen.getByText('main')).toBeInTheDocument()
   })
 })
