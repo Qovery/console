e3970faf5f310e0c43000bbe2144aff9c92e96ac
Julien Dan <41013692+jul-dan@users.noreply.github.com>
2026-08-05 16:58:48 +0200

    feat(onboarding): add confirmation screen before organization creation (#2847)

--- libs/domains/onboarding/feature/src/lib/container/container.tsx
+++ libs/domains/onboarding/feature/src/lib/container/container.tsx
@@ -47,7 +47,7 @@ export function Container(props: PropsWithChildren) {
   const titlesPerRoute = [
     {
       path: '/personalize',
-      title: 'Just a few questions',
+      title: 'To tailor your experience',
     },
     {
       path: '/use-cases',
