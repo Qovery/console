1db96b0b617134b1ba6955384497a186c9b8d319
RemiBonnet <rbonnet@qovery.com>
2026-08-10 10:44:45 +0200

    fix(agentic-workflow): expose stopped deployment logs

--- libs/domains/services/feature/src/lib/hooks/use-service-deployment-and-running-statuses/use-service-deployment-and-running-statuses.ts
+++ libs/domains/services/feature/src/lib/hooks/use-service-deployment-and-running-statuses/use-service-deployment-and-running-statuses.ts
@@ -1,12 +1,33 @@
 import { useQuery } from '@tanstack/react-query'
-import { type AnyService } from '@qovery/domains/services/data-access'
+import { type DeploymentHistoryEnvironmentV2, type Status } from 'qovery-typescript-axios'
+import { type AnyService, isAgenticWorkflow } from '@qovery/domains/services/data-access'
 import { type ServiceStatuses } from '@qovery/shared/interfaces'
+import { mergeDeploymentServices } from '@qovery/shared/util-js'
 import { queries } from '@qovery/state/util-queries'
 import { formatDeploymentStatusLabel, getServiceRunningStatus } from './service-status-utils'
 
 export interface UseDeploymentFullStatusProps {
   environmentId?: string
   service?: AnyService
+}
+
+export function getAgenticWorkflowDeploymentStatus(
+  deploymentHistory: DeploymentHistoryEnvironmentV2[],
+  serviceId: string
+): Status | undefined {
+  const lastDeployment = mergeDeploymentServices(deploymentHistory).find(
+    ({ identifier }) => identifier.service_id === serviceId
+  )
+
+  if (!lastDeployment) return undefined
+
+  return {
+    id: serviceId,
+    execution_id: lastDeployment.execution_id,
+    last_deployment_date: lastDeployment.auditing_data.created_at,
+    state: lastDeployment.status,
+    status_details: lastDeployment.status_details,
+  } as Status
 }
 
 export function useServiceDeploymentAndRunningStatuses({ environmentId = '', service }: UseDeploymentFullStatusProps) {
@@ -20,15 +41,27 @@ export function useServiceDeploymentAndRunningStatuses({ environmentId = '', ser
     ...queries.services.deploymentStatus(environmentId, serviceId),
   })
 
-  const deploymentStatusLabel = formatDeploymentStatusLabel(deploymentStatus)
-  const runningStatusOverride = getServiceRunningStatus({ service, runningStatus, deploymentStatus })
+  const { data: agenticWorkflowDeploymentStatus } = useQuery({
+    ...queries.environments.deploymentHistoryV2({ environmentId, pageSize: 100 }),
+    select: (deploymentHistory) => getAgenticWorkflowDeploymentStatus(deploymentHistory ?? [], serviceId),
+    enabled: Boolean(environmentId) && Boolean(serviceId) && isAgenticWorkflow(service),
+  })
+
+  const resolvedDeploymentStatus = deploymentStatus ?? agenticWorkflowDeploymentStatus
+
+  const deploymentStatusLabel = formatDeploymentStatusLabel(resolvedDeploymentStatus)
+  const runningStatusOverride = getServiceRunningStatus({
+    service,
+    runningStatus,
+    deploymentStatus: resolvedDeploymentStatus,
+  })
 
   const data: ServiceStatuses = {
     runningStatus: runningStatusOverride,
-    ...(deploymentStatus
+    ...(resolvedDeploymentStatus
       ? {
           deploymentStatus: {
-            ...deploymentStatus,
+            ...resolvedDeploymentStatus,
             stateLabel: deploymentStatusLabel,
           },
         }
