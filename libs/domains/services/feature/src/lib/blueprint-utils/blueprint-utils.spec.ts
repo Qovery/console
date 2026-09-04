1e9b944d0eaacee0f716d8638971bcc044344980
Romain Billard <billard.rom@gmail.com>
2026-09-04 15:54:42 +0200

    feat(blueprints): add support for the new displayName property

--- libs/domains/services/feature/src/lib/blueprint-utils/blueprint-utils.spec.ts
+++ libs/domains/services/feature/src/lib/blueprint-utils/blueprint-utils.spec.ts
@@ -1,4 +1,5 @@
-import { formatBlueprintName, isBlueprintCompatibleWithCluster } from './blueprint-utils'
+import type { BlueprintItem } from 'qovery-typescript-axios'
+import { formatBlueprintName, getBlueprintDisplayName, isBlueprintCompatibleWithCluster } from './blueprint-utils'
 
 describe('formatBlueprintName', () => {
   it.each([
@@ -7,6 +8,22 @@ describe('formatBlueprintName', () => {
     ['AWS S3 Bucket', 'AWS S3 Bucket'],
   ])('formats %s as %s', (name, expectedName) => {
     expect(formatBlueprintName(name)).toBe(expectedName)
+  })
+})
+
+describe('getBlueprintDisplayName', () => {
+  const blueprint = {
+    name: 'aws-rds-mysql',
+  } as BlueprintItem
+
+  it('prefers the catalog display name', () => {
+    expect(getBlueprintDisplayName({ ...blueprint, displayName: 'Amazon RDS for MySQL' } as BlueprintItem)).toBe(
+      'Amazon RDS for MySQL'
+    )
+  })
+
+  it('formats the catalog name when the display name is missing', () => {
+    expect(getBlueprintDisplayName(blueprint)).toBe('AWS RDS MySQL')
   })
 })
 
