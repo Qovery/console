3748c47ea8e1d23770fec18f21fbd1c2d119898e
RemiBonnet <rbonnet@qovery.com>
2026-09-09 15:47:53 +0200

    fix(automation): remove overview view all runs button

--- libs/domains/services/feature/src/lib/service-overview/service-overview.tsx
+++ libs/domains/services/feature/src/lib/service-overview/service-overview.tsx
@@ -229,24 +229,7 @@ function ServiceOverviewContent({
             {isAgenticWorkflow(service) && <AgenticWorkflowWebhookSection webhookUrl={service.webhook.url} />}
             {isAgenticWorkflow(service) && (
               <Section id="runs" className="scroll-mt-24 gap-4">
-                <div className="flex items-center justify-between gap-4">
-                  <Heading>Last 5 runs</Heading>
-                  <Link
-                    as="button"
-                    color="neutral"
-                    variant="outline"
-                    size="sm"
-                    to="/organization/$organizationId/project/$projectId/environment/$environmentId/service/$serviceId/runs"
-                    params={{
-                      organizationId: environment.organization.id,
-                      projectId: environment.project.id,
-                      environmentId: environment.id,
-                      serviceId: service.id,
-                    }}
-                  >
-                    View all runs
-                  </Link>
-                </div>
+                <Heading>Last 5 runs</Heading>
                 <AgentTaskRuns agentTaskId={service.id} agentTaskName={service.name} limit={5} />
               </Section>
             )}
